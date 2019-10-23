b80a3b1210cfdcf7eef6d40bc0e97da0eae98d3d
kerry.cho <cdh0012@inveniacorp.com>
2019-10-23 17:06:12 +0900

    Add workflows

---
+++ updata-VC-Compiler.ps1
@@ -0,0 +1,2 @@
+(New-Object Net.WebClient).DownloadFile('https://www.microsoft.com/en-us/download/confirmation.aspx?id=4422', 'VC-Compiler-KB2519277.exe')
+echo REG ADD "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /V %cd%\VC-Compiler-KB2519277.exe /T REG_SZ /D WIN7RTM /F
