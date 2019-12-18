0ff099913011f17af8a0a92b14a788c6918433ce
Shady Ibraheem <shadyysf@github.com>
2019-12-18 17:42:05 -0500

    Revert "Added Az 3.1.0 in hosted image"
    This reverts commit 125779e366e5209c98fe969ae39286be0ad8ec82.

--- images/linux/scripts/installers/1604/azpowershell.sh
+++ images/linux/scripts/installers/1604/azpowershell.sh
@@ -13,7 +13,6 @@ sudo pwsh -Command 'Save-Module -Name Az -LiteralPath /usr/share/az_1.6.0 -Requi
 sudo pwsh -Command 'Save-Module -Name Az -LiteralPath /usr/share/az_2.3.2 -RequiredVersion 2.3.2 -Force'
 sudo pwsh -Command 'Save-Module -Name Az -LiteralPath /usr/share/az_2.6.0 -RequiredVersion 2.6.0 -Force'
 sudo pwsh -Command 'Save-Module -Name Az -LiteralPath /usr/share/az_2.8.0 -RequiredVersion 2.8.0 -Force'
-sudo pwsh -Command 'Save-Module -Name Az -LiteralPath /usr/share/az_3.1.0 -RequiredVersion 3.1.0 -Force'
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
@@ -41,11 +40,6 @@ if ! pwsh -Command '$actualPSModulePath = $env:PSModulePath ; $env:PSModulePath 
     if (!(get-module -listavailable -name Az.accounts)) {
         Write-Host "Az Module was not installed"; $env:PSModulePath = $actualPSModulePath; exit 1
     }
-    $env:PSModulePath = $actualPSModulePath
-    $actualPSModulePath = $env:PSModulePath ; $env:PSModulePath = "/usr/share/az_3.1.0:" + $env:PSModulePath;
-    if (!(get-module -listavailable -name Az.accounts)) {
-        Write-Host "Az Module was not installed"; $env:PSModulePath = $actualPSModulePath; exit 1
-    }
     $env:PSModulePath = $actualPSModulePath'; then
     exit 1
 fi
@@ -57,4 +51,3 @@ DocumentInstalledItem "Az Module (1.6.0)"
 DocumentInstalledItem "Az Module (2.3.2)"
 DocumentInstalledItem "Az Module (2.6.0)"
 DocumentInstalledItem "Az Module (2.8.0)"
-DocumentInstalledItem "Az Module (3.1.0)"
