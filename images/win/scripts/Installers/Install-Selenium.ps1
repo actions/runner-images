537e9da92924cf17e3aec1171b9b407c1171ce0e
Ivan Nosar <v-ivnosa@microsoft.com>
2019-12-19 16:19:58 +0300

    Install Selenium (draft)

---
+++ images/win/scripts/Installers/Install-Selenium.ps1
@@ -0,0 +1,21 @@
+################################################################################
+##  File:  Install-Selenium.ps1
+##  Desc:  Install Selenium Server standalone
+################################################################################
+
+# Acquire latest Selenium release number from GitHub API
+$latestReleaseUrl = "https://api.github.com/repos/SeleniumHQ/selenium/releases/latest"
+$latestReleaseInfo = Invoke-RestMethod -Uri $latestReleaseUrl
+$seleniumVersionString = $latestReleaseInfo.name.Split(" ")[1]
+$seleniumVersion = [version]::Parse($seleniumVersionString)
+
+# Download Selenium
+Write-Host "Downloading selenium-server-standalone v$seleniumVersion..."
+
+$seleniumReleaseUrl = "https://selenium-release.storage.googleapis.com/$($seleniumVersion.ToString(2))/selenium-server-standalone-$($seleniumVersion.ToString(3)).jar"
+$seleniumBinPath = "C:\selenium\selenium-server-standalone.jar"
+Invoke-WebRequest -UseBasicParsing -Uri $seleniumReleaseUrl -OutFile $seleniumBinPath
+
+setx "CLASSPATH" "$($seleniumBinPath):$($env:CLASSPATH)" /M
+
+exit 0
