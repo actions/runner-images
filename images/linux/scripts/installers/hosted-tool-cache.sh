fefb311721afb10653469ac900cbc014c1f5e2a9
Mihran <44965305+m-minasyan@users.noreply.github.com>
2020-01-23 15:12:36 +0400

    Minor bug fixes

--- images/linux/scripts/installers/hosted-tool-cache.sh
+++ images/linux/scripts/installers/hosted-tool-cache.sh
@@ -13,17 +13,17 @@ set -e
 AGENT_TOOLSDIRECTORY=/opt/hostedtoolcache
 mkdir $AGENT_TOOLSDIRECTORY
 echo "AGENT_TOOLSDIRECTORY=$AGENT_TOOLSDIRECTORY" | tee -a /etc/environment
-
 chmod -R 777 $AGENT_TOOLSDIRECTORY
 
 echo "Configure npm to use github package registry for '@actions' scope"
-npm config set @actions:registry "https://${TOOLCACHE_REGISTRY"
+npm config set @actions:registry "https://${TOOLCACHE_REGISTRY}"
 
 # Execute in /tmp folder to avoid node_modules creation in $HOME
 pushd /tmp
 
 # GitHub Package Registry doesn't support downloading public packages without auth so we have to authorize
 echo "Configure auth for github package registry"
+TOOLCACHE_REGISTRY="npm.pkg.github.com"
 echo "//${TOOLCACHE_REGISTRY}/:_authToken=${GITHUB_FEED_TOKEN}" > ".npmrc"
 echo "Installing npm-toolcache..."
 TOOLSET_PATH="$INSTALLER_SCRIPT_FOLDER/toolcache.json"
@@ -34,7 +34,7 @@ for PACKAGE_NAME in ${PACKAGE_LIST[@]}; do
     PACKAGE_VERSIONS=($(jq -r ".[\"$PACKAGE_NAME\"] | .[]" $TOOLSET_PATH))
     for PACKAGE_VERSION in ${PACKAGE_VERSIONS[@]}; do
         echo "Install ${PACKAGE_NAME}@${PACKAGE_VERSION}"
-        npm install ${PACKAGE_NAME}@${PACKAGE_VERSION} --registry=${TOOLCACHE_REGISTRY}
+        npm install ${PACKAGE_NAME}@${PACKAGE_VERSION}
 
         exit_code=$?
         if [ $exit_code -ne 0 ]; then
