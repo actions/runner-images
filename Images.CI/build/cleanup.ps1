0487222941c3c3d29add6e088438a0c891a94e60
Dmitry Shibanov <shibanov-1997@inbox.ru>
2020-01-28 12:10:59 +0300

    [WIP] Move image ci scripts (#8)
    * add files for moving ci
    
    * remove unused files
    
    * remove extra comment
    
    * add timestamp generation id
    
    * rename capture name prefix
    
    * minor changes
    
    * extra changes
    
    * minor changes

---
+++ Images.CI/build/cleanup.ps1
@@ -0,0 +1,26 @@
+param(
+    [String] [Parameter (Mandatory=$true)] [ValidateNotNullOrEmpty()]
+    [ValidateSet('Windows2019-Azure','Windows2016-Azure','ubuntu1604','ubuntu1804')]
+    $Image,
+    [String] [Parameter (Mandatory=$true)] $ResourcesNamePrefix,
+    [String] [Parameter (Mandatory=$true)] $ClientId,
+    [String] [Parameter (Mandatory=$true)] $ClientSecret,
+    [String] [Parameter (Mandatory=$true)] $SubscriptionId,
+    [String] [Parameter (Mandatory=$true)] $TenantId
+)
+
+az login --service-principal --username $ClientId --password $ClientSecret --tenant $TenantId
+
+$TempResourceGroupName = "${ResourcesNamePrefix}_${Image}"
+
+Write-Host "ResourcesNamePrefix = $ResourcesNamePrefix"
+Write-Host "TempResourceGroupName = $TempResourceGroupName"
+
+$groupExist = az group exists --name $TempResourceGroupName --subscription $SubscriptionId
+if ($groupExist -eq "true") {
+	Write-Host "Found a match, deleting temporary files"
+	az group delete --name $TempResourceGroupName --subscription $SubscriptionId --yes
+	Write-Host "Temporary group was deleted succesfully" -ForegroundColor Green
+} else {
+	Write-Host "No temporary groups found"
+}
