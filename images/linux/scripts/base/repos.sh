69d91f503df957d8c390b66505bd090a03680053
Andrey Mishechkin <v-andmis@microsoft.com>
2020-02-04 16:40:24 +0400

    debug 'echo' has been removed from repos.sh

--- images/linux/scripts/base/repos.sh
+++ images/linux/scripts/base/repos.sh
@@ -5,7 +5,6 @@
 ################################################################################
 
 LSB_RELEASE=$(lsb_release -rs)
-echo "Ubuntu release: $LSB_RELEASE"
 
 # Install Microsoft repository
 wget https://packages.microsoft.com/config/ubuntu/$LSB_RELEASE/packages-microsoft-prod.deb
