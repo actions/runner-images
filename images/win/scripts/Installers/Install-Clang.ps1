4983576a0b8ebabdf3fa240c1c3aafb85cd1a91e
Ivan Nosar <v-ivnosa@microsoft.com>
2020-02-04 18:22:33 +0300

    Install LLVM on Windows

---
+++ images/win/scripts/Installers/Install-Clang.ps1
@@ -0,0 +1,6 @@
+################################################################################
+##  File:  Install-Clang.ps1
+##  Desc:  Install Clang compiler as a part of LLVM framework for Windows
+################################################################################
+
+choco install -y llvm
