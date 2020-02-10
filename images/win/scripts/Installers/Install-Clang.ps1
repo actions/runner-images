9c06dae2e32a6f9fefa528aa5b764237ca293626
Ivan Nosar <v-ivnosa@microsoft.com>
2020-02-10 13:58:06 +0300

    Remove Clang

--- images/win/scripts/Installers/Install-Clang.ps1
+++
@@ -1,6 +0,0 @@
-################################################################################
-##  File:  Install-Clang.ps1
-##  Desc:  Install Clang compiler as a part of LLVM framework for Windows
-################################################################################
-
-choco install -y llvm
