3aeecf18ab9f06b710b58c3cfd1f2d64f8826593
Mikhail Timofeev <v-mitim@microsoft.com>
2020-02-18 20:32:24 +0300

    add WDK validation

--- images/win/scripts/Installers/Windows2019/Validate-WDK.ps1
+++ images/win/scripts/Installers/Windows2019/Validate-WDK.ps1
@@ -3,11 +3,51 @@
 ##  Desc:  Validate the installation of the Windows Driver Kit
 ################################################################################
 
+function Get-WDKExtensionPackage {
+    $vsProgramData = Get-Item -Path "C:\ProgramData\Microsoft\VisualStudio\Packages\_Instances"
+    $instanceFolders = Get-ChildItem -Path $vsProgramData.FullName
+
+    if($instanceFolders -is [array])
+    {
+        Write-Host "More than one instance installed"
+        exit 1
+    }
+
+    $stateContent = Get-Content -Path ($instanceFolders.FullName + '\state.packages.json')
+    $state = $stateContent | ConvertFrom-Json
+    $WDKPackageVersion = ($state.packages | where { $_.id -eq "Microsoft.Windows.DriverKit" }).version
+
+    if (!$WDKPackageVersion)
+    {
+        Write-Host "WDK package for Visual studio was not found"
+        exit 1
+    }
+
+    return $WDKPackageVersion
+}
+
+function Get-WDKVersion
+{
+    $WDKVersion = (Get-WmiObject Win32_Product -Filter "Name = 'Windows Driver Kit'").version
+
+    if (!$WDK)
+    {
+        Write-Host "WDK was not found"
+        exit 1
+    }
+
+    return $WDKVersion
+}
+
+$WDKVersion = Get-WDKVersion
+$WDKPackageVersion = Get-WDKExtensionPackage
+
 # Adding description of the software to Markdown
 $SoftwareName = "Windows Driver Kit"
 
 $Description = @"
-_Version:_ 10.0.18362.0<br/>
+_WDK Version:_ $WDKVersion<br/>
+_WDK Visual Studio Extension Version:_ $WDKPackageVersion<br/>
 "@
 
 Add-SoftwareDetailsToMarkdown -SoftwareName $SoftwareName -DescriptionMarkdown $Description
