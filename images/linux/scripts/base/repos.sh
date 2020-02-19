cc8bdd738467c8803411af1e5571f8ba6b95d2a3
root <root@DESKTOP-51T2FG1.localdomain>
2020-02-19 09:56:16 +0400

    dotnetcore-sdk.sh has been refactored for using in both Ubuntu 16/18 deployments

--- images/linux/scripts/base/repos.sh
+++ images/linux/scripts/base/repos.sh
@@ -9,7 +9,11 @@ LSB_RELEASE=$(lsb_release -rs)
 # Install Microsoft repository
 wget https://packages.microsoft.com/config/ubuntu/$LSB_RELEASE/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
-apt-get update
+apt-get install apt-transport-https
 
 # Install Microsoft GPG public key
 curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-prod bionic main" > /etc/apt/sources.list.d/dotnetdev.list'
+apt-get update
