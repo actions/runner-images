cb18129a4f31957af79b4d3e592cbfdc0e96e6c2
Mikhail Timofeev <v-mitim@microsoft.com>
2020-02-20 13:46:38 +0300

    remove pwsh fix

--- images/linux/scripts/installers/1804/powershellcore.sh
+++ images/linux/scripts/installers/1804/powershellcore.sh
@@ -12,9 +12,6 @@ LSB_RELEASE=$(lsb_release -rs)
 # Install Powershell
 apt-get install -y powershell
 
-# Temp fix based on: https://github.com/PowerShell/PowerShell/issues/9746 
-sudo apt remove libicu64
-
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
 if ! command -v pwsh; then
