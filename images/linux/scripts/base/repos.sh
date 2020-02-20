a3c8fe11533a3bc4cdc3c45ca145a5d4162858ae
Andrey Mishechkin (Akvelon INC) <v-andmis@microsoft.com>
2020-02-20 18:26:31 +0400

    Adding the Ubuntu 18 repository has been revoved from repos.sh

--- images/linux/scripts/base/repos.sh
+++ images/linux/scripts/base/repos.sh
@@ -13,7 +13,7 @@ apt-get install -y apt-transport-https
 
 # Install Microsoft GPG public key
 curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-prod bionic main" > /etc/apt/sources.list.d/dotnetdev.list'
 apt-get update
