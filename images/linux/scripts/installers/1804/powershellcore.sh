b2a3a7f4cc592e766fa7318d1f6f2c6a4be2db1b
Andrey Mishechkin (Akvelon INC) <v-andmis@microsoft.com>
2020-02-23 12:07:38 +0400

    Debugging the Ubuntu18 image buld

--- images/linux/scripts/installers/1804/powershellcore.sh
+++ images/linux/scripts/installers/1804/powershellcore.sh
@@ -8,12 +8,15 @@
 source $HELPER_SCRIPTS/document.sh
 
 LSB_RELEASE=$(lsb_release -rs)
+echo "Addin the repository [microsoft-ubuntu-bionic-prod] to /etc/apt/sources.list.d/dotnetdev.list"
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-prod bionic main" > /etc/apt/sources.list.d/dotnetdev.list'
 
 # Install Powershell
+echo "Running the PowerShell core installation"
 apt-get install -y powershell
 
 # Temp fix based on: https://github.com/PowerShell/PowerShell/issues/9746
+echo "Remove the libicu64"
 sudo apt remove libicu64
 
 # Run tests to determine that the software installed as expected
