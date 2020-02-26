699e797368ab93e7468e9ab6f57914e53739d616
Mihran Minasyan <>
2020-02-26 22:18:58 +0400

    Delete docker.sh
    Deleted docker.sh, because its do the same thing

--- images/linux/scripts/installers/docker-moby.sh
+++ images/linux/scripts/installers/docker-moby.sh
@@ -37,6 +37,18 @@ else
     set +e
 fi
 
+docker pull node:10
+docker pull node:12
+docker pull buildpack-deps:stretch
+docker pull node:10-alpine
+docker pull node:12-alpine
+docker pull debian:8
+docker pull debian:9
+docker pull alpine:3.7
+docker pull alpine:3.8
+docker pull alpine:3.9
+docker pull alpine:3.10
+
 ## Add version information to the metadata file
 echo "Documenting Docker version"
 docker_version=$(docker -v)
