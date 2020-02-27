fd86f55d9fa325c8b7520757c1a76f9620fa812f
Nikita Bykov <v-nibyko@microsoft.com>
2020-02-27 11:23:42 +0300

    Fixed script

--- images/linux/scripts/installers/bazel.sh
+++ images/linux/scripts/installers/bazel.sh
@@ -8,6 +8,9 @@
 source $HELPER_SCRIPTS/document.sh
 
 # Install bazel
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+apt-get update -y
 apt-get install -y bazel
 
 # Run tests to determine that the software installed as expected
