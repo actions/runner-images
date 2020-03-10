b116401e20f0d0d266a1b254774fdaf506fa2954
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2020-03-10 11:49:14 +0000

    move azure-cli install to defence

--- images/win/scripts/Installers/Install-AzureCli.ps1
+++ images/win/scripts/Installers/Install-AzureCli.ps1
@@ -3,7 +3,7 @@
 ##  Desc:  Install Azure CLI
 ################################################################################
 
-choco install azure-cli -y
+Install-Choco "azure-cli"
 
 $AzureCliExtensionPath = Join-Path $Env:CommonProgramFiles 'AzureCliExtensionDirectory'
 New-Item -ItemType "directory" -Path $AzureCliExtensionPath
