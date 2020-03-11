5e91840910157a32aa10a5534d447e355bda33d5
Mikhail Timofeev <v-mitim@microsoft.com>
2020-03-11 14:20:49 +0300

    change Install-Wix

--- images/win/scripts/Installers/Windows2019/Install-Wix.ps1
+++ images/win/scripts/Installers/Windows2019/Install-Wix.ps1
@@ -7,6 +7,8 @@ Import-Module -Name ImageHelpers -Force;
 
 choco install wixtoolset -y --force
 
+$extensionUrl = "https://wixtoolset.gallerycdn.vsassets.io/extensions/wixtoolset/wixtoolsetvisualstudio2019extension/1.0.0.4/1563296438961/Votive2019.vsix"
+$extensionName = "Votive2019.vsix"
+
 #Installing VS extension 'Wix Toolset Visual Studio 2019 Extension'
-Install-VsixExtension -Url 'https://wixtoolset.gallerycdn.vsassets.io/extensions/wixtoolset/wixtoolsetvisualstudio2019extension/1.0.0.4/1563296438961/Votive2019.vsix' -Name 'Votive2019.vsix'
-#return $exitCode
+Install-VsixExtension -Url $extensionUrl -Name $extensionName
