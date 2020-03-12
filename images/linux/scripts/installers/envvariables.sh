d9f22812003fccf3b0822ad8a9f1ee76645291d8
Mikhail Timofeev <v-mitim@microsoft.com>
2020-03-13 00:12:02 +0300

    remove comma and quotes

--- images/linux/scripts/installers/envvariables.sh
+++ images/linux/scripts/installers/envvariables.sh
@@ -1,6 +1,6 @@
 #Set ImageVersion and ImageOS env variables
-"echo ImageVersion=$IMAGE_VERSION | tee -a /etc/environment",
-"echo ImageOS=$IMAGE_OS | tee -a /etc/environment"
+echo ImageVersion=$IMAGE_VERSION | tee -a /etc/environment
+echo ImageOS=$IMAGE_OS | tee -a /etc/environment
 
 # This directory is supposed to be created in $HOME and owned by user(https://github.com/actions/virtual-environments/issues/491)
 mkdir -p /etc/skel/.config/configstore
