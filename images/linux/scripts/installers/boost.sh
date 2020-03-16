6be8521bf6af383665f9d418f3e2d00a0fc68851
Aleksandr Chebotov <v-aleche@microsoft.com>
2020-03-16 12:51:40 +0300

    depreacted BOOST_ROOT env variable

--- images/linux/scripts/installers/boost.sh
+++ images/linux/scripts/installers/boost.sh
@@ -7,18 +7,16 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
 
+TOOLSET_PATH="$INSTALLER_SCRIPT_FOLDER/toolcache.json"
 BOOST_LIB=/usr/local/share/boost
+BOOST_VERSIONS=$(cat toolset.json | jq -r 'to_entries[] | select(.key | match("boost")) | .value[] +".0"')
 
 # Install Boost
-for BOOST_VERSION in ${BOOST_VERSIONS//,/ }
+for BOOST_VERSION in ${BOOST_VERSIONS}
 do
-    BOOST_SYMLINK_VER=`echo "${BOOST_VERSION//[.]/_}"`
-    BOOST_ROOT="BOOST_ROOT_$BOOST_SYMLINK_VER"
+    BOOST_SYMLINK_VER=$(echo "${BOOST_VERSION//[.]/_}")
+    BOOST_ROOT_VERSION="BOOST_ROOT_$BOOST_SYMLINK_VER"
 
-    echo "$BOOST_ROOT=$BOOST_LIB/$BOOST_VERSION" | tee -a /etc/environment
-    if [[ $BOOST_VERSION == $BOOST_DEFAULT ]]; then
-        echo "BOOST_ROOT=$BOOST_LIB/$BOOST_VERSION" | tee -a /etc/environment
-    fi
-
+    echo "$BOOST_ROOT_VERSION=$BOOST_LIB/$BOOST_VERSION" | tee -a /etc/environment
     DocumentInstalledItem "Boost C++ Libraries $BOOST_VERSION"
 done
