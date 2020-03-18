66c53c10652ecc041896e55c36c613599260032d
Dariy.Nurgaleev <d.nurgaleev@trueengineering.ru>
2020-03-19 01:36:41 +0700

    added parameters for validation.

--- images/win/scripts/Installers/Validate-SQLExpress.ps1
+++ images/win/scripts/Installers/Validate-SQLExpress.ps1
@@ -2,29 +2,30 @@
 ##  File:  Validate-SQLExpress.ps1
 ##  Desc:  Validate Microsoft SQL Express installation
 ################################################################################
+
+#Parameters for database access
+$sqlUser = "sa"
+$sqlPassword = "P@ssword!!"
+$sqlInstance = "SQL2019"
+
 function Test-SqlConnection {
     param(
         [Parameter(Mandatory)]
         [string]$ServerName,
-
         [Parameter(Mandatory)]
         [string]$IntegratedSecurity,
-
         [Parameter(Mandatory)]
         [string]$UserName,
-
         [Parameter(Mandatory)]
         [string]$Password
     )
-
     $ErrorActionPreference = 'Stop'
-
     try {
         $connectionString = 'Server={0};Integrated Security={1};User ID={2};Password={3}' -f $ServerName,$IntegratedSecurity,$UserName,$Password
         Write-Host -Object "Trying to connect to SQL Express instance: $ServerName"
         $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $connectionString
         $sqlConnection.Open()
-        Write-Host -Object "Connection to database: {0} in in was successful. Version of SQL Express is: {1}" -f $sqlConnection.Database,$sqlConnection.ServerVersion
+        Write-Host -Object "Connection to database: {0} was successful. Version of SQL Express is: {1}" -f $sqlConnection.Database,$sqlConnection.ServerVersion
         return $sqlConnection.ServerVersion
     } catch {
         Write-Host -Object "Connection to SQL Express cannot be established."
@@ -34,17 +35,16 @@ function Test-SqlConnection {
         $sqlConnection.Close()
     }
 }
-$instanceName = "$env:computername\SQL2019"
-$version = Test-SqlConnection -ServerName $instanceName -IntegratedSecurity "false" -UserName "sa" -Password "P@ssword!!"
+$instanceName = "$env:computername\$sqlInstance"
+$version = Test-SqlConnection -ServerName $instanceName -IntegratedSecurity "false" -UserName $sqlUser -Password $sqlPassword
 
 # Adding description of the software to Markdown
 $SoftwareName = "Git"
-
 $Description = @"
 _Version:_ $version<br/>
-_InstanceName:_ SQL2019<br/>
-_Username:_ sa<br/>
-_Password:_ P@ssword!!<br/>
+_InstanceName:_ $sqlInstance<br/>
+_Username:_ $sqlUser<br/>
+_Password:_ $sqlPassword<br/>
 _Default Path:_ C:\Program Files (x86)\Microsoft SQL Server
 "@
 
