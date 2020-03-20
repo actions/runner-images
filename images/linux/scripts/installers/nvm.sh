8c4ad0f17be5c248c69f558bccf344a1d5ca9ff9
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-20 12:01:57 +0300

    add changes and test on minimized image

--- images/linux/scripts/installers/nvm.sh
+++ images/linux/scripts/installers/nvm.sh
@@ -1,11 +1,18 @@
 source $HELPER_SCRIPTS/document.sh
 
-export NVM_DIR="$HOME/.nvm"
-mkdir $NVM_DIR
+#groupadd nvm
+#usermod -aG nvm root
+#chown :nvm ./nvm
+export NVM_DIR="/opt/.nvm"
+mkdir -m 777 $NVM_DIR
+#chmod g+ws $NVM_DIR
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-echo "source $NVM_DIR/nvm.sh --install" >> "$HOME/.bash_profile"
-source $HOME/.bash_profile
-
+ls $NVM_DIR
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "/etc/profile"
+#echo "source $NVM_DIR/nvm.sh --install" >> /etc/profile
+#sudo source /etc/profile
+#exec bash
 if ! command -v nvm; then
     echo "nvm was not installed"
     exit 1
