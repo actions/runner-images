44e85eb15a399d52607fecb6375548270ae3b5c0
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-20 13:36:32 +0300

    get script to run on full ubuntu image

--- images/linux/scripts/installers/nvm.sh
+++ images/linux/scripts/installers/nvm.sh
@@ -1,22 +1,22 @@
+#!/bin/bash
+################################################################################
+##  File:  nvm.sh
+##  Desc:  Installs Nvm
+################################################################################
+
+# Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
 
-#groupadd nvm
-#usermod -aG nvm root
-#chown :nvm ./nvm
 export NVM_DIR="/opt/.nvm"
 mkdir -m 777 $NVM_DIR
-#chmod g+ws $NVM_DIR
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 ls $NVM_DIR
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "/etc/profile"
-#echo "source $NVM_DIR/nvm.sh --install" >> /etc/profile
-#sudo source /etc/profile
-#exec bash
+
 if ! command -v nvm; then
     echo "nvm was not installed"
     exit 1
 fi
-echo "nvm versio is"
-nvm --version
-#DocumentInstalledItem "nvm ($(nvm --version))"
+
+DocumentInstalledItem "nvm ($(nvm --version))"
