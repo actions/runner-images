7114c42594e9245163a52b2c4628ceeef44806cd
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-24 23:32:54 +0300

    add new line to etc

--- images/linux/scripts/installers/nvm.sh
+++ images/linux/scripts/installers/nvm.sh
@@ -15,7 +15,7 @@ ls $NVM_DIR
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 chmod -R 777 $NVM_DIR
 echo "NVM_DIR=$NVM_DIR" | tee -a /etc/environment
-
+echo "[[ -s $NVM_DIR/nvm.sh ]] && . $NVM_DIR/nvm.sh" | tee -a /etc/environment
 if ! command -v nvm; then
     echo "nvm was not installed"
     exit 1
