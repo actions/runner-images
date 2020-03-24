0903a6eaf2e5cc11d2e5a250c4a5aaa929681145
Nikita Bykov <v-nibyko@microsoft.com>
2020-03-24 15:56:42 +0300

    fixed swift.sh

--- images/linux/scripts/installers/swift.sh
+++ images/linux/scripts/installers/swift.sh
@@ -6,11 +6,16 @@
 
 # Install # Install
 image_label="$(lsb_release -rs)"
-wget https://swift.org/builds/swift-5.1.5-release/ubuntu${image_label//./}/swift-5.1.5-RELEASE/swift-5.1.5-RELEASE-ubuntu$image_label.tar.gz
-tar xzf swift-5.1.5-RELEASE-ubuntu$image_label.tar.gz
-sudo mv swift-5.1.5-RELEASE-ubuntu$image_label /usr/share/swift
-sudo echo "PATH=/usr/share/swift/usr/bin:$PATH" | sudo tee -a /etc/environment
-source  ~/.bashrc
+swift_version="5.1.5"
+
+wget -P /tmp https://swift.org/builds/swift-$swift_version-release/ubuntu${image_label//./}/swift-$swift_version-RELEASE/swift-$swift_version-RELEASE-ubuntu$image_label.tar.gz
+tar xzf /tmp/swift-$swift_version-RELEASE-ubuntu$image_label.tar.gz
+sudo mv swift-$swift_version-RELEASE-ubuntu$image_label /usr/share/swift
+
+SWIFT_DIR="/usr/share/swift/usr/bin/swift"
+SWIFT_PATH="/usr/share/swift/usr/bin"
+sudo ln -s "$SWIFT_DIR" /usr/local/bin/swift
+echo "SWIFT_PATH =$SWIFT_PATH" | sudo tee -a /etc/environment
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
