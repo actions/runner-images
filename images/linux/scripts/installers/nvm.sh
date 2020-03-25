942304b40260ab49e66246a2d08e3de57b15db0e
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-25 09:52:32 +0300

    fix nvm.sh

--- images/linux/scripts/installers/nvm.sh
+++ images/linux/scripts/installers/nvm.sh
@@ -15,7 +15,10 @@ ls $NVM_DIR
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 chmod -R 777 $NVM_DIR
 echo "NVM_DIR=$NVM_DIR" | tee -a /etc/environment
-echo "[ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh" | tee -a /etc/environment
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/profile.d/nvm.sh
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completio' >> /etc/profile.d/nvm.sh
+source /etc/profile.d/nvm.sh
+
 if ! command -v nvm; then
     echo "nvm was not installed"
     exit 1
