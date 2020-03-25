0c16fe7671158afb619e30f57e23363641f86175
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-25 15:30:12 +0300

    add comments

--- images/linux/scripts/installers/nvm.sh
+++ images/linux/scripts/installers/nvm.sh
@@ -8,12 +8,14 @@
 source $HELPER_SCRIPTS/document.sh
 
 export NVM_DIR="/opt/.nvm"
+# should create directory with full permissions.
 mkdir -m 777 $NVM_DIR
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# should recursively obtain subdirectories with full permissions
 chmod -R 777 $NVM_DIR
 echo "NVM_DIR=$NVM_DIR" | tee -a /etc/environment
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/profile.d/nvm.sh
-echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completio' >> /etc/profile.d/nvm.sh
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> /etc/profile.d/nvm.sh
 source /etc/profile.d/nvm.sh
 
 if ! command -v nvm; then
