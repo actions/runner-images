b3e38a6509fec0a60dd6ab831b17faedfae63d27
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-25 16:11:40 +0300

    remoe bash_completion

--- images/linux/scripts/installers/nvm.sh
+++ images/linux/scripts/installers/nvm.sh
@@ -15,7 +15,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 chmod -R 777 $NVM_DIR
 echo "NVM_DIR=$NVM_DIR" | tee -a /etc/environment
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/profile.d/nvm.sh
-echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> /etc/profile.d/nvm.sh
 source /etc/profile.d/nvm.sh
 
 if ! command -v nvm; then
