b7acae67af77fd16cb6d446fd79d87182f381624
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-26 16:21:17 +0300

    minimize image

--- images/linux/scripts/installers/nvm.sh
+++ images/linux/scripts/installers/nvm.sh
@@ -15,6 +15,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 chmod -R 777 $NVM_DIR
 echo "NVM_DIR=$NVM_DIR" | tee -a /etc/environment
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/skel/.bashrc
+source /etc/skel/.bashrc
 
 if ! command -v nvm; then
     echo "nvm was not installed"
