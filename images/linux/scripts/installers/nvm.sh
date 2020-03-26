b5b584555beda4f9a9437792a23d40d1b6b07e93
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-03-26 10:48:35 +0300

    add nvm.sh to bashrc

--- images/linux/scripts/installers/nvm.sh
+++ images/linux/scripts/installers/nvm.sh
@@ -14,8 +14,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 # should recursively obtain subdirectories with full permissions
 chmod -R 777 $NVM_DIR
 echo "NVM_DIR=$NVM_DIR" | tee -a /etc/environment
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/profile.d/nvm.sh
-source /etc/profile.d/nvm.sh
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/skel/.bashrc
 
 if ! command -v nvm; then
     echo "nvm was not installed"
