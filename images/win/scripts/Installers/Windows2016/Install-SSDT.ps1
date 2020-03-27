64dc2fe2b127a7e3cb4d2dbd75052da5339964ee
Dibir Magomedsaygitov <v-dimago@microsoft.com>
2020-03-27 15:24:28 +0300

    change the lint to download ssdt

--- images/win/scripts/Installers/Windows2016/Install-SSDT.ps1
+++ images/win/scripts/Installers/Windows2016/Install-SSDT.ps1
@@ -6,7 +6,7 @@
 Import-Module -Name ImageHelpers -Force
 
 #SSDT for Visual Studio 2017 (15.9.3)
-$InstallerURI = 'https://download.microsoft.com/download/5/2/D/52DEF429-5B17-470C-82D9-654116080B15/SSDT-Setup-ENU.exe'
+$InstallerURI = 'https://download.microsoft.com/download/e/c/f/ecfccf97-b32c-4c31-b2fc-bff558635096/SSDT-Setup-ENU.exe'
 $InstallerName = 'SSDT-Setup-ENU.exe'
 $logFilePath = "$env:TEMP\ssdtlog.txt"
 $ArgumentList = ('/install', 'INSTALLALL', '/passive', '/norestart', "/log `"$logFilePath`"")
