69ff6d6d7cebc150eb01d7c7493b7feb97383427
Dibir Magomedsaygitov <v-dimago@microsoft.com>
2020-03-27 18:23:22 +0300

    update the download link for ssdt for win2016

--- images/win/scripts/Installers/Windows2016/Install-SSDT.ps1
+++ images/win/scripts/Installers/Windows2016/Install-SSDT.ps1
@@ -5,8 +5,9 @@
 
 Import-Module -Name ImageHelpers -Force
 
-#SSDT for Visual Studio 2017 (15.9.3)
-$InstallerURI = 'https://download.microsoft.com/download/e/c/f/ecfccf97-b32c-4c31-b2fc-bff558635096/SSDT-Setup-ENU.exe'
+#SSDT for Visual Studio 2017
+#The link down below points to the latest version of SSDT for Visual Studio 2017
+$InstallerURI = 'https://go.microsoft.com/fwlink/?linkid=2124518'
 $InstallerName = 'SSDT-Setup-ENU.exe'
 $logFilePath = "$env:TEMP\ssdtlog.txt"
 $ArgumentList = ('/install', 'INSTALLALL', '/passive', '/norestart', "/log `"$logFilePath`"")
