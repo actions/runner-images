430a51e1613fab6be86e25ada122cec60109b6e1
Dariy.Nurgaleev <d.nurgaleev@trueengineering.ru>
2020-03-31 22:07:12 +0700

    Removed useless last line.

--- images/win/scripts/Installers/Install-PostgreSQL.ps1
+++ images/win/scripts/Installers/Install-PostgreSQL.ps1
@@ -2,9 +2,12 @@ $ErrorActionPreference = "Stop"
 
 Import-Module -Name ImageHelpers
 
+#Install latest PostgreSQL
 choco install postgresql
 
+#Get Path to pg_ctl.exe
 $paths=(Get-CimInstance Win32_Service -Filter "Name LIKE 'postgresql-%'").PathName
+#Parse output of command above to obtain pure path
 $pgbin=$paths.split('"')[1].replace("\pg_ctl.exe", "")
+#Added PostgreSQL bin path into PATH variable.
 Add-MachinePathItem $pgbin
-$env:Path = Get-MachinePath
