ec8752f3125ca8c52e9383aff5c6776986286b4c
Dariy Nurgaleev <50947177+Darleev@users.noreply.github.com>
2020-04-10 15:49:42 +0700

    spaces

--- images/win/scripts/Installers/Install-PostgreSQL.ps1
+++ images/win/scripts/Installers/Install-PostgreSQL.ps1
@@ -6,8 +6,8 @@ Import-Module -Name ImageHelpers
 choco install postgresql
 
 #Get Path to pg_ctl.exe
-$paths=(Get-CimInstance Win32_Service -Filter "Name LIKE 'postgresql-%'").PathName
+$paths = (Get-CimInstance Win32_Service -Filter "Name LIKE 'postgresql-%'").PathName
 #Parse output of command above to obtain pure path
-$pgbin=$paths.split('"')[1].replace("\pg_ctl.exe", "")
+$pgbin = $paths.split('"')[1].replace("\pg_ctl.exe", "")
 #Added PostgreSQL bin path into PATH variable.
 Add-MachinePathItem $pgbin
