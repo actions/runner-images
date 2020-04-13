1f9b024259f8302e0ae35765d30e3f827f60fecb
Andy Mishechkin <v-andmis@microsoft.com>
2020-04-13 15:13:20 +0400

    Ubuntu 14.04 image pulling has been added

--- images/linux/scripts/installers/docker-moby.sh
+++ images/linux/scripts/installers/docker-moby.sh
@@ -44,6 +44,7 @@ docker pull alpine:3.7
 docker pull alpine:3.8
 docker pull alpine:3.9
 docker pull alpine:3.10
+docker pull ubuntu:14.04
 
 ## Add version information to the metadata file
 echo "Documenting Docker version"
