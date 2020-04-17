d2f31e25f5eff232291ac006ba48c91dce588625
Andy Mishechkin <v-andmis@microsoft.com>
2020-04-17 18:06:31 +0400

    extra space has been removed

--- images/linux/scripts/base/repos.sh
+++ images/linux/scripts/base/repos.sh
@@ -16,5 +16,4 @@ curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-
 apt-get update
