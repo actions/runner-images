21aef12789586eb3d00b3af5223f6be4a2d246de
LuisEnMarroquin <mluis651@gmail.com>
2020-04-20 07:07:28 -0500

    Changed open-cobol to OpenCOBOL

--- images/linux/scripts/installers/open-cobol.sh
+++ images/linux/scripts/installers/open-cobol.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 ################################################################################
 ##  File:  open-cobol.sh
-##  Desc:  Installs open-cobol
+##  Desc:  Installs OpenCOBOL
 ################################################################################
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
 
-# Install open-cobol
-apt-get update
-apt-get install gcc open-cobol -y
+# Install OpenCOBOL
+apt-get install open-cobol -y
 cobc --version
 
 # Run tests to determine that the software installed as expected
-echo "Testing open-cobol installation"
+echo "Testing OpenCOBOL installation"
 if ! command -v cobc; then
-    echo "open-cobol was not installed"
+    echo "OpenCOBOL was not installed"
     exit 1
 fi
 
 # Document what was added to the image
 echo "Lastly, document the installed version"
-DocumentInstalledItem "Open-Cobol ($(echo $(cobc -V) | cut -c 18-22))"
+DocumentInstalledItem "OpenCOBOL ($(echo $(cobc -V) | cut -d ' ' -f3))"
