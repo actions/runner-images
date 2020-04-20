7bc975797ddcd4719e7e85f927899a5a8b155e62
Dariy.Nurgaleev <d.nurgaleev@trueengineering.ru>
2020-04-20 22:40:14 +0700

    changed chmod parameters for a+X

--- images/linux/scripts/installers/1604/android.sh
+++ images/linux/scripts/installers/1604/android.sh
@@ -38,7 +38,7 @@ else
 fi
 
 # Add required permissions
-chmod -R 755 ${ANDROID_SDK_ROOT}
+chmod -R a+X ${ANDROID_SDK_ROOT}
 
 # Install the following SDKs and build tools, passing in "y" to accept licenses.
 echo "y" | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager \
