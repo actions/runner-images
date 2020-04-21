8e8fbb4f76959b798a7b8013ed4792cf753c52dd
Maksim Petrov <47208721+vmapetr@users.noreply.github.com>
2020-04-21 11:58:27 +0300

    Improve Windows helpers functions stability (#723)
    * Rework windows InstallHelpers; Add retry logic to install function

--- images/win/scripts/Installers/Install-NET48.ps1
+++ images/win/scripts/Installers/Install-NET48.ps1
@@ -6,8 +6,8 @@
 Import-Module -Name ImageHelpers -Force
 
 # .NET 4.8 Dev pack
-$InstallerURI = "https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0307177e14752e359fde5423ab583e43/ndp48-devpack-enu.exe"
-$InstallerName = "NDP48-DevPack-ENU.exe"
-$ArgumentList = ('Setup', '/passive', '/norestart' )
+$InstallerName = "ndp48-devpack-enu.exe"
+$InstallerUrl = "https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0307177e14752e359fde5423ab583e43/${InstallerName}"
+$ArgumentList = ("Setup", "/passive", "/norestart")
 
-Install-EXE -Url $InstallerURI -Name $InstallerName -ArgumentList $ArgumentList
+Install-Binary -Url $InstallerUrl -Name $InstallerName -ArgumentList $ArgumentList
