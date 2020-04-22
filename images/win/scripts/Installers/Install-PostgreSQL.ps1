73b520f371095f801a63cf2c31e154d7da9cbea3
Darii Nurgaleev <Darleev@github.com>
2020-04-22 16:34:55 +0700

    added correct quotas

--- images/win/scripts/Installers/Install-PostgreSQL.ps1
+++ images/win/scripts/Installers/Install-PostgreSQL.ps1
@@ -11,7 +11,7 @@ Set-SystemVariable -SystemVariable PGUSER -Value $postgresusr
 Set-SystemVariable -SystemVariable PGPASSWORD -Value $postgrespwd
 #Install latest PostgreSQL
 
-choco install postgresql --params '/Password:$postgrespwd'
+cinst postgresql --params "/Password:$env:PGPASSWORD" --params-global
 
 #Get Path to pg_ctl.exe
 $paths = (Get-CimInstance Win32_Service -Filter "Name LIKE 'postgresql-%'").PathName
@@ -19,3 +19,4 @@ $paths = (Get-CimInstance Win32_Service -Filter "Name LIKE 'postgresql-%'").Path
 $pgbin = $paths.split('"')[1].replace("\pg_ctl.exe", "")
 #Added PostgreSQL bin path into PATH variable.
 Add-MachinePathItem $pgbin
+
