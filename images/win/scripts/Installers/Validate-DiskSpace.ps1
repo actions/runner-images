67496802a9dac184a287297b7d8f2836c3b895a7
Nikita Bykov <v-nibyko@microsoft.com>
2020-04-28 09:21:35 +0300

    fixed naming

--- images/win/scripts/Installers/Validate-DiskSpace.ps1
+++ images/win/scripts/Installers/Validate-DiskSpace.ps1
@@ -7,7 +7,7 @@ $availableSpaceMB =  [math]::Round((Get-PSDrive -Name C).Free / 1MB)
 $minimumFreeSpaceMB = 15 * 1024
 
 Write-Host "Available disk space: $availableSpaceMB MB"
-if ($availableFreeSpace -le $minimumFreeSpaceGB)
+if ($availableSpaceMB -le $minimumFreeSpaceGB)
 {
     Write-Host "Not enough disk space on the image (minimum available space: $minimumFreeSpaceMB MB)"
     exit 1
