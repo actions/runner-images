9ac1714e3921ee8c10e2c3ef5cc654cdf9fba5fa
Mikhail Timofeev <v-mitim@microsoft.com>
2020-04-28 15:01:05 +0300

    libcurl4

--- images/linux/scripts/installers/1804/basic.sh
+++ images/linux/scripts/installers/1804/basic.sh
@@ -29,7 +29,7 @@ echo "Install jq"
 apt-get install -y --no-install-recommends jq
 
 echo "Install libcurl4"
-apt-get install -y --no-install-recommends libcurl3
+apt-get install -y --no-install-recommends libcurl4
 
 echo "Install libunwind8"
 apt-get install -y --no-install-recommends libunwind8
