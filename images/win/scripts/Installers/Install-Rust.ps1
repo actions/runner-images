ed0d9563b0507f52c0da527bcfbd97944655d5d9
Dibir Magomedsaygitov <v-dimago@microsoft.com>
2020-05-05 19:41:06 +0300

    revert changes for win

--- images/win/scripts/Installers/Install-Rust.ps1
+++ images/win/scripts/Installers/Install-Rust.ps1
@@ -6,8 +6,8 @@
 Import-Module -Name ImageHelpers
 
 # Rust Env
-$env:RUSTUP_HOME="C:\users\default\.rustup"
-$env:CARGO_HOME="C:\users\default\.cargo"
+$env:RUSTUP_HOME="C:\Rust\.rustup"
+$env:CARGO_HOME="C:\Rust\.cargo"
 
 # Download the latest rustup-init.exe for Windows x64
 # See https://rustup.rs/#
@@ -19,6 +19,33 @@ Invoke-WebRequest -UseBasicParsing -Uri "https://win.rustup.rs/x86_64" -OutFile 
 # Delete rustup-init.exe when it's no longer needed
 Remove-Item -Path .\rustup-init.exe
 
+# Add Rust binaries to the path
+Add-MachinePathItem "$env:CARGO_HOME\bin"
+$env:Path = Get-MachinePath
+
 # Install common tools
 rustup component add rustfmt clippy
 cargo install bindgen cbindgen
+
+# Run script at startup for all users
+$cmdRustSymScript = @"
+@echo off
+
+if exist $env:CARGO_HOME (
+    if not exist %USERPROFILE%\.cargo (
+        mklink /J %USERPROFILE%\.cargo $env:CARGO_HOME
+    )
+)
+
+if exist $env:RUSTUP_HOME (
+    if not exist %USERPROFILE%\.rustup (
+        mklink /J %USERPROFILE%\.rustup $env:RUSTUP_HOME
+    )
+)
+"@
+
+$cmdPath = "C:\Rust\rustsym.bat"
+$cmdRustSymScript | Out-File -Encoding ascii -FilePath $cmdPath
+
+# Update Run key to run a script at logon
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "RUSTSYM" -Value $cmdPath
