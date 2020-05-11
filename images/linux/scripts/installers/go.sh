27a633c450d74d8304e5b4c4460f7c3ea6e6079e
Vladimir Safonkin <vsafonkin@gmail.com>
2020-05-11 18:26:20 +0300

    Install Go to hostedtoolcache directory on Ubuntu image (#848)
    * Move go location to the toolcache

--- images/linux/scripts/installers/go.sh
+++ images/linux/scripts/installers/go.sh
@@ -13,8 +13,8 @@ golangTags="/tmp/golang_tags.json"
 #   $2=IsDefaultVersion (true or false)
 function InstallGo () {
     version=$( getFullGoVersion $1 )
-    downloadVersion=$version.linux-amd64.tar.gz
-    goFolder=/usr/local/go$1
+    downloadVersion="go$version.linux-amd64.tar.gz"
+    goFolder="$AGENT_TOOLSDIRECTORY/go/$version/x64"
 
     echo "Install Go $version"
     curl -sL https://dl.google.com/go/${downloadVersion} -o ${downloadVersion}
@@ -22,7 +22,10 @@ function InstallGo () {
     tar -C $goFolder -xzf $downloadVersion --strip-components=1 go
     rm $downloadVersion
     echo "GOROOT_${1//./_}_X64=$goFolder" | tee -a /etc/environment
-    DocumentInstalledItem "Go $1 ($($goFolder/bin/go version))"
+    DocumentInstalledItem "Go $version ($($goFolder/bin/go version))"
+
+    # Create symlink in old location /usr/local/go<version> to new location
+    ln -s $goFolder /usr/local/go$version
 
     # If this version of Go is to be the default version,
     # symlink it into the path and point GOROOT to it.
@@ -37,8 +40,8 @@ function getFullGoVersion () {
     local pattern="refs/tags/go$1([.0-9]{0,3})$"
     local query='[.[] | select( .ref | test($pattern))] | .[-1] | .ref'
     refValue=$(jq --arg pattern "$pattern" "$query" $golangTags)
-    version=$(echo "$refValue" | cut -d '/' -f 3)
-    version=$(echo "${version//\"}") # go1.12.17
+    version=$(echo "$refValue" | cut -d '/' -f 3 | awk -F 'go' '{print $2}')
+    version=$(echo "${version//\"}") # 1.12.17
     echo $version
 }
 
