982fdc2c0bef1ef48155aed969d78be2daacd23c
Sergey Dolin <v-sedoli@micorosoft.com>
2020-05-12 13:21:58 +0500

    Add helper doc

--- images/linux/scripts/helpers/github-api.sh
+++ images/linux/scripts/helpers/github-api.sh
@@ -4,6 +4,14 @@
 ##  Desc:  Helper functions for github api
 ################################################################################
 
+# The function to get the URL of the most resent release for linux amd64 
+# architecture 
+#
+#  Accepted parameters
+#  1st parameter is organization/repo of github repository
+#  2nd parameter is desirable extension of the release tarball
+#  It is possible to pass the organization/repo as 2 separated params
+
 function getLatestReleaseURI {
      if [[ $# -eq 0 ]];then
         echo "usage: " >&2
