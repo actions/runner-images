7caeb3ce17507a3312528c2cc45379c11122ef7d
Dibir Magomedsaygitov <v-dimago@microsoft.com>
2020-05-13 17:44:12 +0300

    implement logic that gets 3 latest versions of ghc

--- images/win/scripts/Installers/Install-Haskell.ps1
+++ images/win/scripts/Installers/Install-Haskell.ps1
@@ -3,11 +3,23 @@
 ##  Desc:  Install Haskell for Windows
 ################################################################################
 
+# Get last 3 latest versions of GHC
+[Array] $ChocoVersionsOutput = & choco search ghc --allversions --limit-output
+$RawVersionsList = $ChocoVersionsOutput | Where-Object { $_.StartsWith("ghc|") } | ForEach-Object { $_.TrimStart("ghc|") }
+$VersionsList = $RawVersionsList | ForEach-Object { [Version]::Parse($_) } | Sort-Object -Descending
+
+$MajorMinorVersions = $VersionsList | ForEach-Object { [System.Version]::Parse($_.ToString(2)) } | Get-Unique
+$MajorMinorVersionsToInstall = $MajorMinorVersions | Sort-Object -Descending | Select-Object -First 3
+
+$LatestGhcVersions = $MajorMinorVersionsToInstall | ForEach-Object {
+    $version = $_
+    return $VersionsList | Where-Object {
+        return ($_.Major -eq $version.Major) -and ($_.Minor -eq $version.Minor)
+    } | Select-Object -First 1
+}
 
 # The latest version will be installed as a default
-$GhcVersions = @('8.6.5', '8.8.3', '8.10.1')
-
-ForEach ($version in $GhcVersions)
+ForEach ($version in $LatestGhcVersions)
 {
     Write-Host "Installing ghc $version..."
     Choco-Install -PackageName ghc -ArgumentList "--version", $version, "-m"
