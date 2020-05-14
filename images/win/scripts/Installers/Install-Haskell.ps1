8ffd5cd28c85c30fb8e2ee7930b54a3e671f577e
Dibir Magomedsaygitov <v-dimago@microsoft.com>
2020-05-14 21:33:50 +0300

    fix versions order

--- images/win/scripts/Installers/Install-Haskell.ps1
+++ images/win/scripts/Installers/Install-Haskell.ps1
@@ -6,7 +6,7 @@
 # Get 3 latest versions of GHC
 [Version[]] $ChocoVersionsOutput = & choco search ghc --allversions --limit-output | Where-Object { $_.StartsWith("ghc|") } | ForEach-Object { $_.TrimStart("ghc|") }
 $MajorMinorGroups = $ChocoVersionsOutput | Sort-Object -Descending | Group-Object { $_.ToString(2) } | Select-Object -First 3
-$VersionsList = $MajorMinorGroups | ForEach-Object { $_.Group | Select-Object -First 1 }
+$VersionsList = $MajorMinorGroups | ForEach-Object { $_.Group | Select-Object -First 1 } | Sort-Object
 
 # The latest version will be installed as a default
 ForEach ($version in $VersionsList)
