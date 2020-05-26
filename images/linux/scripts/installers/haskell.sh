1c4531ccd86a76a05fe7b797665611fccb6c3d1d
Mikhail Timofeev <v-mitim@microsoft.com>
2020-05-26 22:27:51 +0300

    change version retrieval to awk

--- images/linux/scripts/installers/haskell.sh
+++ images/linux/scripts/installers/haskell.sh
@@ -6,7 +6,6 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
-source $HELPER_SCRIPTS/os.sh
 
 # Install Herbert V. Riedel's PPA for managing multiple version of ghc on ubuntu.
 # https://launchpad.net/~hvr/+archive/ubuntu/ghc
@@ -14,8 +13,8 @@ apt-get install -y software-properties-common
 add-apt-repository -y ppa:hvr/ghc
 apt-get update
 
-# Get 3 latest minor Haskell version and latest exact Cabal version
-ghcVersions=$(apt-cache search "^ghc-" | grep -Po '\d{1,}\.\d{1,}' | sort --unique --version-sort | tail -3)
+# Get 3 latest Haskell version and latest Cabal version
+ghcVersions=$(apt-cache search "^ghc-" | grep -Po '(\d*\.){2}\d*' | awk 'BEGIN {FS=OFS=SUBSEP="."}{if (arr[$1,$2] < $3) arr[$1,$2] = $3} END {for (i in arr) print i,arr[i]}' | sort --version-sort | tail -3)
 cabalVersion=$(apt-cache search cabal-install-[0-9] | grep -Po '\d*\.\d*' | sort --unique --version-sort | tail -1)
 
 for version in $ghcVersions; do
@@ -31,7 +30,7 @@ curl -sSL https://get.haskellstack.org/ | sh
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
 # Check all ghc versions
 for version in $ghcVersions; do
-    if ! command -v /opt/ghc/${version}.*/bin/ghc; then
+    if ! command -v /opt/ghc/$version/bin/ghc; then
         echo "ghc $version was not installed"
         exit 1
     fi
@@ -52,6 +51,6 @@ fi
 echo "Lastly, documenting what we added to the metadata file"
 DocumentInstalledItem "Haskell Cabal ($(/opt/cabal/$cabalVersion/bin/cabal --version))"
 for version in $ghcVersions; do
-    DocumentInstalledItem "GHC ($(/opt/ghc/${version}.*/bin/ghc --version))"
+    DocumentInstalledItem "GHC ($(/opt/ghc/$version/bin/ghc --version))"
 done
 DocumentInstalledItem "Haskell Stack ($(stack --version))"
