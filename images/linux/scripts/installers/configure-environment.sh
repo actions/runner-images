ec010dcb936b034db3da87f3ca72edd0c68e0808
Mikhail Timofeev <v-mitim@microsoft.com>
2020-05-26 15:53:51 +0300

    Add swap to /mnt

--- images/linux/scripts/installers/configure-environment.sh
+++ images/linux/scripts/installers/configure-environment.sh
@@ -5,3 +5,8 @@ echo ImageOS=$IMAGE_OS | tee -a /etc/environment
 # This directory is supposed to be created in $HOME and owned by user(https://github.com/actions/virtual-environments/issues/491)
 mkdir -p /etc/skel/.config/configstore
 echo 'export XDG_CONFIG_HOME=$HOME/.config' | tee -a /etc/skel/.bashrc
+
+# Change waagent entries to use /mnt for swapfile
+sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
+sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' /etc/waagent.conf
+sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=4096/g' /etc/waagent.conf
