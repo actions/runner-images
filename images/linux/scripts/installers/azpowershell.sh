810871855682a8a8cdddba6a04e403214f669810
Dibir Magomedsaygitov <v-dimago@microsoft.com>
2020-05-27 12:57:34 +0300

    minor fixes and return ubuntu 20 part

--- images/linux/scripts/installers/azpowershell.sh
+++ images/linux/scripts/installers/azpowershell.sh
@@ -6,9 +6,37 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
+source $HELPER_SCRIPTS/os.sh
+
+# Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
+if isUbuntu20 ; then
+    latestVersion=$(pwsh -Command '(Find-Module -Name Az).Version')
+    modulePath="/usr/share/az_$latestVersion"
+    echo "Save Az Module ($latestVersion) to $modulePath"
+    pwsh -Command "Save-Module -Name Az -LiteralPath $modulePath -RequiredVersion $latestVersion -Force"
+
+    # Run tests to determine that the software installed as expected
+    echo "Testing to make sure that script performed as expected, and basic scenarios work"
+    if ! pwsh -Command "\$env:PSModulePath = '${modulePath}:' + \$env:PSModulePath
+        if ( -not (Get-Module -ListAvailable -Name Az.Accounts)) {
+            Write-Host 'Az Module was not installed'
+            exit 1
+        }"; then
+        exit 1
+    fi
+
+    # Document what was added to the image
+    DocumentInstalledItem "Az Module ($latestVersion)"
+    exit 0
+fi
 
 # List of versiion
-versions=(1.0.0 1.6.0 2.3.2 2.6.0 2.8.0 3.1.0 3.5.0 3.8.0)
+if isUbuntu20 ; then
+    latestVersion=$(pwsh -Command '(Find-Module -Name Az).Version')
+    versions=($latestVersion)
+else
+    versions=(1.0.0 1.6.0 2.3.2 2.6.0 2.8.0 3.1.0 3.5.0 3.8.0)
+fi
 
 # Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
 for version in ${versions[@]}; do
@@ -18,14 +46,13 @@ done
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
 for version in ${versions[@]}; do
-    pwsh -Command '
-        $actualPSModulePath = $env:PSModulePath;
-        $env:PSModulePath = "/usr/share/az_${version}:" + $env:PSModulePath;
-        if (!(get-module -listavailable -name Az.accounts))
-        {
-            Write-Host "Az Module was not installed"; $env:PSModulePath = $actualPSModulePath; exit 1
-        }
-        $env:PSModulePath = $actualPSModulePath'
+    modulePath="/usr/share/az_$version"
+    pwsh -Command
+        "\$env:PSModulePath = '${modulePath}:' + \$env:PSModulePath;
+        if ( -not (Get-Module -ListAvailable -Name Az.Accounts)) {
+            Write-Host 'Az Module was not installed'
+            exit 1
+        }";
     if [ $? -eq 1 ]; then
         exit 1
     fi
