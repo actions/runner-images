9996b2bba3bbe95fbec5ccb5dfbf3ba26f36e671
Aleksandr Chebotov <v-aleche@microsoft.com>
2020-05-27 10:59:45 +0300

    update IEDriverServer url

--- images/win/scripts/Installers/Install-IEWebDriver.ps1
+++ images/win/scripts/Installers/Install-IEWebDriver.ps1
@@ -2,31 +2,37 @@
 ##  File:  Install-SeleniumWebDrivers.ps1
 ##  Desc:  Install Selenium Web Drivers
 ################################################################################
-$DestinationPath = "$($env:SystemDrive)\"
-$DriversZipFile = "SeleniumWebDrivers.zip"
-Write-Host "Destination path: [$DestinationPath]"
-Write-Host "Selenium drivers download and install..."
+
+# https://www.selenium.dev/downloads/
+# 32 bit Windows IE (recommended)
 try {
-    Invoke-WebRequest -UseBasicParsing -Uri "https://seleniumwebdrivers.blob.core.windows.net/seleniumwebdrivers/${DriversZipFile}" -OutFile $DriversZipFile
-}
-catch {
-    Write-Error "[!] Failed to download $DriversZipFile"
+    $LatestReleaseUrl = "https://selenium-release.storage.googleapis.com/"
+    $LatestReleaseInfo = Invoke-RestMethod -Uri $latestReleaseUrl
+    $LatestIEVersion = $latestReleaseInfo.ListBucketResult.Contents | Where-Object Key -match "IEDriverServer_Win32" | Sort-Object LastModified | Select-Object -ExpandProperty Key -Last 1
+    $IEDriverUrl = -join ($LatestReleaseUrl, $LatestIEVersion)
+} catch {
+    Write-Error "[!] Failed to get IEDriver version [$LatestReleaseUrl]: $_"
     exit 1
 }
 
-$TempSeleniumDir = Join-Path $Env:TEMP "SeleniumWebDrivers"
-Extract-7Zip -Path $DriversZipFile -DestinationPath $Env:TEMP
-Remove-Item $DriversZipFile
-
-$SeleniumWebDriverPath = Join-Path $DestinationPath "SeleniumWebDrivers"
-$IEDriverPathTemp = Join-Path $TempSeleniumDir 'IEDriver'
-
-if (-not (Test-Path -Path $SeleniumWebDriverPath)) {
-    New-Item -Path $SeleniumWebDriverPath -ItemType "directory"
+# Download IE selenium driver
+try {
+    Write-Host "Selenium IEDriverServer download and install..."
+    $DriversZipFile = Start-DownloadWithRetry -Url $IEDriverUrl -Name "SeleniumWebDrivers.zip"
+}
+catch {
+    Write-Error "[!] Failed to download $IEDriverUrl"
+    exit 1
 }
 
-Move-Item -Path "$IEDriverPathTemp" -Destination $SeleniumWebDriverPath
+$SeleniumWebDriverPath = Join-Path $env:SystemDrive "SeleniumWebDrivers"
+$IEDriverPath = Join-Path $SeleniumWebDriverPath "IEDriver"
+if (-not (Test-Path -Path $IEDriverPath)) {
+    $null = New-Item -Path $IEDriverPath -ItemType Directory -Force
+}
 
-Write-Host "Setting the environment variables"
+Extract-7Zip -Path $DriversZipFile -DestinationPath $IEDriverPath
+Remove-Item $DriversZipFile
 
+Write-Host "Setting the IEWebDriver environment variables"
 setx IEWebDriver "C:\SeleniumWebDrivers\IEDriver" /M
