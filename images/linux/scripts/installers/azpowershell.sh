8fe56133df43c44bedad015a2d9fd4b20e28be4f
Dibir Magomedsaygitov <v-dimago@microsoft.com>
2020-05-27 14:38:02 +0300

    remove code duplication

--- images/linux/scripts/installers/azpowershell.sh
+++ images/linux/scripts/installers/azpowershell.sh
@@ -8,29 +8,7 @@
 source $HELPER_SCRIPTS/document.sh
 source $HELPER_SCRIPTS/os.sh
 
-# Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
-if isUbuntu20 ; then
-    latestVersion=$(pwsh -Command '(Find-Module -Name Az).Version')
-    modulePath="/usr/share/az_$latestVersion"
-    echo "Save Az Module ($latestVersion) to $modulePath"
-    pwsh -Command "Save-Module -Name Az -LiteralPath $modulePath -RequiredVersion $latestVersion -Force"
-
-    # Run tests to determine that the software installed as expected
-    echo "Testing to make sure that script performed as expected, and basic scenarios work"
-    if ! pwsh -Command "\$env:PSModulePath = '${modulePath}:' + \$env:PSModulePath
-        if ( -not (Get-Module -ListAvailable -Name Az.Accounts)) {
-            Write-Host 'Az Module was not installed'
-            exit 1
-        }"; then
-        exit 1
-    fi
-
-    # Document what was added to the image
-    DocumentInstalledItem "Az Module ($latestVersion)"
-    exit 0
-fi
-
-# List of versiion
+# List of version
 if isUbuntu20 ; then
     latestVersion=$(pwsh -Command '(Find-Module -Name Az).Version')
     versions=($latestVersion)
@@ -52,8 +30,9 @@ for version in ${versions[@]}; do
         if ( -not (Get-Module -ListAvailable -Name Az.Accounts)) {
             Write-Host 'Az Module was not installed'
             exit 1
-        }";
-    if [ $? -eq 1 ]; then
+        }"
+    if [ $? -ne 0 ]; then
+        echo "Az version $version is not installed"
         exit 1
     fi
 done
