86574f7352cdacc2a4c7ddc7a4dd0a7afffd13f1
Vladimir Safonkin <vsafonkin@gmail.com>
2020-05-28 08:56:44 +0300

    Move installation to git.sh

--- images/linux/scripts/installers/git.sh
+++ images/linux/scripts/installers/git.sh
@@ -17,6 +17,9 @@ git --version
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt-get install -y --no-install-recommends git-lfs
 
+# Install git-ftp
+apt-get install git-ftp -y
+
 # Run tests to determine that the software installed as expected
 echo "Testing git installation"
 if ! command -v git; then
@@ -28,6 +31,11 @@ if ! command -v git-lfs; then
     echo "git-lfs was not installed"
     exit 1
 fi
+echo "Testing git-ftp installation"
+if ! command -v git-ftp; then
+    echo "git-ftp was not installed"
+    exit 1
+fi
 
 # Document what was added to the image
 echo "Lastly, document the installed versions"
@@ -35,6 +43,7 @@ echo "Lastly, document the installed versions"
 DocumentInstalledItem "Git ($(git --version 2>&1 | cut -d ' ' -f 3))"
 # git-lfs/2.6.1 (GitHub; linux amd64; go 1.11.1)
 DocumentInstalledItem "Git Large File Storage (LFS) ($(git-lfs --version 2>&1 | cut -d ' ' -f 1 | cut -d '/' -f 2))"
+DocumentInstalledItem "Git-ftp ($(git-ftp --version | cut -d ' ' -f 3))"
 
 #Install hub
 snap install hub --classic
