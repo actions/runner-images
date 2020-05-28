75a098304df6c8892ea8e386956026c1ddc31673
Vladimir Safonkin <vsafonkin@gmail.com>
2020-05-28 13:05:18 +0300

    Add download retry helper

---
+++ images/linux/scripts/helpers/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+################################################################################
+##  File:  install.sh
+##  Desc:  Helper functions for installing tools
+################################################################################
+
+download_with_retries() {
+# Due to restrictions of bash functions, positional arguments is used here.
+# In case if you using latest argument NAME, you should also set value to all previous parameters.
+# Example: download_with_retries $ANDROID_SDK_URL "." "android_sdk.zip"
+    local URL="$1"
+    local DEST="${2:-.}"
+    local NAME="${3:-${URL##*/}}"
+    echo "Downloading $URL..."
+    wget $URL   --output-document="$DEST/$NAME" \
+                --tries=30 \
+                --wait 30 \
+                --retry-connrefused \
+                --retry-on-host-error \
+                --retry-on-http-error=429,500,503 \
+                --no-verbose
+    if [ $? != 0 ]; then
+        echo "Could not download $URL; Exiting build!"
+        exit 1
+    fi
+    return 0
+}
