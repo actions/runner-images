49c229b389cd7886a752001cd777de10751f14ea
Aleksandr Chebotov <v-aleche@microsoft.com>
2020-05-28 09:40:34 +0300

    use latest kind version

--- images/linux/scripts/installers/kind.sh
+++ images/linux/scripts/installers/kind.sh
@@ -6,12 +6,10 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
-source $HELPER_SCRIPTS/apt.sh
 
 # Install KIND
-KIND_VERSION="v0.7.0"
-
-curl -L -o /usr/local/bin/kind "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-linux-amd64"
+URL=$(curl -s https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r '.assets[].browser_download_url | select(contains("kind-linux-amd64"))')
+curl -L -o /usr/local/bin/kind $URL
 chmod +x /usr/local/bin/kind
 
 # Run tests to determine that the software installed as expected
