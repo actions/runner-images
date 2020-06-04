76b6fce3973a5b3c0e27ef9649bf49ef39c608c1
Mikhail Timofeev <48208649+miketimofeev@users.noreply.github.com>
2020-06-04 15:09:41 +0300

    [Ubuntu 20] Add docker-moby, sbt, github-cli, prepare toolcache directory (#1002)
    * add docker-moby,sbt,github-cli toolcache ruby, pypy
    
    * remove toolcache
    
    * add agentoolsdirectory
    
    * remove toolcache and toolset files
    
    * move directory creation for all Ubuntus'

--- images/linux/scripts/installers/configure-environment.sh
+++ images/linux/scripts/installers/configure-environment.sh
@@ -5,3 +5,9 @@ echo ImageOS=$IMAGE_OS | tee -a /etc/environment
 # This directory is supposed to be created in $HOME and owned by user(https://github.com/actions/virtual-environments/issues/491)
 mkdir -p /etc/skel/.config/configstore
 echo 'export XDG_CONFIG_HOME=$HOME/.config' | tee -a /etc/skel/.bashrc
+
+# Prepare directory and env variable for toolcache
+AGENT_TOOLSDIRECTORY=/opt/hostedtoolcache
+mkdir $AGENT_TOOLSDIRECTORY
+echo "AGENT_TOOLSDIRECTORY=$AGENT_TOOLSDIRECTORY" | tee -a /etc/environment
+chmod -R 777 $AGENT_TOOLSDIRECTORY
