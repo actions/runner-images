e65c1c0bc59ff2c87683b90ab0f09559da7d150d
Andrey Mishechkin (Akvelon INC) <v-andmis@microsoft.com>
2020-06-10 19:28:07 +0400

    [Zeit] has been removed from vercel script

--- images/linux/scripts/installers/vercel.sh
+++ images/linux/scripts/installers/vercel.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 ################################################################################
-##  File:  zeit-now.sh
-##  Desc:  Installs the Zeit Now CLI
+##  File:  vercel.sh
+##  Desc:  Installs the Vercel CLI
 ################################################################################
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
 
-# Install the Zeit Vercel CLI
+# Install the Vercel CLI
 npm i -g vercel
 
 # Validate the installation
 echo "Validate the installation"
 if ! command -v vercel; then
-    echo "Zeit Vercel CLI was not installed"
+    echo "Vercel CLI was not installed"
     exit 1
 fi
 
@@ -22,10 +22,10 @@ ln -s /usr/local/bin/vercel /usr/local/bin/now
 
 echo "Validate the link"
 if ! command -v now; then
-    echo "Now link to Zeit Vercel CLI was not created"
+    echo "[Now] command link to Vercel CLI was not created"
     exit 1
 fi
 
 # Document the installed version
 echo "Document the installed version"
-DocumentInstalledItem "Zeit Vercel CLI ($(vercel --version))"
+DocumentInstalledItem "Vercel CLI ($(vercel --version))"
