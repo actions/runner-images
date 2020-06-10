ddd29fa4cfb8a59bc6d50c35c4db3d89fd28e36f
Nikita Bykov <v-nibyko@microsoft.com>
2020-06-10 20:27:59 +0300

    fixed boost.sh

--- images/linux/scripts/installers/boost.sh
+++ images/linux/scripts/installers/boost.sh
@@ -10,6 +10,7 @@ source $HELPER_SCRIPTS/document.sh
 TOOLSET_PATH="$INSTALLER_SCRIPT_FOLDER/toolcache.json"
 BOOST_LIB="$AGENT_TOOLSDIRECTORY/boost"
 BOOST_VERSIONS=$(cat $TOOLSET_PATH | jq -r '.toolcache[] | select(.name | contains("boost")) | .versions[]')
+BOOST_ARCH=$(cat $TOOLSET_PATH | jq -r '.toolcache[] | select(.name | contains("boost")) | .arch')
 
 # Install Boost
 for BOOST_VERSION in ${BOOST_VERSIONS}
@@ -17,6 +18,6 @@ do
     BOOST_SYMLINK_VER=$(echo "${BOOST_VERSION//[.]/_}")
     BOOST_ROOT_VERSION="BOOST_ROOT_$BOOST_SYMLINK_VER"
 
-    echo "$BOOST_ROOT_VERSION=$BOOST_LIB/$BOOST_VERSION" | tee -a /etc/environment
+    echo "$BOOST_ROOT_VERSION=$BOOST_LIB/$BOOST_VERSION/$BOOST_ARCH" | tee -a /etc/environment
     DocumentInstalledItem "Boost C++ Libraries $BOOST_VERSION"
 done
