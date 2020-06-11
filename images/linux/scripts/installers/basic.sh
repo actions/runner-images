ae55a9241bd01685d1259dc351daf56162bca45b
Vladimir Safonkin <vsafonkin@gmail.com>
2020-06-11 14:46:41 +0300

    Add linux utils

--- images/linux/scripts/installers/basic.sh
+++ images/linux/scripts/installers/basic.sh
@@ -150,6 +150,18 @@ apt-get install -y --no-install-recommends gnupg2
 echo "Install lib32z1"
 apt-get install -y --no-install-recommends lib32z1
 
+echo "Install m4"
+apt-get install -y --no-install-recommends m4
+
+echo "Install bison"
+apt-get install -y --no-install-recommends bison
+
+echo "Install flex"
+apt-get install -y --no-install-recommends flex
+
+echo "Install texinfo"
+apt-get install -y --no-install-recommends texinfo
+
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
 for cmd in curl file ftp jq netcat ssh parallel rsync shellcheck sudo telnet time unzip wget zip; do
@@ -190,3 +202,7 @@ DocumentInstalledItemIndent "zip"
 DocumentInstalledItemIndent "zstd"
 DocumentInstalledItemIndent "gnupg2"
 DocumentInstalledItemIndent "lib32z1"
+DocumentInstalledItemIndent "m4"
+DocumentInstalledItemIndent "bison"
+DocumentInstalledItemIndent "flex"
+DocumentInstalledItemIndent "texinfo"
