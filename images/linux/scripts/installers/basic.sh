13b21cd65ef46cab0e124eff9e083bef5076c827
Vladimir Safonkin <vsafonkin@gmail.com>
2020-06-11 16:20:15 +0300

    Move packages to list

--- images/linux/scripts/installers/basic.sh
+++ images/linux/scripts/installers/basic.sh
@@ -11,28 +11,60 @@ source $HELPER_SCRIPTS/os.sh
 
 set -e
 
+common_packages="dnsutils
+                 file
+                 ftp
+                 iproute2
+                 iputils-ping
+                 jq
+                 libunwind8
+                 locales
+                 netcat
+                 openssh-client
+                 rsync
+                 shellcheck
+                 sudo
+                 telnet
+                 time
+                 tzdata
+                 unzip
+                 upx
+                 wget
+                 zip
+                 zstd
+                 libxkbfile-dev
+                 pkg-config
+                 libsecret-1-dev
+                 libxss1
+                 libgconf-2-4
+                 dbus
+                 xvfblibgbm-dev
+                 libgtk-3-0
+                 tk
+                 fakeroot
+                 dpkg
+                 rpm
+                 xz-utils
+                 xorriso
+                 zsync
+                 curl
+                 parallel
+                 gnupg2
+                 lib32z1
+                 m4
+                 bison
+                 flex
+                 texinfo"
+
+for package in $packages; do
+    echo "Install $package"
+    sudo apt-get install -y --no-install-recommends $package
+done
+
 if isUbuntu20 ; then
     echo "Install python2"
     apt-get install -y --no-install-recommends python-is-python2
 fi
-
-echo "Install dnsutils"
-apt-get install -y --no-install-recommends dnsutils
-
-echo "Install file"
-apt-get install -y --no-install-recommends file
-
-echo "Install ftp"
-apt-get install -y --no-install-recommends ftp
-
-echo "Install iproute2"
-apt-get install -y --no-install-recommends iproute2
-
-echo "Install iputils-ping"
-apt-get install -y --no-install-recommends iputils-ping
-
-echo "Install jq"
-apt-get install -y --no-install-recommends jq
 
 echo "Install libcurl"
 if isUbuntu18 ; then
@@ -44,123 +76,6 @@ if isUbuntu20 ; then
 fi
 
 apt-get install -y --no-install-recommends $libcurelVer
-
-echo "Install libunwind8"
-apt-get install -y --no-install-recommends libunwind8
-
-echo "Install locales"
-apt-get install -y --no-install-recommends locales
-
-echo "Install netcat"
-apt-get install -y --no-install-recommends netcat
-
-echo "Install openssh-client"
-apt-get install -y --no-install-recommends openssh-client
-
-echo "Install rsync"
-apt-get install -y --no-install-recommends rsync
-
-echo "Install shellcheck"
-apt-get install -y --no-install-recommends shellcheck
-
-echo "Install sudo"
-apt-get install -y --no-install-recommends sudo
-
-echo "Install telnet"
-apt-get install -y --no-install-recommends telnet
-
-echo "Install time"
-apt-get install -y --no-install-recommends time
-
-echo "Install tzdata"
-apt-get install -y --no-install-recommends tzdata
-
-echo "Install unzip"
-apt-get install -y --no-install-recommends unzip
-
-echo "Install upx"
-apt-get install -y --no-install-recommends upx
-
-echo "Install wget"
-apt-get install -y --no-install-recommends wget
-
-echo "Install zip"
-apt-get install -y --no-install-recommends zip
-
-echo "Install zstd"
-apt-get install -y --no-install-recommends zstd
-
-echo "Install libxkbfile"
-apt-get install -y --no-install-recommends libxkbfile-dev
-
-echo "Install pkg-config"
-apt-get install -y --no-install-recommends pkg-config
-
-echo "Install libsecret-1-dev"
-apt-get install -y --no-install-recommends libsecret-1-dev
-
-echo "Install libxss1"
-apt-get install -y --no-install-recommends libxss1
-
-echo "Install libgconf-2-4"
-apt-get install -y --no-install-recommends libgconf-2-4
-
-echo "Install dbus"
-apt-get install -y --no-install-recommends dbus
-
-echo "Install xvfb"
-apt-get install -y --no-install-recommends xvfb
-
-echo "Install libgbm-dev"
-apt-get install -y --no-install-recommends libgbm-dev
-
-echo "Install libgtk"
-apt-get install -y --no-install-recommends libgtk-3-0
-
-echo "Install tk"
-apt install -y tk
-
-echo "Install fakeroot"
-apt-get install -y --no-install-recommends fakeroot
-
-echo "Install dpkg"
-apt-get install -y --no-install-recommends dpkg
-
-echo "Install rpm"
-apt-get install -y --no-install-recommends rpm
-
-echo "Install xz-utils"
-apt-get install -y --no-install-recommends xz-utils
-
-echo "Install xorriso"
-apt-get install -y --no-install-recommends xorriso
-
-echo "Install zsync"
-apt-get install -y --no-install-recommends zsync
-
-echo "Install curl"
-apt-get install -y --no-install-recommends curl
-
-echo "Install parallel"
-apt-get install -y --no-install-recommends parallel
-
-echo "Install gnupg2"
-apt-get install -y --no-install-recommends gnupg2
-
-echo "Install lib32z1"
-apt-get install -y --no-install-recommends lib32z1
-
-echo "Install m4"
-apt-get install -y --no-install-recommends m4
-
-echo "Install bison"
-apt-get install -y --no-install-recommends bison
-
-echo "Install flex"
-apt-get install -y --no-install-recommends flex
-
-echo "Install texinfo"
-apt-get install -y --no-install-recommends texinfo
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
