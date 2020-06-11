e15a0479444a849b1453c50cf8c067ec92ae8540
Andy Mishechkin <v-andmis@microsoft.com>
2020-06-11 13:43:22 +0400

    [Zeit] terms have been removed from Vercel deployment

--- images/linux/scripts/installers/vercel.sh
+++ images/linux/scripts/installers/vercel.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 ################################################################################
-##  File:  zeit-now.sh
-##  Desc:  Installs the Zeit Now CLI
+##  File:  vercel.sh
+##  Desc:  Installs the Vercel CLI
 ################################################################################
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
 
-# Install the Zeit Vercel CLI
+# Install the Vercel CLI
 npm i -g vercel
 
 # Validate the installation
 echo "Validate the installation"
 if ! command -v vercel; then
-    echo "Zeit Vercel CLI was not installed"
+    echo "Vercel CLI was not installed"
     exit 1
 fi
 
-echo "Creating the symlink now to vercel"
+echo "Creating the symlink for [now] command to vercel CLI"
 ln -s /usr/local/bin/vercel /usr/local/bin/now
 
 echo "Validate the link"
 if ! command -v now; then
-    echo "Now link to Zeit Vercel CLI was not created"
+    echo "[Now] symlink to Vercel CLI was not created"
     exit 1
 fi
 
 # Document the installed version
 echo "Document the installed version"
-DocumentInstalledItem "Zeit Vercel CLI ($(vercel --version))"
+DocumentInstalledItem "Vercel CLI ($(vercel --version))"
