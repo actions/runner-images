3f2c3b824b9f55931cae2ab64c8f3e02c1bb656d
Greg Swallow <gswallow@developertown.com>
2020-06-16 14:50:51 -0400

    Paragraphs (or stanzas) should cover 1 topic only.

---
+++ images/linux/scripts/base/limits.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e -u -o pipefail
+
+echo '* soft nofile 65536' >> /etc/security/limits.conf
+echo '* hard nofile 65536' >> /etc/security/limits.conf
+echo 'session required pam_limits.so' >> /etc/pam.d/common-session
+echo 'session required pam_limits.so' >> /etc/pam.d/common-session-noninteractive
+echo 'DefaultLimitNOFILE=65536' >> /etc/systemd/system.conf
