258933d43f0ae4475cdbbafc7d47f1610771b528
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-06-17 17:36:48 +0300

    fixing install go

--- images/win/scripts/Installers/Install-Go.ps1
+++ images/win/scripts/Installers/Install-Go.ps1
@@ -6,26 +6,22 @@
 Import-Module -Name ImageHelpers -Force -DisableNameChecking
 
 # Get Go content from toolset
-$goTools = Get-ToolsetContent | Select-Object -ExpandProperty toolcache | Where-Object Name -eq "go"
-foreach ($goTool in $goTools)
+$goTool = Get-ToolsetContent | Select-Object -ExpandProperty toolcache | Where-Object Name -eq "go"
+
+$toolPath = Join-Path $env:AGENT_TOOLSDIRECTORY $goTool.name
+foreach($goVersion in $goTool.versions)
 {
-    $toolPath = Join-Path $env:AGENT_TOOLSDIRECTORY $goTool.name
-    foreach($goVersion in $goTool.versions)
-    {
-        # Check if version folder exists
-        $expectedVersionPath = Join-Path $toolPath $goVersion
-
-        # Take latest installed version in case if toolset version contains wildcards
-        $foundVersion = Get-Item $expectedVersionPath `
-                        | Sort-Object -Property {[version]$_.name} -Descending `
-                        | Select-Object -First 1
-
-        # Check for required architecture folder
-        $foundVersionArchPath = Join-Path $foundVersion $goTool.arch
-
-
-        $envName = "GOROOT_{0}_{1}_X64" -f $goVersion.split(".")
-
-        setx $envName "$foundVersionArchPath" /M
+    if ($goVersion.Split(".").Length -lt 3) {
+        $goVersion += ".*"
     }
+    # Check if version folder exists
+    $expectedVersionPath = Join-Path $toolPath $goVersion
+    # Take latest installed version in case if toolset version contains wildcards
+    $foundVersion = Get-Item $expectedVersionPath `
+                    | Sort-Object -Property {[version]$_.name} -Descending `
+                    | Select-Object -First 1
+    # Check for required architecture folder
+    $foundVersionArchPath = Join-Path $foundVersion $goTool.arch
+    $envName = "GOROOT_{0}_{1}_X64" -f $goVersion.split(".")
+    setx $envName "$foundVersionArchPath" /M
 }
