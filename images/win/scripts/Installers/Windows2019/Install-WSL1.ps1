a4bb165f7cd5181bb3332ee4263f496c4d2711f5
Sergey Dolin <v-sedoli@micorosoft.com>
2020-06-19 17:48:40 +0500

    Add WSL

---
+++ images/win/scripts/Installers/Windows2019/Install-WSL1.ps1
@@ -0,0 +1,8 @@
+################################################################################
+##  File:  Install-WSL1.ps1
+##  Desc:  Install Windows Subsystem for Linux
+################################################################################
+
+Write-Host "Install Windows Subsystem for Linux"
+
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
