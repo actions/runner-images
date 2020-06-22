fde1e58c4ced2717aae15d57c1118930c70cc788
Darii Nurgaleev <Darleev@github.com>
2020-06-23 02:04:31 +0700

    Removed PostgreSQL server from path.

--- images/win/scripts/Installers/Install-PostgreSQL.ps1
+++ images/win/scripts/Installers/Install-PostgreSQL.ps1
@@ -9,11 +9,11 @@ Set-SystemVariable -SystemVariable PGUSER -Value $pgUser
 Set-SystemVariable -SystemVariable PGPASSWORD -Value $pgPwd
 
 #Install latest PostgreSQL
-Choco-Install -PackageName postgresql -ArgumentList "--params", "/Password:$pgPwd", "--params-global", "--debug", "--verbose"
+Choco-Install -PackageName postgresql -ArgumentList "--params", "/Password:$pgPwd", "/NoPath", "--params-global", "--debug", "--verbose"
 
 #Get Path to pg_ctl.exe
 $pgPath = (Get-CimInstance Win32_Service -Filter "Name LIKE 'postgresql-%'").PathName
 #Parse output of command above to obtain pure path
 $pgBin = Split-Path -Path $pgPath.split('"')[1]
 #Added PostgreSQL bin path into PATH variable
-Add-MachinePathItem $pgBin
+Set-SystemVariable -SystemVariable PGBIN -Value $pgBin
