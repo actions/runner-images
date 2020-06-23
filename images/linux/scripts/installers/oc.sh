c6f10804fcfa67b000cb1bbbb73967d459f86deb
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2020-06-23 11:27:30 -0700

    add oc cli tool 3.11.x

---
+++ images/linux/scripts/installers/oc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+################################################################################
+##  File:  aws.sh
+##  Desc:  Installs the AWS CLI
+################################################################################
+
+# Source the helpers
+source $HELPER_SCRIPTS/document.sh
+# Install the oc CLI
+curl "https://mirror.openshift.com/pub/openshift-v3/clients/3.11.232/linux/oc.tar.gz" > oc.tar.gz
+tar xvzf oc.tar.gz
+rm oc.tar.gz
+mv oc /usr/local/bin
+
+# Validate the installation
+echo "Validate the installation"
+if ! command -v oc; then
+    echo "oc was not installed"
+    exit 1
+fi
+
+# Document the installed version
+echo "Document the installed version"
+DocumentInstalledItem "oc CLI ($(oc --version 2>&1))"
