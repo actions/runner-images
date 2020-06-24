0ad2dba7ed62faf83f0e670f16e63919339b53a2
Dibir Magomedsaygitov <v-dimago@microsoft.com>
2020-06-24 17:22:09 +0300

    update aws cli v1 to v2 on ubuntu20

--- images/linux/scripts/installers/aws.sh
+++ images/linux/scripts/installers/aws.sh
@@ -7,12 +7,20 @@
 # Source the helpers
 source $HELPER_SCRIPTS/document.sh
 
-# Install the AWS CLI
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-rm awscli-bundle.zip
-rm -rf awscli-bundle
+# Install the AWS CLI v1 on Ubuntu16 and Ubuntu18, and AWS CLI v2 on Ubuntu20
+if isUbuntu20 ; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+    rm awscliv2.zip
+    rm -rf aws
+else
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+    unzip awscli-bundle.zip
+    ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+    rm awscli-bundle.zip
+    rm -rf awscli-bundle
+fi
 
 # Validate the installation
 echo "Validate the installation"
