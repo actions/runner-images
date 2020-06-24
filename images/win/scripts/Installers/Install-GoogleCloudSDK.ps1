4c34ad64df449c238c3fc4f523a75421ec8aad7d
Aleksandr Chebotov <47745270+al-cheb@users.noreply.github.com>
2020-06-24 11:37:36 +0300

    Add Google Cloud SDK for Windows (#1102)
    * Add Google Cloud SDK
    
    * Add Validate-GoogleCloudSDK.ps1 script

---
+++ images/win/scripts/Installers/Install-GoogleCloudSDK.ps1
@@ -0,0 +1,9 @@
+################################################################################
+##  File:  Install-GoogleCloudSDK.ps1
+##  Desc:  Install Google Cloud SDK
+################################################################################
+
+# https://cloud.google.com/sdk/docs/downloads-interactive
+$googleCloudSDKInstaller = "https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe"
+$argumentList = @("/S", "/allusers", "/noreporting")
+Install-Binary -Url $googleCloudSDKInstaller -Name "GoogleCloudSDKInstaller.exe" -ArgumentList $argumentList
