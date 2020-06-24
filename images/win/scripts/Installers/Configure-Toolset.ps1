b459aba73f98bf03c7b0ed67673c190a2c62aae4
Maksim Petrov <v-mapetr@microsoft.com>
2020-06-24 17:27:33 +0300

    Move default version functions from install-toolset to configure-toolset

--- images/win/scripts/Installers/Configure-Toolset.ps1
+++ images/win/scripts/Installers/Configure-Toolset.ps1
@@ -4,7 +4,34 @@
 ##  Desc:  Configure Toolset
 ################################################################################
 
-function Set-ToolEnvironment
+Function Set-DefaultToolVersion
+{
+    param
+    (
+        [string] $ToolName,
+        [string] $ToolVersion,
+        [string] $ToolArchitecture
+    )
+
+    $ToolPath = Join-Path -Path $env:AGENT_TOOLSDIRECTORY -ChildPath $ToolName
+    $ToolPathVersion = Join-Path -Path $ToolPath -ChildPath $ToolVersion -AdditionalChildPath $ToolArchitecture -Resolve
+
+    Write-Host "Use ${ToolName} ${ToolVersion} as a system ${ToolName}..."
+    if ($ToolPath -eq "Python")
+    {
+        Add-MachinePathItem -PathItem $ToolPathVersion | Out-Null
+        Add-MachinePathItem -PathItem "$ToolPathVersion\Scripts" | Out-Null
+    }
+
+    if ($ToolName -eq "go")
+    {
+        Add-MachinePathItem -PathItem "$ToolPathVersion\bin" | Out-Null
+        # Set the GOROOT environment variable.
+        setx GOROOT "$ToolPathVersion" /M | Out-Null
+    }
+}
+
+Function Set-ToolEnvironment
 {
     param
     (
@@ -21,8 +48,7 @@ function Set-ToolEnvironment
         $installationDir = Join-Path -Path $ToolPathVersion -ChildPath $ToolArchitecture
         $envName = "BOOST_ROOT_{0}" -f ($ToolVersion.Replace('.', '_'))
     }
-
-    if ($ToolName -eq "go")
+    elseif ($ToolName -eq "go")
     {
         $foundVersion = Get-Item $ToolPathVersion `
                         | Sort-Object -Property {[version]$_.name} -Descending `
@@ -30,6 +56,11 @@ function Set-ToolEnvironment
 
         $installationDir = Join-Path -Path $foundVersion -ChildPath $ToolArchitecture
         $envName = "GOROOT_{0}_{1}_X64" -f $ToolVersion.split(".")
+    }
+    else
+    {
+        Write-Host "No configuration for $ToolName $ToolVersion ($ToolArchitecture) is required. Skipping..."
+        return
     }
 
     setx $envName $installationDir /M | Out-Null
@@ -39,7 +70,7 @@ $ErrorActionPreference = "Stop"
 
 Import-Module -Name ImageHelpers -Force -DisableNameChecking
 
-$toolsToConfigure = @("Boost", "Go")
+$toolsToConfigure = @("Python", "Boost", "Go")
 $tools = Get-ToolsetContent | Select-Object -ExpandProperty toolcache `
                             | Where-Object { $toolsToConfigure -contains $_.name }
 
@@ -48,7 +79,19 @@ foreach ($tool in $tools)
     Write-Host "Configure $($tool.name)..."
     foreach ($toolVersion in $tool.versions)
     {
+        ### Skip Python because it has no environment variables, except for default version
+        if ($tool.name -eq "Python")
+        {
+            break
+        }
+
         Write-Host "Set $($tool.Name) $toolVersion environment..."
         Set-ToolEnvironment -ToolVersion $toolVersion -ToolArchitecture $tool.arch -ToolName $tool.name
+    }
+
+    if ($null -ne $tool.default)
+    {
+        Write-Host "Set $($tool.Name) default version environment..."
+        Set-DefaultToolVersion -ToolVersion $tool.default -ToolArchitecture $tool.arch -ToolName $tool.name
     }
 }
