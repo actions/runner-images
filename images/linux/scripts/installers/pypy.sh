ecf1424d2d8c6312ef33b50610601224b91a9ea6
Vladimir Safonkin <vsafonkin@gmail.com>
2020-06-25 09:12:05 +0300

    Fix download helper

--- images/linux/scripts/installers/pypy.sh
+++ images/linux/scripts/installers/pypy.sh
@@ -72,7 +72,7 @@ function InstallPyPy
 
 # Installation PyPy
 uri="https://downloads.python.org/pypy/"
-download_with_retries $uri "/tmp" "pypyUrls.html"
+curl $uri -4 -s --compressed -o "/tmp/pypyUrls.html"
 pypyVersions="$(cat /tmp/pypyUrls.html | grep 'linux64' | awk -v uri="$uri" -F'>|<' '{print uri$5}')"
 
 toolsetJson="$INSTALLER_SCRIPT_FOLDER/toolset.json"
