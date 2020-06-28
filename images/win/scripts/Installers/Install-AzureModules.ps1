ab05d10719a8d68892064b526b2991c37e3e5828
Maksim Petrov <v-mapetr@microsoft.com>
2020-06-28 05:32:30 +0300

    Move azureModules to toolset

--- images/win/scripts/Installers/Install-AzureModules.ps1
+++ images/win/scripts/Installers/Install-AzureModules.ps1
@@ -3,59 +3,33 @@
 ##  Desc:  Install Azure PowerShell modules
 ################################################################################
 
+$ErrorActionPreference = "Stop"
+
+Import-Module -Name ImageHelpers -Force
+
 Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
 
-#### NOW The correct Modules need to be saved in C:\Modules
+# The correct Modules need to be saved in C:\Modules
 $installPSModulePath = 'C:\Modules'
-if(-not (Test-Path -LiteralPath $installPSModulePath))
+if (-not (Test-Path -LiteralPath $installPSModulePath))
 {
-    Write-Host "Creating '$installPSModulePath' folder to store PowerShell Azure modules"
+    Write-Host "Creating ${installPSModulePath} folder to store PowerShell Azure modules"
     $null = New-Item -Path $installPSModulePath -ItemType Directory
 }
 
-# Powershell Azure modules to install
-$psAzureModulesToInstall = @{
-    "azurerm" = @(
-        "2.1.0"
-        "3.8.0"
-        "4.2.1"
-        "5.1.1"
-        "6.7.0"
-        "6.13.1"
-    )
+# Get modules content from toolset
+$modules = Get-ToolsetContent | Select-Object -ExpandProperty azureModules
 
-    "azure" = @(
-        "2.1.0"
-        "3.8.0"
-        "4.2.1"
-        "5.1.1"
-        "5.3.0"
-    )
-
-    "az" = @(
-        "1.0.0"
-        "1.6.0"
-        "2.3.2"
-        "2.6.0"
-        "3.1.0"
-        "3.5.0"
-        "3.8.0"
-        "4.3.0"
-    )
-}
-
-# Download Azure PowerShell modules
-foreach($psmoduleName in $psAzureModulesToInstall.Keys)
+foreach ($module in $modules)
 {
-    Write-Host "Installing '$psmoduleName' to the '$installPSModulePath' path:"
-    $psmoduleVersions = $psAzureModulesToInstall[$psmoduleName]
-    foreach($psmoduleVersion in $psmoduleVersions)
+    Write-Host "Installing $($module.name) to the ${installPSModulePath} path:"
+    foreach ($version in $module.versions)
     {
-        $psmodulePath = Join-Path $installPSModulePath "${psmoduleName}_${psmoduleVersion}"
-        Write-Host " - $psmoduleVersion [$psmodulePath]"
+        $modulePath = Join-Path -Path $installPSModulePath -ChildPath "$($module.name)_${version}"
+        Write-Host " - $version [$modulePath]"
         try
         {
-            Save-Module -Path $psmodulePath -Name $psmoduleName -RequiredVersion $psmoduleVersion -Force -ErrorAction Stop
+            Save-Module -Path $modulePath -Name $module.name -RequiredVersion $version -Force -ErrorAction Stop
         }
         catch
         {
