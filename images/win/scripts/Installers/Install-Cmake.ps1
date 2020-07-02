ed655b22e357fdb6f0f39133527e4cd2358bc3e8
Maxim Lobanov <v-malob@microsoft.com>
2020-07-02 08:09:38 +0300

    debug info

--- images/win/scripts/Installers/Install-Cmake.ps1
+++ images/win/scripts/Installers/Install-Cmake.ps1
@@ -6,7 +6,11 @@
 Choco-Install -PackageName cmake.install -ArgumentList "--installargs",'ADD_CMAKE_TO_PATH=""System""'
 
 # Restart current session
-Invoke-Command { & "powershell.exe" } -NoNewScope
+Write-Host "debug 1"
+Invoke-Command { Run-PesterTests -TestFile "Cmake" } -NoNewScope
+Write-Host "debug 2"
+Invoke-Command { Run-PesterTests -TestFile "Cmake" }
+Write-Host "debug 3"
 
 # Run Pester Test
 Run-PesterTests -TestFile "Cmake"
