9de5dfef44017c6c7ca12e2254c9978073546df5
Mikhail Timofeev <v-mitim@microsoft.com>
2020-07-02 11:11:56 +0300

    pretify script a bit

--- images/linux/scripts/installers/aws-sam-cli.sh
+++ images/linux/scripts/installers/aws-sam-cli.sh
@@ -2,29 +2,31 @@
 ################################################################################
 ##  File:  aws-sam-cli.sh
 ##  Desc:  Installs AWS SAM CLI
-##         Must be run after toolset installation
+##         Requires Pyton >=3.6, must be run after toolset installation
 ################################################################################
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/document.sh
 
-# Install aws sam cli
+# Download latest aws sam cli sources
 TarballUrl=$(curl -s https://api.github.com/repos/awslabs/aws-sam-cli/releases/latest | jq -r '.tarball_url')
 TarballPath="/tmp/aws-sam-cli.tar.gz"
 wget $TarballUrl -O $TarballPath
 tar -xzvf $TarballPath -C /tmp
-cd /tmp/awslabs-aws-sam-cli*
+SourcesDir=$(echo /tmp/awslabs-aws-sam-cli*)
+
 # Use python 3.7 from toolcache to install aws sam, setuptools module required for the installation
 Python3Dir=$(echo ${AGENT_TOOLSDIRECTORY}/Python/3.7*/x64)
 Python3BinDir=$(echo ${Python3Dir}/bin)
 export PATH="$Python3Dir:$Python3BinDir:$PATH"
 python3 -m pip install setuptools
+cd $SourcesDir
 python3 setup.py install
 sudo ln -sf ${Python3BinDir}/sam /usr/local/bin/sam
 
 # Cleanup downloaded files
 rm -rf $TarballPath
-rm -rf /tmp/awslabs-aws-sam-cli*
+rm -rf $SourcesDir
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
