30400d585230e9cecd53be6a6217ed630be30506
Maxim Lobanov <v-malob@microsoft.com>
2020-07-02 16:22:53 +0300

    Update Install-Pester.ps1

--- images/win/scripts/Installers/Install-Pester.ps1
+++ images/win/scripts/Installers/Install-Pester.ps1
@@ -1,4 +1,6 @@
+Import-Module -Name ImageHelpers -Force
+
 # Upgrade Pester to 4.10.1
 Install-Module Pester -RequiredVersion 4.10.1 -Scope AllUsers -SkipPublisherCheck -Force
 
-Add-AssertionOperator -Name ReturnZeroExitCode -Test  $function:ShouldReturnZeroExitCode
+Add-AssertionOperator -Name ReturnZeroExitCode -Test $function:ShouldReturnZeroExitCode
