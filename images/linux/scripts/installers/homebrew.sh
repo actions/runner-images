7c3279e3b61b74c3af2c89ab8fd396da15fdf182
Mikhail Timofeev <v-mitim@microsoft.com>
2020-07-03 13:13:46 +0300

    add comment and move installation

--- images/linux/scripts/installers/homebrew.sh
+++ images/linux/scripts/installers/homebrew.sh
@@ -2,6 +2,7 @@
 ################################################################################
 ##  File:  homebrew.sh
 ##  Desc:  Installs the Homebrew on Linux
+##  Caveat: Brew MUST NOT be used to install any tool during the image build to avoid dependencies, which may come along with the tool
 ################################################################################
 
 # Source the helpers
