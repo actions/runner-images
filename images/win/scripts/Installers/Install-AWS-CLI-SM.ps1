53eecd6367b617156ae4007903dc918996098ef0
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-07-04 12:26:39 +0300

    move to one file

--- images/win/scripts/Installers/Install-AWS-CLI-SM.ps1
+++
@@ -1,9 +0,0 @@
-################################################################################
-##  File:  Install-AWS-CLI-SM.ps1
-##  Desc:  Install AWS CLI Session Manager
-################################################################################
-
-$sessionManagerName = "SessionManagerPluginSetup.exe"
-$sessionManagerUrl = "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/windows/$sessionManagerName"
-Install-Binary -Url $sessionManagerUrl -Name $sessionManagerName -ArgumentList ("/silent", "/install")
-Add-MachinePathItem -PathItem "C:\Program Files\Amazon\SessionManagerPlugin\bin"
