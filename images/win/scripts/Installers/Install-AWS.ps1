0fa2df6ae037dfbed7d49e74b38a6647cddc4ec3
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-07-06 12:06:01 +0300

    move logic to one file

--- images/win/scripts/Installers/Install-AWS.ps1
+++ images/win/scripts/Installers/Install-AWS.ps1
@@ -20,3 +20,13 @@ $sessionManagerName = "SessionManagerPluginSetup.exe"
 $sessionManagerUrl = "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/windows/$sessionManagerName"
 Install-Binary -Url $sessionManagerUrl -Name $sessionManagerName -ArgumentList ("/silent", "/install")
 Add-MachinePathItem -PathItem "C:\Program Files\Amazon\SessionManagerPlugin\bin"
+
+$sessionMessage = session-manager-plugin
+
+if ($sessionMessage -Match "*plugin was installed successfully*") {
+    Write-Host "$sessionMessage"
+    exit 0
+} else {
+    Write-Host "$sessionMessage"
+    exit 1
+}
