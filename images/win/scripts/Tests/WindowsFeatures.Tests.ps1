09de26981f0202b999c64a58276814ba32961ba4
Vladimir Safonkin <vsafonkin@gmail.com>
2020-07-15 08:34:07 +0300

    Reworked wsl tests

--- images/win/scripts/Tests/WindowsFeatures.Tests.ps1
+++ images/win/scripts/Tests/WindowsFeatures.Tests.ps1
@@ -10,26 +10,23 @@ Describe "WindowsFeatures" {
         $testCases += @{ FeatureName = "BITS" }
         $testCases += @{ FeatureName = "DSC-Service" }
     }
+    if (Test-isWin19) {
+        $testCases += @{ FeatureName = "Microsoft-Windows-Subsystem-Linux" }
+    }
 
     It "Windows Feature <FeatureName> is installed" -TestCases $testCases {
         (Get-WindowsFeature -Name $FeatureName).InstallState | Should -Be "Installed"
+    }
+
+    if (Test-isWin19) {
+        it "Check WSL is on path" {
+            (Get-Command -Name 'wsl') | Should -ReturnZeroExitCode
+        }
     }
 }
 
 Describe "ContainersFeature" {
     It "Windows containers feature is installed" {
         (Get-WindowsFeature -Name "Containers").InstallState | Should -Be "Installed"
-    }
-}
-
-if (Test-isWin19) {
-    Describe "WSL" {
-        It "WSL is installed" {
-            (Get-WindowsFeature -Name "Microsoft-Windows-Subsystem-Linux").InstallState | Should -Be "Installed"
-        }
-
-        It "Check WSL is on path" {
-            (Get-Command -Name 'wsl') | Should -ReturnZeroExitCode
-        }
     }
 }
