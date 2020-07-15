b73f3dd0c2fc78171db3eec2965fe756ed020246
Aleksandr Chebotov <v-aleche@microsoft.com>
2020-07-15 09:26:07 +0300

    fixed rust test

--- images/win/scripts/Tests/Rust.Tests.ps1
+++ images/win/scripts/Tests/Rust.Tests.ps1
@@ -1,18 +1,10 @@
 Describe "Rust" {
-    Push-Location -Path $env:UserProfile
-    if ( -not ((Test-Path -Path ".rustup") -and (Test-Path -Path ".cargo")))
-    {
-        $null = New-Item -Name ".rustup" -Value "C:\Rust\.rustup" -ItemType Junction
-        $null = New-Item -Name ".cargo" -Value "C:\Rust\.cargo" -ItemType Junction
-    }
-    Pop-Location
-
     $rustTools = @(
-        @{command = "rustup"; binPath = "C:\Rust\.cargo\bin\rustup.exe"}
-        @{command = "rustc"; binPath = "C:\Rust\.cargo\bin\rustc.exe"}
-        @{command = "cargo"; binPath = "C:\Rust\.cargo\bin\cargo.exe"}
-        @{command = "cargo audit"; binPath = "C:\Rust\.cargo\bin\cargo-audit.exe"}
-        @{command = "cargo outdated"; binPath = "C:\Rust\.cargo\bin\cargo-outdated.exe"}
+        @{ToolName = "rustup"; binPath = "C:\Rust\.cargo\bin\rustup.exe"}
+        @{ToolName = "rustc"; binPath = "C:\Rust\.cargo\bin\rustc.exe"}
+        @{ToolName = "cargo"; binPath = "C:\Rust\.cargo\bin\cargo.exe"}
+        @{ToolName = "cargo audit"; binPath = "C:\Rust\.cargo\bin\cargo-audit.exe"}
+        @{ToolName = "cargo outdated"; binPath = "C:\Rust\.cargo\bin\cargo-outdated.exe"}
     )
 
     $rustEnvNotExists = @(
@@ -28,8 +20,7 @@ Describe "Rust" {
         [Environment]::GetEnvironmentVariables("Machine").ContainsKey($envVar) | Should -BeFalse
     }
 
-    It "<command> is installed to the '<binPath>' folder" -TestCases $rustTools {
-        "$command --version" | Should -ReturnZeroExitCode
+    It "<ToolName> is installed to the '<binPath>' folder" -TestCases $rustTools {
         $binPath | Should -Exist
     }
 }
