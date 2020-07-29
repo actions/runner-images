20e696c0922c57a3a91cd58e5412ae1d81789208
Vladimir Safonkin <vsafonkin@gmail.com>
2020-07-29 11:58:45 +0300

    Create symlink for cabal

--- images/linux/scripts/installers/haskell.sh
+++ images/linux/scripts/installers/haskell.sh
@@ -55,11 +55,12 @@ if ! command -v stack; then
     exit 1
 fi
 
-# Create symlink for ghc in /usr/bin
+# Create symlink for ghc and cabal in /usr/bin
 ln -s "/opt/ghc/$defaultGHCVersion/bin/ghc" "/usr/bin/ghc"
+ln -s "/opt/cabal/$cabalVersion/bin/cabal" "/usr/bin/cabal"
 
 # Document what was added to the image
 echo "Lastly, documenting what we added to the metadata file"
-DocumentInstalledItem "Haskell Cabal ($(/opt/cabal/$cabalVersion/bin/cabal --version))"
+DocumentInstalledItem "Haskell Cabal ($(cabal --version))"
 DocumentInstalledItem "GHC ($(ghc --version))"
 DocumentInstalledItem "Haskell Stack ($(stack --version))"
