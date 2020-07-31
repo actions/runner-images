5c21bd503072a271af1ac9611c96d6501bc09d2b
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-07-31 10:56:05 +0300

    change naming

--- images/linux/scripts/installers/r.sh
+++ images/linux/scripts/installers/r.sh
@@ -5,10 +5,10 @@ source $HELPER_SCRIPTS/document.sh
 source $HELPER_SCRIPTS/os.sh
 
 # install R
-version=$(getOSVersionLabel)
+osLabel=$(getOSVersionLabel)
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $version-cran40/"
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $osLabel-cran40/"
 
 sudo apt update
 sudo apt install r-base
