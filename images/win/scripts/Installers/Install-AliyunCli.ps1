207567daa36d8cd27d238dd2790f9b648dcff502
Mikhail Timofeev <v-mitim@microsoft.com>
2020-08-03 16:50:20 +0300

    fix typo

--- images/win/scripts/Installers/Install-AliyunCli.ps1
+++ images/win/scripts/Installers/Install-AliyunCli.ps1
@@ -7,7 +7,7 @@ Write-Host "Download Latest aliyun-cli archive"
 $url = 'https://api.github.com/repos/aliyun/aliyun-cli/releases/latest'
 # Explicitly set type to string since match returns array by default
 [System.String] $aliyunLatest = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "aliyun-cli-windows"
-$aliyunArchivePath = Start-DownloadWithRetry -Url $aliyunLatest -Name "aliyin-cli.zip"
+$aliyunArchivePath = Start-DownloadWithRetry -Url $aliyunLatest -Name "aliyun-cli.zip"
 
 Write-Host "Expand aliyun-cli archive"
 $aliyunPath = "C:\aliyun-cli"
