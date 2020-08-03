439532f42db1403a0272c7d8546e09918a947731
Mikhail Timofeev <v-mitim@microsoft.com>
2020-08-03 16:10:33 +0300

    change aliyun-cli installation

--- images/win/scripts/Installers/Install-AliyunCli.ps1
+++ images/win/scripts/Installers/Install-AliyunCli.ps1
@@ -3,6 +3,15 @@
 ##  Desc:  Install Alibaba Cloud CLI
 ################################################################################
 
-Choco-Install -PackageName aliyun-cli
+Write-Host "Download Latest aliyun-cli archive"
+$url = 'https://api.github.com/repos/aliyun/aliyun-cli/releases/latest'
+# Explicitly set type to string since match returns array by default
+[System.String] $aliyunLatest = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "aliyun-cli-windows"
+$aliyunArchivePath = Start-DownloadWithRetry -Url $aliyunLatest -Name "aliyin-cli.zip"
+
+Write-Host "Expand aliyun-cli archive"
+# Expand aliyun.exe to System32 directory
+$DestinationPath = [System.Environment]::SystemDirectory
+Extract-7Zip -Path $aliyunArchivePath -DestinationPath $DestinationPath
 
 Invoke-PesterTests -TestFile "Tools" -TestName "AliyunCli"
