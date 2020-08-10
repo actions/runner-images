98afb8924a2828681ce4b37a95d9a6584ac2317a
Sergey Dolin <v-sedoli@micorosoft.com>
2020-08-10 21:38:46 +0500

    Install NSIS from Source Forge

--- images/win/scripts/Installers/Install-NSIS.ps1
+++ images/win/scripts/Installers/Install-NSIS.ps1
@@ -3,7 +3,9 @@
 ##  Desc:  Install NSIS
 ################################################################################
 
-Choco-Install -PackageName nsis
+# Choco-Install -PackageName nsis
+
+Install-Binary -Url https://jztkft.dl.sourceforge.net/project/nsis/NSIS%203/3.05/nsis-3.05-setup.exe -Name nsis-3.05-setup.exe  -ArgumentList ('/S')
 
 $NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
 Add-MachinePathItem $NsisPath
