56af7768b99f729ff764cd12bcbb622110382997
Sergey Dolin <v-sedoli@micorosoft.com>
2020-08-11 15:57:57 +0500

    Switch to 3.06.1

--- images/win/scripts/Installers/Install-NSIS.ps1
+++ images/win/scripts/Installers/Install-NSIS.ps1
@@ -3,9 +3,7 @@
 ##  Desc:  Install NSIS
 ################################################################################
 
-# Choco-Install -PackageName nsis
-
-Install-Binary -Url https://jztkft.dl.sourceforge.net/project/nsis/NSIS%203/3.05/nsis-3.05-setup.exe -Name nsis-3.05-setup.exe  -ArgumentList ('/S')
+Install-Binary -Url 'https://downloads.sourceforge.net/project/nsis/NSIS%203/3.06.1/nsis-3.06.1-setup.exe' -Name  nsis-3.06.1-setup.exe -ArgumentList ('/S')
 
 $NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
 Add-MachinePathItem $NsisPath
