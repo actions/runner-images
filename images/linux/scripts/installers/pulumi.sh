7a84fe5a64792f8247f71493746c5cc0b88b2c2a
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-08-20 22:35:51 +0300

    change Pulumi installation script

--- images/linux/scripts/installers/pulumi.sh
+++ images/linux/scripts/installers/pulumi.sh
@@ -8,8 +8,18 @@
 source $HELPER_SCRIPTS/document.sh
 
 # Install Pulumi
-curl -fsSL https://get.pulumi.com | sh
-cp -r ~/.pulumi/bin/* /usr/local/bin/
+VERSION=$(curl --fail --silent -L "https://www.pulumi.com/latest-version")
+TARBALL_URL="https://get.pulumi.com/releases/sdk/pulumi-v${VERSION}-linux-x64.tar.gz"
+TARBALL_DEST=$(mktemp -t pulumi.tar.gz.XXXXXXXXXX)
+curl -L -o "${TARBALL_DEST}" "${TARBALL_URL}"
+EXTRACT_DIR=$(mktemp -d pulumi.XXXXXXXXXX)
+tar zxf "${TARBALL_DEST}" -C "${EXTRACT_DIR}"
+if [ -d "${EXTRACT_DIR}/pulumi/bin" ]; then
+    mv "${EXTRACT_DIR}/pulumi/bin" "/usr/local/bin/"
+else
+    cp -r "${EXTRACT_DIR}/pulumi/." "/usr/local/bin/"
+fi
+rm -rf ${TARBALL_DEST} ${EXTRACT_DIR}
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
