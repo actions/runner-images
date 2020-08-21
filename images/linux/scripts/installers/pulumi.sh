e768deb54051b5577bb224324403b3c3e3cddbc2
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-08-21 15:05:59 +0300

    removed legacy install, use /tmp, change auto-documentation, use download function instead of curl

--- images/linux/scripts/installers/pulumi.sh
+++ images/linux/scripts/installers/pulumi.sh
@@ -10,16 +10,8 @@ source $HELPER_SCRIPTS/document.sh
 # Install Pulumi
 VERSION=$(curl --fail --silent -L "https://www.pulumi.com/latest-version")
 TARBALL_URL="https://get.pulumi.com/releases/sdk/pulumi-v${VERSION}-linux-x64.tar.gz"
-TARBALL_DEST=$(mktemp -t pulumi.tar.gz.XXXXXXXXXX)
-curl -L -o "${TARBALL_DEST}" "${TARBALL_URL}"
-EXTRACT_DIR=$(mktemp -d pulumi.XXXXXXXXXX)
-tar zxf "${TARBALL_DEST}" -C "${EXTRACT_DIR}"
-if [ -d "${EXTRACT_DIR}/pulumi/bin" ]; then
-    mv "${EXTRACT_DIR}/pulumi/bin" "/usr/local/bin/"
-else
-    cp -r "${EXTRACT_DIR}/pulumi/." "/usr/local/bin/"
-fi
-rm -rf ${TARBALL_DEST} ${EXTRACT_DIR}
+download_with_retries ${TARBALL_URL} "/tmp" pulumi-v${VERSION}.tar.gz
+tar --strip=1 -xvf /tmp/pulumi-v${VERSION}.tar.gz -C /usr/local/bin
 
 # Run tests to determine that the software installed as expected
 echo "Testing to make sure that script performed as expected, and basic scenarios work"
@@ -29,6 +21,5 @@ if ! command -v pulumi ; then
 fi
 
 # Document what was added to the image
-pulumi_version="$(pulumi version)"
 echo "Lastly, documenting what we added to the metadata file"
-DocumentInstalledItem "Pulumi ($pulumi_version)"
+DocumentInstalledItem "Pulumi $(pulumi version)"
