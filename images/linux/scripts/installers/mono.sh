034dcc820bedb4a939c552e40f46a59a4b4f78d6
John McKenna <john.mckenna@whitehatsec.com>
2020-08-21 12:10:19 +0100

    Downgrade Mono

--- images/linux/scripts/installers/mono.sh
+++ images/linux/scripts/installers/mono.sh
@@ -10,7 +10,7 @@ LSB_CODENAME=$(lsb_release -cs)
 # wget "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF" -O out && sudo apt-key add out && rm out
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-$LSB_CODENAME main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb https://download.mono-project.com/repo/ubuntu stable-$LSB_CODENAME/snapshots/5.12.0.301 main" | tee /etc/apt/sources.list.d/mono-official-stable.list
 apt-get update
 apt-get install -y --no-install-recommends apt-transport-https mono-complete nuget
 
