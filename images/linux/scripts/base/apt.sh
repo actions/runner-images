2881a81b22886c8b13a8c9a982de512f82161a95
Aleksandr Chebotov <v-aleche@microsoft.com>
2020-09-08 14:40:56 +0300

    move apt pkgs to the  toolset.json

--- images/linux/scripts/base/apt.sh
+++ images/linux/scripts/base/apt.sh
@@ -11,8 +11,10 @@ systemctl disable apt-daily-upgrade.service
 # Configure apt to always assume Y
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
-# Use apt-fast for parallel downloads
+# Install aria2 and jq
 apt-get install aria2
+apt-get install jq
+# Use apt-fast for parallel downloads
 add-apt-repository -y ppa:apt-fast/stable
 apt-get update
-apt-get -y install apt-fast
+apt-get install apt-fast
