096464f38eb8597540566cead683c8a72b65a8f6
Chris Gavin <chris@chrisgavin.me>
2020-09-08 15:23:49 +0100

    Tidy up CodeQL bundle Linux installer script in response to review comments.

--- images/linux/scripts/installers/codeql-bundle.sh
+++ images/linux/scripts/installers/codeql-bundle.sh
@@ -4,6 +4,8 @@
 ##  Desc:  Install the CodeQL CLI Bundle to the toolcache.
 ################################################################################
 
+source $HELPER_SCRIPTS/install.sh
+
 # Retrieve the name of the CodeQL bundle preferred by the Action (in the format codeql-bundle-YYYYMMDD).
 codeql_bundle_name="$(curl -sSL https://raw.githubusercontent.com/github/codeql-action/main/src/defaults.json | jq -r .bundleVersion)"
 # Convert the bundle name to a version number (0.0.0-YYYYMMDD).
@@ -12,8 +14,9 @@ codeql_bundle_version="0.0.0-${codeql_bundle_name##*-}"
 extraction_directory="$AGENT_TOOLSDIRECTORY/CodeQL/$codeql_bundle_version/x64"
 mkdir -p "$extraction_directory"
 
->&2 echo "Downloading CodeQL bundle $codeql_bundle_version..."
-curl -sSL "https://github.com/github/codeql-action/releases/download/$codeql_bundle_name/codeql-bundle.tar.gz" | tar -xzC "$extraction_directory"
+echo "Downloading CodeQL bundle $codeql_bundle_version..."
+download_with_retries "https://github.com/github/codeql-action/releases/download/$codeql_bundle_name/codeql-bundle.tar.gz" "/tmp" "codeql-bundle.tar.gz"
+tar -xzf "/tmp/codeql-bundle.tar.gz" -C "$extraction_directory"
 
 # Test that the tool has been extracted successfully.
 "$AGENT_TOOLSDIRECTORY/CodeQL/$codeql_bundle_version/x64/codeql/codeql" version
