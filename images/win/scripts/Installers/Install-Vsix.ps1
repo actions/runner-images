40333f6adf0aefc6c82101f192325492811956c7
Andrey Mishechkin (GITHUB INC) <v-andmis@microsoft.com>
2020-09-09 08:55:15 +0400

    Install-Vsix.ps1 has been created

---
+++ images/win/scripts/Installers/Install-Vsix.ps1
@@ -0,0 +1,21 @@
+###################################################################################
+##  File:  Install-Vsix.ps1
+##  Desc:  Install the Visual Studio Extensions from toolset.json
+###################################################################################
+
+Import-Module -Name ImageHelpers -Force
+$ErrorActionPreference = "Stop"
+
+$toolset = Get-ToolsetContent
+$requiredVsixs = $toolset.visualStudio.vsix
+
+if (Test-IsWin19) {
+    $VsVersion = '2019'
+}
+else {
+    $VsVersion = '2017'
+}
+
+$requiredVsixs | ForEach-Object {
+    Install-VsixExtension -url $_.url -name $_.name -VsVersion $VsVersion
+}
