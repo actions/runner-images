d1ae794365db8871fae4743e20a6a364cf4ff0c8
Andrey Mishechkin (GITHUB INC) <v-andmis@microsoft.com>
2020-09-11 20:14:21 +0400

    minor fix in Vsix.Tests

--- images/win/scripts/Tests/Vsix.Tests.ps1
+++ images/win/scripts/Tests/Vsix.Tests.ps1
@@ -5,7 +5,7 @@ Describe "Vsix" {
     $allPackages = (Get-VSSetupInstance | Select-VsSetupInstance -Product *).Packages
     $testCases = $requiredVsixs | ForEach-Object { @{ VsixId = $_.Id;  AllPackages = $allPackages }}
 
-    It "VSIX Extension <VsixId>" -TestCases $testCases {
+    It "Extension <VsixId>" -TestCases $testCases {
         $objVsix = $AllPackages | Where-Object { $_.id -eq $VsixId }
         $objVsix | Should -Not -BeNullOrEmpty
     }
