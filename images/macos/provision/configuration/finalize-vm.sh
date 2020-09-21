36fb92995ab1a08dbcb8b7e7345b91c98ec9d9f1
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-09-21 17:16:22 +0300

    add build vm assets

--- images/macos/provision/configuration/finalize-vm.sh
+++ images/macos/provision/configuration/finalize-vm.sh
@@ -12,6 +12,12 @@ fi
 # Put documentation to $HOME root
 cp $HOME/image-generation/output/software-report/systeminfo.txt $HOME/image-generation/output/software-report/systeminfo.md $HOME/
 
+# Put build vm assets scripts to proper directory
+sudo mkdir -p /usr/local/opt/$USER/scripts
+sudo chmod ugo+rwx /usr/local/opt/$USER/scripts
+sudo chown $USER:admin /usr/local/opt/$USER
+mv $HOME/image-generation/assets/* /usr/local/opt/$USER/scripts
+
 # Clean up npm cache which collected during image-generation
 # we have to do that here because `npm install` is run in a few different places during image-generation
 npm cache clean --force
