d4335cc52e0569b7e5ac9720b10ddbecaede0995
Aleksandr Chebotov <v-aleche@microsoft.com>
2020-09-21 11:18:28 +0300

    set chmod -R 777 /opt

---
+++ images/linux/scripts/installers/post-deployment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+################################################################################
+##  File: post-deployment.sh
+##  Desc: Post deployment actions
+################################################################################
+
+# set chmod -R 777 /opt
+if [[ -d "/opt" ]]; then
+    echo "chmod -R 777 /opt"
+    chmod -R 777 /opt
+fi
