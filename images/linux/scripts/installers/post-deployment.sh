fe9f74e51ed37a716f61a7e9de6286bf57ef55c8
Aleksandr Chebotov <v-aleche@microsoft.com>
2020-09-22 13:55:21 +0300

    rm helper and installer folder in post-deployment

--- images/linux/scripts/installers/post-deployment.sh
+++ images/linux/scripts/installers/post-deployment.sh
@@ -9,3 +9,8 @@ if [[ -d "/opt" ]]; then
     echo "chmod -R 777 /opt"
     chmod -R 777 /opt
 fi
+
+# remove installer and helper folders
+rm -rf $HELPER_SCRIPT_FOLDER
+rm -rf $INSTALLER_SCRIPT_FOLDER
+chmod 755 $IMAGE_FOLDER
