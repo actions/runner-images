56633cec4a6ad6c1eb0e4755e8d47b7c4eda31b2
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2020-09-30 20:13:40 +0300

    refactor and rename scripts

--- images/win/post-generation/DotnetPath.ps1
+++ images/win/post-generation/DotnetPath.ps1
@@ -4,19 +4,9 @@ $dotnetPath = "$env:USERPROFILE\.dotnet\tools"
 if ($latestPath.Contains("C:\Users\VssAdministrator\.dotnet\tools"))
 {
     $latestPath = $latestPath.Replace("C:\Users\VssAdministrator\.dotnet\tools", "$dotnetPath")
-    $isPathUpdated = $true
 }
 
 if (-not $latestPath.Contains($dotnetPath))
 {
     $latestPath = "$dotnetPath;$latestPath"
-    Write-Host "Added .dotnet\tools $dotnetPath to PATH"
-    $isPathUpdated = $true
 }
-else
-{
-    Write-Host "$dotnetPath already in PATH"
-}
-
-$updatedPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::Machine)
-Write-Host "Windows PATH: $updatedPath"
--- images/win/post-generation/MercurialPath.ps1
+++ images/win/post-generation/MercurialPath.ps1
@@ -5,12 +5,4 @@ if (Test-Path -Path $hgPath)
 {
     $latestPath = "$hgPath;$latestPath"
     Write-Host "Added Mercurial to PATH"
-    $isPathUpdated = $true
 }
-else
-{
-    Write-Host "Didn't find Mercurial at $hgPath. Skipping adding PATH to it."
-}
-
-$updatedPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::Machine)
-Write-Host "Windows PATH: $updatedPath"
