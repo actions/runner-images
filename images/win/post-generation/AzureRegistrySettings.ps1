91ee0cc2682fcc58b746667164239c906c9293a7
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2020-10-01 10:48:50 +0300

    rename and remove scripts

--- images/win/post-generation/AzureRegistrySettings.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference="Stop"
-
-# Remove 3 registry settings that are left behind when sysprepping. Having those registry settings together with a
-# race condition that kicks in when trying to run custom scripts extensions and stops us from reimaging machines.
-Remove-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Azure\HandlerState' -Name 'Incarnation' -Force  -ErrorAction Ignore
-Remove-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Azure\HandlerState' -Name 'InVmSvdSeqNo' -Force  -ErrorAction Ignore
-Remove-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Azure\HandlerState' -Name 'LastGoalStateMethod' -Force  -ErrorAction Ignore
--- images/win/post-generation/MercurialPath.ps1
+++
@@ -1,8 +0,0 @@
-$hgPath = "$Env:ProgramFiles\Mercurial\"
-$latestPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::Machine)
-
-if (Test-Path -Path $hgPath)
-{
-    $latestPath = "$hgPath;$latestPath"
-    Write-Host "Added Mercurial to PATH"
-}
