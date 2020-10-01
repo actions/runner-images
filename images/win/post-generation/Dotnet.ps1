c101287fd83cc310e38f6d19d3a9547324882373
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2020-10-01 11:41:42 +0300

    rework dotnet script

--- images/win/post-generation/Dotnet.ps1
+++ images/win/post-generation/Dotnet.ps1
@@ -1,12 +1,9 @@
 $latestPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::Machine)
-
 $dotnetPath = "$env:USERPROFILE\.dotnet\tools"
-if ($latestPath.Contains("C:\Users\VssAdministrator\.dotnet\tools"))
-{
-    $latestPath = $latestPath.Replace("C:\Users\VssAdministrator\.dotnet\tools", "$dotnetPath")
-}
 
 if (-not $latestPath.Contains($dotnetPath))
 {
     $latestPath = "$dotnetPath;$latestPath"
 }
+
+[System.Environment]::SetEnvironmentVariable('PATH', $latestPath, [System.EnvironmentVariableTarget]::Machine)
