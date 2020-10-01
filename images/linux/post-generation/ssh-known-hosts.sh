95a2b93e3044b84e635809d5066a2b6d3fe2d8f2
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-10-01 16:41:53 +0300

    moved key import to git installation

--- images/linux/post-generation/ssh-known-hosts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Add well-known SSH host keys to known_hosts
-ssh-keyscan -t rsa github.com >> /etc/ssh/ssh_known_hosts
-ssh-keyscan -t rsa ssh.dev.azure.com >> /etc/ssh/ssh_known_hosts
