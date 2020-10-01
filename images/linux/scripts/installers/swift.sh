7532a0865019142d1006293f83bc1914c61b4d8e
Sergey Dolin <v-sedoli@micorosoft.com>
2020-10-01 13:20:50 +0500

    Fix swift installation

--- images/linux/scripts/installers/swift.sh
+++ images/linux/scripts/installers/swift.sh
@@ -6,12 +6,15 @@
 
 set -e
 set -o pipefail
+set -x
 
 # Install
 image_label="$(lsb_release -rs)"
-swift_version=$(curl -s https://swift.org/download/ | grep -m1 "id=\"swift-" | awk -F"[ <]" '{print $4}')
+curl -s -L https://swift.org/download > /tmp/c
+swift_version=$(awk -F"[ <]" '/id="swift-/ {print $4; exit}' /tmp/c)
 
 wget -P /tmp https://swift.org/builds/swift-$swift_version-release/ubuntu${image_label//./}/swift-$swift_version-RELEASE/swift-$swift_version-RELEASE-ubuntu$image_label.tar.gz
+
 tar xzf /tmp/swift-$swift_version-RELEASE-ubuntu$image_label.tar.gz
 mv swift-$swift_version-RELEASE-ubuntu$image_label /usr/share/swift
 
