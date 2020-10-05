958f252984ea7f3fb1c332f50c5b84e248b1a365
Vladimir Safonkin <vsafonkin@gmail.com>
2020-10-05 13:31:53 +0300

    Add check for python path

--- images/linux/scripts/installers/pipx-packages.sh
+++ images/linux/scripts/installers/pipx-packages.sh
@@ -8,11 +8,17 @@
 export PATH="$PATH:/opt/pipx_bin"
 
 toolset="$INSTALLER_SCRIPT_FOLDER/toolset.json"
-pipx_packages=$(jq -r ".pipx[]" $toolset)
+pipx_packages=$(jq ".pipx[] .package" $toolset)
 
 for package in $pipx_packages; do
-    echo "Install $package"
-    pipx install $package
+    python_path=$(jq ".pipx[] | select(.package == $package) .python" $toolset)
+    if [ "$python_path" = "default" ]; then
+        echo "Install $package into default python"
+        pipx install $package
+    else
+        echo "Install $package into python $python_path"
+        pipx install $package --python $python_path
+    fi
 done
 
 # Run tests to determine that the software installed as expected
