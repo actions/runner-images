44f565e921258049b201af149ba6fc340cdc7759
Mikhail Timofeev <v-mitim@microsoft.com>
2020-10-06 14:32:37 +0300

    fix comment

--- images/macos/provision/core/haskell.sh
+++ images/macos/provision/core/haskell.sh
@@ -4,7 +4,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 export PATH="$HOME/.ghcup/bin:$PATH"
 echo 'export PATH="$PATH:$HOME/.ghcup/bin"' >> "$HOME/.bashrc"
 
-# ghcup output looks like this "x ghc 8.8.3 base-4.13.0.0 hls-powered", need to take all the first versions only(8.8.3 in that case) and avoid pre-release ones
+# ghcup output looks like this "ghc 8.6.4 base-4.12.0.0 hls-powered", need to take all the first versions only(8.6.4 in that case) and avoid pre-release ones
 availableVersions=$(ghcup list -t ghc -r | grep -v "prerelease" | awk '{print $2}')
 
 # Install 3 latest major versions(For instance 8.6.5, 8.8.4, 8.10.2)
