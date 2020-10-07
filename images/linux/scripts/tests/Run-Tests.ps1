2734e80e5edef0982a16fd348f2466835e43c7b7
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-10-07 17:30:00 +0300

    add startup logic

--- images/linux/scripts/tests/Run-Tests.ps1
+++ images/linux/scripts/tests/Run-Tests.ps1
@@ -1,1 +1,23 @@
+param (
+    [Parameter(Mandatory=$true)]
+    $WorkingDirectory,
+    [Parameter(Mandatory=$true)]
+    $OutputFile
+)
+
+$ErrorActionPreference = "Stop"
+
+Import-Module Pester
+Set-Location -Path $WorkingDirectory
+
+$configuration = [PesterConfiguration] @{
+    Output = @{ Verbosity = "Detailed" }
+    TestResult = @{
+        Enabled = $true
+        OutputPath = $OutputFile;
+        OutputFormat = "NUnitXML"
+    }
+}
+
+Invoke-Pester -Configuration $configuration
 Invoke-PesterTests "*"
