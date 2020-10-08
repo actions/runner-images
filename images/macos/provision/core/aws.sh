d383b87562af43bef2277e8c2bf4589484cd12c1
Mikhail Timofeev <v-mitim@microsoft.com>
2020-10-08 19:52:11 +0300

    installation workaround

--- images/macos/provision/core/aws.sh
+++ images/macos/provision/core/aws.sh
@@ -5,6 +5,16 @@ brew install awscli
 
 echo Installing aws sam cli...
 brew tap aws/tap
+
+# Workaround for broken aws-sam-cli formula after this pr https://github.com/aws/homebrew-tap/pull/138
+cd "$(brew --repo aws/tap)"
+currentCommitId=$(git rev-parse --short HEAD)
+brokenCommitId="9d91002"
+if [ $currentCommitId == $brokenCommitId ]; then
+    # Checkout the latest working commit
+    git checkout b5b3afed932d
+fi
+
 brew install aws-sam-cli
 
 echo "Install aws cli session manager"
