6a1710a597d7e0e7f8c86f0cccd163316579694f
Sam <samcoe@users.noreply.github.com>
2020-10-08 13:16:57 +0200

    Update gh windows install script to use official release assets

--- images/win/scripts/Installers/Install-GitHub-CLI.ps1
+++ images/win/scripts/Installers/Install-GitHub-CLI.ps1
@@ -3,7 +3,14 @@
 ##  Desc:  Install GitHub CLI
 ################################################################################
 
-Choco-Install -PackageName gh
+Write-Host "Get the latest gh version..."
+$GhReleasesJson = Invoke-RestMethod "https://api.github.com/repos/cli/cli/releases/latest"
+$WindowsMsiAssetNamePattern = "windows_amd64.msi"
+$WindowsMsiAsset = $GhReleasesJson.assets | Where-Object { $_.name.EndsWith($WindowsMsiAssetNamePattern) } | Select-Object -First 1
+$Name = $WindowsMsiAsset.name
+$DownloadUrl = $WindowsMsiAsset.browser_download_url
+
+Install-Binary -Url $DownloadUrl -Name $Name
 
 Add-MachinePathItem "C:\Program Files (x86)\GitHub CLI"
 
