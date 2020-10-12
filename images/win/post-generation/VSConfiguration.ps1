f33118c5dc913db70d0712414e121acd803cb65e
Darii Nurgaleev <Darleev@github.com>
2020-10-12 21:46:49 +0700

    removed from old file and renamed choco.

--- images/win/post-generation/VSConfiguration.ps1
+++ images/win/post-generation/VSConfiguration.ps1
@@ -2,6 +2,3 @@ $vsInstallRoot = Get-VisualStudioPath
 $devEnvPath = "$vsInstallRoot\Common7\IDE\devenv.exe"
 
 cmd.exe /c "`"$devEnvPath`" /updateconfiguration"
-
-# Step to avoid initial delay for choco scripts
-choco upgrade chocolatey
