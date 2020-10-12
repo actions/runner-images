ed38c86cbb585a410e6c74d9d423752db2a47756
Leonid Lapshin <v-llapshin@microsoft.com>
2020-10-12 17:44:31 +0300

    Move linux provision scripts to virtual-environments (#1714)
    * add linux-related scripts from MMS provisioner
    
    * removed EOL
    
    * removed Ubuntu condition
    
    * moved PATH checking to cleanup section
    
    * add clarification for cgroups names
    
    * names alignment
    
    * removed memory-configuration and agent specific scripts
    
    * renamed folder to psot-generation, added template instructions
    
    * moved key import to git installation
    
    * moved PATH check to post-deployment instead of cleanup script
    
    * add scripts
    
    * output tests file
    
    * add startup logic
    
    * add powershell module for furute helpers
    
    * removed unused modules
    
    * copy to tmp folder because of permissions, copy to opt on post-generation step
    
    * removed Pester test
    
    * change post-generation location
    
    * incorrect if statement
    
    Co-authored-by: Leonid Lapshin <originalnoe-nazvanie@yandex.ru>

--- images/linux/scripts/installers/post-deployment.sh
+++ images/linux/scripts/installers/post-deployment.sh
@@ -4,6 +4,8 @@
 ##  Desc: Post deployment actions
 ################################################################################
 
+mv -f /imagegeneration/post-generation /opt
+
 # set chmod -R 777 /opt
 if [[ -d "/opt" ]]; then
     echo "chmod -R 777 /opt"
@@ -14,3 +16,11 @@ fi
 rm -rf $HELPER_SCRIPT_FOLDER
 rm -rf $INSTALLER_SCRIPT_FOLDER
 chmod 755 $IMAGE_FOLDER
+
+# Check PATH
+if [[ $PATH == \"*\" ]]
+then
+    echo "ERROR: PATH contains quotes"
+    echo "PATH = $PATH"
+    exit 1
+fi
