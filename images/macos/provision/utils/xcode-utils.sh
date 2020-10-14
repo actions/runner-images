0aa67caa326b8b17d571b2d0bf8e99f6781df65c
Maxim Lobanov <v-malob@microsoft.com>
2020-10-14 07:31:00 +0300

    Update xcode-utils.sh

--- images/macos/provision/utils/xcode-utils.sh
+++ images/macos/provision/utils/xcode-utils.sh
@@ -2,6 +2,11 @@ createXamarinProvisionatorSymlink() {
     local XCODE_VERSION="$1"
     local FULL_VERSION=$(echo "${XCODE_VERSION}.0.0" | cut -d'.' -f 1,2,3)
 
+    # temporary trick for 12.0.1
+    if [[ $XCODE_VERSION == "12" ]]; then
+        FULL_VERSION="12.0.1"
+    fi
+
     if [ $FULL_VERSION != $XCODE_VERSION ]; then
         ln -sf "/Applications/Xcode_${XCODE_VERSION}.app" "/Applications/Xcode_${FULL_VERSION}.app"
     fi
@@ -10,10 +15,10 @@ createXamarinProvisionatorSymlink() {
 getXcodeVersionToInstall() {
     local XCODE_VERSION="$1"
 
-    if  [[ ! $XCODE_VERSION =~ "_beta" ]]; then
-        echo "${XCODE_VERSION//_/ }"
-    elif [[ $XCODE_VERSION == "12" ]]; then
+    if [[ $XCODE_VERSION == "12" ]]; then
         echo "12.0.1"
+    elif [[ ! $XCODE_VERSION =~ "_beta" ]]; then
+        echo "${XCODE_VERSION//_/ }"
     else
         local XCODE_BETA="${XCODE_VERSION/_/ }"
         echo "$(xcversion list | sort -r | grep -m 1 "$XCODE_BETA")"
