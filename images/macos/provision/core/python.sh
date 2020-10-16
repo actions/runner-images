8b2de721f6193988503e0fecbbb615a884f9e3b5
Mikhail Timofeev <v-mitim@microsoft.com>
2020-10-16 18:17:02 +0300

    remove brew installation, add pip

--- images/macos/provision/core/python.sh
+++ images/macos/provision/core/python.sh
@@ -11,8 +11,8 @@ echo "Brew Installing Python 3"
 /usr/local/bin/brew unlink python@3.8
 /usr/local/bin/brew link python@3.8
 
-echo "Brew Installing Python 2"
-# Create local tap with formula due to python2 formula depreciation
-/usr/local/bin/brew tap-new local/python2
-FORMULA_PATH=$(/usr/local/bin/brew extract python@2 local/python2 | grep "Homebrew/Library/Taps")
-/usr/local/bin/brew install $FORMULA_PATH
+echo "Install pip for Python 2"
+# Set permissions to install pip modules in the main packages directory
+sudo chmod -R 777 /Library/Python/2.7
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
