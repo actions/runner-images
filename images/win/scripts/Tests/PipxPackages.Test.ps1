723e4ccb877619a062dd718065122450708c33d1
Nikita Bykov <v-nibyko@microsoft.com>
2020-10-16 12:36:59 +0300

    fixed naming

--- images/win/scripts/Tests/PipxPackages.Test.ps1
+++
@@ -1,7 +0,0 @@
-Describe "PipxPackages" {
-    $pipxToolset = (Get-ToolsetContent).pipx
-    $testCases = $pipxToolset | ForEach-Object { @{package = $_.package; cmd = $_.cmd} }
-    It "<package>" -TestCases $testCases {
-        "$cmd" | Should -ReturnZeroExitCode
-    }
-}
