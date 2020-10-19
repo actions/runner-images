7352e0f14440b5dd74340cefd452f4c8ac147770
Andrey Mishechkin (GITHUB INC) <v-andmis@microsoft.com>
2020-10-19 19:35:21 +0400

    ln -sf $(/usr/local/bin/brew --prefix openssl) /usr/local/opt/openssl has been added

--- images/macos/provision/core/openssl.sh
+++ images/macos/provision/core/openssl.sh
@@ -4,3 +4,4 @@ echo Installing OpenSSL...
 /usr/local/bin/brew install openssl
 
 ln -sf $(/usr/local/bin/brew --prefix openssl)/bin/openssl /usr/local/bin/openssl
+ln -sf $(/usr/local/bin/brew --prefix openssl) /usr/local/opt/openssl
