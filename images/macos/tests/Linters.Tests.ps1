00bb4818cec8f8676369d59f0eb33f7802ca37f2
Nikita Bykov <v-nibyko@microsoft.com>
2020-10-21 15:50:53 +0300

    fixed pipx packages tests

--- images/macos/tests/Linters.Tests.ps1
+++ images/macos/tests/Linters.Tests.ps1
@@ -3,10 +3,6 @@ Import-Module "$PSScriptRoot/../helpers/Tests.Helpers.psm1"
 $os = Get-OSVersion
 
 Describe "Linters" {
-    It "yamllint" {
-        "yamllint --version" | Should -ReturnZeroExitCode
-    }
-
     It "SwiftLint" -Skip:($os.IsHighSierra) {
         "swiftlint version" | Should -ReturnZeroExitCode
     }
