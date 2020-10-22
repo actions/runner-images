b15b529a63ab0eb9c75f599ee0c0020cbdbebd08
Mikhail Timofeev <v-mitim@microsoft.com>
2020-10-22 19:28:30 +0300

    add workaround for hs

--- images/macos/provision/core/python.sh
+++ images/macos/provision/core/python.sh
@@ -14,7 +14,16 @@ echo "Brew Installing Python 3"
 echo "Install pip for Python 2"
 # Set permissions to install pip modules in the main packages directory
 sudo chmod -R 777 /Library/Python/2.7
+sudo chmod -R 777 /System/Library/Frameworks/Python.framework/Versions/2.7
 # Create symlink for backwards compatibility
 ln -s /usr/bin/python /usr/local/bin/python
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
+# Workaround for High Sierra pip, since it installs into /usr/bin. On the modern macOSs pip installation directory is /usr/local/bin
+if ! is_HighSierra; then
+    python get-pip.py
+else
+    sudo chmod 777 /usr/bin
+    python get-pip.py
+    ln -s /usr/bin/pip /usr/local/bin/pip
+    sudo chmod 755 /usr/bin
+fi
