565113e55cdfdb42a9dfc851956f018ad5c43dd8
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-10-26 23:25:48 +0300

    fix user on rust and brew scripts

--- images/linux/post-generation/homebrew-permissions.sh
+++ images/linux/post-generation/homebrew-permissions.sh
@@ -5,7 +5,7 @@
 brew_folder="/home/linuxbrew/"
 if [ -d "$brew_folder" ]; then
     brew_folder_owner=$(ls -ld $brew_folder | awk '{print $3}')
-    if [ "$USER" != "$brew_folder_owner" ]; then
-        chown "$USER":docker -R $brew_folder
+    if [ "$SUDO_USER" != "$brew_folder_owner" ]; then
+        chown "$SUDO_USER":docker -R $brew_folder
     fi
 fi
--- images/linux/post-generation/rust-permissions.sh
+++ images/linux/post-generation/rust-permissions.sh
@@ -5,7 +5,7 @@
 rust_folder="/usr/share/rust"
 if [ -d "$rust_folder" ]; then
     rust_folder_owner=$(ls -ld $rust_folder | awk '{print $3}')
-    if [ "$USER" != "$rust_folder_owner" ]; then
-        chown "$USER":docker -R $rust_folder
+    if [ "$SUDO_USER" != "$rust_folder_owner" ]; then
+        chown "$SUDO_USER":docker -R $rust_folder
     fi
 fi
