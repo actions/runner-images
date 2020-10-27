0b0696cf828a0da2ebf9346614154f0cc971d02e
Leonid Lapshin <originalnoe-nazvanie@yandex.ru>
2020-10-27 14:26:25 +0300

    set user on a factory stage

--- images/linux/post-generation/homebrew-permissions.sh
+++ images/linux/post-generation/homebrew-permissions.sh
@@ -2,10 +2,18 @@
 
 # Fix permissions for Homebrew
 # https://github.com/actions/virtual-environments/issues/1568
+
 brew_folder="/home/linuxbrew/"
+
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    homebrew_user="runner"
+else
+    homebrew_user="vsts"
+fi
+
 if [ -d "$brew_folder" ]; then
     brew_folder_owner=$(ls -ld $brew_folder | awk '{print $3}')
-    if [ "$SUDO_USER" != "$brew_folder_owner" ]; then
-        chown "$SUDO_USER":docker -R $brew_folder
+    if [ "$homebrew_user" != "$brew_folder_owner" ]; then
+        chown "$homebrew_user":docker -R $brew_folder
     fi
 fi
--- images/linux/post-generation/rust-permissions.sh
+++ images/linux/post-generation/rust-permissions.sh
@@ -2,10 +2,18 @@
 
 # Fix permissions for the Rust folder
 # https://github.com/actions/virtual-environments/issues/572
+
 rust_folder="/usr/share/rust"
+
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    rust_user="runner"
+else
+    rust_user="vsts"
+fi
+
 if [ -d "$rust_folder" ]; then
     rust_folder_owner=$(ls -ld $rust_folder | awk '{print $3}')
-    if [ "$SUDO_USER" != "$rust_folder_owner" ]; then
-        chown "$SUDO_USER":docker -R $rust_folder
+    if [ "$rust_user" != "$rust_folder_owner" ]; then
+        chown "$rust_user":docker -R $rust_folder
     fi
 fi
