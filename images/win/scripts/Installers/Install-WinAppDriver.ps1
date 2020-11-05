f07a74c6941110a022a2a4e5621fa2c1f60a70bc
Mikhail Timofeev <v-mitim@microsoft.com>
2020-11-06 02:34:47 +0300

    update driver

--- images/win/scripts/Installers/Install-WinAppDriver.ps1
+++ images/win/scripts/Installers/Install-WinAppDriver.ps1
@@ -3,8 +3,8 @@
 ##  Desc:  Install Windows Application Driver (WinAppDriver)
 ####################################################################################
 
-$InstallerName = "WindowsApplicationDriver.msi"
-$InstallerUrl = "https://github.com/Microsoft/WinAppDriver/releases/download/v1.1/${InstallerName}"
+$InstallerName = "WindowsApplicationDriver_1.2.1.msi"
+$InstallerUrl = "https://github.com/Microsoft/WinAppDriver/releases/download/v1.2.1/${InstallerName}"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-Binary -Url $InstallerUrl -Name $InstallerName
