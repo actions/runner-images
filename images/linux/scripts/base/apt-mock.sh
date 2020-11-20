15e6536821015283ba18f6015e243415433d39d7
Sergey Dolin <v-sedoli@microsoft.com>
2020-11-20 09:21:29 +0500

    final refactoring

--- images/linux/scripts/base/apt-mock.sh
+++ images/linux/scripts/base/apt-mock.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-set -x
 # A temporary workaround for https://github.com/Azure/azure-linux-extensions/issues/1238
 
 prefix=/usr/local/bin
@@ -17,38 +16,34 @@ while [ \$i -le 30 ];do
   $real_tool "\$@" 2>\$err
   result=\$?
   cat \$err >&2
-  if [ \$result -eq  0 ];then
-    # no errors, continue
-    rm \$err
+
+  # no errors, continue
+  test \$result -eq  0 && break
+
+  retry=false
+
+  if grep -q 'Could not get lock' \$err;then
+    # apt db locked needs retry
+    retry=true
+  elif grep -q 'Could not open file /var/lib/apt/lists' \$err;then
+    # apt update is not completed, needs retry
+    retry=true
+  elif grep -q 'IPC connect call failed' \$err;then
+    ## TODO:
+    ## the delay should help with gpg-agent not ready
+    ## if it is not then uncomment the folloing lines in order to
+    ## force restart gpg-agent
+    #  pkill -9 gpg-agent
+    #  sleep 1
+    #  source <(gpg-agent --daemon)
+    retry=true
+  fi
+
+  rm \$err
+  if [ \$retry = false ]; then
     break
   fi
-  # some error exists, check for apt db locked
-  grep -q 'Could not get lock' \$err
-  result=\$?
-  if [ \$result -ne  0 ];then
-    # some error exists, check for apt update is not complete
-    grep -q 'Could not open file /var/lib/apt/lists' \$err
-    result=\$?
-    if [ \$result -ne  0 ];then
-      # some error exists, check for gpg-agent is busy or not run yet
-      grep -q 'IPC connect call failed' \$err
-      result=\$?
-      if [ \$result -ne  0 ];then
-        # some unhandled error, continue
-        rm \$err
-        break
-      # # TODO:
-      # # the delay should help with gpg-agent not ready
-      # # if it is not uncomment the folloing lines in order to
-      # # force restart gpg-agent
-      # else
-      #  pkill -9 gpg-agent
-      #  sleep 1
-      #  source <(gpg-agent --daemon)
-      fi
-    fi
-  fi
-  rm \$err
+
   sleep 5
   echo "...retry \$i"
   i=\$((i + 1))
@@ -56,4 +51,3 @@ done
 EOT
   chmod +x $prefix/$tool
 done
-
