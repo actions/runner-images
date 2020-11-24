3a7123ebeb09c231e0394358b5b52314618611b1
Elvis Shi <45674121+Elvis-Shi@users.noreply.github.com>
2020-11-24 08:20:02 -0800

    Add BizTalk Server project build component to build agent machine. (#1960)
    * Add BizTalk Project Build components, including msi and vsix.
    
    * Add BizTalk Build Components to software report.
    
    * Use registry/folder check replace actually BizTalk Project building for testing purpose.
    
    * Remove unnecessary try/catch.
    
    * Make sure BizTalk related software report only for Windows 2019 OS.
    
    * Add statement to make sure BizTalk test only invoked in Win19 env.
    
    * Fix test failue when no test runs use "skip"
    
    * Update the TestName to be identity to BizTalk.Test.ps1

--- images/win/scripts/SoftwareReport/SoftwareReport.Generator.ps1
+++ images/win/scripts/SoftwareReport/SoftwareReport.Generator.ps1
@@ -139,6 +139,15 @@ $markdown += New-MDList -Style Unordered -Lines @(
 $markdown += New-MDHeader "MSYS2" -Level 3
 $markdown += Get-PacmanVersion
 $markdown += New-MDNewLine
+
+if(Test-IsWin19)
+{
+    $markdown += New-MDHeader "BizTalk Server" -Level 3
+    $markdown += Get-BizTalkVersion
+    $markdown += New-MDNewLine
+}
+
+$markdown += New-MDHeader "Notes:" -Level 5
 $markdown += @'
 ```
 Location: C:\msys64
