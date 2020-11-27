57ca82ba08bb6073a1541cb4b592ea7f83d70fb0
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2020-11-27 11:20:58 +0100

    [Ubuntu] cmake.sh: Extract URL directly from API output
    Co-authored by @al-cheb, thanks!

--- images/linux/scripts/installers/cmake.sh
+++ images/linux/scripts/installers/cmake.sh
@@ -9,8 +9,8 @@ echo "Checking to see if the installer script has already been run"
 if command -v cmake; then
     echo "cmake is already installed"
 else
-	VERSION=$(curl -s https://api.github.com/repos/Kitware/CMake/releases/latest | jq -r '.tag_name | ltrimstr("v")')
-	curl -sL https://github.com/Kitware/CMake/releases/download/v${VERSION}/cmake-${VERSION}-Linux-x86_64.sh -o cmakeinstall.sh \
+	url=$(curl -s https://api.github.com/repos/Kitware/CMake/releases/latest | jq -r '.assets[].browser_download_url | select(contains("Linux-x86_64.sh"))')
+	curl -sL ${url} -o cmakeinstall.sh \
 	&& chmod +x cmakeinstall.sh \
 	&& ./cmakeinstall.sh --prefix=/usr/local --exclude-subdir \
 	&& rm cmakeinstall.sh
