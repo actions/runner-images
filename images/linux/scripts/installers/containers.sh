ebe4df0322efc188b788d2378c911d7f1f12a6ea
Darii Nurgaleev <Darleev@github.com>
2020-12-02 13:11:29 +0700

    create array for tools.

--- images/linux/scripts/installers/containers.sh
+++ images/linux/scripts/installers/containers.sh
@@ -5,19 +5,19 @@
 ################################################################################
 
 # Install podman, buildah, scopeo container's tools
+install_packages=(podman buildah skopeo)
 source /etc/os-release
 sh -c "echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
 wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
 apt-key add - < Release.key
 apt-get update -qq
-apt-get -qq -y install podman buildah skopeo
+apt-get -qq -y install ${install_packages[@]}
 mkdir -p /etc/containers
 echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | tee /etc/containers/registries.conf
 
 # Check that tools are installed properly
-cmd_packages=(podman buildah skopeo)
 echo "Testing to make sure that script performed as expected, and basic scenarios work" 
-for cmd in ${cmd_packages[@]}; do 
+for cmd in ${install_packages[@]}; do 
     if ! command -v $cmd; then 
         echo "$cmd was not installed" 
         exit 1 
