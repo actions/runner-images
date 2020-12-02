87a1b4ab9dffc04726f86ec1c36f1ea7ca73cc8a
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2020-12-02 15:20:34 +0300

    resolve issue

--- images/linux/scripts/installers/oc.sh
+++ images/linux/scripts/installers/oc.sh
@@ -4,6 +4,8 @@
 ##  Desc:  Installs the OC CLI
 ################################################################################
 
+source $HELPER_SCRIPTS/install.sh
+
 # Install the oc CLI
 DOWNLOAD_URL="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz"
 PACKAGE_TAR_NAME="oc.tar.gz"
