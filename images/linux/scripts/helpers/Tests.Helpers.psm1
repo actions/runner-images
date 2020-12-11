a52a6f3666024892d9b7d24a57e420c68c361039
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2020-12-11 10:39:36 +0300

    fix issues

--- images/linux/scripts/helpers/Tests.Helpers.psm1
+++ images/linux/scripts/helpers/Tests.Helpers.psm1
@@ -1,5 +1,5 @@
 # Invokes command and validate that the exit code is 0
-Import-Module (Join-Path $PSScriptRoot "SoftwareReport.Helpers.psm1") -DisableNameChecking
+Import-Module "$PSScriptRoot/../SoftwareReport/SoftwareReport.Helpers.psm1" -DisableNameChecking
 
 function Validate-ZeroExitCode($command) {
     $result = Get-CommandResult $command
