79cd84037af7cb07afd9eef66be0aaea07dfcb1f
Darii Nurgaleev <Darleev@github.com>
2020-12-15 21:03:35 +0700

    invoke-tests change

--- images/macos/provision/utils/invoke-tests.sh
+++ images/macos/provision/utils/invoke-tests.sh
@@ -2,6 +2,6 @@ invoke_tests() {
     local TEST_FILE="$1"
     local TEST_NAME="$2"
 
-    pwsh -Command "Import-Module '$HOME/image-generation/tests/Tests.Helpers.psm1' -DisableNameChecking
+    pwsh -Command "Import-Module '$HOME/image-generation/helpers/Tests.Helpers.psm1' -DisableNameChecking
         Invoke-PesterTests -TestFile \"$TEST_FILE\" -TestName \"$TEST_NAME\""
 } 
