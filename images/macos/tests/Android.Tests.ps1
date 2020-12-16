3d6fce2aaf1a4fc9464ed0c6e37d770dae26be75
Mikhail Timofeev <v-mitim@microsoft.com>
2020-12-16 09:52:52 +0300

    change test to ndk 22

--- images/macos/tests/Android.Tests.ps1
+++ images/macos/tests/Android.Tests.ps1
@@ -68,13 +68,13 @@ Describe "Android" {
         }
     }
 
-    Context "Legacy NDK versions" -Skip:($os.IsBigSur) {
-        It "Android NDK version is 21" {
-            $ndkBundlePath = Join-Path $ANDROID_SDK_DIR "ndk-bundle" "source.properties"
-            $rawContent = Get-Content $ndkBundlePath -Raw
-            $rawContent | Should -BeLikeExactly "*Revision = 21.*"
-        }
+    It "Android NDK version is 22" {
+        $ndkBundlePath = Join-Path $ANDROID_SDK_DIR "ndk-bundle" "source.properties"
+        $rawContent = Get-Content $ndkBundlePath -Raw
+        $rawContent | Should -BeLikeExactly "*Revision = 22.*"
+    }
 
+    Context "Legacy NDK versions" -Skip:($os.IsBigSur) {
         It "Android NDK version r18b is installed" {
             $ndk18BundlePath = Join-Path $ANDROID_SDK_DIR "ndk" "18.1.5063045" "source.properties"
             $rawContent = Get-Content $ndk18BundlePath -Raw
