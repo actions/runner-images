fe911e4eb38cfb8aaf091379a36d843eb54bc8f5
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2020-12-17 20:39:44 +0300

    small fix

--- images/linux/scripts/installers/vercel.sh
+++ images/linux/scripts/installers/vercel.sh
@@ -9,11 +9,6 @@ source $HELPER_SCRIPTS/invoke-tests.sh
 # Install the Vercel CLI
 npm i -g vercel
 
-# Run tests to determine that the software installed as expected
-echo "Testing to make sure that script performed as expected, and basic scenarios work"
-invoke_tests "CLI.Tools" "Vercel CLI"
-
-
 echo "Creating the symlink for [now] command to vercel CLI"
 ln -s /usr/local/bin/vercel /usr/local/bin/now
 
