2b93b033775c49e5bf41fefb4b627352cd0cc0f8
Dibir Magomedsaygitov <61747324+dibir-magomedsaygitov@users.noreply.github.com>
2020-12-17 09:52:09 +0300

    [Ubuntu] Implement first Pester tests (#2270)
    * implement first pester tests
    
    * add comment for azcopy test
    
    * remove extra importing and old function
    
    * resolve comments
    
    * fix typo

---
+++ images/linux/scripts/helpers/SoftwareReport.Helpers.psm1
@@ -0,0 +1,23 @@
+function Take-OutputPart {
+    param (
+        [Parameter(ValueFromPipeline)]
+        [string] $toolOutput,
+        [string] $Delimiter = " ",
+        [int[]] $Part
+    )
+    $parts = $toolOutput.Split($Delimiter, [System.StringSplitOptions]::RemoveEmptyEntries)
+    $selectedParts = $parts[$Part]
+    return [string]::Join($Delimiter, $selectedParts)
+}
+
+function New-MDNewLine {
+    param (
+        [int] $Count = 1
+    )
+    $newLineSymbol = [System.Environment]::NewLine
+    return $newLineSymbol * $Count
+}
+
+function Restore-UserOwner {
+    sudo chown -R ${env:USER}: $env:HOME
+}
