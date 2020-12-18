8d831ac1713848b5261233a21908387b11be49d7
Mikhail Timofeev <v-mitim@microsoft.com>
2020-12-18 12:52:31 +0300

    add utils.sh

--- images/macos/provision/core/swiftlint.sh
+++ images/macos/provision/core/swiftlint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e -o pipefail
 
+source ~/utils/utils.sh
+
 echo "Install SwiftLint"
 swiftlintUrl=$(curl -s "https://api.github.com/repos/realm/SwiftLint/releases/latest" | jq -r '.assets[].browser_download_url | select(contains("SwiftLint.pkg"))')
 download_with_retries $swiftlintUrl "/tmp" "SwiftLint.pkg"
