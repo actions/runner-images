bae28e51f78325c63ef46b257900d13fe55b0bcb
Vladimir Safonkin <vsafonkin@github.com>
2020-12-22 11:33:45 +0300

    Add tests for all installed ghc versions

--- images/linux/scripts/tests/Common.Tests.ps1
+++ images/linux/scripts/tests/Common.Tests.ps1
@@ -1,5 +1,21 @@
 Describe "Haskell" {
-    It "GHC" {
+
+    $GHCCommonPath = "/opt/ghc"
+    $GHCVersions = Get-ChildItem -Path $GHCCommonPath | Where-Object { $_.Name -match "\d+\.\d+" }
+    Write-Host $GHCVersions
+    
+    $testCases = $GHCVersions | ForEach-Object { @{ GHCPath = "${_}/bin/ghc"} }
+    
+    It "Number of Installed GHC versions" {
+        $GHCVersions.Count | Should -Be 3
+    }
+
+    It "GHC version <GHCPath>" -TestCases $testCases {
+            param ([string] $GHCPath)
+            "$GHCPath --version" | Should -ReturnZeroExitCode
+    }
+
+    It "Default GHC" {
         "ghc --version" | Should -ReturnZeroExitCode
     }
 
