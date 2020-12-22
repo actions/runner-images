5aa3bce108bb1e1ec8d8ef52f23a8411d4b499c7
Vladimir Safonkin <vsafonkin@github.com>
2020-12-22 12:57:42 +0300

    Minor fix

--- images/linux/scripts/tests/Android.Tests.ps1
+++ images/linux/scripts/tests/Android.Tests.ps1
@@ -39,7 +39,7 @@ Describe "Android" {
     }
 
 
-    It "Packages" {
+    Context "Packages" {
         $testCases = $androidPackages | ForEach-Object { @{ PackageName = $_ } }
 
         It "<PackageName>" -TestCases $testCases {
