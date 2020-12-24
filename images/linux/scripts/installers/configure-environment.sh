8fc11a79a7b7f68bd3ff0247e157439372e79e3a
Vladimir Safonkin <vsafonkin@github.com>
2020-12-24 22:02:16 +0300

    Fix 1604 and 2004 templates

--- images/linux/scripts/installers/configure-environment.sh
+++ images/linux/scripts/installers/configure-environment.sh
@@ -26,5 +26,6 @@ chmod -R 777 $AGENT_TOOLSDIRECTORY
 # https://www.suse.com/support/kb/doc/?id=000016692
 echo 'vm.max_map_count=262144' | tee -a /etc/sysctl.conf
 
+# Create symlink for tests running
 chmod -R 777 $HELPER_SCRIPTS/invoke-tests.sh
 ln -s $HELPER_SCRIPTS/invoke-tests.sh /usr/local/bin
