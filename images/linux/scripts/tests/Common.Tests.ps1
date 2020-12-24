e6d53b0cbb94bbcad2f8c027e719a29585006fc7
Vladimir Safonkin <vsafonkin@github.com>
2020-12-24 18:36:33 +0300

    Remove Haskell tests from commons tests

--- images/linux/scripts/tests/Common.Tests.ps1
+++ images/linux/scripts/tests/Common.Tests.ps1
@@ -15,35 +15,3 @@ Describe "Swift" {
         "swiftc --version" | Should -ReturnZeroExitCode
     }
 }
-
-Describe "Haskell" {
-
-    $GHCCommonPath = "/opt/ghc"
-    $GHCVersions = Get-ChildItem -Path $GHCCommonPath | Where-Object { $_.Name -match "\d+\.\d+" }
-    
-    $testCase = @{ GHCVersions = $GHCVersions }
-
-    It "GHC directory contains three version of GHC" -TestCases $testCase {
-        param ([object] $GHCVersions)
-        $GHCVersions.Count | Should -Be 3
-    }
-
-    $testCases = $GHCVersions | ForEach-Object { @{ GHCPath = "${_}/bin/ghc"} }
-
-    It "GHC version <GHCPath>" -TestCases $testCases {
-            param ([string] $GHCPath)
-            "$GHCPath --version" | Should -ReturnZeroExitCode
-    }
-
-    It "Default GHC" {
-        "ghc --version" | Should -ReturnZeroExitCode
-    }
-
-    It "Cabal" {
-        "cabal --version" | Should -ReturnZeroExitCode
-    }
-
-    It "Stack" {
-        "stack --version" | Should -ReturnZeroExitCode
-    }
-}
