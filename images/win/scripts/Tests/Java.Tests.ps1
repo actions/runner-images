a73cf26c0564f8a08feee73f3b75cb36047966ab
Nikita Bykov <v-nibyko@microsoft.com>
2021-01-08 18:27:26 +0300

    fixed tests

--- images/win/scripts/Tests/Java.Tests.ps1
+++ images/win/scripts/Tests/Java.Tests.ps1
@@ -2,9 +2,9 @@ Describe "Java" {
     $jdkVersions = (Get-ToolsetContent).java.versions
     $defaultVersion = (Get-ToolsetContent).java.default
 
-    It "Java ${defaultVersion} is default" {
+    It "Java <DefaultJavaVersion> is default" -TestCases @(@{ DefaultJavaVersion = $defaultVersion }) {
         $actualJavaPath = Get-EnvironmentVariable "JAVA_HOME"
-        $expectedJavaPath = Get-EnvironmentVariable "JAVA_HOME_${defaultVersion}_X64"
+        $expectedJavaPath = Get-EnvironmentVariable "JAVA_HOME_${DefaultJavaVersion}_X64"
 
         $actualJavaPath | Should -Not -BeNullOrEmpty
         $expectedJavaPath | Should -Not -BeNullOrEmpty
@@ -21,14 +21,16 @@ Describe "Java" {
     }
 
     foreach ($version in $jdkVersions) {
-        It "Java ${version}" {
-            $javaVariableValue = Get-EnvironmentVariable "JAVA_HOME_${version}_X64"
+        $jdk = @{ jdkVersion = $version }
+
+        It "Java ${version}" -TestCases $jdk {
+            $javaVariableValue = Get-EnvironmentVariable "JAVA_HOME_${jdkVersion}_X64"
             $javaVariableValue | Should -Not -BeNullOrEmpty
             $javaPath = Join-Path $javaVariableValue "bin\java"
-    
+
             $result = Get-CommandResult "`"$javaPath`" -version"
             $result.ExitCode | Should -Be 0
-            $result.Output[0] | Should -Match ([regex]::Escape("openjdk version `"${Version}."))
+            $result.Output[0] | Should -Match ([regex]::Escape("openjdk version `"${jdkVersion}."))
         }
     }
 }
