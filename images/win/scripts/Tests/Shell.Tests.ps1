08f1ffd79cda5a00af7573d352a2bce10733c9ff
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-01-12 15:51:47 +0300

    fix tests

--- images/win/scripts/Tests/Shell.Tests.ps1
+++ images/win/scripts/Tests/Shell.Tests.ps1
@@ -1,21 +1,13 @@
 Describe "Shell" {
-    $IsWin16 = Test-IsWin16
-
     $shellTestCases = @(
-        @{Name = "C:\shells\gitbash.exe"; Target = "$env:ProgramFiles\Git\bin\bash.exe"}
+        @{Name = "C:\shells\gitbash.exe"; Target = "$env:ProgramFiles\Git\bin\bash.exe"},
+        @{Name = "C:\shells\msys2bash.cmd"; Target = $null}
     )
+    if (Test-IsWin19) {
+        $shellTestCases += @{Name = "C:\shells\wslbash.exe"; Target = "$env:SystemRoot\System32\bash.exe"}
+    }
 
     It "<Name> target to <Target>" -TestCases $shellTestCases {
-        (Get-Item $Name).Target | Should -BeExactly $Target
-    }
-
-    It "C:\shells\msys2bash.cmd exists" {
-        "C:\shells\msys2bash.cmd" | Should -Exist
-    }
-
-    It "C:\shells\wslbash.exe target to $env:SystemRoot\System32\bash.exe" -Skip:$IsWin16 {
-        $Name = "C:\shells\wslbash.exe"
-        $Target = "$env:SystemRoot\System32\bash.exe"
         (Get-Item $Name).Target | Should -BeExactly $Target
     }
 }
