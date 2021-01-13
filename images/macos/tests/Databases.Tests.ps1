0a255311b1880d4158ec2d70d2359fb1052378d8
Darii Nurgaleev <50947177+Darleev@users.noreply.github.com>
2021-01-14 00:46:44 +0700

    [macOS] Pester tests invocation after install. (#2421)
    * [macOS] Fail Pester tests on error
    
    * return it back
    
    * [macOS] Pester tests invokation after install
    
    * fix nitpicks
    
    * CocoaPods fix

---
+++ images/macos/tests/Databases.Tests.ps1
@@ -0,0 +1,16 @@
+Describe "Mongo" {
+    It "mongodb" {
+        "mongo --version" | Should -ReturnZeroExitCode
+        "mongod --version"| Should -ReturnZeroExitCode
+    }
+}
+
+Describe "PostgreSQL" {
+    It "PostgreSQL-Client" {
+        "psql --version" | Should -ReturnZeroExitCode
+    }
+
+    It "PostgreSQL-Server" {
+        "pg_config --version" | Should -ReturnZeroExitCode
+    }
+}
