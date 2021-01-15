7c6117e6ec7c92e1717864296290fbf4a5c3e8f0
Darii Nurgaleev <50947177+Darleev@users.noreply.github.com>
2021-01-15 21:12:26 +0700

    [Ubuntu] Get toolset value function. (#2435)
    * [macOS] Fail Pester tests on error
    
    * return it back
    
    * [Ubuntu] add get_toolset_values function
    
    * added fix
    
    * fix all nitpicks
    
    * try to get rid brackets
    
    * removed brackets.
    
    * pipx-package fix

--- images/linux/scripts/installers/php.sh
+++ images/linux/scripts/installers/php.sh
@@ -7,15 +7,14 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/etc-environment.sh
 source $HELPER_SCRIPTS/os.sh
+source $HELPER_SCRIPTS/install.sh
 
 # add repository
 apt-add-repository ppa:ondrej/php -y
 apt-get update
 
 # Install PHP
-toolset="$INSTALLER_SCRIPT_FOLDER/toolset.json"
-
-php_versions=$(jq -r '.php.versions[]' $toolset)
+php_versions=$(get_toolset_value '.php.versions[]')
 
 for version in $php_versions; do
     echo "Installing PHP $version"
