47ec1558df0be7a0dc1f3029e362d27c253df6a9
Andy Mishechkin <v-andmis@microsoft.com>
2021-01-17 15:22:25 +0400

    Chromium has been added to software report

--- images/linux/scripts/SoftwareReport/SoftwareReport.Generator.ps1
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Generator.ps1
@@ -193,12 +193,16 @@ $markdown += New-MDList -Style Unordered -Lines @(
 )
 
 $markdown += New-MDHeader "Browsers and Drivers" -Level 3
-$markdown += New-MDList -Style Unordered -Lines @(
+$browsersAndDriversList = @(
     (Get-ChromeVersion),
     (Get-ChromeDriverVersion),
     (Get-FirefoxVersion),
     (Get-GeckodriverVersion)
 )
+if (Test-IsUbuntu20) {
+    $browsersAndDriversList += @(Get-ChromiumVersion)
+}
+$markdown += New-MDList -Style Unordered -Lines ($browsersAndDriversList | Sort-Object)
 
 $markdown += New-MDHeader ".NET Core SDK" -Level 3
 $markdown += New-MDList -Style Unordered -Lines @(
