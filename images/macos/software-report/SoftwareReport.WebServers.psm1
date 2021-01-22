67d70ee818082f757f286fabcee6fe3379cefe9e
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-01-22 12:21:59 +0300

    add apache and nginx

---
+++ images/macos/software-report/SoftwareReport.WebServers.psm1
@@ -0,0 +1,52 @@
+function Get-ApacheVersion {
+    $name = "httpd"
+    $port = 80
+    $version = brew list $name --versions | Take-Part -Part 1
+    $serviceStatus = (brew services list) -match $name | Take-Part -Part 1
+    $configFile = "$(brew --prefix)/etc/httpd/httpd.conf"
+    return [PsCustomObject]@{
+        "Name" = $name
+        "Version" = $version
+        "ServiceStatus" = $serviceStatus
+        "ConfigFile" = $configFile
+        "ListenPort" = $port
+    }
+}
+
+function Get-NginxVersion {
+    $name = "nginx"
+    $port = 80
+    $version = brew list $name --versions | Take-Part -Part 1
+    $serviceStatus = (brew services list) -match $name | Take-Part -Part 1
+    $configFile = "$(brew --prefix)/etc/nginx/nginx.conf"
+    return [PsCustomObject]@{
+        "Name" = $name
+        "Version" = $version
+        "ServiceStatus" = $serviceStatus
+        "ConfigFile" = $configFile
+        "ListenPort" = $port
+    }
+}
+function Build-WebServersSection {
+    $output = ""
+    $output += New-MDHeader "Web Servers" -Level 3
+    $output += @(
+        (Get-ApacheVersion),
+        (Get-NginxVersion)
+    ) | Sort-Object Name | New-MDTable
+    
+    $output += New-MDHeader "Notes:" -Level 5
+    $output += @'
+```
+    1. Start the service formula immediately
+    brew services start $service
+    2. List all managed services
+    brew services list | grep $service
+    3. Port 80 availability check
+    sudo lsof -i :80 | grep LISTEN
+    4. Stop the service formula immediately
+    brew services stop "$service"
+```
+'@
+    return $output
+}
