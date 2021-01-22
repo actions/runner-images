8d4c5e060cb01b0cb2f5797c02f987acb8739b16
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-01-22 09:31:09 +0300

    add link to the issue

--- images/linux/scripts/installers/docker-compose.sh
+++ images/linux/scripts/installers/docker-compose.sh
@@ -8,6 +8,7 @@ source $HELPER_SCRIPTS/os.sh
 # Install latest docker-compose from releases
 URL=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.assets[].browser_download_url | select(contains("docker-compose-Linux-x86_64"))' | head -1)
 if isUbuntu16 || isUbuntu18 ; then
+    # https://github.com/docker/compose/issues/8048
     URL="https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Linux-x86_64"
 fi
 curl -L $URL -o /usr/local/bin/docker-compose
