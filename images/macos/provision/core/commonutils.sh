782cc017b594dd6d2f7c1cfcb08ac32579468132
MaksimZhukov <v-mazhuk@microsoft.com>
2021-01-25 16:05:41 +0300

    Export the USE_BAZEL_VERSION variable to the bashrc

--- images/macos/provision/core/commonutils.sh
+++ images/macos/provision/core/commonutils.sh
@@ -16,6 +16,7 @@ done
 # Specify Bazel version 3.7.1 to install due to the bug with 4.0.0: https://github.com/bazelbuild/bazel/pull/12882
 if is_Less_Catalina; then
     export USE_BAZEL_VERSION="3.7.1"
+    echo "export USE_BAZEL_VERSION=${USE_BAZEL_VERSION}" >> "${HOME}/.bashrc"
 fi
 
 # Invoke bazel to download bazel version via bazelisk
