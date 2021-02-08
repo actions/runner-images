c49bbc7081c0656aa1a0aa61f73b464ee6408ead
Dmitry Shibanov <dmitry-shibanov@github.com>
2021-02-08 12:03:31 +0300

    remove aliases and old node versions.

--- images/macos/provision/core/nvm.sh
+++ images/macos/provision/core/nvm.sh
@@ -11,18 +11,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$VERSION/install.sh | bash
 if [ $? -eq 0 ]; then
         . ~/.bashrc
         nvm --version
-        nvm install lts/boron # 6.x
-        nvm install lts/carbon # 8.x
         nvm install lts/dubnium # 10.x
         nvm install lts/erbium # 12.x
-        nvm install v13 # 13.x
         nvm install v14 # 14.x
-        nvm alias node6 lts/boron
-        nvm alias node8 lts/carbon
-        nvm alias node10 lts/dubnium
-        nvm alias node12 lts/erbium
-        nvm alias node13 v13
-        nvm alias node14 v14
         # set system node as default
         nvm alias default system
 else
