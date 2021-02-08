6e9cc7b74083c18281d7f0c51917d8bdb9e576b7
Mikhail Timofeev <48208649+miketimofeev@users.noreply.github.com>
2021-02-08 17:33:44 +0300

    [ubuntu] remove npm installation (#2649)

--- images/linux/scripts/installers/nodejs.sh
+++ images/linux/scripts/installers/nodejs.sh
@@ -9,7 +9,6 @@ curl -sL https://raw.githubusercontent.com/mklement0/n-install/stable/bin/n-inst
 ~/n/bin/n lts
 npm install -g grunt gulp n parcel-bundler typescript newman
 npm install -g --save-dev webpack webpack-cli
-npm install -g npm
 rm -rf ~/n
 
 # Install Yarn repository and key
