6ee6c1b75454c195a350a7cbf74f7b84e4dfc8c3
Dmitry Shibanov <dmitry-shibanov@github.com>
2021-02-11 23:35:38 +0300

    resolve comments

--- images/macos/provision/core/nvm.sh
+++ images/macos/provision/core/nvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e -o pipefail
 ###########################################################################
-# The script installs node version manager with node versions 6,8,10 and 12
+# The script installs node version manager with node versions 10,12 and 14
 #
 ###########################################################################
 source ~/utils/utils.sh
@@ -11,9 +11,12 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$VERSION/install.sh | bash
 if [ $? -eq 0 ]; then
         . ~/.bashrc
         nvm --version
-        nvm install lts/dubnium # 10.x
-        nvm install lts/erbium # 12.x
-        nvm install v14 # 14.x
+        nodeVersions=("v10" "v12" "v14")
+        for version in ${nodeVersions[@]}
+        do
+                nvm install $version
+        done
+
         # set system node as default
         nvm alias default system
 else
