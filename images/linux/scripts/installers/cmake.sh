04e385fe38e8f3521715ffecaba5f32065833d23
whywaita <whywaita@whywrite.it>
2021-02-17 20:08:35 +0900

    become to able to catch Linux and linux in name of assets

--- images/linux/scripts/installers/cmake.sh
+++ images/linux/scripts/installers/cmake.sh
@@ -11,7 +11,7 @@ if command -v cmake; then
 else
 	json=$(curl -s "https://api.github.com/repos/Kitware/CMake/releases")
 	latest_tag=$(echo $json | jq -r '.[] | select(.prerelease==false).tag_name' | sort --unique --version-sort | tail -1)
-	sh_url=$(echo $json | jq -r ".[] | select(.tag_name==\"${latest_tag}\").assets[].browser_download_url | select(endswith(\"linux-x86_64.sh\"))")
+	sh_url=$(echo $json | jq -r ".[] | select(.tag_name==\"${latest_tag}\").assets[].browser_download_url | select(endswith(\"inux-x86_64.sh\"))")
 	curl -sL ${sh_url} -o cmakeinstall.sh \
 	&& chmod +x cmakeinstall.sh \
 	&& ./cmakeinstall.sh --prefix=/usr/local --exclude-subdir \
