504a1fe2b60284742f7625a9fa52a0e8dcedc284
DuvalCaruso <65207815+Duv4lC4ruso@users.noreply.github.com>
2021-02-18 03:12:25 -0500

    Update cmake.sh

--- images/linux/scripts/installers/cmake.sh
+++ images/linux/scripts/installers/cmake.sh
@@ -6,9 +6,9 @@
 
 # Test to see if the software in question is already installed, if not install it
 echo "Checking to see if the installer script has already been run"
-if command -v cmake; then
+ command -v cmake; 
     echo "cmake is already installed"
-else
+    
 	json=$(curl -s "https://api.github.com/repos/Kitware/CMake/releases")
 	latest_tag=$(echo $json | jq -r '.[] | select(.prerelease==false).tag_name' | sort --unique --version-sort | grep -v "rc" | tail -1)
 	sh_url=$(echo $json | jq -r ".[] | select(.tag_name==\"${latest_tag}\").assets[].browser_download_url | select(endswith(\"inux-x86_64.sh\"))")
@@ -16,6 +16,6 @@ else
 	&& chmod +x cmakeinstall.sh \
 	&& ./cmakeinstall.sh --prefix=/usr/local --exclude-subdir \
 	&& rm cmakeinstall.sh
-fi
+
 
 invoke_tests "Tools" "Cmake"
