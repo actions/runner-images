68f075d37a35ce17aa3d8dc5c1428f5c4d4696d0
unknown <ethan.fry@thermofisher.com>
2021-02-20 01:11:06 +0000

    setting perms and path with meteor in original install location

--- images/win/scripts/Installers/Configure-Antivirus.ps1
+++ images/win/scripts/Installers/Configure-Antivirus.ps1
@@ -34,6 +34,6 @@ if (Test-Path $atpRegPath) {
 }
 
 Write-Host "Set antivirus parameters"
-Set-MpPreference -ScanAvgCPULoadFactor 5 -ExclusionPath "D:\", "C:\"
+Set-MpPreference -ScanAvgCPULoadFactor 5 -ExclusionPath "C:\", "D:\", "E:\"
 $processes = @('choco.exe', 'candle.exe', 'heat.exe','light.exe','meteor.bat','mongod.exe','node.exe','python.exe','git.exe')
 Add-MpPreference -ExclusionProcess $processes
