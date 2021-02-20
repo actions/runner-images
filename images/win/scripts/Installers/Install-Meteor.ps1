55880e47a0a2db79b91c8384bbe711006c0eebbd
unknown <ethan.fry@thermofisher.com>
2021-02-20 15:50:49 +0000

    Moving to c:\meteor with new ACL- MeteorResourceGroup04

--- images/win/scripts/Installers/Install-Meteor.ps1
+++ images/win/scripts/Installers/Install-Meteor.ps1
@@ -5,27 +5,9 @@
 
 Choco-Install -PackageName meteor
 
-# Prefetch the recent Meteor releases
-meteor update --release 2.0
-meteor update --release 1.21.12
-meteor update --release 1.11
-meteor update --release 1.10.3
-meteor update --release 1.10.2
-meteor update --release 1.10.1
-meteor update --release 1.9.3
-meteor update --release 1.9.2
-meteor update --release 1.9.1
-meteor update --release 1.9
-meteor update --release 1.8.3
-meteor update --release 1.8.2
-meteor update --release 1.8.1
-meteor update --release 1.8.0
-
-
 # Copy meteor out of installing user's homedir into a general location
-$copypath = "C:\Users\All Users\AppData\Local"
-$meteorpath = "$copypath\.meteor"
-Copy-Item $env:LOCALAPPDATA\.meteor $copypath -Recurse
+$meteorpath = "C:\meteor"
+Copy-Item $env:LOCALAPPDATA\.meteor $meteorpath -Recurse
 
 # Set perms on the meteor install path for everyone
 $Acl = Get-ACL $meteorpath
@@ -36,3 +18,19 @@ Set-Acl $meteorpath $Acl
 # Add the new Meteor installation into the PATH for all users. 
 $OldPath = Get-ItemProperty -Path "HKLM:SYSTEM\CurrentControlSet\Control\Session Manager\Environment" -Name "Path"
 Set-ItemProperty -Path "HKLM:SYSTEM\CurrentControlSet\Control\Session Manager\Environment" -Name "Path" -Value "$OldPath;$meteorpath"
+
+# Prefetch the recent Meteor releases
+# meteor update --release 2.0
+# meteor update --release 1.21.12
+# meteor update --release 1.11
+# meteor update --release 1.10.3
+# meteor update --release 1.10.2
+# meteor update --release 1.10.1
+# meteor update --release 1.9.3
+# meteor update --release 1.9.2
+# meteor update --release 1.9.1
+# meteor update --release 1.9
+# meteor update --release 1.8.3
+# meteor update --release 1.8.2
+# meteor update --release 1.8.1
+# meteor update --release 1.8.0
