9fc9577a4c7b9680159ed8da8a0fd4894bdbcb24
Ethan Fry <ethan.fry@thermofisher.com>
2021-02-24 11:45:25 -0800

    merge

--- images/win/scripts/Installers/Configure-Antivirus.ps1
+++ images/win/scripts/Installers/Configure-Antivirus.ps1
@@ -1,4 +1,3 @@
 Write-Host "Set antivirus parameters"
-Set-MpPreference -ScanAvgCPULoadFactor 5 -ExclusionPath "C:\", "D:\", "E:\"
 $processes = @('choco.exe', 'candle.exe', 'heat.exe','light.exe','meteor.bat','mongod.exe','node.exe','python.exe','git.exe')
-Add-MpPreference -ExclusionProcess $processes
+Set-MpPreference -ScanAvgCPULoadFactor 5 -ExclusionPath "D:\", "C:\" -ExclusionProcess $processes
--- images/win/scripts/Installers/Install-Meteor.ps1
+++ images/win/scripts/Installers/Install-Meteor.ps1
@@ -21,19 +21,3 @@ Set-Acl $meteorpath $Acl
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 $newpath = "$oldpath;$meteorpath"
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
-
-# Prefetch the recent Meteor releases
-# meteor update --release 2.0
-# meteor update --release 1.21.12
-# meteor update --release 1.11
-# meteor update --release 1.10.3
-# meteor update --release 1.10.2
-# meteor update --release 1.10.1
-# meteor update --release 1.9.3
-# meteor update --release 1.9.2
-# meteor update --release 1.9.1
-# meteor update --release 1.9
-# meteor update --release 1.8.3
-# meteor update --release 1.8.2
-# meteor update --release 1.8.1
-# meteor update --release 1.8.0
