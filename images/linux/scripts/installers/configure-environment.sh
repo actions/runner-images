e8242a6acf230b6282c3c8c10cb5d9f13dc61aad
Mikhail Timofeev <v-mitim@microsoft.com>
2021-03-02 17:11:28 +0300

    set BASH_ENV var

--- images/linux/scripts/installers/configure-environment.sh
+++ images/linux/scripts/installers/configure-environment.sh
@@ -6,6 +6,8 @@ echo ImageOS=$IMAGE_OS | tee -a /etc/environment
 
 # Create a file to store user-related global environment variables 
 touch /etc/profile.d/env_vars.sh
+# Set BASH_ENV variable pointed to file with user-related global environment variables for non-interactive sessions
+echo "BASH_ENV=/etc/profile.d/env_vars.sh" | tee -a /etc/environment
 
 # This directory is supposed to be created in $HOME and owned by user(https://github.com/actions/virtual-environments/issues/491)
 mkdir -p /etc/skel/.config/configstore
