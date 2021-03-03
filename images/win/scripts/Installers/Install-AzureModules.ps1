408a379ec09d9a3ebb8d274d7b8af160d1ae6c6c
Amruta Kawade <amrutakawade@github.com>
2021-03-03 16:23:33 +0530

    [Windows] Az Modules Size Reduction On Image

--- images/win/scripts/Installers/Install-AzureModules.ps1
+++ images/win/scripts/Installers/Install-AzureModules.ps1
@@ -36,6 +36,46 @@ foreach ($module in $modules)
         }
     }
 
+    if($null -ne $module.url)
+    {
+        $assets = Invoke-RestMethod $module.url
+    }
+
+    foreach ($version in $module.zipversions)
+    {
+        # Install modules from GH Release
+        if($null -ne $assets) 
+        {
+            $asset = $assets | Where-Object version -like $version `
+                         | Select-Object -ExpandProperty files `
+                         | Select-Object -First 1
+
+            Write-Host "Installing $($module.name) $version ..."
+            if ($null -ne $asset) {
+                Start-DownloadWithRetry -Url $asset.download_url -Name $asset.filename -DownloadPath $installPSModulePath
+            } else {
+                Write-Host "Asset was not found in versions manifest"
+                exit 1
+            }
+        }
+        # Install modules from powershell gallery
+        else 
+        {
+            $modulePath = Join-Path -Path $installPSModulePath -ChildPath "${moduleName}_${version}"
+            Write-Host " - $version [$modulePath]"
+            try
+            {
+                Save-Module -Path $modulePath -Name $moduleName -RequiredVersion $version -Force -ErrorAction Stop
+                Compress-Archive -Path $modulePath -DestinationPath "${modulePath}.zip"
+                Remove-Item $modulePath -Recurse -Force
+            }
+            catch
+            {
+                Write-Host "Error: $_"
+                exit 1
+            }
+        }
+    }
     # Append default tool version to machine path
     if ($null -ne $module.default)
     {
