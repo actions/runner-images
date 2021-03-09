a0c2c6d57feb04d1dd7a7b875e39dc1e20d63d8d
Nikita Bykov <v-nibyko@microsoft.com>
2021-03-09 14:56:41 +0300

    Move java to the toolcache

--- images/macos/provision/core/openjdk.sh
+++ images/macos/provision/core/openjdk.sh
@@ -1,27 +1,36 @@
 #!/bin/bash -e -o pipefail
 source ~/utils/utils.sh
 
-createEnvironmentVariable() {
-    local VARIABLE_NAME=$1
-    local JAVA_VERSION=$2
-    if [[ $JAVA_VERSION == "8" ]]; then
-        JAVA_VERSION="1.${JAVA_VERSION}"
-    fi
-    local JAVA_PATH=$(/usr/libexec/java_home -v${JAVA_VERSION})
-    echo "export ${VARIABLE_NAME}=${JAVA_PATH}" >> "${HOME}/.bashrc"
-}
+JAVA_TOOLCACHE_PATH=$AGENT_TOOLSDIRECTORY/Java_Adoptium_jdk
 
-brew tap AdoptOpenJDK/openjdk
+installJavaFromAdoptOpenJDK() {
+    local JAVA_VERSION=$1
+
+    javaRelease=$(curl -s "https://api.adoptopenjdk.net/v3/assets/latest/$JAVA_VERSION/hotspot")
+    archivePath=$(echo $javaRelease | jq -r '[.[] | select(.binary.os=="mac") | .binary.package.link][0]')
+    fullVersion=$(echo $javaRelease | jq -r '[.[] | select(.binary.os=="mac") | .version.openjdk_version][0]')
+    javaToolcacheVersionPath=$JAVA_TOOLCACHE_PATH/$fullVersion
+    javaToolcacheVersionArchPath=$javaToolcacheVersionPath/x64
+
+    download_with_retries $archivePath /tmp OpenJDK$JAVA_VERSION.tar.gz
+    mkdir /tmp/jdk-$fullVersion && tar -xzf /tmp/OpenJDK$JAVA_VERSION.tar.gz -C /tmp/jdk-$fullVersion
+    mkdir -p $javaToolcacheVersionArchPath && mv /tmp/jdk-$fullVersion/*/* $javaToolcacheVersionArchPath
+
+    local JAVA_HOME_PATH=$javaToolcacheVersionArchPath/Contents/Home
+    if [[ $JAVA_VERSION == $JAVA_DEFAULT ]]; then
+        echo "export JAVA_HOME=${JAVA_HOME_PATH}" >> "${HOME}/.bashrc"
+        export PATH="$JAVA_HOME_PATH:$PATH"
+    fi
+    echo "export JAVA_HOME_${JAVA_VERSION}_X64=${JAVA_HOME_PATH}" >> "${HOME}/.bashrc"
+}
 
 JAVA_VERSIONS_LIST=($(get_toolset_value '.java.versions | .[]'))
 JAVA_DEFAULT=$(get_toolset_value '.java.default')
+
 for JAVA_VERSION in "${JAVA_VERSIONS_LIST[@]}"
 do
-    brew install --cask "adoptopenjdk${JAVA_VERSION}"
-    createEnvironmentVariable "JAVA_HOME_${JAVA_VERSION}_X64" $JAVA_VERSION
+    installJavaFromAdoptOpenJDK $JAVA_VERSION
 done
-
-createEnvironmentVariable "JAVA_HOME" $JAVA_DEFAULT
 
 echo Installing Maven...
 brew_smart_install "maven"
--- images/macos/tests/Java.Tests.ps1
+++ images/macos/tests/Java.Tests.ps1
@@ -31,19 +31,7 @@ Describe "Java" {
 
     $testCases | ForEach-Object {
         Context $_.Title {
-            It "Version is found by 'java_home'" -TestCases $_ {
-                "/usr/libexec/java_home -v${Version}" | Should -ReturnZeroExitCode
-            }
-
-            if ($_.Title -ne "Default") {
-                It "Version is valid" -TestCases $_ {
-                    $javaRootPath = "/Library/Java/JavaVirtualMachines/adoptopenjdk-${Title}.jdk/Contents/Home"
-                    $javaBinPath = Join-Path $javaRootPath "/bin/java"
-                    Validate-JavaVersion -JavaCommand "$javaBinPath -version" -ExpectedVersion $Version
-                }
-            }
-
-            It "<EnvVariable>" -TestCases $_ {
+            It "Java <Version>" -TestCases $_ {
                 $envVariablePath = Get-EnvironmentVariable $EnvVariable
                 $javaBinPath = Join-Path $envVariablePath "/bin/java"
                 Validate-JavaVersion -JavaCommand "$javaBinPath -version" -ExpectedVersion $Version
