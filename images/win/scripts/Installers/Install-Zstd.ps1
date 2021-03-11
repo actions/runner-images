4b36e9a34ccba28431325811f9619e3ecb1ec226
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-03-11 21:54:55 +0300

    cut messages

--- images/win/scripts/Installers/Install-Zstd.ps1
+++ images/win/scripts/Installers/Install-Zstd.ps1
@@ -3,16 +3,15 @@
 ##  Desc:  Install zstd
 ################################################################################
 
-Write-Host "Download Latest zstd-win64 archive"
 $url = "https://api.github.com/repos/facebook/zstd/releases/latest"
 # Explicitly set type to string since match returns array by default
 [System.String] $zstdLatest = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "zstd-.+-win64.zip$"
 $zstdArchivePath = Start-DownloadWithRetry -Url $zstdLatest -Name "zstd-win64.zip"
 
-Write-Host "Expand zstd-win64 archive"
 $toolPath = "C:\tools"
+$zstdPath = Join-Path $toolPath zstd
 Extract-7Zip -Path $zstdArchivePath -DestinationPath $toolPath
-Rename-Item -Path "${toolPath}\zstd*" -NewName zstd
+Move-Item -Path "${zstdPath}*" -Destination $zstdPath
 
 # Add zstd-win64 to PATH
 Add-MachinePathItem $zstdPath
