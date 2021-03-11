4c5121fa94d4af19c84fd1fc7e9ad9388a0bc19a
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-03-11 11:51:50 +0300

    install from GitHub releases

--- images/win/scripts/Installers/Install-Zstd.ps1
+++ images/win/scripts/Installers/Install-Zstd.ps1
@@ -3,6 +3,18 @@
 ##  Desc:  Install zstd
 ################################################################################
 
-Choco-Install -PackageName zstandard
+Write-Host "Download Latest zstd-win64 archive"
+$url = "https://api.github.com/repos/facebook/zstd/releases/latest"
+# Explicitly set type to string since match returns array by default
+[System.String] $zstdLatest = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "zstd-.+-win64.zip$"
+$zstdArchivePath = Start-DownloadWithRetry -Url $zstdLatest -Name "zstd-win64.zip"
+
+Write-Host "Expand zstd-win64 archive"
+$zstdPath = "C:\tools\zstd"
+New-Item -Path $zstdPath -ItemType Directory -Force
+Extract-7Zip -Path $zstdArchivePath -DestinationPath $zstdPath
+
+# Add zstd-win64 to PATH
+Add-MachinePathItem $zstdPath
 
 Invoke-PesterTests -TestFile "Tools" -TestName "Zstd"
