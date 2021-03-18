fa1dd8a6cc2de85063b7d09ee558e817ba7704f3
Mikhail Timofeev <v-mitim@microsoft.com>
2021-03-18 19:44:03 +0300

    add source install.sh

--- images/linux/scripts/installers/sbt.sh
+++ images/linux/scripts/installers/sbt.sh
@@ -4,6 +4,8 @@
 ##  Desc:  Installs sbt
 ################################################################################
 
+source $HELPER_SCRIPTS/install.sh
+
 # Install latest sbt release
 sbtVersion=$(curl -s -L "https://api.github.com/repos/sbt/sbt/releases" | jq -r '.[] | select(.prerelease==false).name' | sort --unique --version-sort | grep -ve "-.*" | tail -1)
 download_with_retries "https://github.com/sbt/sbt/releases/download/v${sbtVersion}/sbt-${sbtVersion}.tgz" "/tmp" "sbt.tgz"
