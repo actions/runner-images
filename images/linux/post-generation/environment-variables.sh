dff1318e9640a18e0da19d2364d5fc420c7177a3
Mikhail Timofeev <v-mitim@microsoft.com>
2021-03-18 17:11:44 +0300

    Remove "export" word

--- images/linux/post-generation/environment-variables.sh
+++ images/linux/post-generation/environment-variables.sh
@@ -5,5 +5,5 @@
 default_user=$(cut -d: -f1 /etc/passwd | tail -1)
 home_directory="/home/${default_user}"
 
-echo "export XDG_CONFIG_HOME=${home_directory}/.config" | tee -a /etc/environment
-echo "export NVM_DIR=${home_directory}/.nvm" | tee -a /etc/environment
+echo "XDG_CONFIG_HOME=${home_directory}/.config" | tee -a /etc/environment
+echo "NVM_DIR=${home_directory}/.nvm" | tee -a /etc/environment
