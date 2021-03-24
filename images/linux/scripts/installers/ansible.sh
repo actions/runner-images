8171ff2281d5842efddef42303b59ab7a8756202
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-03-24 20:42:29 +0300

    update info

--- images/linux/scripts/installers/ansible.sh
+++ images/linux/scripts/installers/ansible.sh
@@ -4,6 +4,8 @@
 ##  Desc:  Installs Ansible
 ################################################################################
 
+# this script is used only on Ubuntu 16.04
+# for Ubuntu 18.04 and 20.04 we use pipx ansible-base package
 # Install latest Ansible
 add-apt-repository ppa:ansible/ansible
 apt-get update
