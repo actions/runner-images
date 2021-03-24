a4340fe57f758e9bc3f65ce63e84c06689e5d4eb
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-03-24 14:51:20 +0300

    minor fixes

--- images/linux/scripts/SoftwareReport/SoftwareReport.Java.psm1
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Java.psm1
@@ -1,13 +1,12 @@
 function Get-JavaVersions {
     $toolcachePath = Join-Path $env:AGENT_TOOLSDIRECTORY "Java_Adopt_jdk"
-    [Array]$javaToolcacheVersions = Get-ChildItem $toolcachePath -Name | Sort-Object -Descending
+    $javaToolcacheVersions = Get-ChildItem $toolcachePath -Name | Sort-Object -Descending
 
     return $javaToolcacheVersions | ForEach-Object {
         $majorVersion = $_.split(".")[0]
-        $defaultJavaPath = Get-Item env:JAVA_HOME
+        $defaultJavaPath = $env:JAVA_HOME
         $javaPath = Get-Item env:JAVA_HOME_${majorVersion}_X64
-
-        # Take semver from the java path
+        
         $defaultPostfix = ($javaPath -eq $defaultJavaPath) ? " (default)" : ""
 
         [PSCustomObject] @{
