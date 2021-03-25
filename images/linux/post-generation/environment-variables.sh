08825617b6fb3a3c39e8c4c524af55036f23166f
Mikhail Timofeev <v-mitim@microsoft.com>
2021-03-25 17:55:10 +0300

    rename variable

--- images/linux/post-generation/environment-variables.sh
+++ images/linux/post-generation/environment-variables.sh
@@ -3,6 +3,6 @@
 # Replace $HOME with the default user's home directory for environmental variables related to the default user home directory
 
 default_user=$(cut -d: -f1 /etc/passwd | tail -1)
-HOME="/home/${default_user}"
+homeDir="/home/${default_user}"
 
 sed -i "s|\$HOME|$homeDir|g" /etc/environment
