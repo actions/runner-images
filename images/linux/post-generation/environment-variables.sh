666ff07479701470336a83bdcf0f99a73e8f6db0
Mikhail Timofeev <v-mitim@microsoft.com>
2021-03-26 10:46:46 +0300

    get rid of extra variable

--- images/linux/post-generation/environment-variables.sh
+++ images/linux/post-generation/environment-variables.sh
@@ -2,7 +2,5 @@
 
 # Replace $HOME with the default user's home directory for environmental variables related to the default user home directory
 
-default_user=$(cut -d: -f1 /etc/passwd | tail -1)
-homeDir="/home/${default_user}"
-
+homeDir=$(cut -d: -f6 /etc/passwd | tail -1)
 sed -i "s|\$HOME|$homeDir|g" /etc/environment
