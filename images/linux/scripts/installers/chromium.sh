6459a7b2bdf19a71d77d1d4966d53c3c333e8230
Nikita Bykov <v-nibyko@microsoft.com>
2021-03-30 11:44:20 +0300

    added GetChromiumRevision function

--- images/linux/scripts/installers/chromium.sh
+++ images/linux/scripts/installers/chromium.sh
@@ -1,11 +1,35 @@
 #!/bin/bash -e
 
-# Get URL to download the latest Chromium
-LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media"
-REVISION=$(curl -s -S $LASTCHANGE_URL)
-ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
+source $HELPER_SCRIPTS/install.sh
+
+function GetChromiumRevision {
+    CHROME_VERSION=$1
+
+    URL="https://omahaproxy.appspot.com/deps.json?version=${CHROME_VERSION}"
+    REVISION=$(curl -s $URL | jq -r '.chromium_base_position')
+    # Take the first part of revision variable for search
+    FIRST_PART_OF_REVISION=${revision:0:${#revision}/2}
+    URL="https://www.googleapis.com/storage/v1/b/chromium-browser-snapshots/o?delimiter=/&prefix=Linux_x64/${FIRST_PART_OF_REVISION}"
+    VERSIONS=$(curl -s $URL | jq -r '.prefixes[]' | cut -d "/" -f 2 | sort --version-sort)
+
+    RIGHT_REVISION=$(echo $VERSIONS | cut -f 1 -d " ")
+    for version in $VERSIONS; do
+        if [ $revision -lt  $version ]; then
+            echo $RIGHT_REVISION
+            return
+        fi
+        RIGHT_REVISION=$version
+    done
+    echo $RIGHT_REVISION
+}
+
+CHROME_VERSION=$(google-chrome --version | cut -d " " -f 3)
+
+# Get Chromium version corresponding to the Google Chrome version
+REVISION=$(GetChromiumRevision $CHROME_VERSION)
+
+ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F${REVISION}%2Fchrome-linux.zip?alt=media"
 ZIP_FILE="${REVISION}-chromium-linux.zip"
-rm -rf $REVISION
 
 CHROMIUM_DIR="/usr/local/share/chromium"
 CHROMIUM_BIN="$CHROMIUM_DIR/chrome-linux/chrome"
