be27ebfdb31aece2c90fbe1984c1749cbd1b464c
Aleksandr Chebotov <47745270+al-cheb@users.noreply.github.com>
2021-04-02 18:47:50 +0300

    [ubuntu] replace wget to download_with_retries helper (#3087)

--- images/linux/scripts/installers/aliyun-cli.sh
+++ images/linux/scripts/installers/aliyun-cli.sh
@@ -4,9 +4,12 @@
 ##  Desc:  Installs Alibaba Cloud CLI
 ################################################################################
 
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/install.sh
+
 # Install Alibaba Cloud CLI
 URL=$(curl -s https://api.github.com/repos/aliyun/aliyun-cli/releases/latest | jq -r '.assets[].browser_download_url | select(contains("aliyun-cli-linux"))')
-wget -P /tmp $URL
+download_with_retries $URL "/tmp"
 tar xzf /tmp/aliyun-cli-linux-*-amd64.tgz
 mv aliyun /usr/local/bin
 
--- images/linux/scripts/installers/containers.sh
+++ images/linux/scripts/installers/containers.sh
@@ -8,7 +8,7 @@
 install_packages=(podman buildah skopeo)
 source /etc/os-release
 sh -c "echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
+wget -qnv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
 apt-key add Release.key
 apt-get update -qq
 apt-get -qq -y install ${install_packages[@]}
