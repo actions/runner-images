b359aed502b59f385f37c337c871625fb3c5b073
Sergey Dolin <v-sedoli@microsoft.com>
2021-04-02 18:13:47 +0500

    Rework bazel installation to avoid using apt repos

--- images/linux/scripts/installers/bazel.sh
+++ images/linux/scripts/installers/bazel.sh
@@ -4,13 +4,12 @@
 ##  Desc:  Installs Bazel and Bazelisk (A user-friendly launcher for Bazel)
 ################################################################################
 
-# Install bazel
-curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-apt-get update -y
-apt-get install -y bazel
+source $HELPER_SCRIPTS/install.sh
 
-# Install bazelisk
-npm install -g @bazel/bazelisk
+# Install bazel
+bazelVersion=$(curl -s -L "https://api.github.com/repos/bazelbuild/bazel/releases" | jq -r '.[] | select(.prerelease==false).name' | sort --unique --version-sort | grep -ve "-.*" | tail -1)
+download_with_retries "https://github.com/bazelbuild/bazel/releases/download/${bazelVersion}/bazel-${bazelVersion}-installer-linux-x86_64.sh" "/tmp" "bazel-installer.sh"
+chmod +x /tmp/bazel-installer.sh
+sudo /tmp/bazel-installer.sh
 
 invoke_tests "Tools" "Bazel"
