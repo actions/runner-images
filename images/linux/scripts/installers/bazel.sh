152c4baffb7a723ad999d2d63783eccdb83b80eb
Sergey Dolin <v-sedoli@microsoft.com>
2021-04-03 11:18:18 +0500

    Add bazelisk to the installation

--- images/linux/scripts/installers/bazel.sh
+++ images/linux/scripts/installers/bazel.sh
@@ -12,4 +12,10 @@ download_with_retries "https://github.com/bazelbuild/bazel/releases/download/${b
 chmod +x /tmp/bazel-installer.sh
 sudo /tmp/bazel-installer.sh
 
+bazeliskVersion=$(curl -s -L "https://api.github.com/repos/bazelbuild/bazelisk/releases" | jq -r '.[] | select(.prerelease==false).name' | sort --unique --version-sort | grep -ve "-.*" | tail -1)
+
+download_with_retries "https://github.com/bazelbuild/bazelisk/releases/download/${bazeliskVersion}/bazelisk-linux-amd64" "/tmp" "bazelisk"
+chmod +x /tmp/bazelisk
+sudo mv /tmp/bazelisk /usr/local/bin
+
 invoke_tests "Tools" "Bazel"
