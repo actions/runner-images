9caf0bea4f5a10e9fbb57b41e136ed44172eda30
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-04-04 23:52:42 +0300

    debug

--- images/linux/scripts/installers/containers.sh
+++ images/linux/scripts/installers/containers.sh
@@ -6,19 +6,21 @@
 
 source $HELPER_SCRIPTS/os.sh
 
-# Install podman, buildah, scopeo container's tools
 install_packages=(podman buildah skopeo)
-source /etc/os-release
-sh -c "echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
-apt-key add Release.key
-apt-get update -qq
-apt-get -qq -y install ${install_packages[@]}
-mkdir -p /etc/containers
-echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | tee /etc/containers/registries.conf
 
+# Install podman, buildah, scopeo container's tools (on Ubuntu20 these tools can be installed without adding new repository)
 if isUbuntu20 ; then
-    rm /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+    apt-get -y update
+    apt-get -qq -y install ${install_packages[@]}
+else
+    source /etc/os-release
+    sh -c "echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+    wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
+    apt-key add Release.key
+    apt-get update -qq
+    apt-get -qq -y install ${install_packages[@]}
+    mkdir -p /etc/containers
+    echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | tee /etc/containers/registries.conf
 fi
 
 invoke_tests "Tools" "Containers"
