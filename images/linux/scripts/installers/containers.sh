01acdc7a987530e3f7a526a9e455ea455edc42b6
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-04-07 15:19:48 +0300

    document source repo and fix installation

--- images/linux/scripts/installers/containers.sh
+++ images/linux/scripts/installers/containers.sh
@@ -7,20 +7,24 @@
 source $HELPER_SCRIPTS/os.sh
 
 install_packages=(podman buildah skopeo)
+REPO_URL="https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable"
 
 # Install podman, buildah, scopeo container's tools (on Ubuntu20 these tools can be installed without adding new repository)
+source /etc/os-release
+sh -c "echo 'deb ${REPO_URL}/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -qnv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
+apt-key add Release.key
+apt-get update -qq
+apt-get -qq -y install ${install_packages[@]}
+mkdir -p /etc/containers
+echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | tee /etc/containers/registries.conf
+
+# We can safely remove source repo on Ubuntu20
 if isUbuntu20 ; then
-    apt-get -y update
-    apt-get -qq -y install ${install_packages[@]}
-else
-    source /etc/os-release
-    sh -c "echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-    wget -qnv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
-    apt-key add Release.key
-    apt-get update -qq
-    apt-get -qq -y install ${install_packages[@]}
-    mkdir -p /etc/containers
-    echo -e "[registries.search]\nregistries = ['docker.io', 'quay.io']" | tee /etc/containers/registries.conf
+    rm /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 fi
+
+# Document source repo
+echo "containers $REPO_URL" >> $HELPER_SCRIPTS/apt-sources.txt
 
 invoke_tests "Tools" "Containers"
