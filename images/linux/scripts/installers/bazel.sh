4017804df49a2767de15aa38b58a175883168fca
Sergey Dolin <v-sedoli@microsoft.com>
2021-04-07 19:45:02 +0500

    install bazel with bazelisk

--- images/linux/scripts/installers/bazel.sh
+++ images/linux/scripts/installers/bazel.sh
@@ -6,16 +6,10 @@
 
 source $HELPER_SCRIPTS/install.sh
 
-# Install bazel
-bazelVersion=$(curl -s -L "https://api.github.com/repos/bazelbuild/bazel/releases" | jq -r '.[] | select(.prerelease==false).name' | sort --unique --version-sort | grep -ve "-.*" | tail -1)
-download_with_retries "https://github.com/bazelbuild/bazel/releases/download/${bazelVersion}/bazel-${bazelVersion}-installer-linux-x86_64.sh" "/tmp" "bazel-installer.sh"
-chmod +x /tmp/bazel-installer.sh
-sudo /tmp/bazel-installer.sh
+# Install bazelisk
+npm install -g @bazel/bazelisk
 
-bazeliskVersion=$(curl -s -L "https://api.github.com/repos/bazelbuild/bazelisk/releases" | jq -r '.[] | select(.prerelease==false).name' | sort --unique --version-sort | grep -ve "-.*" | tail -1)
-
-download_with_retries "https://github.com/bazelbuild/bazelisk/releases/download/${bazeliskVersion}/bazelisk-linux-amd64" "/tmp" "bazelisk"
-chmod +x /tmp/bazelisk
-sudo mv /tmp/bazelisk /usr/local/bin
+# run bazelisk once in order to instal /usr/local/bin/bazel binary
+bazelisk
 
 invoke_tests "Tools" "Bazel"
