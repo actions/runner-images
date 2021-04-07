787105bf555c537a230485898a71216a89ca370f
Mikhail Timofeev <v-mitim@microsoft.com>
2021-04-07 17:17:24 +0300

    Add nuget.org as a source

--- images/win/post-generation/Dotnet.ps1
+++ images/win/post-generation/Dotnet.ps1
@@ -6,3 +6,6 @@ if (-not $latestPath.Contains($dotnetPath))
     $latestPath = "$dotnetPath;$latestPath"
     [System.Environment]::SetEnvironmentVariable('PATH', $latestPath, [System.EnvironmentVariableTarget]::Machine)
 }
+
+# Add nuget.org package source to the default nuget.config file https://github.com/actions/virtual-environments/issues/3038
+dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
