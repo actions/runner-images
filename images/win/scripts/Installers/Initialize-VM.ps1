e662b19a30723ec7323ebacbdeebafbc59b07d8e
Mikhail Timofeev <48208649+miketimofeev@users.noreply.github.com>
2021-04-22 09:54:12 +0300

    Install vcredist 2010 for windows 2010 from direct links (#3230)

--- images/win/scripts/Installers/Initialize-VM.ps1
+++ images/win/scripts/Installers/Initialize-VM.ps1
@@ -129,7 +129,13 @@ if (Test-IsWin16) {
 
 if (Test-IsWin19) {
     # Install vcredist2010
-    Choco-Install -PackageName vcredist2010
+    $Vc2010x86Name = "vcredist_x86.exe"
+    $Vc2010x86URI = "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/${Vc2010x86Name}"
+    $Vc2010x64Name = "vcredist_x64.exe"
+    $Vc2010x64URI = "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/${Vc2010x64Name}"
+    $ArgumentList = ("/install", "/quiet", "/norestart")
+    Install-Binary -Url $Vc2010x86URI -Name $Vc2010x86Name -ArgumentList $ArgumentList
+    Install-Binary -Url $Vc2010x64URI -Name $Vc2010x64Name -ArgumentList $ArgumentList
 }
 
 # Initialize environmental variable ChocolateyToolsLocation by invoking choco Get-ToolsLocation function
