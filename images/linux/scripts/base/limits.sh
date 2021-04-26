2e42ccef9b72bc9b60d6b8e93740accf9750483f
Rob Burger <rob.burgs@gmail.com>
2021-04-26 12:37:23 +0200

    Add stack hard and soft limits to limits.conf

--- images/linux/scripts/base/limits.sh
+++ images/linux/scripts/base/limits.sh
@@ -7,4 +7,5 @@ echo 'session required pam_limits.so' >> /etc/pam.d/common-session-noninteractiv
 echo 'DefaultLimitNOFILE=65536' >> /etc/systemd/system.conf
 
 # Double stack size from default 8192KB
-ulimit -s 16384
+echo '* soft stack 16384' >> /etc/security/limits.conf
+echo '* hard stack 16384' >> /etc/security/limits.conf
