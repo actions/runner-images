b1f202e5f05f2adc6cbe7a068f6c51bcefe73566
Leonid Lapshin <leonidlapshin@github.com>
2021-04-29 17:29:51 +0300

    [ubuntu] Rework php installation (#3272)

--- images/linux/scripts/SoftwareReport/SoftwareReport.Common.psm1
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Common.psm1
@@ -211,10 +211,11 @@ function Get-SbtVersion {
 
 function Get-PHPVersions {
     $result = Get-CommandResult "apt list --installed" -Multiline
-    return $result.Output | Where-Object { $_ -match "^php\d+\.\d+/"} | ForEach-Object {
+    $aptSourceRepo = Get-AptSourceRepository -PackageName "php"
+    $versionsList = $result.Output | Where-Object { $_ -match "^php\d+\.\d+/"} | ForEach-Object {
         $_ -match "now (?<version>\d+\.\d+\.\d+)-" | Out-Null
-        $Matches.version
-    }
+        $Matches.version }
+    return $versionsList + "(apt source repository: $aptSourceRepo)"
 }
 
 function Get-ComposerVersion {
