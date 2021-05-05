e7c8d6c053ec63dc8d527d4fbf16a056a974d16f
Mikhail Timofeev <v-mitim@microsoft.com>
2021-05-05 15:40:18 +0300

    avoid updating gems

--- images/linux/scripts/installers/ruby.sh
+++ images/linux/scripts/installers/ruby.sh
@@ -8,7 +8,6 @@ source $HELPER_SCRIPTS/os.sh
 source $HELPER_SCRIPTS/install.sh
 
 apt-get install ruby-full
-gem update
 
 # temporary fix for fastlane installation https://github.com/fastlane/fastlane/issues/18642
 if isUbuntu20 ; then
