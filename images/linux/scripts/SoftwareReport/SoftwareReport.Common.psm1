a5ee51f72dcbc3879a91a74c63f95737e2d4a292
Mikhail Timofeev <48208649+miketimofeev@users.noreply.github.com>
2021-05-07 16:35:34 +0300

    Revert "[ubuntu] Rework php installation (#3272)" (#3340)
    This reverts commit b1f202e5f05f2adc6cbe7a068f6c51bcefe73566.

--- images/linux/scripts/SoftwareReport/SoftwareReport.Common.psm1
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Common.psm1
@@ -211,11 +211,10 @@ function Get-SbtVersion {
 
 function Get-PHPVersions {
     $result = Get-CommandResult "apt list --installed" -Multiline
-    $aptSourceRepo = Get-AptSourceRepository -PackageName "php"
-    $versionsList = $result.Output | Where-Object { $_ -match "^php\d+\.\d+/"} | ForEach-Object {
+    return $result.Output | Where-Object { $_ -match "^php\d+\.\d+/"} | ForEach-Object {
         $_ -match "now (?<version>\d+\.\d+\.\d+)-" | Out-Null
-        $Matches.version }
-    return $versionsList + "(apt source repository: $aptSourceRepo)"
+        $Matches.version
+    }
 }
 
 function Get-ComposerVersion {
