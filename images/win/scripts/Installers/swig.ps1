798257c1ee21d5179eaa350f65c93d478f263f02
Nursultan Kassan <84095700+kassannk@users.noreply.github.com>
2021-05-26 10:18:04 +0300

    Delete swig.ps1

--- images/win/scripts/Installers/swig.ps1
+++
@@ -1,8 +0,0 @@
-################################################################################
-##  File:  Install-swig.ps1
-##  Desc:  Install swig
-################################################################################
-
-Choco-Install -PackageName swig.install
-
-Invoke-PesterTests -TestFile "Tools" -TestName "swig"
