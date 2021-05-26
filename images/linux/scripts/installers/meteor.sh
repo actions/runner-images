d9575b2b1afd039c51b78dc91eb3c98b16bdf6cf
james-roberts-tfs <james.roberts2@thermofisher.com>
2021-05-26 10:22:00 -0400

    back to su install of meteor

--- images/linux/scripts/installers/meteor.sh
+++ images/linux/scripts/installers/meteor.sh
@@ -7,12 +7,7 @@
 # Install Meteor
 wget -qO- https://install.meteor.com | sh
 
-# If there is a need to change the meteor install to a particular user:
-# cd ~
-# sudo chown -Rh <username> .meteor/local
-# if not, then in the test, --allow-superuser
-
-meteor_user=$(cut -d: -f1 /etc/passwd | tail -1)
-chown -Rh $meteor_user ~{$meteor_user}/.meteor/local
+# After installation:
+# chown -Rh $meteor_user ~$meteor_user/.meteor/local
 
 invoke_tests "WebServers" "Meteor"
