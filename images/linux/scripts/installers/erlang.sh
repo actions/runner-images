9a6b8dd92ae3148313d0f4eb60711ea6076646e5
Mikhail Timofeev <v-mitim@microsoft.com>
2021-05-31 18:53:04 +0300

    Change rebar installation to use gh releases

--- images/linux/scripts/installers/erlang.sh
+++ images/linux/scripts/installers/erlang.sh
@@ -13,9 +13,12 @@ apt-get update
 apt-get install -y --no-install-recommends esl-erlang
 
 # Install rebar3
-wget -q -O rebar3 https://s3.amazonaws.com/rebar3/rebar3
-chmod +x rebar3
-mv rebar3 /usr/local/bin/rebar3
+json=$(curl -sL "https://api.github.com/repos/erlang/rebar3/releases")
+rebar3Version=$(echo $json | jq -r '.[] | select(.tag_name | contains("-") | not).tag_name' | sort -V | tail -n1)
+rebar3DownloadUrl=$(echo $json | jq -r ".[] | select(.tag_name==\"$rebar3Version\").assets[].browser_download_url")
+download_with_retries $rebar3DownloadUrl "/tmp"
+mv /tmp/rebar3 /usr/local/bin/rebar3
+chmod +x /usr/local/bin/rebar3
 
 invoke_tests "Tools" "erlang"
 
