c3ef905edfa0aed847d2086f881249a2a1e6f3de
james-roberts-tfs <james.roberts2@thermofisher.com>
2021-06-02 04:30:50 -0700

    add capture_container_name to build-image.ps1 for linux

--- images.CI/linux-and-win/build-image.ps1
+++ images.CI/linux-and-win/build-image.ps1
@@ -40,6 +40,7 @@ packer --version
 
 Write-Host "Build $Image VM"
 packer build    -var "capture_name_prefix=$ResourcesNamePrefix" `
+                -var "capture_container_name=$ResourcesNamePrefix" `
                 -var "client_id=$ClientId" `
                 -var "client_secret=$ClientSecret" `
                 -var "install_password=$InstallPassword" `
