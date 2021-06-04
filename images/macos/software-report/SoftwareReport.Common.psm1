7f77b14d6b256835253707c8be7a979eae2bf590
Nursultan Kassan <84095700+kassannk@users.noreply.github.com>
2021-06-04 14:04:12 +0300

    fixed display of versions

--- images/macos/software-report/SoftwareReport.Common.psm1
+++ images/macos/software-report/SoftwareReport.Common.psm1
@@ -488,7 +488,7 @@ function Get-PowershellVersion {
 }
 
 function Get-SwigVersion {
-    $swigVersion = Run-Command "swig -version" | Select-String "SWIG Version -version " | Take-Part -Part 3
+    $swigVersion = Run-Command "swig -version" | Select-Object -First 1 | Take-Part -Part 2
     return "Swig $swigVersion"
 }
 
