9f08ea5e1295f4bbd573f697e9bf020b1e6f33a7
Nikita Bykov <v-nibyko@microsoft.com>
2021-07-05 16:53:25 +0300

    Add a condition to Install-CommonUtils.ps1

--- images/win/scripts/Installers/Install-CommonUtils.ps1
+++ images/win/scripts/Installers/Install-CommonUtils.ps1
@@ -3,7 +3,11 @@ $commonPackages = (Get-ToolsetContent).choco.common_packages
 foreach ($package in $commonPackages)
 {
     Write-Host "Installing ${package.name}"
-    Choco-Install -PackageName $package.name -ArgumentList $package.args
+    if ($package.args) {
+        Choco-Install -PackageName $package.name -ArgumentList $package.args
+    } else {
+        Choco-Install -PackageName $package.name
+    }
 }
 
 npm install -g @bazel/bazelisk
