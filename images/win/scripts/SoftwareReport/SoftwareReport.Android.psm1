e1b303dee811566f095cc1cafd1a59f84429bb53
Mikhail Timofeev <v-mitim@microsoft.com>
2021-07-05 15:31:54 +0300

    Modify software report to output any version number

--- images/win/scripts/SoftwareReport/SoftwareReport.Android.psm1
+++ images/win/scripts/SoftwareReport/SoftwareReport.Android.psm1
@@ -121,7 +121,7 @@ function Get-AndroidPlatformVersions {
 
 function Get-AndroidCommandLineToolsVersion {
     $commandLineTools = Get-AndroidSDKManagerPath
-    (& $commandLineTools --version | Out-String).Trim() -match "^\d+\.{1,}\d+$" | Out-Null
+    (& $commandLineTools --version | Out-String).Trim() -match "^(\d+\.){1,}\d+$" | Out-Null
     $commandLineToolsVersion = $Matches.Values
     return $commandLineToolsVersion
 }
