0a208a4772115c7facac3e0a9b3d37be0309fe16
Nikita Bykov <v-nibyko@microsoft.com>
2021-07-06 14:26:47 +0300

    Remove condition and Write-Host command from Install-CommonUtils.ps1

--- images/win/scripts/Installers/Install-CommonUtils.ps1
+++ images/win/scripts/Installers/Install-CommonUtils.ps1
@@ -2,12 +2,7 @@ $commonPackages = (Get-ToolsetContent).choco.common_packages
 
 foreach ($package in $commonPackages)
 {
-    Write-Host "Installing ${package.name}"
-    if ($package.args) {
-        Choco-Install -PackageName $package.name -ArgumentList $package.args
-    } else {
-        Choco-Install -PackageName $package.name
-    }
+    Choco-Install -PackageName $package.name -ArgumentList $package.args
 }
 
 Invoke-PesterTests -TestFile "ChocoPackages"
