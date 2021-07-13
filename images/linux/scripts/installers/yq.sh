df47f9dde3bb70bcdb4540b88fb3d816896d7164
Yann Jouanique <yann.jouanique@oneacrefund.org>
2021-07-13 22:18:56 +0200

    Get direct link from GH

--- images/linux/scripts/installers/yq.sh
+++ images/linux/scripts/installers/yq.sh
@@ -3,12 +3,11 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
 
-YQ_VERSION=$(curl -s https://api.github.com/repos/mikefarah/yq/releases/latest | jq -r '.tag_name')
 YQ_BINARY=yq_linux_amd64
 
 # As per https://github.com/mikefarah/yq#wget
-echo "Downloading yq version ${YQ_VERSION}"
-YQ_URL="https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz"
+YQ_URL=$(curl -s https://api.github.com/repos/mikefarah/yq/releases/latest | jq -r ".assets[].browser_download_url | select(endswith(\"$YQ_BINARY.tar.gz\"))")
+echo "Downloading latest yq from $YQ_URL"
 
 download_with_retries "$YQ_URL" "/tmp" "${YQ_BINARY}.tar.gz"
 tar xzf "/tmp/${YQ_BINARY}.tar.gz" -C "/tmp"
