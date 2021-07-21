6c974f86eaf148488930774dcc21df83950645f1
mikhailshaganov <mikhailshaganov@github.com>
2021-07-21 18:22:29 +0300

    fixes

--- images/linux/scripts/installers/kotlin.sh
+++ images/linux/scripts/installers/kotlin.sh
@@ -6,13 +6,13 @@
 
 source $HELPER_SCRIPTS/install.sh
 
-KOTLIN_ROOT="/usr/local"
+KOTLIN_ROOT="/usr/share"
 
 URL=$(curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest | jq -r '.assets[].browser_download_url | select(contains("kotlin-compiler"))')
 download_with_retries $URL "/tmp"
 
 unzip -qq /tmp/kotlin-compiler*.zip -d $KOTLIN_ROOT
 rm $KOTLIN_ROOT/kotlinc/bin/*.bat
-ln -sf $KOTLIN_ROOT/kotlinc/bin/* /usr/local/bin/
+ln -sf $KOTLIN_ROOT/kotlinc/bin/* /usr/bin
 
 invoke_tests "Tools" "Kotlin"
