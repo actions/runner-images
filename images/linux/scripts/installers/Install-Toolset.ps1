actions/runner-images

commit 52b67da7854c776a89c73356fb182a28d837b1ad

[Ubuntu] Remove hardcoded toolsToInstall from script (#3701)

images/linux/scripts/installers/Install-Toolset.ps1
@@ -28,9 +28,8 @@ $ErrorActionPreference = "Stop"
 
 # Get toolset content
 $toolset = Get-Content -Path "$env:INSTALLER_SCRIPT_FOLDER/toolset.json" -Raw
-$toolsToInstall = @("Python", "Node", "Go")
 
-$tools = ConvertFrom-Json -InputObject $toolset | Select-Object -ExpandProperty toolcache | Where-Object {$ToolsToInstall -contains $_.Name}
+$tools = ConvertFrom-Json -InputObject $toolset | Select-Object -ExpandProperty toolcache | Where-Object {$_.url -ne $null }
 
 foreach ($tool in $tools) {
     # Get versions manifest for current tool
@@ -51,8 +50,5 @@ foreach ($tool in $tools) {
             exit 1
         }
     }
+    chown -R "$($env:SUDO_USER):$($env:SUDO_USER)" /opt/hostedtoolcache/$tool
 }
-
-chown -R "$($env:SUDO_USER):$($env:SUDO_USER)" /opt/hostedtoolcache/Python
-chown -R "$($env:SUDO_USER):$($env:SUDO_USER)" /opt/hostedtoolcache/node
-chown -R "$($env:SUDO_USER):$($env:SUDO_USER)" /opt/hostedtoolcache/go

commit e75e41446195a264422a506a6b30b8ea51dd3b4b

fix: correct variable to print only tool name
it was broken in #3701

images/linux/scripts/installers/Install-Toolset.ps1
@@ -50,5 +50,5 @@ foreach ($tool in $tools) {
             exit 1
         }
     }
-    chown -R "$($env:SUDO_USER):$($env:SUDO_USER)" /opt/hostedtoolcache/$tool
+    chown -R "$($env:SUDO_USER):$($env:SUDO_USER)" "/opt/hostedtoolcache/$($tool.name)"
 }