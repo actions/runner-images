b2ff138387d499cd821b8a894f4f485bdebe04ec
Maxim Lobanov <v-malob@microsoft.com>
2021-07-27 17:09:19 +0000

    disable docker temporary

--- images/win/scripts/Installers/Install-Docker.ps1
+++ images/win/scripts/Installers/Install-Docker.ps1
@@ -5,11 +5,11 @@
 ##         can continue.
 ################################################################################
 
-Write-Host "Install-Package Docker"
-Install-Package -Name docker -ProviderName DockerMsftProvider -Force
-Start-Service docker
+# Write-Host "Install-Package Docker"
+# Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+# Start-Service docker
 
-Write-Host "Install-Package Docker-Compose"
-Choco-Install -PackageName docker-compose
+# Write-Host "Install-Package Docker-Compose"
+# Choco-Install -PackageName docker-compose
 
-Invoke-PesterTests -TestFile "Docker" -TestName "Docker"
+# Invoke-PesterTests -TestFile "Docker" -TestName "Docker"
