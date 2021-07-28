2fc1c7861efc34b1364cba9f46bfd02bad3e3f74
Maxim Lobanov <v-malob@microsoft.com>
2021-07-28 04:10:10 +0000

    disable net 48 installation

--- images/win/scripts/Installers/Install-NET48.ps1
+++ images/win/scripts/Installers/Install-NET48.ps1
@@ -8,6 +8,6 @@ $InstallerName = "ndp48-devpack-enu.exe"
 $InstallerUrl = "https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0307177e14752e359fde5423ab583e43/${InstallerName}"
 $ArgumentList = ("Setup", "/passive", "/norestart")
 
-Install-Binary -Url $InstallerUrl -Name $InstallerName -ArgumentList $ArgumentList
+# Install-Binary -Url $InstallerUrl -Name $InstallerName -ArgumentList $ArgumentList
 
-Invoke-PesterTests -TestFile "Tools" -TestName "NET48"
+# Invoke-PesterTests -TestFile "Tools" -TestName "NET48"
