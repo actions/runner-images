d6844d84793494b3ba147d6fda3bf7de5b0a6b06
Maxim Lobanov <v-malob@microsoft.com>
2021-07-28 05:50:54 +0000

    disable votice installation

--- images/win/scripts/Installers/Install-Wix.ps1
+++ images/win/scripts/Installers/Install-Wix.ps1
@@ -5,19 +5,24 @@
 
 Choco-Install -PackageName wixtoolset -ArgumentList "--force"
 
-if(Test-IsWin19)
+if (Test-IsWin19)
 {
     $extensionUrl = "https://wixtoolset.gallerycdn.vsassets.io/extensions/wixtoolset/wixtoolsetvisualstudio2019extension/1.0.0.4/1563296438961/Votive2019.vsix"
     $VSver = "2019"
 }
-else
+elseif (Test-IsWin16)
 {
     $extensionUrl = "https://robmensching.gallerycdn.vsassets.io/extensions/robmensching/wixtoolsetvisualstudio2017extension/0.9.21.62588/1494013210879/250616/4/Votive2017.vsix"
     $VSver = "2017"
 }
 
 $extensionName = "Votive$VSver.vsix"
-#Installing VS extension 'Wix Toolset Visual Studio Extension'
-Install-VsixExtension -Url $extensionUrl -Name $extensionName -VSversion $VSver
 
-Invoke-PesterTests -TestFile "Wix"
+if ($extensionUrl -and $VSver)
+{
+    #Installing VS extension 'Wix Toolset Visual Studio Extension'
+    Install-VsixExtension -Url $extensionUrl -Name $extensionName -VSversion $VSver
+
+    Invoke-PesterTests -TestFile "Wix"
+}
+
