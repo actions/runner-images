6186cddf8c152cd6c1802bba06a639457f1e817f
Mikhail Timofeev <v-mitim@microsoft.com>
2021-07-30 11:39:08 +0300

    Hardcode 0.14.1 as 0.15.0 is broken

--- images/macos/provision/core/rust.sh
+++ images/macos/provision/core/rust.sh
@@ -12,7 +12,8 @@ CARGO_HOME=$HOME/.cargo
 
 echo Install common tools...
 rustup component add rustfmt clippy
-cargo install --locked bindgen cbindgen cargo-audit cargo-outdated
+# Temporary hardcode cargo-audit 0.14.1 as 0.15.0 is broken https://docs.rs/crate/cargo-audit/0.15.0
+cargo install --locked bindgen cbindgen cargo-audit --version 0.14.1 cargo-outdated
 
 echo Cleanup Cargo registry cached data...
 rm -rf $CARGO_HOME/registry/*
