c2dece9fad0575f66431fd6e9dbce8dbb7ba80ff
Maxim Lobanov <v-malob@microsoft.com>
2021-08-02 13:03:21 +0000

    change order

--- images/win/scripts/Installers/Install-WindowsFeatures.ps1
+++ images/win/scripts/Installers/Install-WindowsFeatures.ps1
@@ -1,7 +1,9 @@
 # Install .NET Framework 3.5 (required by Chocolatey)
 # Explicitly install all 4.7 sub features to include ASP.Net.
 # As of  1/16/2019, WinServer 19 lists .Net 4.7 as NET-Framework-45-Features
+Write-Host "Enabling Windows Feature 'NET-Framework-Features'"
 Install-WindowsFeature -Name NET-Framework-Features -IncludeAllSubFeature
+Write-Host "Enabling Windows Feature 'NET-Framework-45-Features'"
 Install-WindowsFeature -Name NET-Framework-45-Features -IncludeAllSubFeature
 
 if (Test-IsWin16) {
@@ -18,7 +20,7 @@ if (Test-IsWin16 -or Test-IsWin19) {
         Write-Host "Failed to install FS-iSCSITarget-Server"
         exit 1
     }
-
-    Write-Host "Install Containers feature"
-    Install-WindowsFeature -Name Containers
 }
+
+Write-Host "Enabling Windows Feature 'Containers'"
+Install-WindowsFeature -Name Containers
