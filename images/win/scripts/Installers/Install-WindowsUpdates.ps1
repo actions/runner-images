e68a25e08f74d9374a78170d78e2b4d494ceb07b
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-08-25 21:35:28 +0300

    revert changes in Install-WindowsUpdates.ps1

--- images/win/scripts/Installers/Install-WindowsUpdates.ps1
+++ images/win/scripts/Installers/Install-WindowsUpdates.ps1
@@ -5,10 +5,4 @@
 ################################################################################
 
 Write-Host "Run windows updates"
-# KB5005030 causes Windows Server 2019 virtual machine issues with running tests
-if (Test-IsWin19) {
-    Get-WUInstall -MicrosoftUpdate
-    Write-Host "Hide update KB5005030"
-    Hide-WindowsUpdate -Confirm:$false -KBArticleID "KB5005030"
-}
 Get-WUInstall -MicrosoftUpdate -AcceptAll -Install -IgnoreUserInput -IgnoreReboot
