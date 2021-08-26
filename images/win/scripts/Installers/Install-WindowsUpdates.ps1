52807a632ad48ef89438dc7cadf85eec3784db55
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-08-26 09:33:34 +0300

    do not install KB5004870

--- images/win/scripts/Installers/Install-WindowsUpdates.ps1
+++ images/win/scripts/Installers/Install-WindowsUpdates.ps1
@@ -5,4 +5,10 @@
 ################################################################################
 
 Write-Host "Run windows updates"
+# KB5004870 causes Windows Server 2019 virtual machine to skip tests
+if (Test-IsWin19) {
+    Get-WUInstall -MicrosoftUpdate
+    Write-Host "Hide update KB5004870"
+    Hide-WindowsUpdate -Confirm:$false -KBArticleID "KB5004870"
+}
 Get-WUInstall -MicrosoftUpdate -AcceptAll -Install -IgnoreUserInput -IgnoreReboot
