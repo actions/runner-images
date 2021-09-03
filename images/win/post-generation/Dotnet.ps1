30c23255da69609846ea9b55c0ce22f166097cfe
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-09-03 13:21:44 +0300

    rename to $nugetConfigPath

--- images/win/post-generation/Dotnet.ps1
+++ images/win/post-generation/Dotnet.ps1
@@ -10,9 +10,9 @@ if (-not $latestPath.Contains($dotnetPath))
 # Delete empty nuget.config file created by choco and recreate the config using the 'dotnet nuget list source command'
 # before choco or powershell’s ancient embedded nuget does to prevent the issue with downloading packages from nuget.org
 # https://github.com/actions/virtual-environments/issues/3038
-$nugetPath = "$env:APPDATA\NuGet\NuGet.Config"
-if (Test-Path $nugetPath) 
+$nugetConfigPath = "$env:APPDATA\NuGet\NuGet.Config"
+if (Test-Path $nugetConfigPath) 
 {
-    Remove-Item $nugetPath -Force
+    Remove-Item $nugetConfigPath -Force
 }
 dotnet nuget list source
