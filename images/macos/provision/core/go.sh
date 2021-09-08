b8f2eaa6e5f1de2e4ecdc515530c3a63207bb8f9
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-09-08 17:36:18 +0300

    small changes

--- images/macos/provision/core/go.sh
+++ images/macos/provision/core/go.sh
@@ -3,9 +3,9 @@ source ~/utils/utils.sh
 
 DEFAULT_GO_VERSION=$(get_toolset_value '.go.default')
 echo "Installing Go..."
-brew_smart_install "$DEFAULT_GO_VERSION"
+brew_smart_install "go@${DEFAULT_GO_VERSION}"
 
 # Create symlinks for Go 1.15 to preserve backward compatibility
-ln -sf $(brew --prefix ${DEFAULT_GO_VERSION})/bin/* /usr/local/bin/
+ln -sf $(brew --prefix go@${DEFAULT_GO_VERSION})/bin/* /usr/local/bin/
 
 invoke_tests "Common" "Go"
