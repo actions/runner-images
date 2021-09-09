8d9046325608f83bfcd4cc4ba80595bd2594d50d
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-09-09 21:52:34 +0300

    change comment

--- images/macos/provision/core/golang.sh
+++ images/macos/provision/core/golang.sh
@@ -5,7 +5,7 @@ DEFAULT_GO_VERSION=$(get_toolset_value '.go.default')
 echo "Installing Go..."
 brew_smart_install "go@${DEFAULT_GO_VERSION}"
 
-# Create symlinks for Go 1.15 to preserve backward compatibility
+#Create symlinks to preserve backward compatibility. Symlinks are not created when non-latest go is being installed
 ln -sf $(brew --prefix go@${DEFAULT_GO_VERSION})/bin/* /usr/local/bin/
 
 invoke_tests "Common" "Go"
