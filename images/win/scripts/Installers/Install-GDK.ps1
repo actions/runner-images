96f6d1080692f06c8e1a80cdd9cefdfb69475a22
Maksim Shilov <maksim.shilov@akvelon.com>
2021-09-14 11:21:37 +0300

    Calling pester tests and fetching latest release instad of master

--- images/win/scripts/Installers/Install-GDK.ps1
+++ images/win/scripts/Installers/Install-GDK.ps1
@@ -3,10 +3,22 @@
 ##  Desc:  Install Microsoft Game Developement Kit
 ################################################################################
 
-#Cloning Microsoft GDK
-$repoFolder = "C:\GDK"
-git clone https://github.com/microsoft/GDK.git $repoFolder
+#Fetching Microsoft GDK release
+$tempFolder = "$env:windir\\Temp\\GDK"
 
+$releaseVersion = 'June_2021_Update_2'
+$GDKReleases = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/GDK/releases"
+$GDKRelease = $GDKReleases | Where-Object {$_.tag_name -ceq $releaseVersion}
+$GDKDownloadlink = $GDKRelease.zipball_url
+$GDKZipFilePath = "$($tempFolder)\GDK.Zip"
+New-Item -Path $tempFolder -ItemType Directory -Force | Out-Null
+Invoke-RestMethod -Uri $GDKDownloadlink -Method Get -ContentType "application/zip" -OutFile $GDKZipFilePath
+#Start-DownloadWithRetry -Url $GDKDownloadlink -Name "GDK.zip" -DownloadPath $tempFolder
+Expand-Archive -Path $GDKZipFilePath -DestinationPath $tempFolder
+$GDKSubFolder = (Get-ChildItem -Path $tempFolder -Directory).Name | Where-Object {$_ -like "microsoft-GDK*"}
+$GDKSubFolderPath = "$($tempFolder)\$($GDKSubFolder)"
 #Install GDK
-$GDKInstaller = "$($repoFolder)\pgdk.exe"
+$GDKInstaller = "$($GDKSubFolderPath)\pgdk.exe"
 & $GDKInstaller /quiet /norestart
+
+Invoke-PesterTests -Testfile "Tools" -TestName "Microsoft GDK"
