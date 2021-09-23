5d05ab991832b3492284b6279b148e75cc0da4ab
Maksim Shilov <maksim.shilov@akvelon.com>
2021-09-23 11:05:05 +0300

    Functions to common format

--- images/win/scripts/ImageHelpers/PathHelpers.ps1
+++ images/win/scripts/ImageHelpers/PathHelpers.ps1
@@ -1,19 +1,17 @@
 function Get-SystemVariable {
-    [CmdletBinding()]
     param(
         [string]$SystemVariable
     )
-    return [System.Environment]::GetEnvironmentVariable($SystemVariable, "Machine")
+    [System.Environment]::GetEnvironmentVariable($SystemVariable, "Machine")
 }
 
 function Set-SystemVariable {
-    [CmdletBinding()]
     param(
         [string]$SystemVariable,
         [string]$Value
     )
     [System.Environment]::SetEnvironmentVariable($SystemVariable, $Value, "Machine")
-    return Get-SystemVariable $SystemVariable
+    Get-SystemVariable $SystemVariable
 }
 
 function Get-MachinePath {
@@ -21,7 +19,6 @@ function Get-MachinePath {
 }
 
 function Set-MachinePath {
-    [CmdletBinding()]
     param(
         [string]$NewPath
     )
@@ -29,23 +26,20 @@ function Set-MachinePath {
 }
 
 function Test-MachinePath {
-    [CmdletBinding()]
     param(
         [string]$PathItem
     )
 
     $pathItems = (Get-MachinePath).Split(';')
-
-    return $pathItems.Contains($PathItem)
+    $pathItems.Contains($PathItem)
 }
 
 function Add-MachinePathItem {
-    [CmdletBinding()]
     param(
         [string]$PathItem
     )
 
     $currentPath = Get-MachinePath
     $newPath = $PathItem + ';' + $currentPath
-    return Set-MachinePath -NewPath $newPath
+    Set-MachinePath -NewPath $newPath
 }
