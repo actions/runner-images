94525ea396a6a552e78a5ac611d5810e64d775b6
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-09-29 09:32:13 +0300

    set docker-compose v1

--- images/linux/scripts/installers/docker-compose.sh
+++ images/linux/scripts/installers/docker-compose.sh
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Install latest docker-compose from releases
-URL=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.assets[].browser_download_url | select(endswith("docker-compose-Linux-x86_64"))')
+URL="https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64"
 curl -L $URL -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
