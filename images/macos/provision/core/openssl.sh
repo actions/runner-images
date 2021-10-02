4119a5acc83571fffef448ba0edd385aa5c34a13
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2021-10-02 13:08:07 +0300

    [MacOS] Restore openssl-1.1.x symlink (#4206)

--- images/macos/provision/core/openssl.sh
+++ images/macos/provision/core/openssl.sh
@@ -7,4 +7,7 @@ brew_smart_install "openssl@1.1"
 # Symlink brew openssl@1.1 to `/usr/local/bin` as Homebrew refuses
 ln -sf $(brew --prefix openssl@1.1)/bin/openssl /usr/local/bin/openssl
 
+# Most of buildsystems and scripts look up ssl here
+ln -sf $(brew --cellar openssl@1.1)/1.1* /usr/local/opt/openssl
+
 invoke_tests "OpenSSL"
