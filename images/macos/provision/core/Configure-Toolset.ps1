e17556e90f398c940d625373998cf3f26668c2b1
Nikita Bykov <v-nibyko@microsoft.com>
2021-10-04 09:57:55 +0300

    Add GOROOT env variables

---
+++ images/macos/provision/core/Configure-Toolset.ps1
@@ -0,0 +1,66 @@
+################################################################################
+##  File:  Configure-Toolset.ps1
+##  Team:  CI-Build
+##  Desc:  Configure toolset
+################################################################################
+
+Import-Module "~/image-generation/helpers/Common.Helpers.psm1"
+
+function Get-ToolsetToolFullPath
+{
+    param
+    (
+        [Parameter(Mandatory)] [string] $ToolName,
+        [Parameter(Mandatory)] [string] $ToolVersion,
+        [Parameter(Mandatory)] [string] $ToolArchitecture
+    )
+
+    $toolPath = Join-Path -Path $env:AGENT_TOOLSDIRECTORY -ChildPath $toolName
+    $toolPathVersion = Join-Path -Path $toolPath -ChildPath $toolVersion
+    $foundVersion = Get-Item $toolPathVersion | Sort-Object -Property {[version]$_.name} -Descending | Select-Object -First 1
+    $installationDir = Join-Path -Path $foundVersion -ChildPath $toolArchitecture
+    return $installationDir
+}
+
+function Add-EnvironmentVariable
+{
+    param
+    (
+        [Parameter(Mandatory)] [string] $Name,
+        [Parameter(Mandatory)] [string] $Value,
+        [string] $FilePath = "${env:HOME}/.bashrc"
+    )
+
+    $envVar = "{0}={1}" -f $name, $value
+    Tee-Object -InputObject $envVar -FilePath $filePath -Append
+}
+
+$toolsEnvironment = @{
+    go = @{
+        variableTemplate = "GOROOT_{0}_{1}_X64"
+    }
+}
+
+$toolset = Get-Content -Path "$env:HOME/image-generation/toolset.json" -Raw | ConvertFrom-Json
+
+foreach ($tool in $toolset.toolcache)
+{
+    $toolName = $tool.name
+    $toolArch = $tool.arch
+    $toolEnvironment = $toolsEnvironment[$toolName]
+
+    if (-not $toolEnvironment)
+    {
+        continue
+    }
+
+    foreach ($toolVersion in $tool.versions)
+    {
+        Write-Host "Set $toolName $toolVersion environment variable..."
+        $toolPath = Get-ToolsetToolFullPath -ToolName $toolName -ToolVersion $toolVersion -ToolArchitecture $toolArch
+        $envName = $toolEnvironment.variableTemplate -f $toolVersion.split(".")
+
+        # Add environment variable name=value
+        Add-EnvironmentVariable -Name $envName -Value $toolPath
+    }
+}
