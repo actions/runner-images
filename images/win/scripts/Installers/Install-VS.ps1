3a9506be6f1477e6039af807cf44c31678514ee0
josetr <37419832+josetr@users.noreply.github.com>
2021-10-04 20:41:46 +0100

    Initialize Visual Studio Experimental Instance on the correct user

--- images/win/scripts/Installers/Install-VS.ps1
+++ images/win/scripts/Installers/Install-VS.ps1
@@ -31,12 +31,6 @@ if ($instanceFolders -is [array])
     exit 1
 }
 
-$vsInstallRoot = (Get-VisualStudioInstance).InstallationPath
-
-# Initialize Visual Studio Experimental Instance
-# The Out-Null cmdlet is required to ensure PowerShell waits until the '/ResetSettings' command fully completes.
-& "$vsInstallRoot\Common7\IDE\devenv.exe" /RootSuffix Exp /ResetSettings General.vssettings /Command File.Exit | Out-Null
-
 # Updating content of MachineState.json file to disable autoupdate of VSIX extensions
 $newContent = '{"Extensions":[{"Key":"1e906ff5-9da8-4091-a299-5c253c55fdc9","Value":{"ShouldAutoUpdate":false}},{"Key":"Microsoft.VisualStudio.Web.AzureFunctions","Value":{"ShouldAutoUpdate":false}}],"ShouldAutoUpdate":false,"ShouldCheckForUpdates":false}'
 Set-Content -Path "$vsInstallRoot\Common7\IDE\Extensions\MachineState.json" -Value $newContent
