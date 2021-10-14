b4de222b8f20eb13fd6218d48db2d2f66991d62a
agorti <66666946+agorti@users.noreply.github.com>
2021-10-14 10:22:48 +0530

    fix(ubuntu): update JAR name

--- images/linux/scripts/installers/selenium.sh
+++ images/linux/scripts/installers/selenium.sh
@@ -8,10 +8,10 @@
 source $HELPER_SCRIPTS/install.sh
 
 # Download selenium standalone server
-SELENIUM_JAR_NAME="selenium-server-standalone.jar"
+SELENIUM_JAR_NAME="selenium-server.jar"
 SELENIUM_JAR_PATH="/usr/share/java"
 SELENIUM_LATEST_VERSION_URL="$(curl -s https://api.github.com/repos/SeleniumHQ/selenium/releases/latest |\
-    jq -r '.assets[].browser_download_url | select(contains("selenium-server-standalone") and endswith(".jar"))')"
+    jq -r '.assets[].browser_download_url | select(contains("selenium-server") and endswith(".jar"))')"
 download_with_retries $SELENIUM_LATEST_VERSION_URL $SELENIUM_JAR_PATH $SELENIUM_JAR_NAME
 
 # Add SELENIUM_JAR_PATH environment variable
