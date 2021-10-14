205cf6293d20fdde3e6c4b55db5708e9a10792ec
Aleksandr Chebotov <v-aleche@microsoft.com>
2021-10-14 15:37:46 +0300

    revert PR 3766

--- images/win/scripts/Installers/Install-Git.ps1
+++ images/win/scripts/Installers/Install-Git.ps1
@@ -9,16 +9,6 @@ Choco-Install -PackageName git -ArgumentList '--installargs="/VERYSILENT /NOREST
 # Install hub
 Choco-Install -PackageName hub
 
-# Install GVFS
-$url = "https://api.github.com/repos/microsoft/VFSForGit/releases/latest"
-[System.String] $gvfsLatest = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "SetupGVFS.+\.exe$"
-$gvfsInstallerPath = Start-DownloadWithRetry -Url $gvfsLatest -Name "SetupGVFS.exe"
-
-# Start-Process waits on the entire process tree but Wait-Process only waits on the initiating process(GVFS.Service.UI.exe)
-$env:GVFS_UNATTENDED = "1"
-$argList = "/VERYSILENT", "/NORESTART", "/NOCANCEL", "/SP-", "/CLOSEAPPLICATIONS", "/RESTARTAPPLICATIONS", "/SUPPRESSMSGBOXES"
-Start-Process $gvfsInstallerPath -ArgumentList $argList -PassThru | Wait-Process
-
 # Disable GCM machine-wide
 [Environment]::SetEnvironmentVariable("GCM_INTERACTIVE", "Never", [System.EnvironmentVariableTarget]::Machine)
 
