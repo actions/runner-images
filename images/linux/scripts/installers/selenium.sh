514a1b30d7e615a10fba2ab0835c2d3bbed460d4
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-10-14 15:52:44 +0300

    pin selenium version to 3.141.59

--- images/linux/scripts/installers/selenium.sh
+++ images/linux/scripts/installers/selenium.sh
@@ -7,11 +7,12 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
 
-# Download selenium standalone server
+# Temporarily download Selenium 3.141.59, since 4.* can contain some breaking changes
 SELENIUM_JAR_NAME="selenium-server-standalone.jar"
 SELENIUM_JAR_PATH="/usr/share/java"
-SELENIUM_LATEST_VERSION_URL="$(curl -s https://api.github.com/repos/SeleniumHQ/selenium/releases/latest |\
-    jq -r '.assets[].browser_download_url | select(contains("selenium-server-standalone") and endswith(".jar"))')"
+SELENIUM_LATEST_VERSION_URL="https://github.com/SeleniumHQ/selenium/releases/download/selenium-3.141.59/selenium-server-standalone-3.141.59.jar"
+#SELENIUM_LATEST_VERSION_URL="$(curl -s https://api.github.com/repos/SeleniumHQ/selenium/releases/latest |\
+#    jq -r '.assets[].browser_download_url | select(contains("selenium-server-standalone") and endswith(".jar"))')"
 download_with_retries $SELENIUM_LATEST_VERSION_URL $SELENIUM_JAR_PATH $SELENIUM_JAR_NAME
 
 # Add SELENIUM_JAR_PATH environment variable
