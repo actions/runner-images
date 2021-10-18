0affe2eb03a537266d6eb065aaa7938da753ab7b
Jared Szechy <jared@boldpenguin.com>
2021-10-18 19:06:03 -0400

    Add no doc to gems installs

--- images/macos/provision/core/rubygem.sh
+++ images/macos/provision/core/rubygem.sh
@@ -8,7 +8,7 @@ gemsToInstall=$(get_toolset_value '.ruby.rubygems | .[]')
 if [ -n "$gemsToInstall" ]; then
     for gem in $gemsToInstall; do
         echo "Installing gem $gem"
-        gem install $gem
+        gem install --no-document $gem
     done
 fi
 
