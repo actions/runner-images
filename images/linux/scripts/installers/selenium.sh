fb49886f7cc6667bd5356daf0cbfd45384ba1ccb
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-10-19 19:25:19 +0300

    debug

--- images/linux/scripts/installers/selenium.sh
+++ images/linux/scripts/installers/selenium.sh
@@ -10,9 +10,9 @@ source $HELPER_SCRIPTS/install.sh
 # Temporarily download Selenium 3.141.59, since 4.* can contain some breaking changes
 SELENIUM_MAJOR_VERSION=$(get_toolset_value '.selenium.version')
 SELENIUM_JAR_NAME="selenium-server-standalone.jar"
-SELENIUM_RELEASE_URL="https://api.github.com/repos/SeleniumHQ/selenium/releases?per_page=100"
 SELENIUM_JAR_PATH="/usr/share/java"
-tag_name=$(curl -s $SELENIUM_RELEASE_URL | jq -r ".[] | select(.prerelease==false).tag_name" | sort --unique --version-sort | grep "selenium-${SELENIUM_MAJOR_VERSION}.*" | tail -1)
+json=$(curl -s "https://api.github.com/repos/SeleniumHQ/selenium/releases?per_page=100")
+tag_name=$(echo $json | jq -r ".[] | select(.prerelease==false).tag_name" | sort --unique --version-sort | grep "selenium-${SELENIUM_MAJOR_VERSION}.*" | tail -1)
 SELENIUM_DOWNLOAD_URL=$(echo $json | jq -r ".[] | select(.tag_name==\"${tag_name}\").assets[].browser_download_url | select(contains(\"selenium-server-standalone\") and endswith(\".jar\"))")
 download_with_retries $SELENIUM_DOWNLOAD_URL $SELENIUM_JAR_PATH $SELENIUM_JAR_NAME
 
