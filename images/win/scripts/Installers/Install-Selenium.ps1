5c6cdfd390050fb83be8aeb92c107b61fcc83f03
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-10-19 20:01:22 +0300

    rework selenium installation

--- images/win/scripts/Installers/Install-Selenium.ps1
+++ images/win/scripts/Installers/Install-Selenium.ps1
@@ -9,11 +9,16 @@ $seleniumFileName = "selenium-server-standalone.jar"
 
 New-Item -ItemType directory -Path $seleniumDirectory
 
-# Temporarily download Selenium 3.141.59, since 4.* can contain some breaking changes
-#$url = "https://api.github.com/repos/SeleniumHQ/selenium/releases"
-#[System.String] $seleniumReleaseUrl = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "selenium-server-standalone-.+.jar"
+# Temporarily download Selenium 3.*, since 4.* can contain some breaking changes
+$seleniumMajorVersion = (Get-ToolsetContent).selenium.version
+$json = (Invoke-RestMethod -Uri "https://api.github.com/repos/SeleniumHQ/selenium/releases?per_page=100")
+$seleniumFullVersion = $json.Where{ -not $_.prerelease}.tag_name | `
+                             ForEach-Object {$_.Split("-")[1]} | `
+                             Sort-Object {[Version]$_} -Descending | `
+                             Where-Object {$_ -like "$seleniumMajorVersion.*"} | `
+                             Select-Object -First 1
+[System.String] $seleniumDownloadUrl = $json.assets.browser_download_url -match "selenium-server-standalone-$seleniumFullVersion.jar"
 
-$seleniumDownloadUrl = "https://github.com/SeleniumHQ/selenium/releases/download/selenium-3.141.59/selenium-server-standalone-3.141.59.jar"
 Start-DownloadWithRetry -Url $seleniumDownloadUrl -Name $seleniumFileName -DownloadPath $seleniumDirectory
 
 # Add SELENIUM_JAR_PATH environment variable
