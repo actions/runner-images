61476e1e82b0b86d969b02bdb4fdd32494d850cd
Mikhail Koliada <mikhailkoliada@github.com>
2021-10-28 12:43:56 +0300

    [Ubuntu] fix apt http headers and proxy settings
    More information on every option can be found
    in the man 5 apt.conf and man 1 apt-transport pages

--- images/linux/scripts/base/apt.sh
+++ images/linux/scripts/base/apt.sh
@@ -14,6 +14,13 @@ echo "APT::Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries
 # Configure apt to always assume Y
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
+# Fix bad proxy and http headers settings
+cat <<EOF >> /etc/apt/apt.conf.d/99bad_proxy
+Acquire::http::Pipeline-Depth 0;
+Acquire::http::No-Cache true;
+Acquire::BrokenProxy    true;
+EOF
+
 # Uninstall unattended-upgrades
 apt-get purge unattended-upgrades
 
