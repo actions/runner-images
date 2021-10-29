630e25ab40b6ee4f18843292b14098d4d9dd1610
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2021-10-29 17:29:28 +0300

    minor fix

--- images/macos/software-report/SoftwareReport.Browsers.psm1
+++ images/macos/software-report/SoftwareReport.Browsers.psm1
@@ -70,10 +70,6 @@ function Build-BrowserWebdriversEnvironmentTable {
         @{
             "Name" = "GECKOWEBDRIVER"
             "Value" = $env:GECKOWEBDRIVER
-        },
-        @{
-            "Name" = "SELENIUM_JAR_PATH"
-            "Value" = $env:SELENIUM_JAR_PATH
         }
     ) | ForEach-Object {
         [PSCustomObject] @{
