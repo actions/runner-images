6621c4098c9a5d127f394a7301acef4bb6a4576d
Mikhail Koliada <mikhailkoliada@github.com>
2021-11-02 16:33:16 +0300

    [Windows] make choco dummy call for currently installed version

--- images/win/post-generation/Choco.ps1
+++ images/win/post-generation/Choco.ps1
@@ -1,2 +1,5 @@
-# Step to avoid initial delay for choco scripts
-choco upgrade chocolatey
+# Step to avoid initial delay for choco scripts.
+# On calling the first choco command
+# Chocolatey spends 2 minutes cleaning up and removing unneeded packages in the environment.
+$ChocoVersion = choco --version
+choco upgrade chocolatey --version=$ChocoVersion
