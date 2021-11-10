1ea25c0832a8bf5e2aea190b16092996546ad9a1
Maksim Shilov <maksim.shilov@akvelon.com>
2021-11-10 23:57:21 +0300

    Defining version to install after brew tap, fixing version to install

--- images/macos/provision/core/mongodb.sh
+++ images/macos/provision/core/mongodb.sh
@@ -5,10 +5,10 @@ source ~/utils/utils.sh
 # Install latest release version of MongoDB Community Edition
 # https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x
 toolsetVersion=$(get_toolset_value '.mongodb.version')
-versionToInstall=$(brew search --formulae /mongodb-community@$toolsetVersion/ | awk -F'/' '{print $3}' | tail -1)
 
-echo "Installing mongodb $versionToInstall"
 brew tap mongodb/brew
+versionToInstall=$(brew search --formulae /mongodb-community@$toolsetVersion/ | awk -F'/' '{print $3}' | tail -1)
+echo "Installing mongodb $versionToInstall"
 brew_smart_install "$versionToInstall"
 
 invoke_tests "Databases" "MongoDB"
