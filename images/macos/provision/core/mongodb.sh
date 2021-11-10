dd326a5256c565403f0d8e1995cc29d745e87a48
Maksim Shilov <maksim.shilov@akvelon.com>
2021-11-10 10:23:06 +0300

    Define mongodb version installed in the toolset

--- images/macos/provision/core/mongodb.sh
+++ images/macos/provision/core/mongodb.sh
@@ -4,9 +4,10 @@ source ~/utils/utils.sh
 # MongoDB object-value database
 # Install latest release version of MongoDB Community Edition
 # https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x
+toolsetVersion=$(get_toolset_value '.mongodb.version')
 
-echo "Installing mongodb..."
+echo "Installing mongodb $toolsetVersion"
 brew tap mongodb/brew
-brew_smart_install "mongodb-community@5.0"
+brew_smart_install "mongodb-community@$toolsetVersion"
 
 invoke_tests "Databases" "MongoDB"
