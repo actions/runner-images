4bae8a0ebc0f021380603d26e8433a701b7da1d6
Maksim Shilov <maksim.shilov@akvelon.com>
2021-11-10 17:05:27 +0300

    Searching for latest version of a tool based on a toolset version

--- images/macos/provision/core/mongodb.sh
+++ images/macos/provision/core/mongodb.sh
@@ -5,9 +5,10 @@ source ~/utils/utils.sh
 # Install latest release version of MongoDB Community Edition
 # https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x
 toolsetVersion=$(get_toolset_value '.mongodb.version')
+versionToInstall=$(brew search --formulae /mongodb-community@$toolsetVersion/ | awk -F'/' '{print $3}' | tail -1)
 
-echo "Installing mongodb $toolsetVersion"
+echo "Installing mongodb $versionToInstall"
 brew tap mongodb/brew
-brew_smart_install "mongodb-community@$toolsetVersion"
+brew_smart_install "mongodb-community@$versionToInstall"
 
 invoke_tests "Databases" "MongoDB"
