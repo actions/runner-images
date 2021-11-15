2cd5c60cac9a3d2771af80f559fe5be9870cea81
Mikhail Koliada <mikhailkoliada@github.com>
2021-11-15 11:38:36 +0300

    Drop curly braces

--- images/win/scripts/Installers/Install-LLVM.ps1
+++ images/win/scripts/Installers/Install-LLVM.ps1
@@ -4,7 +4,7 @@
 ################################################################################
 
 $llvmVersion = (Get-ToolsetContent).llvm.version
-$latestChocoVersion = Get-LatestChocoPackageVersion -TargetVersion ${llvmVersion} -PackageName "llvm"
-Choco-Install -PackageName llvm -ArgumentList "--version ${latestChocoVersion}"
+$latestChocoVersion = Get-LatestChocoPackageVersion -TargetVersion $llvmVersion -PackageName "llvm"
+Choco-Install -PackageName llvm -ArgumentList "--version $latestChocoVersion"
 
 Invoke-PesterTests -TestFile "LLVM"
