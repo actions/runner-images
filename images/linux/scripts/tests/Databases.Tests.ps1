1b583e05e53be25d716f5707c48a22b17b039275
Maksim Shilov <89912354+shilovmaksim@users.noreply.github.com>
2021-11-16 11:48:38 +0300

    [Ubuntu] Pin PostgreSQL version installed in the toolset (#4510)
    * Pin PostgreSQL version installed in the toolset
    
    * Source the helpers and resolving comments

--- images/linux/scripts/tests/Databases.Tests.ps1
+++ images/linux/scripts/tests/Databases.Tests.ps1
@@ -9,11 +9,18 @@ Describe "MongoDB" {
 }
 
 Describe "PostgreSQL" {
-
     It "PostgreSQL Service" {
         "sudo systemctl start postgresql" | Should -ReturnZeroExitCode
         (Get-CommandResult "pg_isready").Output | Should -Be "/var/run/postgresql:5432 - accepting connections"
         "sudo systemctl stop postgresql" | Should -ReturnZeroExitCode 
+    }
+
+    It "PostgreSQL version should correspond to the version in the toolset" {
+        $toolsetVersion = (Get-ToolsetContent).postgresql.version
+        # Client version
+        (psql --version).split()[-1] | Should -BeLike "$toolsetVersion*"
+        # Server version
+        (pg_config --version).split()[-1] | Should -BeLike "$toolsetVersion*"
     }
 }
 
