a8efa83229fe886bfa656e45a14b4fffff6f21ad
Maxim Lobanov <maxim-lobanov@github.com>
2021-11-18 17:37:33 +0000

    Use Expand-Archive instead of Extract-7Zip since 7zip is not installed yet

--- images/win/scripts/Installers/Install-Docker.ps1
+++ images/win/scripts/Installers/Install-Docker.ps1
@@ -18,7 +18,7 @@ Write-Host "Install docker-wincred"
 $dockerCredLatestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/docker/docker-credential-helpers/releases/latest"
 $dockerCredDownloadUrl = $dockerCredLatestRelease.assets.browser_download_url -match "docker-credential-wincred-.+\.zip" | Select-Object -First 1
 $dockerCredArchive = Start-DownloadWithRetry -Url $dockerCredDownloadUrl
-Extract-7Zip -Path $dockerCredArchive -DestinationPath "C:\Program Files\Docker"
+Expand-Archive -Path $dockerCredArchive -DestinationPath "C:\Program Files\Docker"
 
 Write-Host "Download docker images"
 $dockerImages = (Get-ToolsetContent).docker.images
