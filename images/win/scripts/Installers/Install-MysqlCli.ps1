494d6f05a2a1bc79d7b7fefb564ee4d3b3ac997a
Mikhail Koliada <mikhailkoliada@github.com>
2021-11-25 14:55:24 +0300

    [Windows] pin mysql release version in toolset

--- images/win/scripts/Installers/Install-MysqlCli.ps1
+++ images/win/scripts/Installers/Install-MysqlCli.ps1
@@ -11,9 +11,9 @@ $ArgumentList = ("/install", "/quiet", "/norestart")
 Install-Binary -Url $InstallerURI -Name $InstallerName -ArgumentList $ArgumentList
 
 ## Downloading mysql
-$MysqlMajorMinor = (Get-ToolsetContent).Mysql.version
-$MysqlFullVersion = ((Invoke-WebRequest -Uri https://dev.mysql.com/downloads/mysql/${MysqlMajorMinor}.html).Content | Select-String -Pattern "${MysqlMajorMinor}\.\d+").Matches.Value
-$MysqlVersionUrl = "https://dev.mysql.com/get/Downloads/MySQL-${MysqlMajorMinor}/mysql-${MysqlFullVersion}-winx64.zip"
+$MysqlMajorVersion = (Get-ToolsetContent).mysql.major_version
+$MysqlFullVersion = (Get-ToolsetContent).mysql.full_version
+$MysqlVersionUrl = "https://dev.mysql.com/get/Downloads/MySQL-${MysqlMajorVersion}/mysql-${MysqlFullVersion}-winx64.zip"
 
 $MysqlArchPath = Start-DownloadWithRetry -Url $MysqlVersionUrl -Name "mysql.zip"
 
