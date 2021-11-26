af645a7b5e433a9fcc3ea1d6388896e0e3a8cad8
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2021-11-26 12:39:13 +0300

    [Ubuntu] get rid of post-gen rust script (#4575)

--- images/linux/scripts/installers/rust.sh
+++ images/linux/scripts/installers/rust.sh
@@ -7,8 +7,8 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/etc-environment.sh
 
-export RUSTUP_HOME=/usr/share/rust/.rustup
-export CARGO_HOME=/usr/share/rust/.cargo
+export RUSTUP_HOME=/etc/skel/.rustup
+export CARGO_HOME=/etc/skel/.cargo
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=stable --profile=minimal
 
@@ -19,19 +19,10 @@ source $CARGO_HOME/env
 rustup component add rustfmt clippy
 cargo install --locked bindgen cbindgen cargo-audit cargo-outdated
 
-# Permissions
-chmod -R 777 $(dirname $RUSTUP_HOME)
-
 # Cleanup Cargo cache
 rm -rf ${CARGO_HOME}/registry/*
 
 # Update /etc/environemnt
-prependEtcEnvironmentPath "${CARGO_HOME}/bin"
-
-# Rust Symlinks are added to a default profile /etc/skel
-pushd /etc/skel
-ln -sf $RUSTUP_HOME .rustup
-ln -sf $CARGO_HOME .cargo
-popd
+prependEtcEnvironmentPath '$HOME/.cargo/bin'
 
 invoke_tests "Tools" "Rust"
