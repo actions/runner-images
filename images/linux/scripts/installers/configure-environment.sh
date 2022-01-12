55b874650b60be0cae4f3e89d916d173201e570c
Aleksandr Chebotov <v-aleche@microsoft.com>
2022-01-12 16:45:36 +0300

    check daemon.conf config exists

--- images/linux/scripts/installers/configure-environment.sh
+++ images/linux/scripts/installers/configure-environment.sh
@@ -35,4 +35,7 @@ ln -s $HELPER_SCRIPTS/invoke-tests.sh /usr/local/bin/invoke_tests
 
 # Disable motd updates metadata
 sed -i 's/ENABLED=1/ENABLED=0/g' /etc/default/motd-news
-sed -i 's/UpdateMotd=true/UpdateMotd=false/g' /etc/fwupd/daemon.conf
+
+if [[ -f "/etc/fwupd/daemon.conf" ]]; then
+    sed -i 's/UpdateMotd=true/UpdateMotd=false/g' /etc/fwupd/daemon.conf
+fi
