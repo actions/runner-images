0cd88cf69498098f6d528247b59e8d7272c6e8b1
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2022-01-24 17:58:46 +0300

    resolve issue

--- images/macos/provision/utils/utils.sh
+++ images/macos/provision/utils/utils.sh
@@ -180,9 +180,9 @@ get_github_package_download_url() {
     local API_PAT=$5
     local SEARCH_IN_COUNT="100"
 
-    [ -n "$API_PAT" ] && authString="-H 'Authorization: token $API_PAT'"
+    [ -n "$API_PAT" ] && authString=(-H "Authorization: token ${API_PAT}")
 
-    json=$(curl $authString -s "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases?per_page=${SEARCH_IN_COUNT}")
+    json=$(curl "${authString[@]}" -s "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases?per_page=${SEARCH_IN_COUNT}")
 
     if [[ "$VERSION" == "latest" ]]; then
         tagName=$(echo $json | jq -r '.[] | select(.prerelease==false).tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]" | tail -1)
