de56a2c6dd8068e0624669b7859857d9faf68cf2
Dibir Magomedsaygitov <dibir-magomedsaygitov@github.com>
2022-01-26 11:00:20 +0300

    source the helpers for use with the script

--- images/linux/scripts/installers/cmake.sh
+++ images/linux/scripts/installers/cmake.sh
@@ -4,6 +4,9 @@
 ##  Desc:  Installs CMake
 ################################################################################
 
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/install.sh
+
 # Test to see if the software in question is already installed, if not install it
 echo "Checking to see if the installer script has already been run"
 if command -v cmake; then
