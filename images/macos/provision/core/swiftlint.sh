739dfc6d137c2c32b3083b5f5fba6619230ca85f
Mikhail Timofeev <48208649+miketimofeev@users.noreply.github.com>
2022-01-27 22:09:58 +0300

    Revert "Hardcode swiftlint 0.46.0 (#4953)"
    This reverts commit 736f2fa165d50ddfeb2bbc996a37d62d135ab37e.

--- images/macos/provision/core/swiftlint.sh
+++ images/macos/provision/core/swiftlint.sh
@@ -2,9 +2,7 @@
 source ~/utils/utils.sh
 
 echo "Install SwiftLint"
-# Temporary hardcode swiftlint version due to the issues with installer https://github.com/realm/SwiftLint/issues/3815
-swiftlintUrl="https://github.com/realm/SwiftLint/releases/download/0.46.0/SwiftLint.pkg"
-#swiftlintUrl=$(curl -H "Authorization: token $API_PAT" -s "https://api.github.com/repos/realm/SwiftLint/releases/latest" | jq -r '.assets[].browser_download_url | select(contains("SwiftLint.pkg"))')
+swiftlintUrl=$(curl -H "Authorization: token $API_PAT" -s "https://api.github.com/repos/realm/SwiftLint/releases/latest" | jq -r '.assets[].browser_download_url | select(contains("SwiftLint.pkg"))')
 download_with_retries $swiftlintUrl "/tmp" "SwiftLint.pkg"
 sudo installer -pkg /tmp/SwiftLint.pkg -target /
 rm -rf /tmp/SwiftLint.pkg
