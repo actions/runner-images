f658b6b77661171650f9a062f81f9616880d1879
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2022-01-31 18:02:24 +0300

    Update google-cloud-sdk.sh

--- images/linux/scripts/installers/google-cloud-sdk.sh
+++ images/linux/scripts/installers/google-cloud-sdk.sh
@@ -10,6 +10,7 @@ REPO_URL="https://packages.cloud.google.com/apt"
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] $REPO_URL cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update -y
+# temporary downgrade google-cloud-sdk as python component has linking bugs
 sudo apt-get install -y google-cloud-sdk=369.0.0-0
 
 # remove apt
