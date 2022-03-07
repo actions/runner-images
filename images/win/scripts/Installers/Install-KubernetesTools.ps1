0acccc0a502354e336d278d48651adafe1adfb85
Conrad Johnson <conradjohnsoniii@gmail.com>
2022-03-07 17:46:54 -0500

    Update Install-KubernetesTools.ps1
    Add regex $ to only match on value instead of the new sha256url that gets returns
    
    ```powershell
    $url = 'https://api.github.com/repos/kubernetes-sigs/kind/releases/latest'
    [System.String] $kindDownloadLink = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "kind-windows-amd64$"
    ```
    
    ***$kindDownloadLink*** returns 2 urls that match off **kind-windows-amd64**
    
    ```powershell
    https://github.com/kubernetes-sigs/kind/releases/download/v0.12.0/kind-windows-amd64 https://github.com/kubernetes-sigs/kind/releases/download/v0.12.0/kind-windows-amd64.sha256sum
    ```

--- images/win/scripts/Installers/Install-KubernetesTools.ps1
+++ images/win/scripts/Installers/Install-KubernetesTools.ps1
@@ -6,7 +6,7 @@
 Write-Host "Install Kind"
 # Choco installation can't be used because it depends on docker-desktop
 $url = 'https://api.github.com/repos/kubernetes-sigs/kind/releases/latest'
-[System.String] $kindDownloadLink = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "kind-windows-amd64"
+[System.String] $kindDownloadLink = (Invoke-RestMethod -Uri $url).assets.browser_download_url -match "kind-windows-amd64$"
 $destFilePath = "C:\ProgramData\kind"
 $null = New-Item -Path $destFilePath -ItemType Directory -Force
 Start-DownloadWithRetry -Url $kindDownloadLink -Name "kind.exe" -DownloadPath $destFilePath
