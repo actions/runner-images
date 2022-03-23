278a30023bf0440d692dddf2c562c59d664421bc
Igor Boskovic <igorboskovic3@github.com>
2022-03-23 16:20:35 +0100

    Update aliyun-cli.sh

--- images/linux/scripts/installers/aliyun-cli.sh
+++ images/linux/scripts/installers/aliyun-cli.sh
@@ -8,8 +8,8 @@
 source $HELPER_SCRIPTS/install.sh
 
 # Install Alibaba Cloud CLI
-# Hardcode the version until 3.0.113 or newer is properly released https://github.com/aliyun/aliyun-cli/issues/414
-downloadUrl=$(get_github_package_download_url "aliyun/aliyun-cli" "contains(\"aliyun-cli-linux\") and endswith(\"amd64.tgz\")" "v3.0.112")
+
+downloadUrl="https://aliyuncli.alicdn.com/aliyun-cli-linux-latest-amd64.tgz"
 download_with_retries $downloadUrl "/tmp"
 tar xzf /tmp/aliyun-cli-linux-*-amd64.tgz
 mv aliyun /usr/local/bin
