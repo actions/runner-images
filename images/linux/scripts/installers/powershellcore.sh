49974dde51b34c531f1c3c861ff91db949069948
Aleksandr Chebotov <v-aleche@microsoft.com>
2022-04-26 10:54:57 +0200

    Swap symlinks

--- images/linux/scripts/installers/powershellcore.sh
+++ images/linux/scripts/installers/powershellcore.sh
@@ -16,6 +16,10 @@ if isUbuntu22; then
     make test
     make install
 
+    # Create symlinks
+    ln -s /usr/local/ssl/lib/libssl.so.1.1 /lib/libssl.so.1.0.0
+    ln -s /usr/local/ssl/lib/libcrypto.so.1.1 /lib/libcrypto.so.1.0.0
+
     # make dummy deb pkg
     equivs-control libssl1.1.control
     sed -i 's/<package name; defaults to equivs-dummy>/libssl1.1/g' libssl1.1.control
@@ -25,10 +29,6 @@ if isUbuntu22; then
     wget https://github.com/PowerShell/PowerShell/releases/download/v7.2.2/powershell_7.2.2-1.deb_amd64.deb
     dpkg -i powershell_7.2.2-1.deb_amd64.deb
     rm -rf libssl1.1_1.0_all.deb powershell_7.2.2-1.deb_amd64.deb
-
-    # Create symlinks
-    ln -s /usr/local/ssl/lib/libssl.so.1.1 /lib/libssl.so.1.0.0
-    ln -s /usr/local/ssl/lib/libcrypto.so.1.1 /lib/libcrypto.so.1.0.0
 else
     # Install Powershell
     apt-get install -y powershell
