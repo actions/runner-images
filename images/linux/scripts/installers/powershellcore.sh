84ce5fb7719d3b26427899150364dd3537f85ed4
Aleksandr Chebotov <v-aleche@microsoft.com>
2022-04-26 09:35:53 +0200

    Install PowerShell Core 7.2.2 deb pkg

--- images/linux/scripts/installers/powershellcore.sh
+++ images/linux/scripts/installers/powershellcore.sh
@@ -4,5 +4,18 @@
 ##  Desc:  Installs powershellcore
 ################################################################################
 
+# Build openssl from src
+apt install build-essential checkinstall zlib1g-dev -y
+cd /usr/local/src/
+wget https://www.openssl.org/source/openssl-1.1.1n.tar.gz
+tar -xf openssl-1.1.1n.tar.gz
+cd openssl-1.1.1n
+./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
+make
+make test
+make install
+
 # Install Powershell
-apt-get install -y powershell
+#apt-get install -y powershell
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.2.2/powershell_7.2.2-1.deb_amd64.deb
+dpkg --ignore-depends=libssl1.1 -i powershell_7.2.2-1.deb_amd64.deb
