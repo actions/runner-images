2fc77f2a95674786bdb2a41a6e0dbb4586307dde
Aleksandr Chebotov <v-aleche@microsoft.com>
2022-04-26 16:03:20 +0200

    use download with retries

--- images/linux/scripts/installers/powershellcore.sh
+++ images/linux/scripts/installers/powershellcore.sh
@@ -8,14 +8,13 @@
 source $HELPER_SCRIPTS/os.sh
 
 if isUbuntu22; then
-    # Install libssl
-    wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.2_amd64.deb
-    dpkg -i libssl1.1_1.1.1l-1ubuntu1.2_amd64.deb
+    # Install libssl1.1
+    download_with_retries "http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.2_amd64.deb" "/tmp"
+    dpkg -i /tmp/libssl1.1_1.1.1l-1ubuntu1.2_amd64.deb
 
     # Install Powershell
-    wget https://github.com/PowerShell/PowerShell/releases/download/v7.2.2/powershell_7.2.2-1.deb_amd64.deb
-    dpkg -i powershell_7.2.2-1.deb_amd64.deb
-    rm -rf libssl1.1_1.1.1l-1ubuntu1.2_amd64.deb powershell_7.2.2-1.deb_amd64.deb
+    download_with_retries "https://github.com/PowerShell/PowerShell/releases/download/v7.2.2/powershell_7.2.2-1.deb_amd64.deb" "/tmp"
+    dpkg -i /tmp/powershell_7.2.2-1.deb_amd64.deb
 else
     # Install Powershell
     apt-get install -y powershell
