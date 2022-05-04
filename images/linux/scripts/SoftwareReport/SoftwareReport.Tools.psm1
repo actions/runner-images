dbc05aa991e736b45b9c915be27928584217f5e9
Dusko Dobranic <dusko.dobranic@akvelon.com>
2022-05-04 12:34:43 +0200

    Fix kubectl version report

--- images/linux/scripts/SoftwareReport/SoftwareReport.Tools.psm1
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Tools.psm1
@@ -142,7 +142,7 @@ function Get-KindVersion {
 }
 
 function Get-KubectlVersion {
-    $kubectlVersion = kubectl version --client --short | Take-OutputPart -Part 2 | Take-OutputPart -Part 0 -Delimiter "v"
+    $kubectlVersion = kubectl version --client --short | Select-Object -Last 2 | Select-Object -First 1 | Take-OutputPart -Part 2 | Take-OutputPart -Part 0 -Delimiter "v"
     return "Kubectl $kubectlVersion"
 }
 
