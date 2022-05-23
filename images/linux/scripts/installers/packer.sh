6be651d72f38d3b6b8933d731ea72d5f30fb65e4
Shohei Maeda <11495867+smaeda-ks@users.noreply.github.com>
2022-05-23 16:53:25 +0900

    Use helper function

--- images/linux/scripts/installers/packer.sh
+++ images/linux/scripts/installers/packer.sh
@@ -4,10 +4,13 @@
 ##  Desc:  Installs packer
 ################################################################################
 
+source $HELPER_SCRIPTS/install.sh
+
 # Install Packer
 URL=$(curl -s https://api.releases.hashicorp.com/v1/releases/packer/latest | jq -r '.builds[] | select((.arch=="amd64") and (.os=="linux")).url')
-curl -L -o packer_linux_amd64.zip "${URL}"
-unzip -qq packer_linux_amd64.zip -d /usr/local/bin
-rm -f packer_linux_amd64.zip
+ZIP_NAME="packer_linux_amd64.zip"
+download_with_retries "${URL}" "/tmp" "${ZIP_NAME}"
+unzip -qq "/tmp/${ZIP_NAME}" -d /usr/local/bin
+rm -f "/tmp/${ZIP_NAME}"
 
 invoke_tests "Tools" "Packer"
--- images/linux/scripts/installers/terraform.sh
+++ images/linux/scripts/installers/terraform.sh
@@ -4,10 +4,13 @@
 ##  Desc:  Installs terraform
 ################################################################################
 
+source $HELPER_SCRIPTS/install.sh
+
 # Install Terraform
 URL=$(curl -s https://api.releases.hashicorp.com/v1/releases/terraform/latest | jq -r '.builds[] | select((.arch=="amd64") and (.os=="linux")).url')
-curl -L -o terraform_linux_amd64.zip "${URL}"
-unzip -qq terraform_linux_amd64.zip -d /usr/local/bin
-rm -f terraform_linux_amd64.zip
+ZIP_NAME="terraform_linux_amd64.zip"
+download_with_retries "${URL}" "/tmp" "${ZIP_NAME}"
+unzip -qq "/tmp/${ZIP_NAME}" -d /usr/local/bin
+rm -f "/tmp/${ZIP_NAME}"
 
 invoke_tests "Tools" "Terraform"
