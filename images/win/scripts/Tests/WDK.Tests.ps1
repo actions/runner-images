16f6912f370f47c8ecb000226c66c941e092fea3
Aleksandr Chebotov <47745270+al-cheb@users.noreply.github.com>
2022-06-01 15:13:31 +0200

    Add WDK on Windows Server 2022 (#5652)

--- images/win/scripts/Tests/WDK.Tests.ps1
+++ images/win/scripts/Tests/WDK.Tests.ps1
@@ -1,6 +1,8 @@
-Describe "WDK" -Skip:(Test-IsWin22) {
+Describe "WDK" {
     It "WDK exists" {
-      $WDKVersion = (Get-CimInstance -ClassName Win32_Product -Filter "Name = 'Windows Driver Kit'").Version
+      $regKey = "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+      $installedApplications = Get-ItemProperty -Path $regKey
+      $WDKVersion = $installedApplications | Where-Object DisplayName -eq 'Windows Driver Kit' | Select-Object -First 1 -ExpandProperty DisplayVersion
       $WDKVersion| Should -Not -BeNullOrEmpty
     }
 
