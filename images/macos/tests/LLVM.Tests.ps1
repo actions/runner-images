d403dd36aeae6c86c7fa0d1b9d52e37a235d7577
Aleksandr Chebotov <v-aleche@microsoft.com>
2022-07-05 13:13:31 +0200

    Update LLVM to version 14

--- images/macos/tests/LLVM.Tests.ps1
+++ images/macos/tests/LLVM.Tests.ps1
@@ -1,8 +1,9 @@
-$os = Get-OSVersion
-
 Describe "Clang/LLVM" {
-    It "Clang/LLVM is installed and version is correct" {
+    BeforeAll {
         $toolsetVersion = Get-ToolsetValue 'llvm.version'
+    }
+
+    It "Clang/LLVM <toolsetVersion> is installed and version is correct" {
         $clangVersion = & "$(brew --prefix llvm@$toolsetVersion)/bin/clang" --version
         $clangVersion[0] | Should -BeLike "*${toolsetVersion}*"
     }
