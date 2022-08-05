99da9e2492e84b0c70897c55424cf5a8d87feebb
Aleksandr Chebotov <v-aleche@microsoft.com>
2022-08-05 09:08:48 +0200

    Disable mongodb service by default

--- images/win/scripts/Installers/Install-MongoDB.ps1
+++ images/win/scripts/Installers/Install-MongoDB.ps1
@@ -3,11 +3,19 @@
 ##  Desc:  Install MongoDB
 ####################################################################################
 
+# Install mongodb package
 $toolsetVersion = (Get-ToolsetContent).mongodb.version
 $latestChocoPackage = Get-LatestChocoPackageVersion -TargetVersion $toolsetVersion -PackageName "mongodb"
 Choco-Install -PackageName mongodb -ArgumentList "--version=$latestChocoPackage"
-$mongoPath = (Get-CimInstance Win32_Service -Filter "Name LIKE 'mongodb'").PathName
+
+# Add mongodb to the PATH
+$mongodbService = "mongodb"
+$mongoPath = (Get-CimInstance Win32_Service -Filter "Name LIKE '$mongodbService'").PathName
 $mongoBin = Split-Path -Path $mongoPath.split('"')[1]
 Add-MachinePathItem "$mongoBin"
+
+# Stop and disable mongodb service
+Stop-Service -Name $mongodbService
+Set-Service $mongodbService -StartupType Disabled
 
 Invoke-PesterTests -TestFile "Databases" -TestName "MongoDB"
