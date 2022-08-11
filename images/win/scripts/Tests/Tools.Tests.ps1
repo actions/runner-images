f8b81213ae21de002d0965429b060ed68af3d560
Alexis Saettler <alexis@saettler.org>
2022-08-11 16:17:36 +0200

    [Windows] re-add vcredist_140 test

--- images/win/scripts/Tests/Tools.Tests.ps1
+++ images/win/scripts/Tests/Tools.Tests.ps1
@@ -160,11 +160,10 @@ Describe "Vcpkg" {
 }
 
 Describe "VCRedist" -Skip:(Test-IsWin22) {
-    It "vcredist_2010_x64" {
-        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{1D8E6291-B0D5-35EC-8441-6616F567A0F7}" | Should -Exist
-        "C:\Windows\System32\msvcr100.dll" | Should -Exist
+    It "vcredist_140" -Skip:(Test-IsWin19) {
+        "C:\Windows\System32\vcruntime140.dll" | Should -Exist
     }
-
+    
     It "vcredist_2010_x64" {
         "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{1D8E6291-B0D5-35EC-8441-6616F567A0F7}" | Should -Exist
         "C:\Windows\System32\msvcr100.dll" | Should -Exist
