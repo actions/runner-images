1c49c133eaac06d51a86621916e9bea71ee855d6
Alexis Saettler <alexis@saettler.org>
2022-08-11 16:45:40 +0200

    [Windows] remove vcredist_2010_x64 duplicate test (#6060)
    * [Windows] re-add vcredist_140 test
    
    * remove vcredist_140 test

--- images/win/scripts/Tests/Tools.Tests.ps1
+++ images/win/scripts/Tests/Tools.Tests.ps1
@@ -164,11 +164,6 @@ Describe "VCRedist" -Skip:(Test-IsWin22) {
         "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{1D8E6291-B0D5-35EC-8441-6616F567A0F7}" | Should -Exist
         "C:\Windows\System32\msvcr100.dll" | Should -Exist
     }
-
-    It "vcredist_2010_x64" {
-        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{1D8E6291-B0D5-35EC-8441-6616F567A0F7}" | Should -Exist
-        "C:\Windows\System32\msvcr100.dll" | Should -Exist
-    }
 }
 
 Describe "WebPlatformInstaller" {
