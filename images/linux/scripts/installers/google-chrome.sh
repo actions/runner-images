f1abfa5fdef67ed2f06e93cea1fb01c0b44c1dcf
Aleksandr Chebotov <47745270+al-cheb@users.noreply.github.com>
2022-09-05 12:54:06 +0200

    Fix chromium revision parsing (#6182)

--- images/linux/scripts/installers/google-chrome.sh
+++ images/linux/scripts/installers/google-chrome.sh
@@ -17,7 +17,7 @@ function GetChromiumRevision {
     # Some Google Chrome versions are based on Chromium revisions for which a (usually very old) Chromium release with the same number exist. So far this has heppened with 4 digits long Chromium revisions (1060, 1086).
     # Use the previous Chromium release when this happens to avoid downloading and installing very old Chromium releases that would break image build because of incompatibilities.
     # First reported with: https://github.com/actions/runner-images/issues/5256
-    if [ ${#REVISION} -eq 4 ]; then
+    if [ ${#REVISION} -le 4 ]; then
       CURRENT_REVISIONS=$(curl -s "https://omahaproxy.appspot.com/all.json?os=linux&channel=stable")
       PREVIOUS_VERSION=$(echo "$CURRENT_REVISIONS" | jq -r '.[].versions[].previous_version')
       URL="https://omahaproxy.appspot.com/deps.json?version=${PREVIOUS_VERSION}"
