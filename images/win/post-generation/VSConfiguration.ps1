6fc60e2cfde27f44ec61a2dd83f85cc314a71437
Daniel Cazzulino <daniel@cazzulino.com>
2022-09-23 10:35:09 -0300

    No need to run /updateconfiguration since MEF is already init
    The previous `/updateconfiguration` became a lazy-init at some point in VSMEF.
    
    Since we now open a code file in the editor, the MEF initialization is already happening 
    on the devenv launch step, so there's no need to *also* run `/updateconfiguration`.

--- images/win/post-generation/VSConfiguration.ps1
+++ images/win/post-generation/VSConfiguration.ps1
@@ -5,6 +5,5 @@ $devEnvPath = "$vsInstallRoot\Common7\IDE\devenv.exe"
 # The Out-Null cmdlet is required to ensure PowerShell waits until the '/ResetSettings' command fully completes.
 # The temp/empty C# file is required to ensure VS MEF is fully initialized
 Set-Content -Path "$env:TEMP\temp.cs" -Value "//"
-& "$devEnvPath" "$env:TEMP\temp.cs" /RootSuffix Exp /NoSplash /ResetSettings General.vssettings /Command File.Exit | Out-Null
 
-cmd.exe /c "`"$devEnvPath`" /updateconfiguration"
+& "$devEnvPath" "$env:TEMP\temp.cs" /RootSuffix Exp /NoSplash /ResetSettings General.vssettings /Command File.Exit | Out-Null
