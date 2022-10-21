b50a44be68502ec3961b4ba3a430d4d62be30cc6
Erik Bershel <110455084+erik-bershel@users.noreply.github.com>
2022-10-21 19:03:47 +0200

    Revert VCPKG_ROOT variable

--- images/linux/scripts/installers/vcpkg.sh
+++ images/linux/scripts/installers/vcpkg.sh
@@ -4,17 +4,16 @@
 ##  Desc:  Installs vcpkg
 ################################################################################
 
-# Set env variables for vcpkg
-VCPKG_ROOT=/usr/local/share/vcpkg
-echo "VCPKG_INSTALLATION_ROOT=${VCPKG_ROOT}" | tee -a /etc/environment
-echo "VCPKG_ROOT=${VCPKG_ROOT}" | tee -a /etc/environment
+# Set env variable for vcpkg
+VCPKG_INSTALLATION_ROOT=/usr/local/share/vcpkg
+echo "VCPKG_INSTALLATION_ROOT=${VCPKG_INSTALLATION_ROOT}" | tee -a /etc/environment
 
 # Install vcpkg
-git clone https://github.com/Microsoft/vcpkg $VCPKG_ROOT
+git clone https://github.com/Microsoft/vcpkg $VCPKG_INSTALLATION_ROOT
 
-$VCPKG_ROOT/bootstrap-vcpkg.sh
-$VCPKG_ROOT/vcpkg integrate install
-chmod 0777 -R $VCPKG_ROOT
-ln -sf $VCPKG_ROOT/vcpkg /usr/local/bin
+$VCPKG_INSTALLATION_ROOT/bootstrap-vcpkg.sh
+$VCPKG_INSTALLATION_ROOT/vcpkg integrate install
+chmod 0777 -R $VCPKG_INSTALLATION_ROOT
+ln -sf $VCPKG_INSTALLATION_ROOT/vcpkg /usr/local/bin
 
 invoke_tests "Tools" "Vcpkg"
