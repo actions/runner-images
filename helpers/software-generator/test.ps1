4fdf254ad34aa2f510dfd062e93f0de985c33d12
Maxim Lobanov <maxim-lobanov@github.com>
2022-12-01 17:28:59 +0000

    update test files

--- helpers/software-generator/test.ps1
+++ helpers/software-generator/test.ps1
@@ -3,9 +3,12 @@ using module ./SoftwareReport.Comparer.psm1
 Import-Module $(Join-Path $PSScriptRoot "SoftwareReport.Tools.psm1") -Force
 
 $ErrorActionPreference = "Stop"
+$global:ErrorView = "NormalView"
 
 $jsonReport = Get-Content -Path $(Join-Path $PSScriptRoot "macos-11-DEMO_OUT.json") -Raw
 $parsedReport = [SoftwareReport]::FromJson($jsonReport)
+
+$parsedReport.ToJson() | Out-File -FilePath $(Join-Path $PSScriptRoot "TEST.json") -Force
 
 $markdownReport = $parsedReport.ToMarkdown()
 
