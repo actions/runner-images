b41b925839e9bf2a7d02c0955bf652907f25f5d9
Alexey Ayupov <Alexey-Ayupov@github.com>
2022-12-19 10:11:11 +0100

    [Windows] Update postgresql to version 15

--- images/win/scripts/Installers/Install-PostgreSQL.ps1
+++ images/win/scripts/Installers/Install-PostgreSQL.ps1
@@ -8,10 +8,37 @@ Set-SystemVariable -SystemVariable PGPASSWORD -Value $pgPwd
 
 # Define latest available version to install based on version specified in the toolset
 $toolsetVersion = (Get-ToolsetContent).postgresql.version
-$latestChocoPackage = Get-LatestChocoPackageVersion -TargetVersion $toolsetVersion -PackageName "postgresql"
+#$latestChocoPackage = Get-LatestChocoPackageVersion -TargetVersion $toolsetVersion -PackageName "postgresql"
+$getPostgreReleases =  Invoke-WebRequest -Uri "https://git.postgresql.org/gitweb/?p=postgresql.git;a=tags" -UseBasicParsing 
+$TargetReleases = $getPostgreReleases.Links.href | Where-Object {$_ -match "a=log;h=refs/tags/REL_$toolsetVersion"}
+[Int32]$OutNumber = $null
+$MinorVersions = @()
+foreach ($release in $TargetReleases) {
+  $version = $release.split('/')[-1]
+  if ([Int32]::TryParse($($version.Split('_')[-1]),[ref]$OutNumber)){
+    $MinorVersions += $OutNumber
+  }
+}
+$TargetMinorVersions = ($MinorVersions | Sort-Object)[-1]
 
 # Install latest PostgreSQL
-Choco-Install -PackageName postgresql -ArgumentList "--params", "'/Password:$pgPwd /NoPath'", "--params-global", "--version=$latestChocoPackage"
+#Choco-Install -PackageName postgresql -ArgumentList "-i", "--params", "'/Password:$pgPwd /NoPath'", "--params-global", "--version=$latestChocoPackage"
+$ErrorActionOldValue = $ErrorActionPreference
+$ErrorActionPreference = 'SilentlyContinue'
+foreach ($increment in 9..0) {
+    $url = "https://get.enterprisedb.com/postgresql/postgresql-$toolsetVersion.$TargetMinorVersions-$increment-windows-x64.exe"
+    $checkaccess = [System.Net.WebRequest]::Create($url)
+    $response =$null
+    $response = $checkaccess.GetResponse()
+    if ($response) {
+        $InstallerUrl =  $response.ResponseUri.OriginalString
+        break
+    }
+}
+$ErrorActionPreference = $ErrorActionOldValue
+$InstallerName = $InstallerUrl.Split('/')[-1] 
+$ArgumentList = ("--install_runtimes 0","--superpassword root","--enable_acledit 1","--unattendedmodeui none","--mode unattended")
+Install-Binary -Url $InstallerUrl -Name $InstallerName -ArgumentList $ArgumentList
 
 # Get Path to pg_ctl.exe
 $pgPath = (Get-CimInstance Win32_Service -Filter "Name LIKE 'postgresql-%'").PathName
