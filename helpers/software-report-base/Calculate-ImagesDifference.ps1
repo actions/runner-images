4cd1b147d7a0dda3520a9d47e7a6e28deb83bbc3
Maxim Lobanov <maxim-lobanov@github.com>
2022-12-22 11:45:15 +0100

    Update Calculate-ImagesDifference.ps1

--- helpers/software-report-base/Calculate-ImagesDifference.ps1
+++ helpers/software-report-base/Calculate-ImagesDifference.ps1
@@ -54,7 +54,7 @@ $comparer = [SoftwareReportDifferenceCalculator]::new($previousReport, $currentR
 $comparer.CompareReports()
 $diff = $comparer.GetMarkdownReport()
 
-if ($ReleaseBranch -and $ReadmePath) {
+if ($ReleaseBranchName -and $ReadmePath) {
     # https://github.com/actions/runner-images/blob/releases/macOS-12/20221215/images/macos/macos-12-Readme.md
     $ImageDocsUrl = "https://github.com/actions/runner-images/blob/${ReleaseBranchName}/${ReadmePath}"
     $diff += "`n`n`nFor comprehensive list of software installed on this image please click [here]($ImageDocsUrl)."
