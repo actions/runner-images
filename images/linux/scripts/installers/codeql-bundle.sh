ad83c3400b4a2cc4ea8fc93ac2f03e3cd6b022ef
Angela P Wen <angelapwen@github.com>
2023-01-06 10:48:29 -0800

    Address review comments

--- images/linux/scripts/installers/codeql-bundle.sh
+++ images/linux/scripts/installers/codeql-bundle.sh
@@ -8,17 +8,17 @@ source "$HELPER_SCRIPTS/install.sh"
 
 # Retrieve the name of the CodeQL bundle preferred by the Action (in the format codeql-bundle-YYYYMMDD).
 defaults="$(curl -sSL https://raw.githubusercontent.com/github/codeql-action/v2/src/defaults.json)"
-codeql_bundle_name="$(${defaults} | jq -r .bundleVersion)"
-codeql_cli_version="$(${defaults} | jq -r .cliVersion)"
-prior_codeql_bundle_name="$(${defaults} | jq -r .priorBundleVersion)"
-prior_codeql_cli_version="$(${defaults} | jq -r .priorCliVersion)"
+codeql_tag_name=${jq -r '.bundleVersion' <<< "${defaults}"}
+codeql_cli_version=${jq -r '.cliVersion' <<< "${defaults}"}
+prior_codeql_tag_name=${jq -r '.priorBundleVersion' <<< "${defaults}"}
+prior_codeql_cli_version=${jq -r '.priorCliVersion' <<< "${defaults}"}
 
-# Convert the bundle names to bundles with a version number (x.y.z-YYYYMMDD).
-codeql_bundle_version="${codeql_cli_version}-${codeql_bundle_name##*-}"
-prior_codeql_bundle_version="${prior_codeql_cli_version}-${prior_codeql_bundle_name##*-}"
+# Convert the tag names to bundles with a version number (x.y.z-YYYYMMDD).
+codeql_bundle_version="${codeql_cli_version}-${codeql_tag_name##*-}"
+prior_codeql_bundle_version="${prior_codeql_cli_version}-${prior_codeql_tag_name##*-}"
 
 # For both of the bundle versions, download and name appropriately.
-for bundle in "${codeql_bundle_version} ${codeql_bundle_name}" "${prior_codeql_bundle_version} ${prior_codeql_bundle_name}"
+for bundle in "${codeql_bundle_version} ${codeql_tag_name}" "${prior_codeql_bundle_version} ${prior_codeql_tag_name}"
 do
     set -- "$bundle"
 
@@ -31,9 +31,6 @@ do
 
     echo "Unpacking the downloaded CodeQL bundle archive..."
     tar -xzf "$codeqlArchive" -C "$codeqlToolcachePath"
-
-    # Touch a special file that indicates to the CodeQL Action that this bundle was baked-in to the hosted runner images.
-    touch "$codeqlToolcachePath/pinned-version"
 
     # Touch a file to indicate to the toolcache that setting up CodeQL is complete.
     touch "$codeqlToolcachePath.complete"
