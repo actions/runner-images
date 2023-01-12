7c5093b0ba7b0ced01591ad64e3d9bed6f8adfbb
Alexey Ayupov <Alexey-Ayupov@github.com>
2023-01-12 20:14:37 +0100

    Disable Pester test colors

--- images/win/scripts/ImageHelpers/TestsHelpers.ps1
+++ images/win/scripts/ImageHelpers/TestsHelpers.ps1
@@ -48,7 +48,7 @@ function Invoke-PesterTests {
 
     $configuration = [PesterConfiguration] @{
         Run = @{ Path = $testPath; PassThru = $true }
-        Output = @{ Verbosity = "Detailed" }
+        Output = @{ Verbosity = "Detailed"; RenderMode = "Plaintext"}
     }
     if ($TestName) {
         $configuration.Filter.FullName = $TestName
