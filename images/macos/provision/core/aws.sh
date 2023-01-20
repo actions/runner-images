a29c32ce2e1a1d2c4972871af76fef8ba602fe2a
Rui Chen <rui@chenrui.dev>
2023-01-19 20:59:07 -0500

    macos: installing awscli, aws-sam-cli via brew

--- images/macos/provision/core/aws.sh
+++ images/macos/provision/core/aws.sh
@@ -2,12 +2,9 @@
 source ~/utils/utils.sh
 
 echo Installing aws...
-AWS_CLI_URL="https://awscli.amazonaws.com/AWSCLIV2.pkg"
-download_with_retries $AWS_CLI_URL "/tmp"
-sudo installer -pkg /tmp/AWSCLIV2.pkg -target /
+brew install awscli
 
 echo Installing aws sam cli...
-brew tap aws/tap
 brew install aws-sam-cli
 
 echo "Install aws cli session manager"
