777c3f9135cb5e6f30619ea13cefdccc97ea23ea
Maxim Lobanov <maxim-lobanov@github.com>
2023-02-02 20:37:38 +0000

    return all scripts back

--- images/macos/provision/core/commonutils.sh
+++ images/macos/provision/core/commonutils.sh
@@ -26,69 +26,69 @@ for package in $cask_packages; do
 done
 
 # Load "Parallels International GmbH"
-#if is_Monterey; then
-#    sudo kextload /Applications/Parallels\ Desktop.app/Contents/Library/Extensions/10.9/prl_hypervisor.kext || true
-#fi
+if is_Monterey; then
+    sudo kextload /Applications/Parallels\ Desktop.app/Contents/Library/Extensions/10.9/prl_hypervisor.kext || true
+fi
 
 # Execute AppleScript to change security preferences
 # System Preferences -> Security & Privacy -> General -> Unlock -> Allow -> Not now
-# if is_Monterey; then
-#     if is_Veertu; then
-#         retry=10
-#         while [ $retry -gt 0 ]; do
-#             {
-#                 osascript -e 'tell application "System Events" to get application processes where visible is true'
-#             } && break
+if is_Monterey; then
+    if is_Veertu; then
+        retry=10
+        while [ $retry -gt 0 ]; do
+            {
+                osascript -e 'tell application "System Events" to get application processes where visible is true'
+            } && break
 
-#             retry=$((retry-1))
-#             if [ $retry -eq 0 ]; then
-#                 echo "No retry attempts left"
-#                 exit 1
-#             fi
-#             sleep 10
-#         done
-#     fi
+            retry=$((retry-1))
+            if [ $retry -eq 0 ]; then
+                echo "No retry attempts left"
+                exit 1
+            fi
+            sleep 10
+        done
+    fi
 
-#     osascript $HOME/utils/confirm-identified-developers.scpt $USER_PASSWORD
-# fi
+    osascript $HOME/utils/confirm-identified-developers.scpt $USER_PASSWORD
+fi
 
 # Validate "Parallels International GmbH" kext
-# if is_Monterey; then
-#     echo "Closing System Preferences window if it is still opened"
-#     killall "System Preferences" || true
+if is_Monterey; then
+    echo "Closing System Preferences window if it is still opened"
+    killall "System Preferences" || true
 
-#     echo "Checking parallels kexts"
-#     dbName="/var/db/SystemPolicyConfiguration/KextPolicy"
-#     dbQuery="SELECT * FROM kext_policy WHERE bundle_id LIKE 'com.parallels.kext.%';"
-#     kext=$(sudo sqlite3 $dbName "$dbQuery")
+    echo "Checking parallels kexts"
+    dbName="/var/db/SystemPolicyConfiguration/KextPolicy"
+    dbQuery="SELECT * FROM kext_policy WHERE bundle_id LIKE 'com.parallels.kext.%';"
+    kext=$(sudo sqlite3 $dbName "$dbQuery")
 
-#     if [ -z "$kext" ]; then
-#         echo "Parallels International GmbH not found"
-#         exit 1
-#     fi
+    if [ -z "$kext" ]; then
+        echo "Parallels International GmbH not found"
+        exit 1
+    fi
 
-#     # Create env variable
-#     url=$(brew info --json=v2 --installed | jq -r '.casks[] | select(.name[] == "Parallels Desktop").url')
-#     if [ -z "$url" ]; then
-#         echo "Unable to parse url for Parallels Desktop cask"
-#         exit 1
-#     fi
-#     echo "export PARALLELS_DMG_URL=$url" >> "${HOME}/.bashrc"
-# fi
+    # Create env variable
+    url=$(brew info --json=v2 --installed | jq -r '.casks[] | select(.name[] == "Parallels Desktop").url')
+    if [ -z "$url" ]; then
+        echo "Unable to parse url for Parallels Desktop cask"
+        exit 1
+    fi
+    echo "export PARALLELS_DMG_URL=$url" >> "${HOME}/.bashrc"
+fi
 
 # Invoke bazel to download bazel version via bazelisk
-# bazel
+bazel
 
-# # Install Azure DevOps extension for Azure Command Line Interface
-# az extension add -n azure-devops
+# Install Azure DevOps extension for Azure Command Line Interface
+az extension add -n azure-devops
 
-# # Workaround https://github.com/actions/runner-images/issues/4931
-# # by making Tcl/Tk paths the same on macOS 10.15 and macOS 11
-# if is_BigSur || is_Monterey; then
-#     version=$(brew info tcl-tk --json | jq -r '.[].installed[].version')
-#     ln -s /usr/local/Cellar/tcl-tk/$version/lib/libtcl8.6.dylib /usr/local/lib/libtcl8.6.dylib
-#     ln -s /usr/local/Cellar/tcl-tk/$version/lib/libtk8.6.dylib /usr/local/lib/libtk8.6.dylib
-# fi
+# Workaround https://github.com/actions/runner-images/issues/4931
+# by making Tcl/Tk paths the same on macOS 10.15 and macOS 11
+if is_BigSur || is_Monterey; then
+    version=$(brew info tcl-tk --json | jq -r '.[].installed[].version')
+    ln -s /usr/local/Cellar/tcl-tk/$version/lib/libtcl8.6.dylib /usr/local/lib/libtcl8.6.dylib
+    ln -s /usr/local/Cellar/tcl-tk/$version/lib/libtk8.6.dylib /usr/local/lib/libtk8.6.dylib
+fi
 
 # Invoke tests for all basic tools
-# invoke_tests "BasicTools"
+invoke_tests "BasicTools"
