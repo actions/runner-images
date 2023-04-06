46f5d5470a1a64e8bdc1a5e8e4ba56f30d242019
Alexey-Ayupov <116575425+Alexey-Ayupov@users.noreply.github.com>
2023-04-06 14:40:05 +0200

    Update CI actions according to demands (#7395)

--- images.CI/macos/move-vm.ps1
+++ images.CI/macos/move-vm.ps1
@@ -41,6 +41,8 @@ param(
     [ValidateNotNullOrEmpty()]
     [string]$VIPassword,
 
+    [string]$JobStatus,
+
     [int32]$CpuCount,
 
     [int32]$CoresPerSocketCount,
@@ -62,7 +64,7 @@ try {
 }
 
 $vm = Get-VM $VMName
-if ($env:AGENT_JOBSTATUS -eq 'Failed') {
+if (($env:AGENT_JOBSTATUS -and $env:AGENT_JOBSTATUS -eq 'Failed') -or ($JobStatus -and $JobStatus -eq 'failure')) {
     try {
         if($vm.PowerState -ne "PoweredOff") {
             Stop-VM -VM $vm -Confirm:$false -ErrorAction Stop | Out-Null
