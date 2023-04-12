cbd50182c010ea3e3ff22e07fbe879cb3d180e37
Vasilii Polikarpov <vpolikarpov-akvelon@github.com>
2023-04-12 16:01:55 +0200

    Remove obsolete workaround in Get-PackerVersion

--- images/linux/scripts/SoftwareReport/SoftwareReport.Tools.psm1
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Tools.psm1
@@ -182,10 +182,7 @@ function Get-NvmVersion {
 }
 
 function Get-PackerVersion {
-    # Packer 1.7.1 has a bug and outputs version to stderr instead of stdout https://github.com/hashicorp/packer/issues/10855
-    $result = (Get-CommandResult "packer --version").Output
-    $packerVersion = [regex]::matches($result, "(\d+.){2}\d+").Value
-    return $packerVersion
+    return "Packer $(packer --version)"
 }
 
 function Get-PhantomJSVersion {
