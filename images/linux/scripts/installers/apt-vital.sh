25d6b129910108dfe84e24f380969c486205beb3
Vasilii Polikarpov <vpolikarpov-akvelon@github.com>
2023-04-28 19:01:36 +0200

    Install all vital commands at once

--- images/linux/scripts/installers/apt-vital.sh
+++ images/linux/scripts/installers/apt-vital.sh
@@ -6,7 +6,4 @@
 source $HELPER_SCRIPTS/install.sh
 
 vital_packages=$(get_toolset_value .apt.vital_packages[])
-for package in $vital_packages; do
-    echo "Install $package"
-    apt-get install -y --no-install-recommends $package
-done
+apt-get install -y --no-install-recommends $vital_packages
