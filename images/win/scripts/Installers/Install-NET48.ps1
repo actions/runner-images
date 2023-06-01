9eaafb739e424d68fa4f0b7551baeb972867ef92
Alexey-Ayupov <116575425+Alexey-Ayupov@users.noreply.github.com>
2023-06-01 17:55:44 +0200

    [Windows] fix choco installation (#7657)

--- images/win/scripts/Installers/Install-NET48.ps1
+++ images/win/scripts/Installers/Install-NET48.ps1
@@ -4,10 +4,8 @@
 ################################################################################
 
 # .NET 4.8 Dev pack
-$InstallerName = "ndp48-devpack-enu.exe"
-$InstallerUrl = "https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0307177e14752e359fde5423ab583e43/${InstallerName}"
+$InstallerName = "ndp48-x86-x64-allos-enu.exe"
+$InstallerUrl = "https://download.visualstudio.microsoft.com/download/pr/2d6bb6b2-226a-4baa-bdec-798822606ff1/8494001c276a4b96804cde7829c04d7f/${InstallerName}"
 $ArgumentList = ("Setup", "/passive", "/norestart")
 
 Install-Binary -Url $InstallerUrl -Name $InstallerName -ArgumentList $ArgumentList
-
-Invoke-PesterTests -TestFile "Tools" -TestName "NET48"
