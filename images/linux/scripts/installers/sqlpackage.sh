5f7d4f8b2cd944a7cb2773bba703860b49c201c2
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2023-06-01 08:59:50 +0200

    [Ubuntu] Update libel package version

--- images/linux/scripts/installers/sqlpackage.sh
+++ images/linux/scripts/installers/sqlpackage.sh
@@ -10,7 +10,7 @@ source $HELPER_SCRIPTS/os.sh
 
 # Install libssl1.1 dependency
 if isUbuntu22; then
-    download_with_retries "http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.18_amd64.deb" "/tmp"
+    download_with_retries "http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb" "/tmp"
     dpkg -i /tmp/libssl1.1_1.1.1f-1ubuntu2.18_amd64.deb
 fi
 
