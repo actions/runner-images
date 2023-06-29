ff4a044da771830012865f5d57276a0f4a223acc
Mikhail Koliada <mikhailkoliada@github.com>
2023-06-29 12:50:16 +0200

    [Ubuntu] do not check ssl cert for hhvm

--- images/linux/scripts/installers/hhvm.sh
+++ images/linux/scripts/installers/hhvm.sh
@@ -6,7 +6,9 @@
 
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xB4112585D386EB94
 add-apt-repository https://dl.hhvm.com/ubuntu
+echo 'Acquire::https::dl.hhvm.com::Verify-Peer "false";' > /etc/apt/apt.conf.d/99hhvm-cert
 apt-get update
 apt-get -qq install -y hhvm
+rm /etc/apt/apt.conf.d/99hhvm-cert
 
 invoke_tests "Tools" "HHVM"
