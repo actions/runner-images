922e331a42499dc5c08fe55668d7a5fd487c556f
Vasilii Polikarpov <vpolikarpov-akvelon@github.com>
2023-07-14 14:19:18 +0200

    Install older version for BigSur

--- images/macos/provision/core/swiftlint.sh
+++ images/macos/provision/core/swiftlint.sh
@@ -2,4 +2,9 @@
 source ~/utils/utils.sh
 
 echo Installing Swiftlint...
-brew_smart_install "swiftlint"
+if is_BigSur; then
+    # SwiftLint now requires Swift 5.6 or higher to build, and macOS 12 or higher to run https://github.com/realm/SwiftLint/releases/tag/0.49.0
+    brew_smart_install "swiftlint@0.48.0"
+else
+    brew_smart_install "swiftlint"
+fi
