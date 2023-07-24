fb79e5e2754931fa69e6ad738ae44ff1bd4dab31
sergei-pyshnoi <sergei-pyshnoi@github.com>
2023-07-24 15:12:20 +0200

    change to manual chromedriver instalation

--- images/macos/provision/core/chrome.sh
+++ images/macos/provision/core/chrome.sh
@@ -5,7 +5,22 @@ echo "Installing Chrome..."
 brew_cask_install_ignoring_sha256 "google-chrome"
 
 echo "Installing Chrome Driver"
-brew install --cask chromedriver
+
+$arch=mac-$(get_arch)
+
+CHROME_VERSION_FULL=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version) && CHROME_VERSION_FULL=($CHROME_VERSION_FULL)
+CHROME_VERSION=${CHROME_VERSION_FULL[2]%.*}
+
+echo "Chrome version: " ${CHROME_VERSION_FULL[2]}
+CHROME_VERSIONS_JSON=$(curl -fsSL https://googlechromelabs.github.io/chrome-for-testing/latest-patch-versions-per-build-with-downloads.json)
+CHROMEDRIVER_VERSION=$(echo $CHROME_VERSIONS_JSON | jq -r '.builds["'"$CHROME_VERSION"'"].version')
+
+CHROMEDRIVER_URL=$(echo $CHROME_VERSIONS_JSON | jq -r '.builds["'"$CHROME_VERSION"'"].downloads.chromedriver[] | select(.platform=="'"$arch"'").url')
+
+echo "Installing chromedriver version:" $CHROMEDRIVER_VERSION
+download_with_retries $CHROMEDRIVER_URL "/tmp" "chromedriver_mac.zip"
+unzip -qq /tmp/chromedriver_mac.zip -d /tmp/
+cp /tmp/chromedriver-mac-$arch/chromedriver /usr/local/bin/chromedriver
 
 echo "Installing Selenium"
 brew_smart_install "selenium-server"
