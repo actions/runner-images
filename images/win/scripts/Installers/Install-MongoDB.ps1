50bcd51458938ac0c0287cbf87d1ac20d8effeaa
sergei-pyshnoi <sergei-pyshnoi@github.com>
2023-08-08 16:04:31 +0200

    change version detect | using install helper

--- images/win/scripts/Installers/Install-MongoDB.ps1
+++ images/win/scripts/Installers/Install-MongoDB.ps1
@@ -5,14 +5,25 @@
 
 # Install mongodb package
 $toolsetVersion = (Get-ToolsetContent).mongodb.version
-$latestChocoPackage = Get-LatestChocoPackageVersion -TargetVersion $toolsetVersion -PackageName "mongodb"
+
+$getMongoReleases =  Invoke-WebRequest -Uri "https://www.mongodb.com/docs/upcoming/release-notes/$toolsetVersion" -UseBasicParsing
+$TargetReleases = $getMongoReleases.Links.href | Where-Object {$_ -like "*std-label-$toolsetVersion*"}
+
+$MinorVersions = @()
+foreach ($release in $TargetReleases) {
+    $pattern = '\d+\.\d+\.\d+'
+    $version = $release | Select-String -Pattern $pattern -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
+    $MinorVersions += $version
+  }
+
+$LatestVersion = $MinorVersions[0]
 
 $installDir = "c:\PROGRA~1\MongoDB"
-$downloadURL = "https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-$latestChocoPackage-signed.msi"
-$downloadPath = "C:\temp\mongodb.msi"
-Invoke-Webrequest -URI $downloadURL -OutFile ( New-Item -Path $downloadPath -Force )
-cmd /c start /wait msiexec /q /i $downloadPath INSTALLLOCATION=$installDir ADDLOCAL="all"
-Remove-Item $downloadPath
+$binaryName = "mongodb-windows-x86_64-$LatestVersion-signed.msi"
+$downloadURL = "https://fastdl.mongodb.org/windows/$BinaryName"
+$installArg = "INSTALLLOCATION=$installDir ADDLOCAL=all"
+Install-Binary -Url $downloadURL -Name $binaryName -ArgumentList ("/q","/i","${env:Temp}\$binaryName", $installArg)
+
 
 # Add mongodb to the PATH
 $mongodbService = "mongodb"
