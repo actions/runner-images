f455443a7196aab544dbe1be462efddfb7e4080c
Ilia Shipitsin <ilia-shipitsin@github.com>
2023-09-12 22:06:27 +0200

    use out-null instead

--- images/macos/helpers/Xcode.Installer.psm1
+++ images/macos/helpers/Xcode.Installer.psm1
@@ -176,7 +176,7 @@ function Install-AdditionalSimulatorRuntimes {
 
     Write-Host "Installing Simulator Runtimes for Xcode $Version ..."
     $xcodebuildPath = Get-XcodeToolPath -Version $Version -ToolName "xcodebuild"
-    Invoke-ValidateCommand "$xcodebuildPath -downloadAllPlatforms" | Invoke-ValidateCommand 'xcpretty'
+    Invoke-ValidateCommand "$xcodebuildPath -downloadAllPlatforms" | Out-Null
 }
 
 function Build-XcodeSymlinks {
