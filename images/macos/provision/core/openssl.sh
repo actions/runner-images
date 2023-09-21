dfabaadd219e048a19c0ef7e024e7b3b288cb62f
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2023-09-21 12:52:03 +0200

    Update openssl.sh

--- images/macos/provision/core/openssl.sh
+++ images/macos/provision/core/openssl.sh
@@ -4,11 +4,10 @@ source ~/utils/utils.sh
 echo "Install openssl@1.1"
 brew_smart_install "openssl@1.1"
 
-# Intel-related symlinks, not needed on arm64 for now
-if ! is_VenturaArm64; then
-  # Symlink brew openssl@1.1 to `/usr/local/bin` as Homebrew refuses
-  ln -sf $(brew --prefix openssl@1.1)/bin/openssl /usr/local/bin/openssl
+# Symlink brew openssl@1.1 to `/usr/local/bin` as Homebrew refuses
+ln -sf $(brew --prefix openssl@1.1)/bin/openssl /usr/local/bin/openssl
 
+if ! is_VenturaArm64; then
   # Most of buildsystems and scripts look up ssl here
   ln -sf $(brew --cellar openssl@1.1)/1.1* /usr/local/opt/openssl
 fi
