ed105d016eeb5523421580d8927aa88f3c2f24d8
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2023-09-21 13:57:24 +0200

    [macOS] improve openssl@1.1 installation on arm64 (#8353)

--- images/macos/provision/core/openssl.sh
+++ images/macos/provision/core/openssl.sh
@@ -4,11 +4,15 @@ source ~/utils/utils.sh
 echo "Install openssl@1.1"
 brew_smart_install "openssl@1.1"
 
-# Intel-related symlinks, not needed on arm64 for now
 if ! is_VenturaArm64; then
   # Symlink brew openssl@1.1 to `/usr/local/bin` as Homebrew refuses
   ln -sf $(brew --prefix openssl@1.1)/bin/openssl /usr/local/bin/openssl
+else
+  # arm64 has a different installation prefix for brew
+  ln -sf $(brew --prefix openssl@1.1)/bin/openssl /opt/homebrew/bin/openssl
+fi
 
+if ! is_VenturaArm64; then
   # Most of buildsystems and scripts look up ssl here
   ln -sf $(brew --cellar openssl@1.1)/1.1* /usr/local/opt/openssl
 fi
