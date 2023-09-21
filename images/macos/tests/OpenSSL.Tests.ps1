f0ce5fd60adb907e0a6ec90c3e6342a2bc3d125f
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2023-09-21 12:29:35 +0200

    Update OpenSSL.Tests.ps1

--- images/macos/tests/OpenSSL.Tests.ps1
+++ images/macos/tests/OpenSSL.Tests.ps1
@@ -1,7 +1,6 @@
 Import-Module "$PSScriptRoot/../helpers/Common.Helpers.psm1"
-$os = Get-OSVersion
 
-Describe "OpenSSL" -Skip:($os.IsVenturaArm64) {
+Describe "OpenSSL" {
     Context "OpenSSL Version" {
         It "OpenSSL is available" {
             "openssl version" | Should -ReturnZeroExitCode
