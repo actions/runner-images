8d92ec28c0ef8b068646f936438c43eaad38a3eb
Jesse Houwing <jesse.houwing@gmail.com>
2023-09-22 11:13:12 +0200

    Update Install-AzureCli.ps1

--- images/win/scripts/Installers/Install-AzureCli.ps1
+++ images/win/scripts/Installers/Install-AzureCli.ps1
@@ -11,12 +11,12 @@ Update-Path
 # Store cli extensions outside of the provisioning user's profile
 $azureCliExtensionPath = Join-Path $Env:CommonProgramFiles 'AzureCliExtensionDirectory'
 $null = New-Item -ItemType "Directory" -Path $azureCliExtensionPath
-Set-SystemVariable -SystemVariable "AZURE_EXTENSION_DIR" -value $azureCliExtensionPath
+$env:AZURE_EXTENSION_DIR = Set-SystemVariable -SystemVariable "AZURE_EXTENSION_DIR" -value $azureCliExtensionPath
 
 # Store cli config outside of the provisioning user's profile
 $azureCliConfigPath = Join-Path $Env:CommonProgramFiles 'AzureCliDirectory'
 $null = New-Item -ItemType "Directory" -Path $azureCliConfigPath
-Set-SystemVariable -SystemVariable "AZURE_CONFIG_DIR" -value $azureCliConfigPath
+$env:AZURE_CONFIG_DIR = Set-SystemVariable -SystemVariable "AZURE_CONFIG_DIR" -value $azureCliConfigPath
 
 # Turn use automation recommended settings to speed up first-call on the hosted runner images.
 # See: https://techcommunity.microsoft.com/t5/azure-tools-blog/streamline-configuring-azure-cli-with-az-init/ba-p/3051810
