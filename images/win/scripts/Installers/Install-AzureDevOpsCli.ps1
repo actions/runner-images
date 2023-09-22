a67e88331e97c5a3134358c5dbaafb84e08f5f81
Jesse Houwing <jesse.houwing@gmail.com>
2023-09-22 20:13:21 +0200

    And lets try ignoring the error to see what happens.

--- images/win/scripts/Installers/Install-AzureDevOpsCli.ps1
+++ images/win/scripts/Installers/Install-AzureDevOpsCli.ps1
@@ -23,7 +23,7 @@ Write-Host "Warmup 'az-devops'"
 
 # az devops login requires python package 'keyring~=17.1.1' unfortunately it won't install
 # when az devops is running in a non-interactive session by calling `az devops login`
-$pythonPath = resolve-path (join-path ([System.IO.Path]::GetDirectoryName((Get-Command az).Path)) "..\python.exe")
-Invoke-ValidateCommand -Command "& '$pythonPath' -m pip install keyring~=17.1.1 --target '$env:AZURE_EXTENSION_DIR\azure-devops' -vv --disable-pip-version-check --no-cache-dir" 
+echo "dummy" | az devops login --debug
+az devops logout --debug
 
 Invoke-PesterTests -TestFile "CLI.Tools" -TestName "Azure DevOps CLI"
