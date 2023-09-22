a1a00d60bd26c2519f22c3b021411f95d681a792
Erik Bershel <erik-bershel@github.com>
2023-09-22 19:35:31 +0200

    Fix filters

--- images/win/scripts/Installers/Install-AliyunCli.ps1
+++ images/win/scripts/Installers/Install-AliyunCli.ps1
@@ -6,15 +6,15 @@
 
 Write-Host "Download Latest aliyun-cli archive"
 $repoUrl = "https://api.github.com/repos/aliyun/aliyun-cli/releases/latest"
-$installerFile = "aliyun-cli-windows"
+$installerFileName = "aliyun-cli-windows"
 $assets = (Invoke-RestMethod -Uri $repoUrl).assets
-$downloadUrl = ($assets.browser_download_url -ilike "aliyun-cli-windows-*-amd64.zip") | Select-Object -First 1
-$packagePath = Start-DownloadWithRetry -Url $downloadUrl -Name "$installerFile.zip"
+$downloadUrl = ($assets.browser_download_url -ilike "*aliyun-cli-windows-*-amd64.zip*") | Select-Object -First 1
+$packagePath = Start-DownloadWithRetry -Url $downloadUrl -Name "$installerFileName.zip"
 
 #region Supply chain security - Alibaba Cloud CLI
 $fileHash = (Get-FileHash -Path $packagePath -Algorithm SHA256).Hash
-$hashUrl = ($assets.browser_download_url -match "SHASUMS256.txt") | Select-Object -First 1
-$externalHash = (Invoke-RestMethod -Uri $hashURL).ToString().Split("`n").Where({ $_ -ilike "*$installerFile*" }).Split(' ')[0]
+$hashUrl = ($assets.browser_download_url -match "*SHASUMS256.txt*") | Select-Object -First 1
+$externalHash = (Invoke-RestMethod -Uri $hashURL).ToString().Split("`n").Where({ $_ -ilike "*$installerFileName*" }).Split(' ')[0]
 Use-ChecksumComparison $fileHash $externalHash
 #endregion
 
