4448c51fdc6d88ac477ddb86665942c54843afc8
Jesse Houwing <jesse.houwing@gmail.com>
2023-10-02 21:45:25 +0200

    Improves performance of `az devops`
    Redirects config & cache folders out of user profile
    Forces installation of keyring package through dummy login/logout
    Force package load & compilation by calling `--help` on each sub module

--- images/linux/scripts/installers/azure-devops-cli.sh
+++ images/linux/scripts/installers/azure-devops-cli.sh
@@ -4,7 +4,25 @@
 ##  Desc:  Installed Azure DevOps CLI (az devops)
 ################################################################################
 
+export AZURE_DEVOPS_EXT_CONFIG_DIR=/opt/az-devops/config
+echo "AZURE_DEVOPS_EXT_CONFIG_DIR=$AZURE_DEVOPS_EXT_CONFIG_DIR" | tee -a /etc/environment
+
+export AZURE_DEVOPS_CACHE_DIR=/opt/az-devops/config/cache
+echo "AZURE_DEVOPS_CACHE_DIR=$AZURE_DEVOPS_CACHE_DIR" | tee -a /etc/environment
+
 # install azure devops Cli extension
 az extension add -n azure-devops
+
+# calling az devops login to force it to install `keyring`. Login will actually fail, redirecting error to null
+echo "dummy" | az devops login >> /dev/null
+# calling az devops logout to be sure no credentials remain.
+az devops logout >> /dev/null
+
+# warm-up az devops
+az devops --help >> /dev/null
+az pipelines --help >> /dev/null
+az boards --help >> /dev/null
+az repos --help >> /dev/null
+az artifacts --help >> /dev/null
 
 invoke_tests "CLI.Tools" "Azure DevOps CLI"
