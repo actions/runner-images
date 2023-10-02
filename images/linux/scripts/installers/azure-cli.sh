37611d6cf6ff00cb342507c51bad23d3eb792209
Jesse Houwing <jesse.houwing@gmail.com>
2023-10-02 21:46:20 +0200

    Sets environment variables prior to installation

--- images/linux/scripts/installers/azure-cli.sh
+++ images/linux/scripts/installers/azure-cli.sh
@@ -4,12 +4,6 @@
 ##  Desc:  Installed Azure CLI (az)
 ################################################################################
 
-# Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
-curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
-echo "azure-cli https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt" >> $HELPER_SCRIPTS/apt-sources.txt
-rm -f /etc/apt/sources.list.d/azure-cli.list
-rm -f /etc/apt/sources.list.d/azure-cli.list.save
-
 # AZURE_EXTENSION_DIR shell variable defines where modules are installed
 # https://docs.microsoft.com/en-us/cli/azure/azure-cli-extensions-overview
 export AZURE_EXTENSION_DIR=/opt/az/azcliextensions
@@ -17,6 +11,12 @@ echo "AZURE_EXTENSION_DIR=$AZURE_EXTENSION_DIR" | tee -a /etc/environment
 
 export AZURE_CONFIG_DIR=/opt/az/config
 echo "AZURE_CONFIG_DIR=$AZURE_CONFIG_DIR" | tee -a /etc/environment
+
+# Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
+curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
+echo "azure-cli https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt" >> $HELPER_SCRIPTS/apt-sources.txt
+rm -f /etc/apt/sources.list.d/azure-cli.list
+rm -f /etc/apt/sources.list.d/azure-cli.list.save
 
 # Warm-up CLI
 az --help
