4f9bbc93e2dd92fd5790589f592d65cd7fccdf6c
Jesse Houwing <jesse.houwing@gmail.com>
2023-10-02 21:40:35 +0200

    Redirect `az`'s config to /opt/az/config

--- images/linux/scripts/installers/azure-cli.sh
+++ images/linux/scripts/installers/azure-cli.sh
@@ -10,4 +10,15 @@ echo "azure-cli https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-lin
 rm -f /etc/apt/sources.list.d/azure-cli.list
 rm -f /etc/apt/sources.list.d/azure-cli.list.save
 
+# AZURE_EXTENSION_DIR shell variable defines where modules are installed
+# https://docs.microsoft.com/en-us/cli/azure/azure-cli-extensions-overview
+export AZURE_EXTENSION_DIR=/opt/az/azcliextensions
+echo "AZURE_EXTENSION_DIR=$AZURE_EXTENSION_DIR" | tee -a /etc/environment
+
+export AZURE_CONFIG_DIR=/opt/az/config
+echo "AZURE_CONFIG_DIR=$AZURE_CONFIG_DIR" | tee -a /etc/environment
+
+# Warm-up CLI
+az --help
+
 invoke_tests "CLI.Tools" "Azure CLI"
