709982e0da7fcfbd2ca6b50f703ba1a1560f6c8c
Jesse Houwing <jesse.houwing@gmail.com>
2023-10-03 10:31:36 +0200

    Apply suggestions from code review

--- images/linux/scripts/installers/azure-cli.sh
+++ images/linux/scripts/installers/azure-cli.sh
@@ -7,10 +7,10 @@
 # AZURE_EXTENSION_DIR shell variable defines where modules are installed
 # https://docs.microsoft.com/en-us/cli/azure/azure-cli-extensions-overview
 export AZURE_EXTENSION_DIR=/opt/az/azcliextensions
-echo "AZURE_EXTENSION_DIR=$AZURE_EXTENSION_DIR" | tee -a /etc/environment
+setEtcEnvironmentVariable "AZURE_EXTENSION_DIR" "$AZURE_EXTENSION_DIR"
 
 export AZURE_CONFIG_DIR=/opt/az/config
-echo "AZURE_CONFIG_DIR=$AZURE_CONFIG_DIR" | tee -a /etc/environment
+setEtcEnvironmentVariable "AZURE_CONFIG_DIR" "$AZURE_CONFIG_DIR"
 
 # Install Azure CLI (instructions taken from https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
 curl -fsSL https://aka.ms/InstallAzureCLIDeb | sudo bash
@@ -21,8 +21,8 @@ rm -f /etc/apt/sources.list.d/azure-cli.list.save
 # Warm-up CLI
 
 # az --version performs an update check and caches the results
-az --version >> /dev/null
+az --version > /dev/null
 # az --help forces a command cache rebuild
-az --help >> /dev/null
+az --help > /dev/null
 
 invoke_tests "CLI.Tools" "Azure CLI"
--- images/linux/scripts/installers/azure-devops-cli.sh
+++ images/linux/scripts/installers/azure-devops-cli.sh
@@ -5,24 +5,24 @@
 ################################################################################
 
 export AZURE_DEVOPS_EXT_CONFIG_DIR=/opt/az-devops/config
-echo "AZURE_DEVOPS_EXT_CONFIG_DIR=$AZURE_DEVOPS_EXT_CONFIG_DIR" | tee -a /etc/environment
+setEtcEnvironmentVariable "AZURE_DEVOPS_EXT_CONFIG_DIR" "$AZURE_DEVOPS_EXT_CONFIG_DIR"
 
 export AZURE_DEVOPS_CACHE_DIR=/opt/az-devops/config/cache
-echo "AZURE_DEVOPS_CACHE_DIR=$AZURE_DEVOPS_CACHE_DIR" | tee -a /etc/environment
+setEtcEnvironmentVariable "AZURE_DEVOPS_CACHE_DIR" "$AZURE_DEVOPS_CACHE_DIR"
 
 # install azure devops Cli extension
 az extension add -n azure-devops
 
 # calling az devops login to force it to install `keyring`. Login will actually fail, redirecting error to null
-echo "dummy" | az devops login >> /dev/null
+echo "dummy" | az devops login > /dev/null
 # calling az devops logout to be sure no credentials remain.
-az devops logout >> /dev/null
+az devops logout > /dev/null
 
 # warm-up az devops
-az devops --help >> /dev/null
-az pipelines --help >> /dev/null
-az boards --help >> /dev/null
-az repos --help >> /dev/null
-az artifacts --help >> /dev/null
+az devops --help > /dev/null
+az pipelines --help > /dev/null
+az boards --help > /dev/null
+az repos --help > /dev/null
+az artifacts --help > /dev/null
 
 invoke_tests "CLI.Tools" "Azure DevOps CLI"
