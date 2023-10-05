624ba90e0a6b49fe7dad7d18353db672b6047421
Jesse Houwing <jesse.houwing@gmail.com>
2023-10-05 16:41:08 +0200

    ensures `/opt/az-devops` exists and has permissions

--- images/linux/scripts/installers/azure-devops-cli.sh
+++ images/linux/scripts/installers/azure-devops-cli.sh
@@ -11,6 +11,9 @@ setEtcEnvironmentVariable "AZURE_DEVOPS_EXT_CONFIG_DIR" "$AZURE_DEVOPS_EXT_CONFI
 export AZURE_DEVOPS_CACHE_DIR=/opt/az-devops/config/cache
 setEtcEnvironmentVariable "AZURE_DEVOPS_CACHE_DIR" "$AZURE_DEVOPS_CACHE_DIR"
 
+sudo mkdir /opt/az-devops
+sudo chmod 777 -R /opt/az-devops
+
 # install azure devops Cli extension
 az extension add -n azure-devops
 
