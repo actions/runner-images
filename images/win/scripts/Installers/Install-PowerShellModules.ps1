2925b3ae953afa73b1f3df8916944caeeccc668c
ilia-shipitsin <125650415+ilia-shipitsin@users.noreply.github.com>
2023-10-06 12:03:28 +0200

    [windows] introduce dedicated PowerShell configuration step (#8453)
    * windows: implement separate step for configuring Powershell
    
    * windows: refactor PyPy checksum validation
    
    PyPy checksum validation uses PowerHTML module which is now available
    as a part of powershell profile
    
    * reformat
    
    * Update images/win/scripts/Installers/Configure-PowerShell.ps1
    
    Co-authored-by: Vasilii Polikarpov <126792224+vpolikarpov-akvelon@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Vasilii Polikarpov <126792224+vpolikarpov-akvelon@users.noreply.github.com>

--- images/win/scripts/Installers/Install-PowerShellModules.ps1
+++ images/win/scripts/Installers/Install-PowerShellModules.ps1
@@ -1,12 +1,6 @@
 # Set TLS1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
 
-Write-Host "Setup PowerShellGet"
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-
-# Specifies the installation policy
-Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
-
 # Install PowerShell modules
 $modules = (Get-ToolsetContent).powershellModules
 
