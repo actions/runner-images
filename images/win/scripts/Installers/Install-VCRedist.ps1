432a52b654f73c27e36aaf9e7002933414b8fbf1
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2023-10-09 14:56:15 +0200

    [windows] Fix code style in Install-VCRedist.ps1

--- images/win/scripts/Installers/Install-VCRedist.ps1
+++ images/win/scripts/Installers/Install-VCRedist.ps1
@@ -1,10 +1,15 @@
-# Install vcredist2010
-$Vc2010x86Name = "vcredist_x86.exe"
-$Vc2010x86URI = "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/${Vc2010x86Name}"
-$Vc2010x64Name = "vcredist_x64.exe"
-$Vc2010x64URI = "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/${Vc2010x64Name}"
-$ArgumentList = ("/install", "/quiet", "/norestart")
-Install-Binary -Url $Vc2010x86URI -Name $Vc2010x86Name -ArgumentList $ArgumentList
-Install-Binary -Url $Vc2010x64URI -Name $Vc2010x64Name -ArgumentList $ArgumentList
+################################################################################
+##  File:  Install-VCRedist.ps1
+##  Desc:  Install Visual C++ Redistributable
+################################################################################
+
+$vc2010x86Name = "vcredist_x86.exe"
+$vc2010x86URI = "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/${vc2010x86Name}"
+$vc2010x64Name = "vcredist_x64.exe"
+$vc2010x64URI = "https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/${Vc2010x64Name}"
+$argumentList = ("/install", "/quiet", "/norestart")
+
+Install-Binary -Url $vc2010x86URI -Name $vc2010x86Name -ArgumentList $argumentList
+Install-Binary -Url $vc2010x64URI -Name $vc2010x64Name -ArgumentList $argumentList
 
 Invoke-PesterTests -TestFile "Tools" -TestName "VCRedist"
