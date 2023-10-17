1a9f44f7be328bfa890b438249f6df104262ea0b
sergei-pyshnoi <121864472+sergei-pyshnoi@users.noreply.github.com>
2023-10-17 09:31:49 +0200

    update heroku cli installation script (#8566)

--- images/linux/scripts/installers/heroku.sh
+++ images/linux/scripts/installers/heroku.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -e
 ################################################################################
 ##  File:  heroku.sh
-##  Desc:  Installs Heroku CLI
+##  Desc:  This script installs Heroku CLI. Based on instructions found here: https://devcenter.heroku.com/articles/heroku-cli
 ################################################################################
 
-# Install Heroku CLI
-curl -fsSL https://cli-assets.heroku.com/install.sh | sh
+## Install Heroku CLI
+
+# add heroku repository to apt
+echo "deb https://cli-assets.heroku.com/channels/stable/apt ./" > /etc/apt/sources.list.d/heroku.list
+
+# install heroku's release key for package verification
+curl https://cli-assets.heroku.com/channels/stable/apt/release.key | apt-key add -
+
+# install heroku
+apt-get update -y && apt-get install -y heroku
+
+# remove heroku's apt repository
+rm /etc/apt/sources.list.d/heroku.list
 
 invoke_tests "Tools" "Heroku"
