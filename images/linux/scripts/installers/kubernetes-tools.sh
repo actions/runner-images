0f20e33ab0c547170045d36f9ce00e136a6c770d
sergei-pyshnoi <sergei-pyshnoi@github.com>
2023-10-20 14:04:32 +0200

    remove verbose from curl

--- images/linux/scripts/installers/kubernetes-tools.sh
+++ images/linux/scripts/installers/kubernetes-tools.sh
@@ -9,7 +9,7 @@ source $HELPER_SCRIPTS/install.sh
 
 # Install KIND
 URL=$(get_github_package_download_url "kubernetes-sigs/kind" "contains(\"kind-linux-amd64\")")
-curl -fsSL -v -o /usr/local/bin/kind $URL
+curl -fsSL -o /usr/local/bin/kind $URL
 chmod +x /usr/local/bin/kind
 
 ## Install kubectl
@@ -20,15 +20,15 @@ sudo apt-get update -y && sudo apt-get install -y kubectl
 rm -f /etc/apt/sources.list.d/kubernetes.list
 
 # Install Helm
-curl -fsSL -v https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
 # Install minikube
-curl -fsSL -v -O https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+curl -fsSL -O https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 # Install kustomize
 download_url="https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
-curl -fsSL -v "$download_url" | bash
+curl -fsSL "$download_url" | bash
 mv kustomize /usr/local/bin
 
 invoke_tests "Tools" "Kubernetes tools"
