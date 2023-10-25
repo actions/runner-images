8e43ac1c63bf0c8708bff848405a109d896763d6
Vasilii Polikarpov <vpolikarpov-akvelon@github.com>
2023-10-25 11:38:30 +0200

    Remove libtcl symlinks

--- images/macos/provision/core/commonutils.sh
+++ images/macos/provision/core/commonutils.sh
@@ -82,13 +82,5 @@ bazel
 # Install Azure DevOps extension for Azure Command Line Interface
 az extension add -n azure-devops
 
-# Workaround https://github.com/actions/runner-images/issues/4931
-# by making Tcl/Tk paths the same on macOS 10.15 and macOS 11
-if is_Monterey; then
-    version=$(brew info tcl-tk --json | jq -r '.[].installed[].version')
-    ln -s /usr/local/Cellar/tcl-tk/$version/lib/libtcl8.6.dylib /usr/local/lib/libtcl8.6.dylib
-    ln -s /usr/local/Cellar/tcl-tk/$version/lib/libtk8.6.dylib /usr/local/lib/libtk8.6.dylib
-fi
-
 # Invoke tests for all basic tools
 invoke_tests "BasicTools"
