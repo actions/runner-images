cff51fe6dfa847e22134cf0ddc13b27de232b895
Peter Shepherd <petershepherd@Peters-MBP.lan>
2023-10-26 10:12:42 -0400

    Revert "added windows scripts"
    This reverts commit 9965920d564662d003a287e71ca9defa446295e2.

--- images/win/scripts/Installers/Install-Runner.ps1
+++
@@ -1,4 +0,0 @@
-$runnerDownloadUrl = Get-GitHubPackageDownloadUrl -RepoOwner "Actions" -RepoName "runner" -BinaryName "actions-runner-linux-x64" -Version "latest" -UrlFilter "*{BinaryName}-{Version}.zip"
-$fileName = runnerDownloadUrl.Substring($zip.LastIndexOf("/") + 1)
-New-Item -Path "C:\ProgramData\runner" -ItemType Directory
-$kotlinInstallerPath = Start-DownloadWithRetry -Url $runnerDownloadUrl -Name $fileName -DownloadPath "C:\ProgramData\runner"
