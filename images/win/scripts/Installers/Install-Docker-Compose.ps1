6d305908ce403aec589f24bf0bfaae0cba4f056d
Ilia Shipitsin <ilia-shipitsin@github.com>
2023-10-30 14:25:57 +0100

    [windows] split docker install into 3 scripts
    the idea is to end with 1 script per 1 component for better
    observability

---
+++ images/win/scripts/Installers/Install-Docker-Compose.ps1
@@ -0,0 +1,16 @@
+################################################################################
+##  File:  Install-Docker-Compose.ps1
+##  Desc:  Install Docker Compose.
+################################################################################
+
+Write-Host "Install-Package Docker-Compose v1"
+$versionToInstall = Get-LatestChocoPackageVersion -TargetVersion "1.29" -PackageName "docker-compose"
+Choco-Install -PackageName docker-compose -ArgumentList "--version=$versionToInstall"
+
+Write-Host "Install-Package Docker-Compose v2"
+$dockerComposev2Url = "https://github.com/docker/compose/releases/latest/download/docker-compose-windows-x86_64.exe"
+$cliPluginsDir = "C:\ProgramData\docker\cli-plugins"
+New-Item -Path $cliPluginsDir -ItemType Directory
+Start-DownloadWithRetry -Url $dockerComposev2Url -Name docker-compose.exe -DownloadPath $cliPluginsDir
+
+Invoke-PesterTests -TestFile "Docker" -TestName "DockerCompose"
--- images/win/scripts/Tests/Docker.Tests.ps1
+++ images/win/scripts/Tests/Docker.Tests.ps1
@@ -1,17 +1,8 @@
 Describe "Docker" {
     It "<ToolName>" -TestCases @(
-        @{ ToolName = "docker" }
         @{ ToolName = "docker-compose" }
     ) {
         "$ToolName --version" | Should -ReturnZeroExitCode
-    }
-
-    It "docker-wincred" {
-        "docker-credential-wincred version" | Should -ReturnZeroExitCode
-    }
-
-    It "docker compose v2" {
-        "docker compose version" | Should -ReturnZeroExitCode
     }
 
     It "docker service is up" {
@@ -20,6 +11,25 @@ Describe "Docker" {
 
     It "docker symlink" {
         "C:\Windows\SysWOW64\docker.exe ps" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe "DockerCompose" {
+    It "<ToolName>" -TestCases @(
+        @{ ToolName = "docker-compose" }
+    ) {
+        "$ToolName --version" | Should -ReturnZeroExitCode
+    }
+
+    It "docker compose v2" {
+        "docker compose version" | Should -ReturnZeroExitCode
+    }
+
+}
+
+Describe "DockerWinCred" {
+    It "docker-wincred" {
+        "docker-credential-wincred version" | Should -ReturnZeroExitCode
     }
 }
 
