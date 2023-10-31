d402030e2725ab38157162a6a42054bbb78e9363
Peter Shepehrd <109117968+pshepherd10@users.noreply.github.com>
2023-10-31 16:30:22 -0400

    remove spaces

--- images/linux/scripts/installers/runner-package.sh
+++ images/linux/scripts/installers/runner-package.sh
@@ -7,9 +7,9 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
 
-DOWNLOAD_URL = $(get_github_package_download_url "actions/runner" "contains(\"actions-runner-linux-x64-\") and endswith(\".tar.gz\")" "latest")
+DOWNLOAD_URL=$(get_github_package_download_url "actions/runner" "contains(\"actions-runner-linux-x64-\") and endswith(\".tar.gz\")" "latest")
 
-FILE_NAME = echo ${DOWNLOAD_URL##*/}
+FILE_NAME=echo ${DOWNLOAD_URL##*/}
 
 mkdir -p /opt/runner-cache
 
