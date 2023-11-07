fdb6b52fa8929dc85898241959f12c213f243d65
Peter Shepehrd <109117968+pshepherd10@users.noreply.github.com>
2023-11-07 09:43:55 -0500

    fix syntax

--- images/linux/scripts/installers/runner-package.sh
+++ images/linux/scripts/installers/runner-package.sh
@@ -9,7 +9,7 @@ source $HELPER_SCRIPTS/install.sh
 
 DOWNLOAD_URL=$(get_github_package_download_url "actions/runner" "test(\"actions-runner-linux-x64-[0-9].[0-9]{3}.[0-9].tar.gz\")" "latest")
 
-FILE_NAME=echo ${DOWNLOAD_URL##*/}
+FILE_NAME="${DOWNLOAD_URL##*/}"
 
 sudo mkdir -p /opt/runner-cache
 
