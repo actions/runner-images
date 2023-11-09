f336469cd043b8ba8f6e89a6b8e0037012882fda
Peter Shepehrd <109117968+pshepherd10@users.noreply.github.com>
2023-11-09 16:59:40 -0500

    add test call

--- images/win/scripts/Installers/Install-Runner.ps1
+++ images/win/scripts/Installers/Install-Runner.ps1
@@ -10,3 +10,5 @@ $version = $release.tag_name.Trim("v")
 $downloadUrl = ($release.assets.browser_download_url -ilike "*actions-runner-win-x64-${version}.zip*") | Select-Object -First 1
 New-Item -Path "C:\ProgramData\runner" -ItemType Directory
 $packagePath = Start-DownloadWithRetry -Url $downloadUrl -DownloadPath "C:\ProgramData\runner"
+
+Invoke-PesterTests -TestFile "RunnerCache"
