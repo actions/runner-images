1254bc94a5b09f3a5be861a2a487c2d860e8d2f5
Vasilii Polikarpov <126792224+vpolikarpov-akvelon@users.noreply.github.com>
2023-11-23 11:58:34 +0100

    [Windows] Split VSIX installation helper into two (#8872)

--- images/windows/scripts/build/Install-VSExtensions.ps1
+++ images/windows/scripts/build/Install-VSExtensions.ps1
@@ -14,7 +14,7 @@ $vsixPackagesList | ForEach-Object {
     # Retrieve cdn endpoint to avoid HTTP error 429 https://github.com/actions/runner-images/issues/3074
     $vsixPackage = Get-VsixExtenstionFromMarketplace -ExtensionMarketPlaceName $_
     if ($vsixPackage.FileName.EndsWith(".vsix")) {
-        Install-VsixExtension -Url $vsixPackage.DownloadUri -Name $vsixPackage.FileName
+        Install-VSIXFromUrl $vsixPackage.DownloadUri
     } else {
         Install-Binary `
             -Url $vsixPackage.DownloadUri `
