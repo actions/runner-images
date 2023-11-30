eb0e4ff100b7cd7bf20cc8fbdae059943a2058c2
Vasilii Polikarpov <126792224+vpolikarpov-akvelon@users.noreply.github.com>
2023-11-30 16:53:16 +0100

    [Windows] Move Android and VS helpers to dedicated files (#8903)

--- images/windows/scripts/build/Install-VSExtensions.ps1
+++ images/windows/scripts/build/Install-VSExtensions.ps1
@@ -11,8 +11,9 @@ if (-not $vsixPackagesList) {
 }
 
 $vsixPackagesList | ForEach-Object {
-    # Retrieve cdn endpoint to avoid HTTP error 429 https://github.com/actions/runner-images/issues/3074
-    $vsixPackage = Get-VsixExtenstionFromMarketplace -ExtensionMarketPlaceName $_
+    # Retrieve cdn endpoint to avoid HTTP error 429
+    # https://github.com/actions/runner-images/issues/3074
+    $vsixPackage = Get-VsixInfoFromMarketplace $_
     Write-Host "Installing $vsixPackage"
     if ($vsixPackage.FileName.EndsWith(".vsix")) {
         Install-VSIXFromUrl $vsixPackage.DownloadUri
