ac068402becb4feebb5a315f69d72dae0deb966a
Vasilii Polikarpov <vpolikarpov-akvelon@github.com>
2023-12-06 14:07:35 +0100

    Fix configure-toolset script

--- images/windows/scripts/build/Configure-Toolset.ps1
+++ images/windows/scripts/build/Configure-Toolset.ps1
@@ -4,40 +4,35 @@
 ##  Desc:  Configure Toolset
 ################################################################################
 
-$toolsToConfigure = @(
-    @{
-        name          = "Python"
+$toolEnvConfigs = @{
+    Python = @{
         pathTemplates = @(
             "{0}"
             "{0}\Scripts"
         )
     }
-    @{
-        name           = "Go"
+    go     = @{
         pathTemplates  = @(
             "{0}\bin"
         )
         envVarTemplate = "GOROOT_{0}_{1}_X64"
     }
-)
+}
 
+$tools = Get-ToolsetContent `
+| Select-Object -ExpandProperty toolcache `
+| Where-Object { $toolEnvConfigs.Keys -contains $_.name }
 
 Write-Host "Configure toolset tools environment..."
-foreach ($tool in $toolsToConfigure) {
-    $toolsetData = Get-ToolsetContent `
-    | Select-Object -ExpandProperty toolcache `
-    | Where-Object { $_.name -eq $tool.name }
-
-    foreach ($key in $toolsetData.Keys) {
-        $tool.$key = $toolsetData[$key]
-    }
-
-    if (-not ([string]::IsNullOrEmpty($tool.envVarTemplate))) {
+foreach ($tool in $tools) {
+    $toolEnvConfig = $toolEnvConfigs[$tool.name]
+    
+    if (-not ([string]::IsNullOrEmpty($toolEnvConfig.envVarTemplate))) {
         foreach ($version in $tool.versions) {
             Write-Host "Set $($tool.name) $version environment variable..."
 
             $foundVersionArchPath = Get-TCToolVersionPath -Name $tool.name -Version $version -Arch $tool.arch
-            $envName = $tool.envVarTemplate -f $version.Split(".")
+            $envName = $toolEnvConfig.envVarTemplate -f $version.Split(".")
 
             Write-Host "Set $envName to $foundVersionArchPath"
             [Environment]::SetEnvironmentVariable($envName, $foundVersionArchPath, "Machine")
@@ -49,7 +44,7 @@ foreach ($tool in $toolsToConfigure) {
 
         $toolVersionPath = Get-TCToolVersionPath -Name $tool.name -Version $tool.default -Arch $tool.arch
 
-        foreach ($template in $tool.pathTemplates) {
+        foreach ($template in $toolEnvConfig.pathTemplates) {
             $toolSystemPath = $template -f $toolVersionPath
             Write-Host "Add $toolSystemPath to system PATH..."
             Add-MachinePathItem -PathItem $toolSystemPath | Out-Null
