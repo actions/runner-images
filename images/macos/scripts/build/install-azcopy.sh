371dba15f75758b51fd16169b2392e3de3bd20dc
Erik Bershel <erik-bershel@github.com>
2023-12-12 13:50:58 +0100

    [macOS] Update AzCopy installer to work with versions

--- images/macos/scripts/build/install-azcopy.sh
+++ images/macos/scripts/build/install-azcopy.sh
@@ -5,18 +5,33 @@
 ################################################################################
 
 source ~/utils/utils.sh
+
+# Build variables for download URL
 arch=$(get_arch)
 
-# Check MacOS architecture and if ARM install using brew
-if [ "$arch" == "arm64" ]; then
-    brew_smart_install azcopy
-else
-    # Temporary pin 10.21.2 version of AzCopy until 10.22.0 is fixed
-    archive_path=$(download_with_retry "https://azcopyvnext.azureedge.net/releases/release-10.21.2-20231106/azcopy_darwin_amd64_10.21.2.zip")
-    unzip -qq "$archive_path" -d /tmp/azcopy
-    extract_path=$(echo /tmp/azcopy/azcopy*)
-    cp "$extract_path/azcopy" "/usr/local/bin/azcopy"
-    chmod +x "/usr/local/bin/azcopy"
+if [ "$arch" == "x64" ]; then
+    arch="amd64"
 fi
+
+repo="Azure/azure-storage-azcopy"
+json=$(curl -fsSL "https://api.github.com/repos/${repo}/releases?per_page=100" | jq -r '.[] | select(.prerelease==false)')
+version=$(get_toolset_value '.azcopy.version')
+
+if [[ $version == "latest" ]]; then
+    version=$(echo "$json" | jq -r '.tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]|beta" | tail -n 1 | tr -d 'v')
+elif [[ $version == *"*"* ]]; then
+    version=$(echo "$json" | jq -r '.tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]|beta" | egrep "${version}" | tail -n 1 | tr -d 'v')
+fi
+
+date=$(curl -fsSL "https://api.github.com/repos/${repo}/releases/tags/v${version}" | jq -r '.published_at' | cut -c 1-4,6-7,9-10)
+url="https://azcopyvnext.azureedge.net/releases/release-${version}-${date}/azcopy_darwin_${arch}_${version}.zip"
+
+# Install AzCopy
+archive_path=$(download_with_retry "${url}")
+unzip -qq "$archive_path" -d /tmp/azcopy
+extract_path=$(echo /tmp/azcopy/azcopy*)
+cp "$extract_path/azcopy" "/usr/local/bin/azcopy"
+chmod +x "/usr/local/bin/azcopy"
+
 
 invoke_tests "Common" "AzCopy"
