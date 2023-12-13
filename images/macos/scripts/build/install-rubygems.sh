b7537e4824599c30048f80ce09d45b385265138d
Erik Bershel <erik-bershel@github.com>
2023-12-13 19:28:23 +0100

    [macOS] Force ruby gem to check gems

--- images/macos/scripts/build/install-rubygems.sh
+++ images/macos/scripts/build/install-rubygems.sh
@@ -10,13 +10,13 @@ echo Updating RubyGems...
 gem update --system
 
 # Temporarily install activesupport 7.0.8 due to compatibility issues with cocoapods https://github.com/CocoaPods/CocoaPods/issues/12081
-gem install activesupport -v 7.0.8
+gem install activesupport -v 7.0.8 -P HighSecurity
 
 gemsToInstall=$(get_toolset_value '.ruby.rubygems | .[]')
 if [ -n "$gemsToInstall" ]; then
     for gem in $gemsToInstall; do
         echo "Installing gem $gem"
-        gem install $gem
+        gem install $gem -P HighSecurity
     done
 fi
 
