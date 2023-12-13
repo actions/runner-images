45a30c62e99e1a71f0ad85189f9f55fc0f0b0809
Erik Bershel <erik-bershel@github.com>
2023-12-13 15:28:17 +0100

    Simplification

--- images/macos/scripts/build/install-azcopy.sh
+++ images/macos/scripts/build/install-azcopy.sh
@@ -6,25 +6,13 @@
 
 source ~/utils/utils.sh
 
-# Build variables for download URL
-arch=$(get_arch)
-
-if [ "$arch" == "x64" ]; then
-    arch="amd64"
+if is_Arm64; then
+    # latest is available by https://aka.ms/downloadazcopy-v10-mac-arm64
+    url="https://azcopyvnext.azureedge.net/releases/release-10.21.2-20231106/azcopy_darwin_arm64_10.21.2.zip"
+else
+    # latest is available by https://aka.ms/downloadazcopy-v10-mac
+    url="https://azcopyvnext.azureedge.net/releases/release-10.21.2-20231106/azcopy_darwin_amd64_10.21.2.zip"
 fi
-
-repo="Azure/azure-storage-azcopy"
-json=$(curl -fsSL "https://api.github.com/repos/${repo}/releases?per_page=100" | jq -r '.[] | select(.prerelease==false)')
-version=$(get_toolset_value '.azcopy.version')
-
-if [[ $version == "latest" ]]; then
-    version=$(echo "$json" | jq -r '.tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]|beta" | tail -n 1 | tr -d 'v')
-elif [[ $version == *"*"* ]]; then
-    version=$(echo "$json" | jq -r '.tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]|beta" | egrep "${version}" | tail -n 1 | tr -d 'v')
-fi
-
-date=$(curl -fsSL "https://api.github.com/repos/${repo}/releases/tags/v${version}" | jq -r '.published_at' | cut -c 1-4,6-7,9-10)
-url="https://azcopyvnext.azureedge.net/releases/release-${version}-${date}/azcopy_darwin_${arch}_${version}.zip"
 
 # Install AzCopy
 archive_path=$(download_with_retry "${url}")
