7011401fd89244c5739d6859bdc291b89c54c6b1
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2023-12-29 14:46:15 +0100

    [ubuntu] Remove apt-key from build scripts

--- images/ubuntu/scripts/build/install-mongodb.sh
+++ images/ubuntu/scripts/build/install-mongodb.sh
@@ -7,19 +7,24 @@
 # Source the helpers
 source $HELPER_SCRIPTS/install.sh
 
-REPO_URL="https://repo.mongodb.org/apt/ubuntu"
-os_label=$(lsb_release -cs)
 toolset_version=$(get_toolset_value '.mongodb.version')
+REPO_URL="https://repo.mongodb.org/apt/ubuntu"
+GPG_KEY="/usr/share/keyrings/mongodb-org-$toolset_version.gpg"
+REPO_PATH="/etc/apt/sources.list.d/mongodb-org-$toolset_version.list"
 
-#  Install Mongo DB
-wget -qO - https://www.mongodb.org/static/pgp/server-$toolset_version.asc | sudo apt-key add -
+# add Mongo DB repository to apt
+curl -fsSL https://www.mongodb.org/static/pgp/server-$toolset_version.asc | gpg --dearmor -o $GPG_KEY
+echo "deb [ arch=amd64,arm64 signed-by=$GPG_KEY ] $REPO_URL $(lsb_release -cs)/mongodb-org/$toolset_version multiverse" > $REPO_PATH
 
-echo "deb [ arch=amd64,arm64 ] $REPO_URL $os_label/mongodb-org/$toolset_version multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$toolset_version.list
+# Install Mongo DB
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
-rm /etc/apt/sources.list.d/mongodb-org-$toolset_version.list
+# remove Mongo DB's apt repository
+rm $REPO_PATH
+rm $GPG_KEY
 
+# Document source repo
 echo "mongodb $REPO_URL" >> $HELPER_SCRIPTS/apt-sources.txt
 
 invoke_tests "Databases" "MongoDB"
