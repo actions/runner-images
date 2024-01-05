4b8f0f965c7b328d10ac63e209feb33a4e3676a9
Alexey-Ayupov <116575425+Alexey-Ayupov@users.noreply.github.com>
2024-01-05 14:00:08 +0100

    [Ubuntu] Increase threshold in the Disk free space test (#9109)

--- images/ubuntu/scripts/tests/System.Tests.ps1
+++ images/ubuntu/scripts/tests/System.Tests.ps1
@@ -1,6 +1,6 @@
 Describe "Disk free space" {
-    It "Image has more than 15GB free space" {
+    It "Image has more than 31GB free space" {
         $freeSpace = (Get-PSDrive "/").Free
-        $freeSpace | Should -BeGreaterOrEqual 15GB
+        $freeSpace | Should -BeGreaterOrEqual 31GB
     }
 }
