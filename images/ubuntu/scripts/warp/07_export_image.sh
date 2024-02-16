b7a759dafeb1c48c20d8b7eac1c7261e754b1911
Ankit Gupta <guptaankit015@gmail.com>
2024-02-16 11:48:52 +0530

    steps to execute after vhd image export

--- images/ubuntu/scripts/warp/07_export_image.sh
+++ images/ubuntu/scripts/warp/07_export_image.sh
@@ -7,3 +7,23 @@
 # select the export option on disk page and start export
 
 # you will get a download URL for the .VHD image
+
+# Create a CDI Data Volume in Cluster having kubevirt cluster
+
+# create the VM(VMI) with the above data volume
+
+# Verify VMI is up using virtctl console 
+
+# stop the VM using virtctl(don't delete)
+
+# apply VM export spec, will create a export url for the data volume in .img format
+
+# download this exported image download using wget
+
+# convert this .img image to .qcow2 image
+
+# resize the disk size for qcow2 ~100GB
+
+# create containerd disk image using qcow2 image
+
+# upload this containerd disk image in ecr with new tags
