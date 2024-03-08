e5ad8afaf37b86e7d310ebfefd664659fef48b44
sergei-pyshnoi <sergei-pyshnoi@github.com>
2024-03-08 12:33:38 +0100

    Update Python.Tests.ps1

--- images/macos/scripts/tests/Python.Tests.ps1
+++ images/macos/scripts/tests/Python.Tests.ps1
@@ -3,30 +3,33 @@ Import-Module "$PSScriptRoot/Helpers.psm1" -DisableNameChecking
 
 $os = Get-OSVersion
 
-Describe "Python3" -Skip:($os.IsVenturaArm64 -or $os.IsSonomaArm64) {
+Describe "Python3" {
     It "Python 3 is available" {
         "python3 --version" | Should -ReturnZeroExitCode
     }
-
-    It "Python 3 is installed under /usr/local/bin" {
-        Get-ToolPath "python3" | Should -BeLike "/usr/local/bin*"
+    
+    if ($os.IsVenturaArm64 -or $os.IsSonomaArm64) {
+        It "Python 3 is installed under /opt/homebrew/bin/" {
+            Get-ToolPath "python3" | Should -BeLike "/opt/homebrew/bin/*"
+        }
+    } else {
+        It "Python 3 is installed under /usr/local/bin" {
+            Get-ToolPath "python3" | Should -BeLike "/usr/local/bin*"
+        }
     }
 
     It "Pip 3 is available" {
         "pip3 --version" | Should -ReturnZeroExitCode
     }
 
+    It "Pipx is available" {
+        "pipx --version" | Should -ReturnZeroExitCode
+    }
+
     It "Pip 3 and Python 3 came from the same brew formula" {
         $pip3Path = Split-Path (readlink (which pip3))
         $python3Path = Split-Path (readlink (which python3))
         $pip3Path | Should -BeExactly $python3Path
-    }
-
-}
-
-Describe "Pipx" {
-    It "Pipx is available" {
-        "pipx --version" | Should -ReturnZeroExitCode
     }
 
 }
