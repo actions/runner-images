21f5ee0084b2a941805727a8449ddb9ffab57670
Mikhail Koliada <mikhailkoliada@github.com>
2024-03-13 10:48:48 +0100

    [macOS] pin xcbeautify on OS12

--- images/macos/scripts/build/install-common-utils.sh
+++ images/macos/scripts/build/install-common-utils.sh
@@ -18,7 +18,13 @@ fi
 common_packages=$(get_toolset_value '.brew.common_packages[]')
 for package in $common_packages; do
     echo "Installing $package..."
-    brew_smart_install "$package"
+    if is_Monterey && [[ $package == "xcbeautify" ]]; then
+        # Pin the version on Monterey as 2.0.x requires Xcode >=15.0 which is not available on OS12
+        xcbeautify_path=$(download_with_retry "https://github.com/Homebrew/homebrew-core/blob/d3653e83f9c029a3fddb828ac804b07ac32f7b3b/Formula/x/xcbeautify.rb")
+        brew_smart_install "$xcbeautify_path"
+    else
+        brew_smart_install "$package"
+    fi
 done
 
 cask_packages=$(get_toolset_value '.brew.cask_packages[]')
