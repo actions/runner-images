6ae4e75201c70a090ad308099a190739a8b9316d
Mikhail Koliada <mikhailkoliada@github.com>
2024-03-13 13:12:05 +0100

    use raw

--- images/macos/scripts/build/install-common-utils.sh
+++ images/macos/scripts/build/install-common-utils.sh
@@ -20,7 +20,7 @@ for package in $common_packages; do
     echo "Installing $package..."
     if is_Monterey && [[ $package == "xcbeautify" ]]; then
         # Pin the version on Monterey as 2.0.x requires Xcode >=15.0 which is not available on OS12
-        xcbeautify_path=$(download_with_retry "https://github.com/Homebrew/homebrew-core/blob/d3653e83f9c029a3fddb828ac804b07ac32f7b3b/Formula/x/xcbeautify.rb")
+        xcbeautify_path=$(download_with_retry "https://raw.githubusercontent.com/Homebrew/homebrew-core/d3653e83f9c029a3fddb828ac804b07ac32f7b3b/Formula/x/xcbeautify.rb")
         brew install "$xcbeautify_path"
     else
         brew_smart_install "$package"
