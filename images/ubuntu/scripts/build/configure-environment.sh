6fc5c170b5c59aed7a91ca74e2ad6844c9368035
Leon Zandman <lzandman@rdw.nl>
2024-03-13 15:27:26 +0100

    Fixed some typos.

--- images/ubuntu/scripts/build/configure-environment.sh
+++ images/ubuntu/scripts/build/configure-environment.sh
@@ -19,7 +19,7 @@ set_etc_environment_variable "ACCEPT_EULA" "Y"
 mkdir -p /etc/skel/.config/configstore
 set_etc_environment_variable "XDG_CONFIG_HOME" '$HOME/.config'
 
-# Change waagent entries to use /mnt for swapfile
+# Change waagent entries to use /mnt for swap file
 sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
 sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g' /etc/waagent.conf
 sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=4096/g' /etc/waagent.conf
--- images/ubuntu/scripts/build/install-firefox.sh
+++ images/ubuntu/scripts/build/install-firefox.sh
@@ -9,7 +9,7 @@ source $HELPER_SCRIPTS/install.sh
 source $HELPER_SCRIPTS/etc-environment.sh
 
 # Mozillateam PPA is added manually because sometimes
-# lanuchad portal sends empty answer when trying to add it automatically
+# launchpad portal sends empty answer when trying to add it automatically
 
 REPO_URL="http://ppa.launchpad.net/mozillateam/ppa/ubuntu"
 GPG_FINGERPRINT="0ab215679c571d1c8325275b9bdb3d89ce49ec21"
@@ -27,7 +27,7 @@ rm $REPO_PATH
 # Document apt source repo's
 echo "mozillateam $REPO_URL" >> $HELPER_SCRIPTS/apt-sources.txt
 
-# add to gloabl system preferences for firefox locale en_US, because other browsers have en_US local.
+# add to global system preferences for firefox locale en_US, because other browsers have en_US local.
 # Default firefox local is en_GB
 echo 'pref("intl.locale.requested","en_US");' >> "/usr/lib/firefox/browser/defaults/preferences/syspref.js"
 
