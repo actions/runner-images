5a785d31f5868bccdcafd690c485f3c30a76d7af
Henry Skiba <henry.skiba@frgrisk.com>
2024-03-25 16:32:02 +0800

    globally install playwright and dependencies

--- images/ubuntu/scripts/build/install-playwright.sh
+++ images/ubuntu/scripts/build/install-playwright.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 ################################################################################
-##  File:  install-apt-playwright.sh
+##  File:  install-playwright.sh
 ##  Desc:  Install playwright dependencies
 ################################################################################
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
 
-playwright_packages=$(get_toolset_value .apt.playwright_packages[])
-apt-get install -y --no-install-recommends $playwright_packages
+npm install -g playwright
+npx playwright install --with-deps
