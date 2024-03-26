1aacece63c7328dcfc629bde95f7136f18b58534
Leon Zandman <leon@wirwar.com>
2024-03-26 14:49:06 +0100

    Add WiX bin folder to PATH.

--- images/windows/scripts/build/Install-Wix.ps1
+++ images/windows/scripts/build/Install-Wix.ps1
@@ -5,4 +5,9 @@
 
 Install-ChocoPackage wixtoolset -ArgumentList "--force"
 
+# Add WiX bin folder to the PATH
+$currentPath = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+$NewPath = "$currentPath;$($Env:WIX)\bin"
+[Environment]::SetEnvironmentVariable("PATH", $NewPath, "Machine")
+
 Invoke-PesterTests -TestFile "Wix"
