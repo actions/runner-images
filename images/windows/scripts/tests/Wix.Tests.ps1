deabec24ac3d0404b0298ec393beaf20fc4f35ed
Leon Zandman <leon@wirwar.com>
2024-03-27 10:41:31 +0100

    Added test to check if Wix Toolset binaries are available in PATH.

--- images/windows/scripts/tests/Wix.Tests.ps1
+++ images/windows/scripts/tests/Wix.Tests.ps1
@@ -1,11 +1,5 @@
 Describe "Wix" {
-    BeforeAll {
-      $regKey = "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-      $installedApplications = Get-ItemProperty -Path $regKey
-      $version = ($installedApplications | Where-Object { $_.BundleCachePath -imatch ".*\\WiX\d*\.exe$" } | Select-Object -First 1).DisplayName
-    }
-
-    It "Wix Toolset version from registry" {
-      $version | Should -Not -BeNullOrEmpty
+    It "Wix Toolset binaries have been installed and are available in env:Path" {
+        "candle.exe -?" | Should -ReturnZeroExitCode
     }
 }
