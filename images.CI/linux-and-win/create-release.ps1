6cef202f6877f64f13f8b31c056bb7cdefa32676
ddms <69804007+DominionDMS-Build@users.noreply.github.com>
2024-04-08 18:41:53 -0500

    Update create-release.ps1

--- images.CI/linux-and-win/create-release.ps1
+++ images.CI/linux-and-win/create-release.ps1
@@ -1,45 +1,33 @@
 param(
-    [Parameter (Mandatory)] [UInt32] $BuildId,
-    [Parameter (Mandatory)] [string] $Organization,
-    [Parameter (Mandatory)] [string] $Project,
-    [string] $ImageType,
-    [string] $ManagedImageName,
-    [Parameter (Mandatory)] [string] $DefinitionId,
-    [Parameter (Mandatory)] [string] $AccessToken
+    [Parameter(Mandatory)] [UInt32] $BuildId,
+    [Parameter(Mandatory)] [string] $Organization,
+    [Parameter(Mandatory)] [string] $Project,
+    [Parameter(Mandatory)] [string] $ImageType,
+    [Parameter(Mandatory)] [string] $ManagedImageName,
+    [Parameter(Mandatory)] [string] $DefinitionId,
+    [Parameter(Mandatory)] [string] $AccessToken
 )
 
-
-if (-not $ImageType){
-  $ImageType = $env:ImageType
-  }
-
-if (-not $ManagedImageName) {
-  $ManagedImageName = $env:ManagedImageName
-}
-
+# Convert parameters to JSON body directly to avoid reliance on environment variables
 $Body = @{
     definitionId = $DefinitionId
     variables = @{
-      ImageBuildId = @{
-        value = $BuildId
-      }
-      ImageType = @{
-        value = $ImageType
-      }
-      ManagedImageName = @{
-        value = $ManagedImageName
-      }
+      ImageBuildId = @{ value = $BuildId }
+      ImageType = @{ value = $ImageType }
+      ManagedImageName = @{ value = $ManagedImageName }
     }
-    isDraft = "false"
-} | ConvertTo-Json -Depth 3
+    isDraft = $false
+} | ConvertTo-Json -Depth 5
 
-$URL = "https://vsrm.dev.azure.com/$Organization/$Project/_apis/release/releases?api-version=5.1"
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("'':${AccessToken}"))
+$URL = "https://vsrm.dev.azure.com/$Organization/$Project/_apis/release/releases?api-version=6.0"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":${AccessToken}"))
 $headers = @{
-    Authorization = "Basic ${base64AuthInfo}"
+    Authorization = "Basic $base64AuthInfo"
 }
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls13
-$NewRelease = Invoke-RestMethod $URL -Body $Body -Method "POST" -Headers $headers -ContentType "application/json"
-
-Write-Host "Created release: $($NewRelease._links.web.href)"
+try {
+    $NewRelease = Invoke-RestMethod $URL -Body $Body -Method Post -Headers $headers -ContentType "application/json"
+    Write-Host "Created release: $($NewRelease._links.web.href)"
+} catch {
+    Write-Error "Failed to create release: $_"
+}
