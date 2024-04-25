d2e574166e55d872231db9c8c1285c30a1dd1c23
Mikhail Koliada <88318005+mikhailkoliada@users.noreply.github.com>
2024-04-25 10:22:21 +0200

    [Ubuntu] fix overwrite packer files on unpack

--- images/ubuntu/scripts/build/install-packer.sh
+++ images/ubuntu/scripts/build/install-packer.sh
@@ -10,6 +10,6 @@ source $HELPER_SCRIPTS/install.sh
 # Install Packer
 download_url=$(curl -fsSL https://api.releases.hashicorp.com/v1/releases/packer/latest | jq -r '.builds[] | select((.arch=="amd64") and (.os=="linux")).url')
 archive_path=$(download_with_retry "$download_url")
-unzip -qq "$archive_path" -d /usr/local/bin
+unzip -o -qq "$archive_path" -d /usr/local/bin
 
 invoke_tests "Tools" "Packer"
