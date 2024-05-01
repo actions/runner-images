adef5d3c2e3c3c4c92e74c71bdcb7b0b6da0e477
sssharif <sssharif@github.com>
2024-05-01 10:15:36 -0400

    Remove zip versions from Azuremodules in ubuntu

--- images/ubuntu/scripts/build/Install-PowerShellAzModules.ps1
+++ images/ubuntu/scripts/build/Install-PowerShellAzModules.ps1
@@ -24,20 +24,6 @@ foreach ($module in $modules) {
 
     $assets = Invoke-RestMethod $module.url
 
-    # Get github release asset for each version
-    foreach ($toolVersion in $module.zip_versions) {
-        $asset = $assets | Where-Object version -eq $toolVersion `
-            | Select-Object -ExpandProperty files `
-            | Select-Object -First 1
-
-        if (-not $asset) {
-            Write-Host "Asset for ${moduleName} ${toolVersion} was not found in versions manifest"
-            exit 1
-        }
-
-        Write-Host "Downloading asset for ${moduleName} ${toolVersion}: $($asset.filename)"
-        Invoke-DownloadWithRetry $asset.download_url -Destination "$installPSModulePath/$($asset.filename)"
-    }
 }
 
 Invoke-PesterTests -TestFile "PowerShellModules" -TestName "AzureModules"
