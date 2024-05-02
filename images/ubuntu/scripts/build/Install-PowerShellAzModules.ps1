8c57c5e330f918cff20c90832a7feef6a5cf1039
Vidya Sagar Nimmagaddi <vidyasagarnimmagaddi@Vidyas-MacBook-Pro.local>
2024-05-02 17:20:52 +0530

    Commented AZ zip versions

--- images/ubuntu/scripts/build/Install-PowerShellAzModules.ps1
+++ images/ubuntu/scripts/build/Install-PowerShellAzModules.ps1
@@ -25,7 +25,7 @@ foreach ($module in $modules) {
     $assets = Invoke-RestMethod $module.url
 
     # Get github release asset for each version
-    foreach ($toolVersion in $module.zip_versions) {
+    <#foreach ($toolVersion in $module.zip_versions) {
         $asset = $assets | Where-Object version -eq $toolVersion `
             | Select-Object -ExpandProperty files `
             | Select-Object -First 1
@@ -37,7 +37,7 @@ foreach ($module in $modules) {
 
         Write-Host "Downloading asset for ${moduleName} ${toolVersion}: $($asset.filename)"
         Invoke-DownloadWithRetry $asset.download_url -Destination "$installPSModulePath/$($asset.filename)"
-    }
+    }#>
 }
 
 Invoke-PesterTests -TestFile "PowerShellModules" -TestName "AzureModules"
