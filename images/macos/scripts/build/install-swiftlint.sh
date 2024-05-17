49d462f0680165f3d2eb4f1b263eda856a980535
Erik Bershel <erik-bershel@github.com>
2024-05-17 17:51:03 +0200

    [macOS] Pin Swiftlint on macOS-12 due to Xcode 15.3 requirements

--- images/macos/scripts/build/install-swiftlint.sh
+++ images/macos/scripts/build/install-swiftlint.sh
@@ -14,6 +14,13 @@ if is_BigSur; then
 
   curl -fsSL $FORMULA_URL > $(find $(brew --repository) -name swiftlint.rb)
   HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_FROM_API=1 brew install swiftlint
+elif is_Monterey; then
+  # SwiftLint now requires Xcode 15.3 or higher to build https://github.com/realm/SwiftLint/releases/tag/0.55.1
+  COMMIT=d91dabd087cb0b906c92a825df9e5e5e1a4f59f8
+  FORMULA_URL="https://raw.githubusercontent.com/Homebrew/homebrew-core/$COMMIT/Formula/s/swiftlint.rb"
+
+  curl -fsSL $FORMULA_URL > $(find $(brew --repository) -name swiftlint.rb)
+  HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_FROM_API=1 brew install swiftlint
 else
   brew_smart_install "swiftlint"
 fi
