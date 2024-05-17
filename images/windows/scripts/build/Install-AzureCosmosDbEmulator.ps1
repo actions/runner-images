26f8ba8e5e64e59fe79c5038a4090fa1b146c597
Erik Bershel <erik-bershel@github.com>
2024-05-17 17:38:03 +0200

    Replace with SHA256

--- images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
+++ images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
@@ -5,6 +5,6 @@
 
 Install-Binary -Type MSI `
     -Url "https://aka.ms/cosmosdb-emulator" `
-    -ExpectedSignature "1226440E939A24EB202C2A517CE13F8326EFDE60"
+    -ExpectedSHA256Sum "e2915c766a02d0c0703ae12b28ef992c4f62fe33e65eb482abeeaaaf0ea85237"
 
 Invoke-PesterTests -TestFile "Tools" -TestName "Azure Cosmos DB Emulator"
