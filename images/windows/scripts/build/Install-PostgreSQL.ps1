54112707dd4f1c2db47337f54eefb98d5a3c9cae
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2024-06-07 18:26:36 +0200

    Fix if statement

--- images/windows/scripts/build/Install-PostgreSQL.ps1
+++ images/windows/scripts/build/Install-PostgreSQL.ps1
@@ -11,34 +11,33 @@ $pgPwd = "root"
 [Environment]::SetEnvironmentVariable("PGUSER", $pgUser, "Machine")
 [Environment]::SetEnvironmentVariable("PGPASSWORD", $pgPwd, "Machine")
 
-# Define latest available version to install based on version specified in the toolset
 $toolsetVersion = (Get-ToolsetContent).postgresql.version
-$getPostgreReleases = Invoke-WebRequest -Uri "https://git.postgresql.org/gitweb/?p=postgresql.git;a=tags" -UseBasicParsing
-# Getting all links matched to the pattern (e.g.a=log;h=refs/tags/REL_14)
-$targetReleases = $getPostgreReleases.Links.href | Where-Object { $_ -match "a=log;h=refs/tags/REL_$toolsetVersion" }
-[Int32] $outNumber = $null
-$minorVersions = @()
-foreach ($release in $targetReleases) {
-    $version = $release.split('/')[-1]
-    # Checking if the latest symbol of the release version is actually a number. If yes, add to $minorVersions array
-    if ([Int32]::TryParse($($version.Split('_')[-1]), [ref] $outNumber)) {
-        $minorVersions += $outNumber
-    }
-}
-# Sorting and getting the last one
-$targetMinorVersions = ($minorVersions | Sort-Object)[-1]
-
-# In order to get rid of error messages (we know we will have them), force ErrorAction to SilentlyContinue
-$errorActionOldValue = $ErrorActionPreference
-$ErrorActionPreference = 'SilentlyContinue'
-
-$versionMatches = $toolsetVersion | Select-String -Pattern '\d+\.\d+\.\d+'
-if ($null -ne $versionMatches) {
+if ($null -ne ($toolsetVersion | Select-String -Pattern '\d+\.\d+\.\d+')) {
     $majorVersion = ([version]$toolsetVersion).Major
     $minorVersion = ([version]$toolsetVersion).Minor
     $patchVersion = ([version]$toolsetVersion).Build
     $installerUrl = "https://get.enterprisedb.com/postgresql/postgresql-$majorVersion.$minorVersion-$patchVersion-windows-x64.exe"
 } else {
+    # Define latest available version to install based on version specified in the toolset
+    $getPostgreReleases = Invoke-WebRequest -Uri "https://git.postgresql.org/gitweb/?p=postgresql.git;a=tags" -UseBasicParsing
+    # Getting all links matched to the pattern (e.g.a=log;h=refs/tags/REL_14)
+    $targetReleases = $getPostgreReleases.Links.href | Where-Object { $_ -match "a=log;h=refs/tags/REL_$toolsetVersion" }
+    [Int32] $outNumber = $null
+    $minorVersions = @()
+    foreach ($release in $targetReleases) {
+        $version = $release.split('/')[-1]
+        # Checking if the latest symbol of the release version is actually a number. If yes, add to $minorVersions array
+        if ([Int32]::TryParse($($version.Split('_')[-1]), [ref] $outNumber)) {
+            $minorVersions += $outNumber
+        }
+    }
+    # Sorting and getting the last one
+    $targetMinorVersions = ($minorVersions | Sort-Object)[-1]
+
+    # In order to get rid of error messages (we know we will have them), force ErrorAction to SilentlyContinue
+    $errorActionOldValue = $ErrorActionPreference
+    $ErrorActionPreference = 'SilentlyContinue'
+
     # Install latest PostgreSQL
     # Starting from number 9 and going down, check if the installer is available. If yes, break the loop.
     # If an installer with $targetMinorVersions is not to be found, the $targetMinorVersions will be decreased by 1
