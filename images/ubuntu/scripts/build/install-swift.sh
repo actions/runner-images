bc0c1525799f1a3abd3987ab34295b21937cc4f3
Lukas Bühler <59952941+lukasbbuehler@users.noreply.github.com>
2024-06-10 15:08:25 +0200

    Update install-swift.sh
    Changed to hkps and port 443 for added security

--- images/ubuntu/scripts/build/install-swift.sh
+++ images/ubuntu/scripts/build/install-swift.sh
@@ -18,7 +18,7 @@ archive_path=$(download_with_retry "$archive_url")
 
 # Verifying PGP signature using official Swift PGP key. Referring to https://www.swift.org/install/linux/#Installation-via-Tarball
 # Download and import Swift PGP keys
-gpg --keyserver hkp://keyserver.ubuntu.com:80 \
+gpg --keyserver hkps://keyserver.ubuntu.com:443 \
       --recv-keys \
       '7463 A81A 4B2E EA1B 551F  FBCF D441 C977 412B 37AD' \
       '1BE1 E29A 084C B305 F397  D62A 9F59 7F4D 21A5 6D5F' \
@@ -28,7 +28,7 @@ gpg --keyserver hkp://keyserver.ubuntu.com:80 \
       'A62A E125 BBBF BB96 A6E0  42EC 925C C1CC ED3D 1561' \
       '8A74 9566 2C3C D4AE 18D9  5637 FAF6 989E 1BC1 6FEA' \
       'E813 C892 820A 6FA1 3755  B268 F167 DF1A CF9C E069'
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --refresh-keys Swift
+gpg --keyserver hkps://keyserver.ubuntu.com:443 --refresh-keys Swift
 
 # Download and verify signature
 signature_path=$(download_with_retry "${archive_url}.sig")
