a537e4477d61204624fc870032021e6b1e7964f2
Azure Way <87411939+karol-pieciukiewicz@users.noreply.github.com>
2024-06-13 21:35:04 +0200

    Update image terraform script

--- terraform/image-gallery.tf
+++ terraform/image-gallery.tf
@@ -9,26 +9,9 @@ data "azurerm_resource_group" "automation_resource_group" {
   name     = "rg-${local.prefix}"
 }
 
-data "azurerm_shared_image_gallery" "example" {
+data "azurerm_shared_image_gallery" "imageGallery" {
   name                = "azureway_community_gallery"
   resource_group_name = data.azurerm_resource_group.automation_resource_group.name
-}
-
-resource "azurerm_shared_image_gallery" "imageGallery" {
-  name                = "azureway_community_gallery"
-  resource_group_name = data.azurerm_resource_group.automation_resource_group.name
-  location            = data.azurerm_resource_group.automation_resource_group.location
-
-  sharing {
-    permission = "Community"
-
-    community_gallery {
-      eula ="https://github.com/azure-way/actions-runner-images/blob/main/LICENSE" 
-      prefix ="azureway"
-      publisher_email ="karol@pieciukiewicz.com" 
-      publisher_uri ="https://azureway.cloud"
-    }
-  }  
 }
 
 resource "azurerm_shared_image" "image" {
