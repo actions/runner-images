d6a74be8937a121cb6477af7196038691f435d5b
kishorekumar-anchala <168699259+kishorekumar-anchala@users.noreply.github.com>
2024-06-19 14:56:17 +0530

    Update Install-GoogleCloudCLI.ps1

--- images/windows/scripts/build/Install-GoogleCloudCLI.ps1
+++ images/windows/scripts/build/Install-GoogleCloudCLI.ps1
@@ -7,6 +7,6 @@
 Install-Binary `
   -Url 'https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe' `
   -InstallArgs @("/S", "/allusers", "/noreporting") `
-  -ExpectedSignature '607A3EDAA64933E94422FC8F0C8038850590986C'
+  -ExpectedSignature '607A3EDAA64933E94422FC8F0C80388E0590986C'
 
 Invoke-PesterTests -TestFile "Tools" -TestName "GoogleCloudCLI"
