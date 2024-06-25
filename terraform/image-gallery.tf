b13d11b6f8c2b6e302111700f5aa9d9838f221e2
Azure Way <87411939+karol-pieciukiewicz@users.noreply.github.com>
2024-06-25 17:35:36 +0200

    Move image creation to the base terraform

--- terraform/image-gallery.tf
+++ terraform/image-gallery.tf
@@ -14,19 +14,25 @@ data "azurerm_shared_image_gallery" "imageGallery" {
   resource_group_name = data.azurerm_resource_group.automation_resource_group.name
 }
 
-resource "azurerm_shared_image" "image" {
+data "azurerm_shared_image" "image" {
   name                = "azureway-${var.short_image_name}-${var.image_type_version}"
-  gallery_name        = data.azurerm_shared_image_gallery.imageGallery.name
+  gallery_name        = "azureway_community_gallery"
   resource_group_name = data.azurerm_resource_group.automation_resource_group.name
-  location            = data.azurerm_resource_group.automation_resource_group.location
-  os_type             = var.os_type_map[var.image_type]
-
-  identifier {
-    publisher = var.self_hosted_image_publisher
-    offer     = "${var.self_hosted_image_offer}_${var.image_type}-${var.image_type_version}"
-    sku       = var.self_hosted_image_sku
-  }
 }
+
+# resource "azurerm_shared_image" "image" {
+#   name                = "azureway-${var.short_image_name}-${var.image_type_version}"
+#   gallery_name        = data.azurerm_shared_image_gallery.imageGallery.name
+#   resource_group_name = data.azurerm_resource_group.automation_resource_group.name
+#   location            = data.azurerm_resource_group.automation_resource_group.location
+#   os_type             = var.os_type_map[var.image_type]
+
+#   identifier {
+#     publisher = var.self_hosted_image_publisher
+#     offer     = "${var.self_hosted_image_offer}_${var.image_type}-${var.image_type_version}"
+#     sku       = var.self_hosted_image_sku
+#   }
+# }
 
 resource "time_rotating" "time-rotation" {
   rfc3339         = "2024-06-13T00:00:00Z"
@@ -73,7 +79,7 @@ resource "null_resource" "packer_runner" {
              -var "virtual_network_name=$null" \
              -var "virtual_network_resource_group_name=$null" \
              -var "virtual_network_subnet_name=$null" \
-             -var "managed_image_name=${azurerm_shared_image.image.name}" \
+             -var "managed_image_name=${data.azurerm_shared_image.image.name}" \
              -var "managed_image_resource_group_name=${data.azurerm_resource_group.automation_resource_group.name}" \
              -color=false \
              "${local.imagePath}" 
@@ -88,7 +94,7 @@ resource "null_resource" "packer_runner" {
 }
 
 data "azurerm_image" "image" {
-  name                = azurerm_shared_image.image.name
+  name                = data.azurerm_shared_image.image.name
   resource_group_name = data.azurerm_resource_group.automation_resource_group.name
 
   depends_on = [null_resource.packer_runner]
@@ -96,14 +102,14 @@ data "azurerm_image" "image" {
 
 resource "azurerm_shared_image_version" "example" {
   name                = var.image_version
-  gallery_name        = azurerm_shared_image.image.gallery_name
-  image_name          = azurerm_shared_image.image.name
-  resource_group_name = azurerm_shared_image.image.resource_group_name
-  location            = azurerm_shared_image.image.location
+  gallery_name        = data.azurerm_shared_image.image.gallery_name
+  image_name          = data.azurerm_shared_image.image.name
+  resource_group_name = data.azurerm_shared_image.image.resource_group_name
+  location            = data.azurerm_shared_image.image.location
   managed_image_id    = data.azurerm_image.image.id
 
   target_region {
-    name                   = azurerm_shared_image.image.location
+    name                   = data.azurerm_shared_image.image.location
     regional_replica_count = 1
     storage_account_type   = "Standard_LRS"
   }
--- terraform_base/image-gallery.tf
+++ terraform_base/image-gallery.tf
@@ -25,3 +25,73 @@ resource "azurerm_resource_group" "automation_resource_group" {
   name     = "rg-${local.prefix}"
   location = var.location
 }
+
+resource "azurerm_shared_image" "ubuntu_2004" {
+  name                = "azureway-u-20.04"
+  gallery_name        = azurerm_shared_image_gallery.imageGallery.name
+  resource_group_name = azurerm_resource_group.automation_resource_group.name
+  location            = azurerm_resource_group.automation_resource_group.location
+  os_type             = "Linux"
+
+  identifier {
+    publisher = "Azureway"
+    offer     = "GitHubActionsRunner_ubuntu-20.04"
+    sku       = "Free"
+  }
+}
+
+resource "azurerm_shared_image" "ubuntu_2204" {
+  name                = "azureway-u-22.04"
+  gallery_name        = azurerm_shared_image_gallery.imageGallery.name
+  resource_group_name = azurerm_resource_group.automation_resource_group.name
+  location            = azurerm_resource_group.automation_resource_group.location
+  os_type             = "Linux"
+
+  identifier {
+    publisher = "Azureway"
+    offer     = "GitHubActionsRunner_ubuntu-22.04"
+    sku       = "Free"
+  }
+}
+
+resource "azurerm_shared_image" "ubuntu_2404" {
+  name                = "azureway-u-24.04"
+  gallery_name        = azurerm_shared_image_gallery.imageGallery.name
+  resource_group_name = azurerm_resource_group.automation_resource_group.name
+  location            = azurerm_resource_group.automation_resource_group.location
+  os_type             = "Linux"
+
+  identifier {
+    publisher = "Azureway"
+    offer     = "GitHubActionsRunner_ubuntu-24.04"
+    sku       = "Free"
+  }
+}
+
+resource "azurerm_shared_image" "windows_2019" {
+  name                = "azureway-w-2019"
+  gallery_name        = azurerm_shared_image_gallery.imageGallery.name
+  resource_group_name = azurerm_resource_group.automation_resource_group.name
+  location            = azurerm_resource_group.automation_resource_group.location
+  os_type             = "Windows"
+
+  identifier {
+    publisher = "Azureway"
+    offer     = "GitHubActionsRunner_windows-2019"
+    sku       = "Free"
+  }
+}
+
+resource "azurerm_shared_image" "windows_2022" {
+  name                = "azureway-w-2022"
+  gallery_name        = azurerm_shared_image_gallery.imageGallery.name
+  resource_group_name = azurerm_resource_group.automation_resource_group.name
+  location            = azurerm_resource_group.automation_resource_group.location
+  os_type             = "Windows"
+
+  identifier {
+    publisher = "Azureway"
+    offer     = "GitHubActionsRunner_windows-2022"
+    sku       = "Free"
+  }
+}
