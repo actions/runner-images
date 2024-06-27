63e8de6dfa897f5ba062905764dbe0195e4c5db7
Azure Way <87411939+karol-pieciukiewicz@users.noreply.github.com>
2024-06-27 22:30:01 +0200

    Update cloud foundry signature thumbprint

--- images/windows/scripts/build/Install-CloudFoundryCli.ps1
+++ images/windows/scripts/build/Install-CloudFoundryCli.ps1
@@ -20,7 +20,7 @@ Expand-7ZipArchive -Path $cloudFoundryArchPath -DestinationPath $cloudFoundryCli
 Add-MachinePathItem $cloudFoundryCliPath
 
 # Validate cf signature
-$cloudFoundrySignatureThumbprint = "4C69EDD13930ED01B83DD1D17B09C434DC1F2177"
+$cloudFoundrySignatureThumbprint = "2C6B2F1562698503A6E4A25F2DF058E12E23A190"
 Test-FileSignature -Path "$cloudFoundryCliPath\cf.exe" -ExpectedThumbprint $cloudFoundrySignatureThumbprint
 
 Invoke-PesterTests -TestFile "CLI.Tools" -TestName "CloudFoundry CLI"
