f50c3c301c063d92999b62d42fc463913fa5d59d
Sarathraj Srinivasan <sarathrajsrinivasan@github.com>
2024-07-16 01:42:07 -0500

    Add parallels desktop extension for macOS14 intel machines

--- images/macos/scripts/docs-gen/Generate-SoftwareReport.ps1
+++ images/macos/scripts/docs-gen/Generate-SoftwareReport.ps1
@@ -311,7 +311,7 @@ if ($os.IsBigSur -or $os.IsMonterey) {
     $miscellaneous.AddToolVersion("Zlib", $(Get-ZlibVersion))
 }
 
-if ($os.IsMonterey) {
+if ($os.IsMonterey -or $os.IsSonomaX64) {
     $miscellaneousEnv = $miscellaneous.AddHeader("Environment variables")
     $miscellaneousEnv.AddTable($(Build-MiscellaneousEnvironmentTable))
 
