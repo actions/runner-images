7665ed4ef98fd2694532d99d587f72566aa7d41a
RaviAkshintala <167848931+RaviAkshintala@users.noreply.github.com>
2024-08-08 10:07:02 +0530

    removing the docker composev1 (#10407)
    Co-authored-by: Ravi Akshintala <raviakshintalagithub.com@Ravis-Laptop.local>

--- images/windows/scripts/build/Install-DockerCompose.ps1
+++ images/windows/scripts/build/Install-DockerCompose.ps1
@@ -2,20 +2,6 @@
 ##  File:  Install-Docker-Compose.ps1
 ##  Desc:  Install Docker Compose.
 ################################################################################
-
-Write-Host "Install-Package Docker-Compose v1"
-$dockerComposev1Url = "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Windows-x86_64.exe"
-$checksumsUrl = "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Windows-x86_64.exe.sha256"
-$dockerComposev1Dir = "C:\ProgramData\docker-compose"
-New-Item -Path $dockerComposev1Dir -ItemType Directory
-$externalHash = Get-ChecksumFromUrl -Type "SHA256" `
-    -Url $checksumsUrl `
-    -FileName (Split-Path $dockerComposev1Url -Leaf)
-$dockerComposev1Path = Invoke-DownloadWithRetry -Url $dockerComposev1Url -Path "$dockerComposev1Dir\docker-compose.exe"
-Test-FileChecksum $dockerComposev1Path -ExpectedSHA256Sum $externalHash
-Add-MachinePathItem $dockerComposev1Dir
-Update-Environment
-
 Write-Host "Install-Package Docker-Compose v2"
 $toolsetVersion = (Get-ToolsetContent).docker.components.compose
 $composeVersion = (Get-GithubReleasesByVersion -Repo "docker/compose" -Version "${toolsetVersion}").version
