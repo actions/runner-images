f8049e323bcaae26ce9a571195eeda162a99021c
vidyasagarnimmagaddi <160703874+vidyasagarnimmagaddi@users.noreply.github.com>
2024-08-22 00:27:56 +0530

    adding_kotlin in ubuntu_24 (#10474)

--- images/ubuntu/scripts/tests/Tools.Tests.ps1
+++ images/ubuntu/scripts/tests/Tools.Tests.ps1
@@ -390,7 +390,7 @@ Describe "yq" {
     }
 }
 
-Describe "Kotlin" -Skip:((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22))) {
+Describe "Kotlin" {
     It "kapt" {
         "kapt -version" | Should -ReturnZeroExitCode
     }
