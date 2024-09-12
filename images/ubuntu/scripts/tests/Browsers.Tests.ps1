ea8a7a8f0139b0482cfa6e9f7638758750d38c8c
hemanthmanga <166606741+hemanthmanga@users.noreply.github.com>
2024-09-13 01:14:17 +0530

    Adding MsEdge in ubuntu-24 (#10597)

--- images/ubuntu/scripts/tests/Browsers.Tests.ps1
+++ images/ubuntu/scripts/tests/Browsers.Tests.ps1
@@ -24,7 +24,7 @@ Describe "Chrome" {
     }
 }
 
-Describe "Edge" -Skip:((-not (Test-IsUbuntu20)) -and (-not (Test-IsUbuntu22))) {
+Describe "Edge" {
     It "Edge" {
         "microsoft-edge --version" | Should -ReturnZeroExitCode
     }
