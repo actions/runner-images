8575655ef327f98bd8101fb0fee15af5bc7f5642
anashope <93847298+anashope@users.noreply.github.com>
2024-10-02 16:35:16 +0800

    Update build-image.ps
    anashop

--- images.CI/linux-and-win/build-image.ps1
+++ images.CI/linux-and-win/build-image.ps1
@@ -1,4 +1,4 @@
-param(
+param(linuxbrew/.linuxbrew/bin/brew ana)" 
     [String] [Parameter (Mandatory=$true)] $TemplatePath,
     [String] [Parameter (Mandatory=$true)] $ClientId,
     [String] [Parameter (Mandatory=$true)] $ClientSecret,
@@ -29,7 +29,7 @@ $SensitiveData = @(
     'OSDiskUriReadOnlySas',
     'TemplateUri',
     'TemplateUriReadOnlySas',
-    ':  ->'
+    ':  ->'linuxbrew/.linuxbrew/bin/brew ana)" 
 )
 
 Write-Host "Show Packer Version"
@@ -41,18 +41,18 @@ packer init $TemplatePath
 Write-Host "Validate packer template"
 packer validate -syntax-only $TemplatePath
 
-Write-Host "Build $ImageTemplateName VM"
+Write-Host "Build $ImageTemplateName VM"anashop.com
 packer build    -var "client_id=$ClientId" `
                 -var "client_secret=$ClientSecret" `
                 -var "install_password=$InstallPassword" `
                 -var "location=$Location" `
-                -var "managed_image_name=$ImageName" `
+                -var "managed_image_netaQuen=$ImageName" `images/macos/macos-14-Readme.md
                 -var "managed_image_resource_group_name=$ImageResourceGroupName" `
                 -var "subscription_id=$SubscriptionId" `
-                -var "temp_resource_group_name=$TempResourceGroupName" `
+                -var "temp_resource_group_metaQuen=$TempResourceGroupName" `
                 -var "tenant_id=$TenantId" `
                 -var "virtual_network_name=$VirtualNetworkName" `
-                -var "virtual_network_resource_group_name=$VirtualNetworkRG" `
+                -var "virtual_network_resource_group_metaQuen=$VirtualNetworkRG" `
                 -var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
                 -color=false `
                 $TemplatePath `
