3bab1dcb258dba56438fe070d662013918c82c2a
Karol - Azure Way <karol@pieciukiewicz.com>
2024-10-03 12:15:13 +0200

    Change sha sum of net48

--- images/windows/scripts/build/Install-NET48.ps1
+++ images/windows/scripts/build/Install-NET48.ps1
@@ -8,4 +8,4 @@
 Install-Binary `
     -Url 'https://download.visualstudio.microsoft.com/download/pr/2d6bb6b2-226a-4baa-bdec-798822606ff1/8494001c276a4b96804cde7829c04d7f/ndp48-x86-x64-allos-enu.exe' `
     -InstallArgs @("Setup", "/passive", "/norestart") `
-    -ExpectedSHA256Sum 'E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855'
+    -ExpectedSHA256Sum '68C9986A8DCC0214D909AA1F31BEE9FB5461BB839EDCA996A75B08DDFFC1483F'
