07f291cacf87d07fa5a54728f5988e3d9444754b
susmitamane <susmitamane@github.com>
2024-11-08 17:51:52 +0530

    [MacOS] Add openssl@1.1 back.

--- images/macos/scripts/build/install-openssl.sh
+++ images/macos/scripts/build/install-openssl.sh
@@ -8,8 +8,8 @@ source ~/utils/utils.sh
 
 echo "Install openssl@1.1"
 
-wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
-tar -xvf openssl-1.1.1w.tar.gz
+openssl_pkg=$(download_with_retry "https://www.openssl.org/source/openssl-1.1.1w.tar.gz")
+tar -xvf $openssl_pkg
 cd openssl-1.1.1w
 ./config --prefix=/usr/local/openssl
 make
