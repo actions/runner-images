08198442380d4f43da31c30297980945d77772db
sarathrajsrinivasan <sarathrajsrinivasan@github.com>
2024-11-07 23:12:29 -0600

    [macOS] Add openssl 1.1 back

--- images/macos/scripts/build/install-openssl.sh
+++ images/macos/scripts/build/install-openssl.sh
@@ -17,6 +17,11 @@ sudo make install
 export OPENSSL="/usr/local/openssl/bin"
 echo "export OPENSSL=${OPENSSL}" >> ${HOME}/.bashrc
 
-ln -sf /usr/local/openssl/bin/openssl /usr/local/bin/openssl
+if ! is_Arm64; then
+  ln -sf ${OPENSSL}/openssl /usr/local/bin/openssl
+  ln -sf ${OPENSSL}/openssl /usr/local/opt/openssl
+else
+  ln -sf ${OPENSSL}/openssl /opt/homebrew/bin/openssl
+fi
 
 invoke_tests "OpenSSL"
