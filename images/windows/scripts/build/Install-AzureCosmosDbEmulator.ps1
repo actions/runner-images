cc791c7970e9c16bddfcfcea374abd465020a205
kishorekumar-anchala <168699259+kishorekumar-anchala@users.noreply.github.com>
2024-11-20 14:53:46 +0000

    [Windows] Fix AzCli Signature and Install Azure CosmosDb Emulator thumbprint (#11004)
    * Fix azcli,cosmosdb and remove vscomponent
    
    * Update visual studio signature
    
    * revert toolset-2022.json changes

--- images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
+++ images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
@@ -5,6 +5,6 @@
 
 Install-Binary -Type MSI `
     -Url "https://aka.ms/cosmosdb-emulator" `
-    -ExpectedSHA256Sum "DB9D5E496C5FDAE17C12C03385D2BAC973DA61C280023D9FDC9A6020220BEE41"
+    -ExpectedSHA256Sum "1EF64CF9CD13C611AAC394113A703AF6F525C7C39BFB6B4C3F884D57F6F1D1CF"
 
 Invoke-PesterTests -TestFile "Tools" -TestName "Azure Cosmos DB Emulator"
