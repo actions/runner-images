9994f40b3cffce1173a2eccce92526d75b96a0cb
Dan Merry <dan.merry@accurx.com>
2024-11-28 09:41:11 +0000

    update link

--- images/ubuntu/scripts/build/install-powershell.sh
+++ images/ubuntu/scripts/build/install-powershell.sh
@@ -12,7 +12,7 @@ pwsh_version=$(get_toolset_value .pwsh.version)
 
 # Install Powershell
 if is_ubuntu24; then
-    dependency_path=$(download_with_retry "http://mirrors.kernel.org/ubuntu/pool/main/i/icu/libicu72_72.1-3ubuntu2_amd64.deb")
+    dependency_path=$(download_with_retry "http://archive.ubuntu.com/ubuntu/pool/main/i/icu/libicu74_74.2-1ubuntu4_amd64.deb")
     sudo dpkg -i "$dependency_path"
     package_path=$(download_with_retry "https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/powershell-lts_7.4.2-1.deb_amd64.deb")
     sudo dpkg -i "$package_path"
