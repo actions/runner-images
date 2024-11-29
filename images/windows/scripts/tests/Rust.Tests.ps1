c991a3a6f9f350aad255686f8667296de9fd2b9b
Alexey-Ayupov <116575425+Alexey-Ayupov@users.noreply.github.com>
2024-11-29 11:08:29 +0100

    [Windows] Add Windows 2025 code (#11037)

--- images/windows/scripts/tests/Rust.Tests.ps1
+++ images/windows/scripts/tests/Rust.Tests.ps1
@@ -5,15 +5,23 @@ Describe "Rust" {
         $env:Path += ";$env:CARGO_HOME\bin"
     }
 
-    $rustTools = @(
-        @{ToolName = "rustup"; binPath = "C:\Users\Default\.cargo\bin\rustup.exe"}
-        @{ToolName = "rustc"; binPath = "C:\Users\Default\.cargo\bin\rustc.exe"}
-        @{ToolName = "bindgen.exe"; binPath = "C:\Users\Default\.cargo\bin\bindgen.exe"}
-        @{ToolName = "cbindgen.exe"; binPath = "C:\Users\Default\.cargo\bin\cbindgen.exe"}
-        @{ToolName = "cargo"; binPath = "C:\Users\Default\.cargo\bin\cargo.exe"}
-        @{ToolName = "cargo audit"; binPath = "C:\Users\Default\.cargo\bin\cargo-audit.exe"}
-        @{ToolName = "cargo outdated"; binPath = "C:\Users\Default\.cargo\bin\cargo-outdated.exe"}
-    )
+    if (Test-IsWin25) {
+        $rustTools = @(
+            @{ToolName = "rustup"; binPath = "C:\Users\Default\.cargo\bin\rustup.exe"}
+            @{ToolName = "rustc"; binPath = "C:\Users\Default\.cargo\bin\rustc.exe"}
+            @{ToolName = "cargo"; binPath = "C:\Users\Default\.cargo\bin\cargo.exe"}
+        )
+    } else {
+        $rustTools = @(
+            @{ToolName = "rustup"; binPath = "C:\Users\Default\.cargo\bin\rustup.exe"}
+            @{ToolName = "rustc"; binPath = "C:\Users\Default\.cargo\bin\rustc.exe"}
+            @{ToolName = "bindgen.exe"; binPath = "C:\Users\Default\.cargo\bin\bindgen.exe"}
+            @{ToolName = "cbindgen.exe"; binPath = "C:\Users\Default\.cargo\bin\cbindgen.exe"}
+            @{ToolName = "cargo"; binPath = "C:\Users\Default\.cargo\bin\cargo.exe"}
+            @{ToolName = "cargo audit"; binPath = "C:\Users\Default\.cargo\bin\cargo-audit.exe"}
+            @{ToolName = "cargo outdated"; binPath = "C:\Users\Default\.cargo\bin\cargo-outdated.exe"}
+        )
+    }
 
     $rustEnvNotExists = @(
         @{envVar = "RUSTUP_HOME"}
