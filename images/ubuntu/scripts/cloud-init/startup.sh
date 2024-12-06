c4e2f05810581e338c840ff8a39c8ac26909aa63
Dan Merry <dan.merry@accurx.com>
2024-12-06 12:20:35 +0000

    fix script

--- images/ubuntu/scripts/cloud-init/startup.sh
+++ images/ubuntu/scripts/cloud-init/startup.sh
@@ -9,10 +9,12 @@ sudo wget https://vstsagentpackage.azureedge.net/agent/4.248.1/vsts-agent-linux-
 sudo tar -xf vsts-agent-linux-x64-4.248.1.tar.gz
 sudo curl -sL https://vstsagenttools.blob.core.windows.net/tools/ElasticPools/Linux/16/enableagent.sh -o enableagent.sh ## user setup script
 
-az login --identity --allow-no-subscriptions
-export KEYVAULT_NAME=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.tagsList.[] | select(.name == "KeyVaultName") | .value')
-export DEVOPS_PAT=$(az keyvault secret show  --id "https://${KEYVAULT_NAME}.vault.azure.net/secrets/devops-bot-pat" | jq -r '.value')
+export MANAGED_IDENTITY_ID=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.tagsList.[] | select(.name == "ManagedIdentityId") | .value')
 export AZDO_POOL=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.tagsList.[] | select(.name == "AgentPool") | .value')
+export KEYVAULT_NAME=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq -r '.compute.tagsList.[] | select(.name == "KeyVaultName") | .value')
+
+az login --identity --allow-no-subscriptions --username ${MANAGED_IDENTITY_ID}
+export DEVOPS_PAT=$(az keyvault secret show  --id "https://${KEYVAULT_NAME}.vault.azure.net/secrets/devops-bot-pat" | jq -r '.value')
 
 ## Create log file that enableagent.sh for some reason requires to exist
 sudo mkdir -p /var/log/azure/Microsoft.VisualStudio.Services.TeamServicesAgentLinux
