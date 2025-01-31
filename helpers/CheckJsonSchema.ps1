901125ceec8de03fb0b7e680c86f306aec953346
lawrencegripper <info@grippers.co.uk>
2025-01-31 10:39:03 +0000

    Check outdated pins

--- helpers/CheckJsonSchema.ps1
+++ helpers/CheckJsonSchema.ps1
@@ -1,7 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
 # A JSON schema validator which supports outputting line numbers for errors
-Install-Module -Name GripDevJsonSchemaValidator -Force  -Scope CurrentUser
+# this allows us to put annotations on builds for errors in the JSON files
+# `Test-Json` built in cmdline doesn't. No existing cli tool supports this
+# that I could find either. See: https://github.com/lawrencegripper/gripdev-json-schema-validator
+Install-Module -Name GripDevJsonSchemaValidator -Force -Scope CurrentUser
 
 # Find all toolset JSON files
 $toolsetFiles = Get-ChildItem -Recurse -Filter "toolset-*.json" | Where-Object { $_.Name -notlike "*schema.json" }
@@ -9,16 +12,19 @@ $schemaFilePath = "./schemas/toolset-schema.json"
 
 $toolsetHasErrors = $false
 foreach ($file in $toolsetFiles) {
+    Write-Host ""
     Write-Host "🔍 Validating $($file.FullName)" -ForegroundColor Cyan
 
-    $validationResult = Test-JsonSchema -SchemaPath $schemaFilePath -JsonPath $file.FullName
+    $validationResult = Test-JsonSchema -SchemaPath $schemaFilePath -JsonPath $file.FullName -PrettyPrint $false
 
     if ($validationResult.Valid) {
         Write-Host "✅ JSON is valid." -ForegroundColor Green
     } else {
+        # File has been modified since the commit, enforce validation
         $toolsetHasErrors = $true
         Write-Host "`n❌ JSON validation failed!" -ForegroundColor Red
         Write-Host "   Found the following errors:`n" -ForegroundColor Yellow
+
         $validationResult.Errors | ForEach-Object {
             Write-Host $_.UserMessage
             if ($env:GITHUB_ACTIONS -eq 'true') {
