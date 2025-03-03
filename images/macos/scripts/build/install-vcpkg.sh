8081e79e1b8aa584c3b42c9be6f6648e819eb43e
archita105 <archita105@github.com>
2025-03-03 13:11:01 +0530

    vcpkg update

--- images/macos/scripts/build/install-vcpkg.sh
+++ images/macos/scripts/build/install-vcpkg.sh
@@ -5,6 +5,15 @@
 ################################################################################
 
 source ~/utils/utils.sh
+#!/bin/bash
+
+os=$(sw_vers -productVersion)  # Gets macOS version (e.g., 14.2)
+
+# Check if macOS is Sonoma (14.x) or Sequoia (15.x) and on ARM
+if [[ "$os" == 14.* || "$os" == 15.* ]] && [[ "$(uname -m)" == "arm64" ]]; then
+    VCPKG_INSTALLATION_ROOT=/usr/local/bin/vcpkg
+    echo "VCPKG_INSTALLATION_ROOT set to: $VCPKG_INSTALLATION_ROOT"
+fi
 
 # Set env variable for vcpkg
 VCPKG_INSTALLATION_ROOT=/usr/local/share/vcpkg
