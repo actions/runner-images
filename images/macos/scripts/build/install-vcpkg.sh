760a324cee3e8c5d0e98ec24b3ed04a67543d57a
archita105 <archita105@github.com>
2025-03-04 12:58:26 +0530

    VCPKG update

--- images/macos/scripts/build/install-vcpkg.sh
+++ images/macos/scripts/build/install-vcpkg.sh
@@ -7,14 +7,6 @@
 source ~/utils/utils.sh
 #!/bin/bash
 
-os=$(sw_vers -productVersion)  # Gets macOS version (e.g., 14.2)
-
-# Check if macOS is Sonoma (14.x) or Sequoia (15.x) and on ARM
-if [[ "$os" == 14.* || "$os" == 15.* ]] && [[ "$(uname -m)" == "arm64" ]]; then
-    VCPKG_INSTALLATION_ROOT=/usr/local/bin/vcpkg
-    echo "VCPKG_INSTALLATION_ROOT set to: $VCPKG_INSTALLATION_ROOT"
-fi
-
 # Set env variable for vcpkg
 VCPKG_INSTALLATION_ROOT=/usr/local/share/vcpkg
 echo "export VCPKG_INSTALLATION_ROOT=${VCPKG_INSTALLATION_ROOT}" | tee -a ~/.bashrc
@@ -25,11 +17,11 @@ mkdir -p /Users/runner/.vcpkg
 touch /Users/runner/.vcpkg/vcpkg.path.txt
 
 # Install vcpkg
-git clone https://github.com/Microsoft/vcpkg $VCPKG_INSTALLATION_ROOT
-$VCPKG_INSTALLATION_ROOT/bootstrap-vcpkg.sh
-$VCPKG_INSTALLATION_ROOT/vcpkg integrate install
-chmod -R 0777 $VCPKG_INSTALLATION_ROOT
-ln -sf $VCPKG_INSTALLATION_ROOT/vcpkg /usr/local/bin
+sudo git clone https://github.com/Microsoft/vcpkg $VCPKG_INSTALLATION_ROOT
+sudo $VCPKG_INSTALLATION_ROOT/bootstrap-vcpkg.sh
+sudo $VCPKG_INSTALLATION_ROOT/vcpkg integrate install
+sudo chmod -R 0777 $VCPKG_INSTALLATION_ROOT
+sudo ln -sf $VCPKG_INSTALLATION_ROOT/vcpkg /usr/local/bin
 
 rm -rf /Users/runner/.vcpkg
 
