81bedff321aea87cee21ea1d0c694a798182847c
Prasanjit Sahoo <prasanjitsahoo@github.com>
2025-03-05 15:39:17 +0530

    updating rustup scripts

--- images/macos/scripts/build/install-rust.sh
+++ images/macos/scripts/build/install-rust.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e -o pipefail
 ################################################################################
 ##  File:  install-rust.sh
-##  Desc:  Install Rust
+##  Desc:  Install Rustup and Rust
 ################################################################################
 
 source ~/utils/utils.sh
 
-#echo "Installing Rustup..."
-#brew_smart_install "rustup-init"
-
-#echo "Installing Rust language..."
-#rustup-init -y --no-modify-path --default-toolchain=stable --profile=minimal
-
 RUSTUP_VERSION="1.27.1"  # Set the Rustup version to pin
 
+echo "Downloading Rustup version $RUSTUP_VERSION..."
+curl -LO "https://github.com/rust-lang/rustup/releases/download/${RUSTUP_VERSION}/rustup-init"
+
+echo "Making Rustup executable..."
+chmod +x rustup-init
+
 echo "Installing Rustup version $RUSTUP_VERSION..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain=$RUSTUP_VERSION
+./rustup-init -y --no-modify-path
 
 # Ensure Rustup is available in PATH
 export PATH="$HOME/.cargo/bin:$PATH"
@@ -27,16 +27,10 @@ rustup show active-toolchain
 echo "Initialize environment variables..."
 CARGO_HOME=$HOME/.cargo
 
-echo "Disable rustup self-updating"
-# This is undesirable because:
-# - We will keep rustup updated (just like any other dependency).
-# - rustup does not need to be the latest and greatest.
-# - Self-updating in CI will introduce longer build times for no benefit.
-# - ... which becomes important because self-updating happens in various
-#   places, including `rustup toolchain install`.
+echo "Disable Rustup self-updating"
 rustup set auto-self-update disable
 
-echo "Install common tools..."
+echo "Installing common tools..."
 rustup component add rustfmt clippy
 
 echo "Cleanup Cargo registry cached data..."
