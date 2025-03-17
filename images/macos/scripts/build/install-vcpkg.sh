7b044e31b9ed59188fee2f2d4a530476920aa3d6
archita105 <archita105@github.com>
2025-03-17 11:51:28 +0530

    Installation Path change

--- images/macos/scripts/build/install-vcpkg.sh
+++ images/macos/scripts/build/install-vcpkg.sh
@@ -7,21 +7,20 @@
 source ~/utils/utils.sh
 
 # Set env variable for vcpkg
-VCPKG_INSTALLATION_ROOT=/usr/local/bin/vcpkg
+VCPKG_INSTALLATION_ROOT=/Users/runner/.vcpkg/vcpkg
 echo "export VCPKG_INSTALLATION_ROOT=${VCPKG_INSTALLATION_ROOT}" | tee -a ~/.bashrc
 
 # workaround https://github.com/microsoft/vcpkg/issues/27786
 
 mkdir -p /Users/runner/.vcpkg
 touch /Users/runner/.vcpkg/vcpkg.path.txt
+mkdir -p ~/bin
 
 # Install vcpkg
 git clone https://github.com/Microsoft/vcpkg $VCPKG_INSTALLATION_ROOT
 $VCPKG_INSTALLATION_ROOT/bootstrap-vcpkg.sh
 $VCPKG_INSTALLATION_ROOT/vcpkg integrate install
 chmod -R 0777 $VCPKG_INSTALLATION_ROOT
-ln -sf $VCPKG_INSTALLATION_ROOT/vcpkg /usr/local/bin
-
-rm -rf /Users/runner/.vcpkg
+ln -sf $VCPKG_INSTALLATION_ROOT/vcpkg ~/bin/vcpkg
 
 invoke_tests "Common" "vcpkg"
