a2547b98201714dad9849c3600dae6554f410cea
Erik Bershel <110455084+erik-bershel@users.noreply.github.com>
2025-03-26 12:20:27 +0100

    [macOS] Disable rarely used services, use optimised configs (#11877)

--- images/macos/scripts/build/configure-system.sh
+++ images/macos/scripts/build/configure-system.sh
@@ -6,8 +6,32 @@
 
 source ~/utils/utils.sh
 
+# Set solid color wallpaper
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/System/Library/Desktop Pictures/Solid Colors/Black.png"'
+
 # Close all finder windows because they can interfere with UI tests
 close_finder_window
+
+# Disable Handoff and Continuity
+defaults write com.apple.coreservices.useractivityd ActivityReceivingEnabled -bool false
+defaults write com.apple.coreservices.useractivityd ActivityAdvertisingAllowed -bool false
+
+# Disable graphic effects in System
+defaults write com.apple.universalaccess reduceMotion -bool true
+defaults write com.apple.universalaccess reduceTransparency -bool true
+
+# Disable analytics daemon (requires SIP to be disabled)
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.SubmitDiagInfo.plist
+
+# Disable notification center agent
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist
+
+# Disable Time Machine and it's daemon
+sudo tmutil disable
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.backupd.plist
+
+# Disable Apple Push Notification Service daemon
+sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.apsd.plist
 
 # Remove Parallels Desktop
 # https://github.com/actions/runner-images/issues/6105
@@ -32,11 +56,18 @@ yarn cache clean
 # Clean up temporary directories
 sudo rm -rf ~/utils /tmp/*
 
-# Erase all indexes and wait until the rebuilding process ends,
-# for now there is no way to get status of indexing process, it takes around 3 minutes to accomplish
-sudo mdutil -E /
-sudo log stream | grep -q -E 'mds.*Released.*BackgroundTask' || true
-echo "Indexing completed"
+if is_Ventura; then
+    # Erase all indexes and wait until the rebuilding process ends,
+    # for now there is no way to get status of indexing process, it takes around 3 minutes to accomplish
+    sudo mdutil -E /
+    sudo log stream | grep -q -E 'mds.*Released.*BackgroundTask' || true
+    echo "Indexing completed"
+else
+    # Disable spotlight and MDS
+    sudo mdutil -a -i off
+    sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+    sudo mdutil -a -s | grep "Indexing enabled." && echo "Indexing is still enabled" && exit 1 || true
+fi
 
 # delete symlink for tests running
 sudo rm -f /usr/local/bin/invoke_tests
