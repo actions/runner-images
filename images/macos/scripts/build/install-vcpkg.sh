5c7c5d8f531d45aa72c830f03004e311c30d99c8
archita105 <archita105@github.com>
2025-04-07 11:17:50 +0530

    permission changed

--- images/macos/scripts/build/install-vcpkg.sh
+++ images/macos/scripts/build/install-vcpkg.sh
@@ -7,20 +7,21 @@
 source ~/utils/utils.sh
 
 # Set env variable for vcpkg
-VCPKG_INSTALLATION_ROOT=/Users/runner/.vcpkg/vcpkg
+VCPKG_INSTALLATION_ROOT=/usr/local/share/vcpkg
 echo "export VCPKG_INSTALLATION_ROOT=${VCPKG_INSTALLATION_ROOT}" | tee -a ~/.bashrc
 
 # workaround https://github.com/microsoft/vcpkg/issues/27786
 
 mkdir -p /Users/runner/.vcpkg
 touch /Users/runner/.vcpkg/vcpkg.path.txt
-mkdir -p ~/bin
 
 # Install vcpkg
-git clone https://github.com/Microsoft/vcpkg $VCPKG_INSTALLATION_ROOT
+sudo git clone https://github.com/Microsoft/vcpkg $VCPKG_INSTALLATION_ROOT
 $VCPKG_INSTALLATION_ROOT/bootstrap-vcpkg.sh
 $VCPKG_INSTALLATION_ROOT/vcpkg integrate install
 chmod -R 0777 $VCPKG_INSTALLATION_ROOT
-ln -sf $VCPKG_INSTALLATION_ROOT/vcpkg ~/bin/vcpkg
+ln -sf $VCPKG_INSTALLATION_ROOT/usr/local/bin
+
+rm -rf /Users/runner/.vcpkg
 
 invoke_tests "Common" "vcpkg"
