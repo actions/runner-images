254c1a9c705d59ff64bee2c5a70beb5821be4555
archita105 <archita105@github.com>
2025-04-08 14:04:54 +0530

    Added sudo for Symlink

--- images/macos/scripts/build/install-vcpkg.sh
+++ images/macos/scripts/build/install-vcpkg.sh
@@ -20,7 +20,7 @@ sudo git clone https://github.com/Microsoft/vcpkg $VCPKG_INSTALLATION_ROOT
 sudo $VCPKG_INSTALLATION_ROOT/bootstrap-vcpkg.sh
 $VCPKG_INSTALLATION_ROOT/vcpkg integrate install
 sudo chmod -R 0777 $VCPKG_INSTALLATION_ROOT
-ln -sf $VCPKG_INSTALLATION_ROOT/usr/local/bin
+sudo ln -sf $VCPKG_INSTALLATION_ROOT/usr/local/bin
 
 rm -rf /Users/runner/.vcpkg
 
