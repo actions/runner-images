c6a6aa47af65a21140a45a4f8d841b57c12541ac
usamoi <usamoi@outlook.com>
2025-05-17 13:11:20 +0800

    set rustup profile to default

--- images/macos/scripts/build/install-rust.sh
+++ images/macos/scripts/build/install-rust.sh
@@ -10,13 +10,10 @@ echo "Installing Rustup..."
 brew_smart_install "rustup-init"
 
 echo "Installing Rust language..."
-rustup-init -y --no-modify-path --default-toolchain=stable --profile=minimal
+rustup-init -y --no-modify-path --default-toolchain=stable
 
 echo "Initialize environment variables..."
 CARGO_HOME=$HOME/.cargo
-
-echo "Install common tools..."
-rustup component add rustfmt clippy
 
 echo "Cleanup Cargo registry cached data..."
 rm -rf $CARGO_HOME/registry/*
