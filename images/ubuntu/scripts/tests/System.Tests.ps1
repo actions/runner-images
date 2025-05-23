52019144a83d5d81e4f9f3e3caf454251c20e614
lawrencegripper <i+gitgen@grippers.co.uk>
2025-05-23 14:18:25 +0100

    Fix fwupd intergration test on Ubuntu 22.04
    See:
    
    - https://github.com/actions/runner-images/pull/12225#issuecomment-2904383955

--- images/ubuntu/scripts/tests/System.Tests.ps1
+++ images/ubuntu/scripts/tests/System.Tests.ps1
@@ -10,6 +10,6 @@ Describe "Disk free space" -Skip:(-not [String]::IsNullOrEmpty($env:AGENT_NAME) 
 Describe "fwupd removed" {
     It "Is not present on box" {
         $systemctlOutput = & systemctl list-unit-files fwupd-refresh.timer
-        $systemctlOutput | Should -Match "masked"
+        ($systemctlOutput -eq $null) -or ($systemctlOutput | Should -Match "masked")
     }
 }
