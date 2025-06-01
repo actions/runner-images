32beedfd4f81cb62aca4351ddce24592a334b888
Bigflash03uk <bigflash02@gmail.com>
2025-06-01 02:27:53 +0100

    Delete images directory

--- images/macos/scripts/tests/Linters.Tests.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module "$PSScriptRoot/Helpers.psm1" -DisableNameChecking
-
-$os = Get-OSVersion
-
-Describe "SwiftLint" -Skip:($os.IsVenturaArm64 -or $os.IsSonomaArm64 -or $os.IsSequoiaArm64) {
-    It "SwiftLint" {
-        "swiftlint version" | Should -ReturnZeroExitCode
-    }
-}
