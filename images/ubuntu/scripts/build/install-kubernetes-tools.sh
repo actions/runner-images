fa9c2edd006dbae0f3906c25fb0b3a3cb56bca4e
Kishore Kumar Reddy Anchala <kishorekumar-anchala@github.com>
2025-06-26 19:12:26 +0530

    [ubuntu] Create new key directory

--- images/ubuntu/scripts/build/install-kubernetes-tools.sh
+++ images/ubuntu/scripts/build/install-kubernetes-tools.sh
@@ -21,6 +21,7 @@ install "${kind_binary_path}" /usr/local/bin/kind
 
 ## Install kubectl
 kubectl_minor_version=$(curl -fsSL "https://dl.k8s.io/release/stable.txt" | cut -d'.' -f1,2 )
+sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/$kubectl_minor_version/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/'$kubectl_minor_version'/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
@@ -30,20 +31,23 @@ rm -f /etc/apt/sources.list.d/kubernetes.list
 # Install Helm
 curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-# Temporarily pinning the version
-# Download minikube
-curl -fsSL -O https://storage.googleapis.com/minikube/releases/v1.34.0/minikube-linux-amd64
 
-# Supply chain security - minikube
-minikube_hash=$(get_checksum_from_github_release "kubernetes/minikube" "linux-amd64" "1.34.0" "SHA256")
-use_checksum_comparison "minikube-linux-amd64" "${minikube_hash}"
+# Download and install minikube
+minikube_version="latest"
+minikube_binary_path=$(download_with_retry "https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-linux-amd64")
 
-# Install minikube
-install minikube-linux-amd64 /usr/local/bin/minikube
+# Supply chain security - Minikube
+minikube_hash=$(get_checksum_from_github_release "kubernetes/minikube" "linux-amd64" "${minikube_version}" "SHA256")
+use_checksum_comparison "${minikube_binary_path}" "${minikube_hash}"
+
+install "${minikube_binary_path}" /usr/local/bin/minikube
 
 # Install kustomize
 download_url="https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
 curl -fsSL "$download_url" | bash
 mv kustomize /usr/local/bin
+
+# Cleanup temporary files
+rm -f /tmp/kubernetes-key.gpg
 
 invoke_tests "Tools" "Kubernetes tools"
