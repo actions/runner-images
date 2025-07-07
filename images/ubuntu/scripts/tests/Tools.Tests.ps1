f8533c3a76bfe878fdbdcf35784cd431ff3e8059
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2025-07-07 12:55:40 +0200

    Update Ninja pester test

--- images/ubuntu/scripts/tests/Tools.Tests.ps1
+++ images/ubuntu/scripts/tests/Tools.Tests.ps1
@@ -392,15 +392,16 @@ Describe "Kotlin" {
 }
 
 Describe "Ninja" {
-    New-item -Path "/tmp/ninjaproject" -ItemType Directory -Force
-    Set-Location '/tmp/ninjaproject'
+    BeforeAll {
+        New-item -Path "/tmp/ninjaproject" -ItemType Directory -Force
 @'
 cmake_minimum_required(VERSION 3.10)
 project(NinjaTest NONE)
-'@ | Out-File -FilePath "./CMakeLists.txt"
+'@ | Out-File -FilePath "/tmp/ninjaproject/CMakeLists.txt"
+    }
 
     It "Make a simple ninja project" {
-    "cmake -GNinja /tmp/ninjaproject" | Should -ReturnZeroExitCode
+        "cmake -GNinja -S /tmp/ninjaproject -B /tmp/ninjaproject" | Should -ReturnZeroExitCode
     }
 
     It "build.ninja file should exist" {
@@ -410,5 +411,9 @@ project(NinjaTest NONE)
 
     It "Ninja" {
         "ninja --version" | Should -ReturnZeroExitCode
+    }
+
+    AfterAll {
+        Remove-Item -Path "/tmp/ninjaproject" -Recurse -Force
     }
 }
