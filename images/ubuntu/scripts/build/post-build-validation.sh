b76eb4f813c20c15b6f5e15a39af07d61e9b5de1
Baris Basa <46998262+basabaris@users.noreply.github.com>
2025-08-21 11:19:10 +0200

    Add a condition to run MDE checks

--- images/ubuntu/scripts/build/post-build-validation.sh
+++ images/ubuntu/scripts/build/post-build-validation.sh
@@ -4,16 +4,22 @@
 ##  Desc:  Validate different aspects of the image after build
 ################################################################################
 
-echo "Test microsoft defender not installed using '-d /opt/microsoft/mdatp'"
-# Validate Defender not installed test 1
-if [ -d /opt/microsoft/mdatp ]; then
-    echo "Microsoft Defender for Endpoint is installed."
-    exit 1
-fi
+# Check if ENSURE_MDE_NOT_INSTALLED is true 
+if [[ "${ENSURE_MDE_NOT_INSTALLED}" == "true" ]]; then
+ 
+  echo "Test microsoft defender not installed using '-d /opt/microsoft/mdatp'"
+  # Validate Defender not installed test 1
+  if [ -d /opt/microsoft/mdatp ]; then
+      echo "Microsoft Defender for Endpoint is installed."
+      exit 1
+  fi
 
-echo "Test microsoft defender not installed using 'systemctl list-units --type=service --all | grep mdatp'"
-# Validate Defender not installed test 2
-if systemctl list-units --type=service --all | grep -w mdatp &>/dev/null; then
-    echo "Microsoft Defender for Endpoint is installed."
-    exit 1
+  echo "Test microsoft defender not installed using 'systemctl list-units --type=service --all | grep mdatp'"
+  # Validate Defender not installed test 2
+  if systemctl list-units --type=service --all | grep -w mdatp &>/dev/null; then
+      echo "Microsoft Defender for Endpoint is installed."
+      exit 1
+  fi
+else
+  echo "ENSURE_MDE_NOT_INSTALLED is not true. Skipping Microsoft defender for endpoint installation validations."
 fi
