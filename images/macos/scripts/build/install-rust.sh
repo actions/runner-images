7e149607641cb5f9ddcb3795769607a466d85b79
Erik Bershel <110455084+erik-bershel@users.noreply.github.com>
2025-09-24 13:57:00 +0200

    Revert "[all OSs] Pin Rust to 1.89.0 due to a 1.90.0 breaking change (#13044)"
    This reverts commit 98a1416fbfc4169ebd094ba9e64728fb2dae6124.

--- images/macos/scripts/build/install-rust.sh
+++ images/macos/scripts/build/install-rust.sh
@@ -10,8 +10,7 @@ echo "Installing Rustup..."
 brew_smart_install "rustup-init"
 
 echo "Installing Rust language..."
-# Pin to 1.89.0 until https://github.com/actions/runner-images/issues/13041 && https://github.com/rust-lang/rust/issues/145936 is resolved
-rustup-init -y --no-modify-path --default-toolchain=1.89.0 --profile=minimal
+rustup-init -y --no-modify-path --default-toolchain=stable --profile=minimal
 
 echo "Initialize environment variables..."
 CARGO_HOME=$HOME/.cargo
