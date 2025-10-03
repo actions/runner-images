5641ff0eb6f410f8d2f37d2cb525a789133a35ba
Shamil Mubarakshin <127750046+shamil-mubarakshin@users.noreply.github.com>
2025-10-03 11:25:31 +0200

    [Windows,Ubuntu] Update disk space pester test (#13128)

--- images/ubuntu/scripts/tests/System.Tests.ps1
+++ images/ubuntu/scripts/tests/System.Tests.ps1
@@ -2,8 +2,11 @@
 # If the test is running on the ADO pipeline or on the GitHub actions, the test will be skipped
 Describe "Disk free space" -Skip:(-not [String]::IsNullOrEmpty($env:AGENT_NAME) -or -not [String]::IsNullOrEmpty($env:RUNNER_NAME)) {
     It "Image has enough free space" {
-        $freeSpace = (Get-PSDrive "/").Free
-        $freeSpace | Should -BeGreaterOrEqual 17GB
+        $diskInfo = Get-PSDrive "/"
+        $totalSpaceGB = [math]::Floor(($diskInfo.Used + $diskInfo.Free) / 1GB)
+        $freeSpaceGB = [math]::Floor($diskInfo.Free / 1GB)
+        Write-Host "  [i] Disk size: ${totalSpaceGB} GB; Free space: ${freeSpaceGB} GB"
+        $freeSpaceGB | Should -BeGreaterOrEqual 17
     }
 }
 
