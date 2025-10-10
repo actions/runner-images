ed18dfab032f4a5e9ec1ea2927224fc8314806b5
Erik Bershel <110455084+erik-bershel@users.noreply.github.com>
2025-10-10 17:19:38 +0200

    [macOS] Update Python installer script (#13157)

--- images/macos/scripts/build/install-python.sh
+++ images/macos/scripts/build/install-python.sh
@@ -11,9 +11,19 @@ echo "Installing Python Tooling"
 # Close Finder window
 close_finder_window
 
-echo "Brew Installing Python 3"
-brew_smart_install "python@3.12"
+# Installing latest Homebrew Python 3 to handle python3 and pip3 symlinks
+# When latest Python3 is installed as a dependency for other packages
+# it does not create /usr/local/bin/python3 and /usr/local/bin/pip3
+echo "Brew Installing default Python 3"
+brew_smart_install "python"
 
+# Install specific Python version
+# Update symlinks for python3 and pip3 to point to the specific version
+toolsetVersion=$(get_toolset_value '.python.default')
+echo "Installing Python $toolsetVersion"
+brew_smart_install "python@$toolsetVersion"
+
+# Pipx has its own Python dependency
 echo "Installing pipx"
 
 if is_Arm64; then
