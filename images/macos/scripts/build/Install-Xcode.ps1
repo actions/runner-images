db8d413376de785d0a00c118d31baa1d1821b627
Erik Bershel <110455084+erik-bershel@users.noreply.github.com>
2025-11-25 18:20:57 +0100

    [macOS] Enable runtimes and re-caching for Xcode 26.1 and later (#13343)
    * [macOS] Enable runtimes and re-caching for Xcode 26.1 and later
    
    * Drop Xcode 26.2 beta 2 for macOS 15

--- images/macos/scripts/build/Install-Xcode.ps1
+++ images/macos/scripts/build/Install-Xcode.ps1
@@ -35,7 +35,7 @@ $xcodeVersions | ForEach-Object {
     Write-Host "Configuring Xcode $($_.link) ..."
     Invoke-XcodeRunFirstLaunch -Version $_.link
     Install-XcodeAdditionalSimulatorRuntimes -Version $_.link -Arch $arch -Runtimes $_.install_runtimes
-    if (($_.link -eq "26.0.1")) {
+    if ($_.link -match '^(\d+)\.(\d+)(?:\.(\d+))?$' -and [int]$matches[1] -ge 26) {
         Install-XcodeAdditionalComponents -Version $_.link
         Update-DyldCache -Version $_.link
     }
