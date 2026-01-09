3b57d59ae69a0d75b92d6ea73cec6fc714601acf
Mike Tesch <mitesch@github.com>
2026-01-09 17:12:37 +0000

    Missed file

---
+++ images/ubuntu-slim/scripts/build/install-docker-cli.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+################################################################################
+##  File:  install-docker-cli.sh
+##  Desc:  Install docker cli, but not the engine
+################################################################################
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+# Here is the part that is different
+apt-get install docker-ce-cli
