a37ee55d59a4e86c07df52964d110148be1b4542
Erik Bershel <110455084+erik-bershel@users.noreply.github.com>
2026-01-13 12:57:25 +0100

    Delete images/windows/scripts/tests/BizTalk.Tests.ps1

--- images/windows/scripts/tests/BizTalk.Tests.ps1
+++
@@ -1,6 +0,0 @@
-################################################################################
-##  File:  BizTalk.Tests.ps1
-##  Desc:  Test BizTalk project build component installed.
-################################################################################
-
-# BizTalk is no longer supported on Windows Server 2022/2025
