e99b87ecc6b412e44f40933b39fb29433f6e902d
Paulo Santos <GitPaulo@users.noreply.github.com>
2026-01-16 12:42:03 +0000

    fix: update SSDT extension

--- images/windows/scripts/tests/SSDTExtensions.Tests.ps1
+++ images/windows/scripts/tests/SSDTExtensions.Tests.ps1
@@ -1,13 +1,7 @@
 Describe "SSDTExtensions" {
 #These extensions don't have any proper name in the state.packages.json file, only id is available, which can be found on extension marketplace download page
-
-    $testExtenions = @(
-    @{id = "04a86fc2-dbd5-4222-848e-911638e487fe"}
-    @{id = "717ad572-c4b7-435c-c166-c2969777f718"}
-    )
-
-    It "Extensions id=<id>" -TestCases $testExtenions {
-        $version = Get-VSExtensionVersion -packageName "${id}"
+    It "Extension SSDT" {
+        $version = Get-VSExtensionVersion -packageName "SSDT"
         $version | Should -Not -BeNullOrEmpty
     }
 }
