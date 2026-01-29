5603cc6c78149048fc0542f055ab604bfe04563a
Bradley Smith <70659641+brsm019@users.noreply.github.com>
2026-01-29 13:16:29 +0000

    Configure USE_BAZEL_FALLBACK_VERSION for Bazelisk: Set the pre-installed Bazel version as the fallback so users without a .bazelversion file use the cached binary instead of downloading latest.

--- images/ubuntu/scripts/build/install-bazel.sh
+++ images/ubuntu/scripts/build/install-bazel.sh
@@ -13,4 +13,11 @@ npm install -g @bazel/bazelisk
 # run bazelisk once in order to install /usr/local/bin/bazel binary
 sudo -u $SUDO_USER bazel version
 
+# Get the installed Bazel version from bazelisk
+BAZEL_VERSION=$(sudo -u $SUDO_USER bazel --version | grep "Build label:" | awk '{print $3}')
+
+# Set USE_BAZEL_FALLBACK_VERSION so that users without .bazelversion
+# get the preinstalled version instead of downloading latest
+echo "USE_BAZEL_FALLBACK_VERSION=silent:${BAZEL_VERSION}" >> /etc/environment
+
 invoke_tests "Tools" "Bazel"
