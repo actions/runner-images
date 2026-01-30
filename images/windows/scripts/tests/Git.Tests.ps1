adb09be3a766ceeb00ad9b4698092e00b69c69d0
Trey Valenta <t@trey.net>
2026-01-30 10:05:24 -0800

    [Windows] Git installation tests for SSH hostkeys
    Tests provisioned SSH hostkeys for github.com and ssh.dev.azure.com
    * ssh-keygen is able to parse the ssh_known_hosts files
    * Hostkeys for github.com and ssh.dev.azure.com are verified

--- images/windows/scripts/tests/Git.Tests.ps1
+++ images/windows/scripts/tests/Git.Tests.ps1
@@ -23,3 +23,39 @@ Describe "Git" {
         $env:GCM_INTERACTIVE | Should -BeExactly Never
     }
 }
+
+Describe "SshHostkeys" {
+    $sshHosts = 'github.com', 'ssh.dev.azure.com'
+    $sshTestCases = $sshHosts | ForEach-Object {
+        @{
+            hostName = $_
+        }
+    }
+
+    Context "OpenSSH" {
+        It "KnownHosts for <hostname>" -TestCases $sshTestCases {
+            $KnownHosts = "$env:ProgramData\ssh\ssh_known_hosts"
+            $SshKeyGen = "$env:SystemRoot\System32\OpenSSH\ssh-keygen.exe"
+            & $SshKeyGen -F $hostName -f $KnownHosts | Out-String | Should -Match "Host $hostname found"
+        }
+
+        It "SSH accepts <hostname> keys" -TestCases $sshTestCases {
+            $SSHOptions='-o BatchMode=yes'
+            $SshCmd = "$env:SystemRoot\System32\OpenSSH\ssh.exe"
+            & $SshCmd $SSHOptions -T git@$hostName 2>&1 | Out-String | Should -Not -Match "Host key verification failed."
+        }
+    }
+    Context "Git for Windows" {
+        It "KnownHosts for <hostname>" -TestCases $sshTestCases {
+            $KnownHosts = "$env:ProgramFiles\Git\etc\ssh\ssh_known_hosts"
+            $SshKeyGen = "$env:ProgramFiles\Git\usr\bin\ssh-keygen.exe"
+            & $SshKeyGen -F $hostName -f $KnownHosts | Out-String | Should -Match "Host $hostname found"
+        }
+
+        It "SSH accepts <hostname> keys" -TestCases $sshTestCases {
+            $SSHOptions='-o BatchMode=yes'
+            $SshCmd = "$env:ProgramFiles\git\usr\bin\ssh.exe"
+            & $SshCmd $SSHOptions -T git@$hostName 2>&1 | Out-String | Should -Not -Match "Host key verification failed."
+        }
+    }
+}
