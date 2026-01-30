9781e2728783f055d1512c5fd011d48d3ffe590a
v-AndriiKhyliuk <v-andriikhyliuk@github.com>
2026-01-30 12:06:15 +0100

    Remove Get-BizTalkVersion function
    Removed the Get-BizTalkVersion function from SoftwareReport.Common.psm1.

--- images/windows/scripts/docs-gen/SoftwareReport.Common.psm1
+++ images/windows/scripts/docs-gen/SoftwareReport.Common.psm1
@@ -291,11 +291,6 @@ function Get-YAMLLintVersion {
     yamllint --version | Get-StringPart -Part 1
 }
 
-function Get-BizTalkVersion {
-    $bizTalkReg = Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\BizTalk Server\3.0"
-    return [ToolVersionNode]::new($bizTalkReg.ProductName, $bizTalkReg.ProductVersion)
-}
-
 function Get-PipxVersion {
     pipx --version
 }
