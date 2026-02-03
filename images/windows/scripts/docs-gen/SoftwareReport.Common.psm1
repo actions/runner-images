7a8b98c725caa2de3d43d5232b545e491bb982f8
v-AndriiKhyliuk <v-andriikhyliuk@github.com>
2026-02-03 10:53:31 +0100

    [Windows] Remove unused code (#13606)

--- images/windows/scripts/docs-gen/SoftwareReport.Common.psm1
+++ images/windows/scripts/docs-gen/SoftwareReport.Common.psm1
@@ -247,10 +247,6 @@ function Get-PowerShellModules {
     return $result
 }
 
-function Get-CachedDockerImages {
-    return (docker images --digests --format "* {{.Repository}}:{{.Tag}}").Split("*") | Where-Object { $_ }
-}
-
 function Get-CachedDockerImagesTableData {
     $allImages = docker images --digests --format "*{{.Repository}}:{{.Tag}}|{{.Digest}} |{{.CreatedAt}}"
     if (-not $allImages) {
@@ -289,11 +285,6 @@ function Get-PacmanVersion {
 
 function Get-YAMLLintVersion {
     yamllint --version | Get-StringPart -Part 1
-}
-
-function Get-BizTalkVersion {
-    $bizTalkReg = Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\BizTalk Server\3.0"
-    return [ToolVersionNode]::new($bizTalkReg.ProductName, $bizTalkReg.ProductVersion)
 }
 
 function Get-PipxVersion {
