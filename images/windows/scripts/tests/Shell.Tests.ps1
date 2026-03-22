eb9ce8bc25f5ae2dd10638bfc9558988e8cc0dd5
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2026-03-22 18:06:53 +0100

    Fix pester tests

--- images/windows/scripts/tests/Shell.Tests.ps1
+++ images/windows/scripts/tests/Shell.Tests.ps1
@@ -3,7 +3,7 @@ Describe "Shell" {
         @{Name = "C:\shells\gitbash.exe"; Target = "$env:ProgramFiles\Git\bin\bash.exe"},
         $(if (-not (Test-IsWin11)) { @{Name = "C:\shells\msys2bash.cmd"; Target = $null} }),
         @{Name = "C:\shells\wslbash.exe"; Target = "$env:SystemRoot\System32\bash.exe"}
-    )
+    ) | Where-Object { $_ }
 
     It "<Name> target to <Target>" -TestCases $shellTestCases {
         (Get-Item $Name).Target | Should -BeExactly $Target
