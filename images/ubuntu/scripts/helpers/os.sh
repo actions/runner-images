c3431b0c92df74dfc1cd1beb96d2021b1b79ed52
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2026-05-11 09:20:09 +0200

    Add templates, toolsets, helpers

--- images/ubuntu/scripts/helpers/os.sh
+++ images/ubuntu/scripts/helpers/os.sh
@@ -15,3 +15,35 @@ is_ubuntu24() {
 is_ubuntu26() {
     lsb_release -rs | grep -q '26.04'
 }
+
+is_arm64() {
+    [ "$(uname -m)" = "aarch64" ]
+}
+
+is_x64() {
+    [ "$(uname -m)" = "x86_64" ]
+}
+
+is_ubuntu22_arm64() {
+    is_ubuntu22 && is_arm64
+}
+
+is_ubuntu24_arm64() {
+    is_ubuntu24 && is_arm64
+}
+
+is_ubuntu26_arm64() {
+    is_ubuntu26 && is_arm64
+}
+
+is_ubuntu22_x64() {
+    is_ubuntu22 && is_x64
+}
+
+is_ubuntu24_x64() {
+    is_ubuntu24 && is_x64
+}
+
+is_ubuntu26_x64() {
+    is_ubuntu26 && is_x64
+}
