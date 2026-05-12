2cca6b7f506dbfee914a4f261a881431a26ca5ca
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2026-05-12 17:22:43 +0200

    Fix swift release name

--- images/ubuntu/scripts/build/install-swift.sh
+++ images/ubuntu/scripts/build/install-swift.sh
@@ -12,12 +12,13 @@ source $HELPER_SCRIPTS/os.sh
 # Install
 image_label="ubuntu$(lsb_release -rs)"
 swift_version=$(curl -fsSL "https://api.github.com/repos/apple/swift/releases/latest" | jq -r '.tag_name | match("[0-9.]+").string')
-swift_release_name="swift-${swift_version}-RELEASE-${image_label}"
 
 if is_x64; then
+  swift_release_name="swift-${swift_version}-RELEASE-${image_label}"
   archive_url="https://download.swift.org/swift-${swift_version}-release/${image_label//./}/swift-${swift_version}-RELEASE/${swift_release_name}.tar.gz"
 elif is_arm64; then
-  archive_url="https://download.swift.org/swift-${swift_version}-release/${image_label//./}-aarch64/swift-${swift_version}-RELEASE/${swift_release_name}-aarch64.tar.gz"
+  swift_release_name="swift-${swift_version}-RELEASE-${image_label}-aarch64"
+  archive_url="https://download.swift.org/swift-${swift_version}-release/${image_label//./}-aarch64/swift-${swift_version}-RELEASE/${swift_release_name}.tar.gz"
 else
   echo "Unsupported architecture"
   exit 1
