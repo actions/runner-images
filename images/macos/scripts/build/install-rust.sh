080496b074f01116554453eb8a046b37b0217c04
v-sergei-pyshnoi <121864472+v-sergei-pyshnoi@users.noreply.github.com>
2026-05-19 12:17:28 +0200

    Update Rust installation script to pin rustup version
    Pin rustup installation to a specific commit and update the installation method.

--- images/macos/scripts/build/install-rust.sh
+++ images/macos/scripts/build/install-rust.sh
@@ -7,7 +7,13 @@
 source ~/utils/utils.sh
 
 echo "Installing Rustup..."
-brew_smart_install "rustup-init"
+## Pin rustup due https://github.com/actions/runner-images/issues/14097
+COMMIT=cfffe64fa6fc4d56b3b2299ce07f224191098b6a
+FORMULA_URL="https://raw.githubusercontent.com/Homebrew/homebrew-core/$COMMIT/Formula/r/rustup.rb"
+FORMULA_PATH="$(brew --repository)/Library/Taps/homebrew/homebrew-core/Formula/r/rustup.rb"
+mkdir -p "$(dirname $FORMULA_PATH)"
+curl -fsSL $FORMULA_URL -o $FORMULA_PATH
+HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_FROM_API=1 brew install rustup
 
 echo "Installing Rust language..."
 rustup-init -y --no-modify-path --default-toolchain=stable --profile=minimal
