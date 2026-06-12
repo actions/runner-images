4307f20be74fa5a8f30a3fced2f173040e1cf84f
Sergei Radich <v-sergeiradich@github.com>
2026-06-12 15:05:19 +0200

    Fixed commands order

--- images/macos/scripts/build/install-bicep.sh
+++ images/macos/scripts/build/install-bicep.sh
@@ -7,8 +7,8 @@
 source ~/utils/utils.sh
 
 echo "Installing bicep cli..."
-brew trust azure/bicep
 brew tap azure/bicep
+brew trust azure/bicep
 brew_smart_install bicep
 
 invoke_tests "Common" "Bicep"
