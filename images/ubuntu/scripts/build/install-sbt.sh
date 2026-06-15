098e03f6cce7776e7b7f79e3f862cb407cc83fc8
Georgy <v-georgypuzakov@github.com>
2026-06-15 19:14:23 +0200

    Pin sbt to 1.x to avoid JDK 17+ requirement of sbt 2.x
    sbt 2.0.0 requires JDK 17 or above, which breaks the image build on
    Ubuntu 22.04 (default JDK 11) and Windows Server 2022 (default JDK 8).
    
    - Ubuntu: filter releases to the latest 1.x via resolve_github_release_asset_url
    - Windows: pin chocolatey sbt to 1.12.12
    
    Fixes #14236

--- images/ubuntu/scripts/build/install-sbt.sh
+++ images/ubuntu/scripts/build/install-sbt.sh
@@ -6,8 +6,10 @@
 
 source $HELPER_SCRIPTS/install.sh
 
-# Install latest sbt release
-download_url=$(resolve_github_release_asset_url "sbt/sbt" "endswith(\".tgz\")" "latest")
+# Install the latest sbt 1.x release.
+# sbt 2.x requires JDK 17 or above, while the default JDK on Ubuntu 22.04 is JDK 11.
+# See https://github.com/actions/runner-images/issues/14236
+download_url=$(resolve_github_release_asset_url "sbt/sbt" "endswith(\".tgz\")" "^1\\..+" "false" "true")
 archive_path=$(download_with_retry "$download_url")
 tar zxf "$archive_path" -C /usr/share
 ln -s /usr/share/sbt/bin/sbt /usr/bin/sbt
--- images/windows/scripts/build/Install-Sbt.ps1
+++ images/windows/scripts/build/Install-Sbt.ps1
@@ -3,9 +3,11 @@
 ##  Desc:  Install sbt for Windows
 ################################################################################
 
-# Install the latest version of sbt.
+# Install sbt 1.x.
+# sbt 2.x requires JDK 17 or above, while the default JDK on Windows Server 2022 is JDK 8.
+# See https://github.com/actions/runner-images/issues/14236
 # See https://chocolatey.org/packages/sbt
-Install-ChocoPackage sbt
+Install-ChocoPackage sbt -Version "1.12.12"
 
 $env:SBT_HOME="${env:ProgramFiles(x86)}\sbt"
 
