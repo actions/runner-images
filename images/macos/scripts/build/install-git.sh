d25e7f019a5bc0f65236576a104b22e802cf1649
Sergei Radich <v-sergeiradich@github.com>
2026-08-31 13:14:52 +0200

    Pinned old git-lfs config for Intel images

--- images/macos/scripts/build/install-git.sh
+++ images/macos/scripts/build/install-git.sh
@@ -12,7 +12,19 @@ brew_smart_install "git"
 git config --global --add safe.directory "*"
 
 echo "Installing Git LFS"
-brew_smart_install "git-lfs"
+if is_Arm64; then
+    # git-lfs works fine on ARM64 images
+    brew_smart_install "git-lfs"
+else
+    # For the Intel images git-lfs stopped to work, using pinned commit
+    echo "Installing pinned git-lfs formulae..."
+    COMMIT=70cbd7e455267402af156a5b733a774b15ef9949
+    FORMULA_URL="https://raw.githubusercontent.com/Homebrew/homebrew-core/$COMMIT/Formula/g/git-lfs.rb"
+    FORMULA_PATH="$(brew --repository)/Library/Taps/homebrew/homebrew-core/Formula/g/git-lfs.rb"
+    mkdir -p "$(dirname $FORMULA_PATH)"
+    curl -fsSL $FORMULA_URL -o $FORMULA_PATH
+    HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_FROM_API=1 brew install git-lfs
+fi
 
 # Update global git config
 git lfs install
