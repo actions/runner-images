5a7765a7ccb6aaba1c15f0b188fc88e7d1f869ad
Sergei Radich <v-sergeiradich@github.com>
2026-09-16 13:45:26 +0200

    Removed tools from installation

--- images/macos/scripts/build/install-android-sdk.sh
+++ images/macos/scripts/build/install-android-sdk.sh
@@ -78,8 +78,8 @@ SDKMANAGER=$ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager
 echo "Getting package list..."
 $SDKMANAGER --list
 
-echo "Installing latest cmdline-tools & platform tools..."
-echo y | $SDKMANAGER "cmdline-tools" "platform-tools"
+echo "Installing latest platform tools..."
+echo y | $SDKMANAGER "platform-tools"
 
 echo "Installing latest ndk..."
 for ndk_version in "${android_ndk_major_versions[@]}"
